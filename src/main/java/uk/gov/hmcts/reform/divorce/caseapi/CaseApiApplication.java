555359055f3351430a6096bd27aaf41e1b67e8ad
Nitin Prabhu <prabhunitin61@gmail.com>
2021-02-22 15:21:40 +0000

    Update default template files as per nfdiv service requirements

--- src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.demo;
+package uk.gov.hmcts.reform.divorce.caseapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +7,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 @SpringBootApplication
 @EnableCircuitBreaker
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+public class CaseApiApplication {
 
     public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(CaseApiApplication.class, args);
     }
 }
