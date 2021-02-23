c394bf03017e1d82860944399b803d0637d689b7
Nitin Prabhu <prabhunitin61@gmail.com>
2021-02-23 09:13:25 +0000

    Fix sonar - remove deprecated annotation

--- src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.reform.divorce.caseapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class CaseApiApplication {
 
