482f60b756c72223131185aaf19b646f86330e80
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-01 11:48:34 +0000

    Fix directory path

--- bin/process-and-import-ccd-definition.sh
+++ bin/process-and-import-ccd-definition.sh
@@ -6,14 +6,15 @@ scriptPath=$(dirname $(realpath $0))
 basePath=$(dirname $(dirname $scriptPath))
 ccdDefinitionsPath=$basePath/nfdiv-case-api/ccd-definitions/definitions/development
 
-definitionOutputFile=$basePath/nfdiv-case-api/ccd-definitions/build/ccd-config/ccd-nfd-dev.xlsx
+config_build_dir=$basePath/nfdiv-case-api/ccd-definitions/build/ccd-config
+definitionOutputFile=${config_build_dir}/ccd-nfd-dev.xlsx
 
 cd $basePath
 
 echo "Definition directory: ${ccdDefinitionsPath}"
 echo "Definition spreadsheet ${definitionOutputFile}"
 
-mkdir -p $(dirname ${definitionOutputFile})
+mkdir -p ${config_build_dir}
 
 ${scriptPath}/generate-ccd-definition.sh $ccdDefinitionsPath $definitionOutputFile "-e *-nonprod.json,*-testing.json"
 ${scriptPath}/ccd-import-definition.sh $definitionOutputFile
