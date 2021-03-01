748975fdd2e3f113ed13ed6bc1298579c3cc6448
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-01 12:07:19 +0000

    Fix import  script

--- bin/process-and-import-ccd-definition.sh
+++ bin/process-and-import-ccd-definition.sh
@@ -3,18 +3,15 @@
 set -eu
 
 scriptPath=$(dirname $(realpath $0))
-basePath=$(dirname $(dirname $scriptPath))
-ccdDefinitionsPath=$basePath/nfdiv-case-api/ccd-definitions/definitions/development
 
-config_build_dir=$basePath/nfdiv-case-api/ccd-definitions/build/ccd-config
-definitionOutputFile=${config_build_dir}/ccd-nfd-dev.xlsx
+echo "Script Path ${scriptPath}"
 
-cd $basePath
+root_dir=$(realpath $(dirname ${0})/..)
+config_dir=${root_dir}/ccd-definitions/definitions/development
+build_dir=${root_dir}/ccd-definitions/build/ccd-config
+definitionOutputFile=${build_dir}/ccd-nfd-dev.xlsx
 
-echo "Definition directory: ${ccdDefinitionsPath}"
-echo "Definition spreadsheet ${definitionOutputFile}"
+mkdir -p ${build_dir}
 
-mkdir -p ${config_build_dir}
-
-${scriptPath}/generate-ccd-definition.sh $ccdDefinitionsPath $definitionOutputFile "-e *-nonprod.json,*-testing.json"
+${scriptPath}/generate-ccd-definition.sh $config_dir $definitionOutputFile "-e *-nonprod.json,*-testing.json"
 ${scriptPath}/ccd-import-definition.sh $definitionOutputFile
