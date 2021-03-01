f1ad720d1aed37b2a9d8043e4ea0032a99b88835
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-01 11:24:26 +0000

    Added echo to list directory paths

--- bin/generate-ccd-definition.sh
+++ bin/generate-ccd-definition.sh
@@ -14,9 +14,12 @@ if [[ ! -e ${definition_output_file} ]]; then
    touch ${definition_output_file}
 fi
 
+echo "Definition input directory: ${definition_input_dir}"
+echo "Definition output file: ${definition_output_file}"
+echo "Additional params: ${additionalParameters}"
+
 docker run --rm --name json2xlsx \
   -v ${definition_input_dir}:/tmp/ccd-definition \
   -v ${definition_output_file}:/tmp/ccd-definition.xlsx \
-  -e CCD_DEF_CASE_SERVICE_BASE_URL=${CCD_DEF_CASE_SERVICE_BASE_URL:-http://docker.for.mac.localhost:4000} \
   hmctspublic.azurecr.io/ccd/definition-processor:${definition_processor_version} \
   json2xlsx -D /tmp/ccd-definition -o /tmp/ccd-definition.xlsx ${additionalParameters}
