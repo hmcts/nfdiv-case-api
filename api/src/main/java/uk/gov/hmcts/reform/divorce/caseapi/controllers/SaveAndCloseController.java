058730b36de56647ce50fc7ce141b657c24fdbc7
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-10 15:55:02 +0000

    Fixed review comments

--- api/src/main/java/uk/gov/hmcts/reform/divorce/caseapi/controllers/SaveAndCloseController.java
+++ api/src/main/java/uk/gov/hmcts/reform/divorce/caseapi/controllers/SaveAndCloseController.java
@@ -7,20 +7,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.divorce.caseapi.model.CcdCallbackRequest;
 import uk.gov.hmcts.reform.divorce.caseapi.notification.handler.SaveAndSignOutNotificationHandler;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SAVE_AND_CLOSE;
+import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SUBMITTED_WEBHOOK;
 
 @Slf4j
 @RestController
+@RequestMapping(SAVE_AND_CLOSE)
 public class SaveAndCloseController {
 
     @Autowired
     private SaveAndSignOutNotificationHandler saveAndSignOutNotificationHandler;
 
-    @PostMapping(path = "/notify-applicant", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    @PostMapping(path = SUBMITTED_WEBHOOK, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @ApiOperation(value = "Notifies applicant by sending email using gov notify")
     @ApiResponses(value = {
         @ApiResponse(code = 200, message = "Applicant was successfully notified"),
