85753ee65039caa3244f11f894a858940404b919
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-10 16:01:14 +0000

    Update constants

--- api/src/test/java/uk/gov/hmcts/reform/divorce/caseapi/TestConstants.java
+++ api/src/test/java/uk/gov/hmcts/reform/divorce/caseapi/TestConstants.java
@@ -5,12 +5,7 @@ public final class TestConstants {
     public static final String TEST_FIRST_NAME = "test_first_name";
     public static final String TEST_LAST_NAME = "test_last_name";
 
-    public static final String D8_PETITIONER_FIRST_NAME = "D8PetitionerFirstName";
-    public static final String D8_PETITIONER_LAST_NAME = "D8PetitionerLastName";
-    public static final String D8_PETITIONER_EMAIL = "D8PetitionerEmail";
-    public static final String DIVORCE_OR_DISSOLUTION = "divorceOrDissolution";
-
-    public static final String API_URL = "/notify-applicant";
+    public static final String API_URL = "/save-and-close/submitted";
 
     private TestConstants() {
     }
