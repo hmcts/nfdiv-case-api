ffb1a9e35fbfffa6f8d77727751282c773b55884
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-11 21:52:16 +0000

    Added functional tests

--- api/src/functionalTest/java/uk/gov/hmcts/reform/divorce/caseapi/SaveAndSignOutNotificationTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/reform/divorce/caseapi/SaveAndSignOutNotificationTest.java
@@ -10,10 +10,14 @@ import uk.gov.hmcts.reform.divorce.caseapi.model.CaseDetails;
 import uk.gov.hmcts.reform.divorce.caseapi.model.CcdCallbackRequest;
 import uk.gov.hmcts.reform.divorce.ccd.model.CaseData;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SAVE_AND_CLOSE;
 import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SERVICE_AUTHORIZATION;
@@ -21,7 +25,7 @@ import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SU
 import static uk.gov.hmcts.reform.divorce.ccd.model.enums.DivorceOrDissolution.DIVORCE;
 
 @SpringBootTest
-public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
+public class SaveAndSignOutNotificationTest {
     public static final String TEST_USER_EMAIL = "simulate-delivered@notifications.service.gov.uk";
     public static final String TEST_FIRST_NAME = "John";
     public static final String TEST_LAST_NAME = "Smith";
@@ -33,6 +37,12 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
     @Value("${test-url}")
     private String testUrl;
 
+    @Value("${idam.s2s-auth.url}")
+    private String s2sUrl;
+
+    @Value("${s2s.name}")
+    private String s2sName;
+
     @Test
     public void shouldSendEmailWhenAllTemplateParamsAreValid() {
         Response response = RestAssured
@@ -40,7 +50,7 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
             .relaxedHTTPSValidation()
             .baseUri(testUrl)
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header(SERVICE_AUTHORIZATION, signIn())
+            .header(SERVICE_AUTHORIZATION, generateServiceAuthTokenFor(s2sName))
             .body(
                 CcdCallbackRequest
                     .builder()
@@ -69,7 +79,7 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
             .relaxedHTTPSValidation()
             .baseUri(testUrl)
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header(SERVICE_AUTHORIZATION, signIn())
+            .header(SERVICE_AUTHORIZATION, generateServiceAuthTokenFor(s2sName))
             .body(
                 CcdCallbackRequest
                     .builder()
@@ -102,7 +112,7 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
             .relaxedHTTPSValidation()
             .baseUri(testUrl)
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header(SERVICE_AUTHORIZATION, signIn())
+            .header(SERVICE_AUTHORIZATION, generateServiceAuthTokenFor(s2sName))
             .body(
                 CcdCallbackRequest
                     .builder()
@@ -124,6 +134,56 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
                 + "\"message\":\"email_address is a required property\"}]");
     }
 
+    @Test
+    public void shouldReturn401WhenServiceAuthTokenIsInvalid() {
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(testUrl)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORIZATION, "Bearer invalid_token")
+            .body(
+                CcdCallbackRequest
+                    .builder()
+                    .caseDetails(
+                        CaseDetails
+                            .builder()
+                            .caseData(caseData())
+                            .build()
+                    )
+                    .build()
+            )
+            .when()
+            .post(SAVE_AND_SIGN_OUT_CALLBACK_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(UNAUTHORIZED.value());
+    }
+
+    @Test
+    public void shouldReturn403WhenServiceIsNotAuthorised() {
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(testUrl)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .header(SERVICE_AUTHORIZATION, generateServiceAuthTokenFor("ccd_gw"))
+            .body(
+                CcdCallbackRequest
+                    .builder()
+                    .caseDetails(
+                        CaseDetails
+                            .builder()
+                            .caseData(caseData())
+                            .build()
+                    )
+                    .build()
+            )
+            .when()
+            .post(SAVE_AND_SIGN_OUT_CALLBACK_URL);
+
+        assertThat(response.getStatusCode()).isEqualTo(FORBIDDEN.value());
+    }
+
     private CaseData caseData() {
         CaseData caseData = new CaseData();
         caseData.setD8PetitionerFirstName(TEST_FIRST_NAME);
@@ -131,5 +191,21 @@ public class SaveAndSignOutNotificationTest extends FunctionalTestSuite {
         caseData.setDivorceOrDissolution(DIVORCE);
         caseData.setD8PetitionerEmail(TEST_USER_EMAIL);
         return caseData;
+    }
+
+    private String generateServiceAuthTokenFor(String s2sName) {
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(this.s2sUrl)
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .body(Map.of("microservice", s2sName))
+            .when()
+            .post("/testing-support/lease")
+            .andReturn();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
+
+        return "Bearer " + response.getBody().print();
     }
 }
