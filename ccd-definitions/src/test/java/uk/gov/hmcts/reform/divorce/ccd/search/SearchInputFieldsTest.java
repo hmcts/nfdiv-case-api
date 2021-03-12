f423751afc02340b77c532ff0e3c5385e03deec2
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2021-03-12 09:31:15 +0000

    Minor.introduced Constants

--- ccd-definitions/src/test/java/uk/gov/hmcts/reform/divorce/ccd/search/SearchInputFieldsTest.java
+++ ccd-definitions/src/test/java/uk/gov/hmcts/reform/divorce/ccd/search/SearchInputFieldsTest.java
@@ -26,14 +26,21 @@ class SearchInputFieldsTest {
     private final ConfigBuilder<CaseData, State, UserRole> configBuilder = searchBuildingMockUtil.getConfigBuilder();
     private final Search.SearchBuilder<CaseData, UserRole> searchBuilder = searchBuildingMockUtil.getSearchBuilder();
 
+    public static final String D_8_PETITIONER_FIRST_NAME = "D8PetitionerFirstName";
+    public static final String D_8_PETITIONER_LAST_NAME = "D8PetitionerLastName";
+    public static final String D_8_PETITIONER_EMAIL = "D8PetitionerEmail";
+    public static final String FIRSTNAME = "FirstName";
+    public static final String LASTNAME = "LastName";
+    public static final String EMAIL = "Email";
+
     @Test
     void shouldBuildSearchInputFieldsWithConfigBuilder() {
 
         searchInputFields.applyTo(configBuilder);
 
-        final SearchField searchFieldFirstName = SearchField.builder().id("D8PetitionerFirstName").label("FirstName").build();
-        final SearchField searchFieldLastName = SearchField.builder().id("D8PetitionerLastName").label("LastName").build();
-        final SearchField searchFieldEmail = SearchField.builder().id("D8PetitionerEmail").label("Email").build();
+        final SearchField searchFieldFirstName = SearchField.builder().id(D_8_PETITIONER_FIRST_NAME).label(FIRSTNAME).build();
+        final SearchField searchFieldLastName = SearchField.builder().id(D_8_PETITIONER_LAST_NAME).label(LASTNAME).build();
+        final SearchField searchFieldEmail = SearchField.builder().id(D_8_PETITIONER_EMAIL).label(EMAIL).build();
 
         final List<SearchField> searchFieldList = of(searchFieldFirstName,searchFieldLastName,searchFieldEmail);
 
