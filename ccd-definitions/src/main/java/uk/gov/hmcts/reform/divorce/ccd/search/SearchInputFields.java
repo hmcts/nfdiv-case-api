2d51eb2da593749f993c999676fc8d114285c961
kasi-subramaniam <60706745+kasi-subramaniam@users.noreply.github.com>
2021-03-15 12:25:34 +0000

    NFDIV-524- Adding Search Fields - FirstName,LastName,Email   (#67)
    Adding SearchInput Fields - FirstName,LastName ,Email

--- ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/search/SearchInputFields.java
+++ ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/search/SearchInputFields.java
@@ -1,18 +1,33 @@
 package uk.gov.hmcts.reform.divorce.ccd.search;
 
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.SearchField;
 import uk.gov.hmcts.reform.divorce.ccd.CcdConfiguration;
 import uk.gov.hmcts.reform.divorce.ccd.model.CaseData;
 import uk.gov.hmcts.reform.divorce.ccd.model.State;
 import uk.gov.hmcts.reform.divorce.ccd.model.UserRole;
+
+import java.util.List;
+
+import static java.util.List.of;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.D_8_PETITIONER_EMAIL;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.D_8_PETITIONER_FIRST_NAME;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.D_8_PETITIONER_LAST_NAME;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.EMAIL;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.FIRSTNAME;
+import static uk.gov.hmcts.reform.divorce.ccd.search.constants.CaseFieldsConstants.LASTNAME;
 
 public class SearchInputFields implements CcdConfiguration {
 
     @Override
     public void applyTo(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
 
-        configBuilder
-            .searchInputFields()
-            .caseReferenceField();
+        final List<SearchField> searchFieldList = of(
+                                                    SearchField.builder().label(FIRSTNAME).id(D_8_PETITIONER_FIRST_NAME).build(),
+                                                    SearchField.builder().label(LASTNAME).id(D_8_PETITIONER_LAST_NAME).build(),
+                                                    SearchField.builder().label(EMAIL).id(D_8_PETITIONER_EMAIL).build());
+
+        configBuilder.searchInputFields().caseReferenceField();
+        configBuilder.searchInputFields().fields(searchFieldList);
     }
 }
