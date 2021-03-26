6d50eeab36d299f4adf74fcea36a4e7334217bfa
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-26 16:38:26 +0000

    Updated integration tests for add petitioner roles

--- api/src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
+++ api/src/main/java/uk/gov/hmcts/reform/divorce/caseapi/CaseApiApplication.java
@@ -3,9 +3,20 @@ package uk.gov.hmcts.reform.divorce.caseapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.ccd.client.CaseUserApi;
+import uk.gov.hmcts.reform.divorce.caseapi.clients.FeesAndPaymentsClient;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "uk.gov.hmcts.reform.divorce.caseapi.clients")
+@EnableFeignClients(
+    clients = {
+        IdamApi.class,
+        ServiceAuthorisationApi.class,
+        CaseUserApi.class,
+        FeesAndPaymentsClient.class
+    }
+)
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class CaseApiApplication {
 
