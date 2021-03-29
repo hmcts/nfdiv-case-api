90db6948b75710472aa3efc94488ab9d45b149e3
Nitin Prabhu <prabhunitin61@gmail.com>
2021-03-29 17:35:32 +0100

    Refactored tests

--- api/src/functionalTest/java/uk/gov/hmcts/reform/divorce/caseapi/SolicitorSubmitPetitionTest.java
+++ api/src/functionalTest/java/uk/gov/hmcts/reform/divorce/caseapi/SolicitorSubmitPetitionTest.java
@@ -6,13 +6,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
-import org.springframework.util.ResourceUtils;
 import uk.gov.hmcts.reform.divorce.caseapi.model.CaseDetails;
 import uk.gov.hmcts.reform.divorce.caseapi.model.CcdCallbackRequest;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
@@ -20,6 +15,7 @@ import static org.skyscreamer.jsonassert.JSONCompareMode.STRICT;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.reform.divorce.caseapi.TestResourceUtil.expectedCcdCallbackResponse;
 import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.ABOUT_TO_START_WEBHOOK;
 import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SERVICE_AUTHORIZATION;
 import static uk.gov.hmcts.reform.divorce.caseapi.enums.NotificationConstants.SUBMIT_PETITION;
@@ -59,14 +55,9 @@ public class SolicitorSubmitPetitionTest extends FunctionalTestSuite {
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
 
         assertEquals(
-            expectedCcdCallbackResponse(),
+            expectedCcdCallbackResponse("classpath:responses/ccd-callback-submit-petition.json"),
             response.asString(),
             STRICT
         );
-    }
-
-    private String expectedCcdCallbackResponse() throws IOException {
-        File issueFeesResponseJsonFile = ResourceUtils.getFile("classpath:responses/ccd-callback-submit-petition.json");
-        return new String(Files.readAllBytes(issueFeesResponseJsonFile.toPath()));
     }
 }
