98f8799bbc7ac61ea1b545e9e4a389465fb39cfd
Linus Norton <linusnorton@gmail.com>
2021-03-30 06:50:05 +0100

    Add test to make sonar love me

--- ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/CaseData.java
+++ ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/CaseData.java
@@ -9,7 +9,7 @@ import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.DynamicList;
 import uk.gov.hmcts.ccd.sdk.type.OrderSummary;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
-import uk.gov.hmcts.reform.divorce.ccd.access.PetitionerOwned;
+import uk.gov.hmcts.reform.divorce.ccd.access.DefaultAccess;
 import uk.gov.hmcts.reform.divorce.ccd.model.enums.ChangedNameHow;
 import uk.gov.hmcts.reform.divorce.ccd.model.enums.ConfidentialAddress;
 import uk.gov.hmcts.reform.divorce.ccd.model.enums.DivorceOrDissolution;
@@ -35,7 +35,7 @@ public class CaseData {
     @JsonProperty("divorceOrDissolution")
     @CCD(
         label = "Divorce or Dissolution?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private DivorceOrDissolution divorceOrDissolution;
 
@@ -43,14 +43,14 @@ public class CaseData {
     @CCD(
         label = "Marriage broken irretrievably?",
         hint = "Has petitioner's marriage broken down irretrievably?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8ScreenHasMarriageBroken;
 
     @JsonProperty("D8MarriageIsSameSexCouple")
     @CCD(
         label = "Were the petitioner and the respondent a same-sex couple when they got married?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8MarriageIsSameSexCouple;
 
@@ -60,7 +60,7 @@ public class CaseData {
         hint = "The petitioner’s gender is collected for statistical purposes only.",
         typeOverride = FixedList,
         typeParameterOverride = "Gender",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private Gender d8InferredPetitionerGender;
 
@@ -70,7 +70,7 @@ public class CaseData {
         hint = "The respondent’s gender is collected for statistical purposes only.",
         typeOverride = FixedList,
         typeParameterOverride = "Gender",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private Gender d8InferredRespondentGender;
 
@@ -78,7 +78,7 @@ public class CaseData {
     @CCD(
         label = "Marriage date",
         typeOverride = Date,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8MarriageDate;
 
@@ -86,63 +86,63 @@ public class CaseData {
     @CCD(
         label = "Help with fees reference",
         regex = "([Hh][Ww][Ff]-?)?[0-9a-zA-Z]{3}-?[0-9a-zA-Z]{3}$",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8HelpWithFeesReferenceNumber;
 
     @JsonProperty("D8HelpWithFeesNeedHelp")
     @CCD(
         label = "Need help with fees?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8HelpWithFeesNeedHelp;
 
     @JsonProperty("D8ScreenHasMarriageCert")
     @CCD(
         label = "Petitioner got marriage cert.?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8ScreenHasMarriageCert;
 
     @JsonProperty("D8HelpWithFeesAppliedForFees")
     @CCD(
         label = "Applied for help with fees?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8HelpWithFeesAppliedForFees;
 
     @JsonProperty("D8MarriedInUk")
     @CCD(
         label = "Did the marriage take place in the UK?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8MarriedInUk;
 
     @JsonProperty("D8CertificateInEnglish")
     @CCD(
         label = "Marriage certificate in English?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8CertificateInEnglish;
 
     @JsonProperty("D8CertifiedTranslation")
     @CCD(
         label = "Marriage certificate translation",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8CertifiedTranslation;
 
     @JsonProperty("D8CountryName")
     @CCD(
         label = "Country where marriage took place",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8CountryName;
 
     @JsonProperty("D8MarriagePlaceOfMarriage")
     @CCD(
         label = "Place of marriage",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8MarriagePlaceOfMarriage;
 
@@ -150,14 +150,14 @@ public class CaseData {
     @CCD(
         label = "First name(s)",
         hint = "Include all middle names here",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8PetitionerFirstName;
 
     @JsonProperty("D8PetitionerLastName")
     @CCD(
         label = "Last name",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8PetitionerLastName;
 
@@ -165,77 +165,77 @@ public class CaseData {
     @CCD(
         label = "Petitioner's email address",
         typeOverride = Email,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8PetitionerEmail;
 
     @JsonProperty("PetitionerAgreedToReceiveEmails")
     @CCD(
         label = "Petitioner has agreed to receive notifications and be served (delivered) court documents by email",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo petitionerAgreedToReceiveEmails;
 
     @JsonProperty("JurisdictionPetitionerResidence")
     @CCD(
         label = "Is the petitioner resident?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionPetitionerResidence;
 
     @JsonProperty("JurisdictionRespondentResidence")
     @CCD(
         label = "Is the respondent resident?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionRespondentResidence;
 
     @JsonProperty("JurisdictionPetitionerDomicile")
     @CCD(
         label = "Is the petitioner domiciled?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionPetitionerDomicile;
 
     @JsonProperty("JurisdictionRespondentDomicile")
     @CCD(
         label = "Is the respondent domiciled?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionRespondentDomicile;
 
     @JsonProperty("JurisdictionPetHabituallyResLastTwelveMonths")
     @CCD(
         label = "Has the petitioner been resident for the last twelve months?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionPetHabituallyResLastTwelveMonths;
 
     @JsonProperty("JurisdictionPetHabituallyResLastSixMonths")
     @CCD(
         label = "Has the petitioner been resident for the last six months?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionPetHabituallyResLastSixMonths;
 
     @JsonProperty("JurisdictionResidualEligible")
     @CCD(
         label = "Is residual jurisdiction eligible?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionResidualEligible;
 
     @JsonProperty("JurisdictionBothLastHabituallyResident")
     @CCD(
         label = "Were the both petitioner and respondent last habitually resident, and one still resides?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo jurisdictionBothLastHabituallyResident;
 
     @JsonProperty("LanguagePreferenceWelsh")
     @CCD(
         label = "What language do you want to receive emails and documents in?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo languagePreferenceWelsh;
 
@@ -244,14 +244,14 @@ public class CaseData {
         label = "Has the petitioner changed their name since they got married?",
         hint = "Is the respondent’s current name different to their married name or the name shown on their "
             + "marriage certificate?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8PetitionerNameDifferentToMarriageCert;
 
     @JsonProperty("D8PetitionerNameChangedHow")
     @CCD(
         label = "How did the petitioner change their name?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private ChangedNameHow d8PetitionerNameChangedHow;
 
@@ -259,7 +259,7 @@ public class CaseData {
     @CCD(
         label = "Provide details of how they changed their name",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8PetitionerNameChangedHowOtherDetails;
 
@@ -269,7 +269,7 @@ public class CaseData {
         hint = "Husband or Wife?",
         typeOverride = FixedList,
         typeParameterOverride = "WhoDivorcing",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private WhoDivorcing d8DivorceWho;
 
@@ -277,7 +277,7 @@ public class CaseData {
     @CCD(
         label = "The Petitioner's home address",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8DerivedPetitionerHomeAddress;
 
@@ -285,7 +285,7 @@ public class CaseData {
     @CCD(
         label = "Petitioner's phone number",
         regex = "^[0-9 +().-]{9,}$",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8PetitionerPhoneNumber;
 
@@ -294,7 +294,7 @@ public class CaseData {
         label = "Keep the petitioner's contact details private from the respondent?",
         typeOverride = FixedList,
         typeParameterOverride = "ConfidentialAddress",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private ConfidentialAddress d8PetitionerContactDetailsConfidential;
 
@@ -302,14 +302,14 @@ public class CaseData {
     @CCD(
         label = "Respondent's First name(s)",
         hint = "Include all middle names here",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8RespondentFirstName;
 
     @JsonProperty("D8RespondentLastName")
     @CCD(
         label = "Respondent's Last name",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8RespondentLastName;
 
@@ -318,7 +318,7 @@ public class CaseData {
         label = "Name changed since marriage?",
         hint = "Is the respondent’s current name different to their married name or the name shown on their "
             + "marriage certificate?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8RespondentNameAsOnMarriageCertificate;
 
@@ -326,21 +326,21 @@ public class CaseData {
     @CCD(
         label = "Please explain, if known, how their name has changed since they were married.",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String respNameDifferentToMarriageCertExplain;
 
     @JsonProperty("PetitionerSolicitorName")
     @CCD(
         label = "Petitioner Solicitor’s name",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String petitionerSolicitorName;
 
     @JsonProperty("D8SolicitorReference")
     @CCD(
         label = "Your reference number",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8SolicitorReference;
 
@@ -348,7 +348,7 @@ public class CaseData {
     @CCD(
         label = "Petitioner Solicitor Phone number",
         regex = "^[0-9 +().-]{9,}$",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String petitionerSolicitorPhone;
 
@@ -356,7 +356,7 @@ public class CaseData {
     @CCD(
         label = "Petitioner Solicitor Email",
         typeOverride = Email,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String petitionerSolicitorEmail;
 
@@ -364,7 +364,7 @@ public class CaseData {
     @CCD(
         label = "I confirm I am willing to accept service of all correspondence and orders by email at the email address "
             + "stated above.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo solicitorAgreeToReceiveEmails;
 
@@ -372,35 +372,35 @@ public class CaseData {
     @CCD(
         label = "Firm address/DX address",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String derivedPetitionerSolicitorAddr;
 
     @JsonProperty("PetitionerOrganisationPolicy")
     @CCD(
         label = "Firm address/DX address",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String petitionerOrganisationPolicy;
 
     @JsonProperty("D8MarriagePetitionerName")
     @CCD(
         label = "Petitioner's full name as on marriage certificate",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8MarriagePetitionerName;
 
     @JsonProperty("D8MarriageRespondentName")
     @CCD(
         label = "Respondent's full name as on marriage certificate",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String d8MarriageRespondentName;
 
     @JsonProperty("SolUrgentCase")
     @CCD(
         label = "Is this an urgent jurisdiction case?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo solUrgentCase;
 
@@ -408,14 +408,14 @@ public class CaseData {
     @CCD(
         label = "Any supporting information or instructions?",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String solUrgentCaseSupportingInformation;
 
     @JsonProperty("D8DivorceCostsClaim")
     @CCD(
         label = "Does the petitioner want to claim costs?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8DivorceCostsClaim;
 
@@ -424,56 +424,56 @@ public class CaseData {
         label = "Does the petitioner wish to apply for a financial order?",
         hint = "The court will not start processing your request for a financial order until you submit the separate "
             + "application and pay the fee.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8FinancialOrder;
 
     @JsonProperty("SolServiceMethod")
     @CCD(
         label = "How would you like the respondent to be served?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private SolServiceMethod solServiceMethod;
 
     @JsonProperty("SolStatementOfReconciliationCertify")
     @CCD(
         label = "I have discussed the possibility of a reconciliation with the petitioner.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo solStatementOfReconciliationCertify;
 
     @JsonProperty("SolStatementOfReconciliationDiscussed")
     @CCD(
         label = "I have given the petitioner the names and addresses of persons qualified to help effect a reconciliation.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo solStatementOfReconciliationDiscussed;
 
     @JsonProperty("D8StatementOfTruth")
     @CCD(
         label = "The petitioner believes that the facts stated in this application are true.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo d8StatementOfTruth;
 
     @JsonProperty("SolSignStatementOfTruth")
     @CCD(
         label = "I am duly authorised by the petitioner to sign this statement.",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo solSignStatementOfTruth;
 
     @JsonProperty("SolStatementOfReconciliationName")
     @CCD(
         label = "Your name",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String solStatementOfReconciliationName;
 
     @JsonProperty("SolStatementOfReconciliationFirm")
     @CCD(
         label = "Name of your firm",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String solStatementOfReconciliationFirm;
 
@@ -482,14 +482,14 @@ public class CaseData {
         label = "Additional comments",
         hint = "For the attention of court staff. These comments will not form part of the petition",
         typeOverride = TextArea,
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String statementOfReconciliationComments;
 
     @JsonProperty("SolApplicationFeeInPounds")
     @CCD(
         label = "Solicitor application fee (in pounds)",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String solApplicationFeeInPounds;
 
@@ -498,14 +498,14 @@ public class CaseData {
         label = "How will payment be made?",
         typeOverride = FixedList,
         typeParameterOverride = "SolToPay",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private SolToPay solPaymentHowToPay;
 
     @JsonProperty("PbaNumbers")
     @CCD(
         label = "Account number",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private DynamicList pbaNumbers;
 
@@ -513,21 +513,21 @@ public class CaseData {
     @CCD(
         label = "Enter your reference",
         hint = "This will appear on your statement to help you identify this payment",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private String feeAccountReference;
 
     @JsonProperty("SolApplicationFeeOrderSummary")
     @CCD(
         label = "Here are your order details",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private OrderSummary solApplicationFeeOrderSummary;
 
     @JsonProperty("LastNameChangedWhenMarried")
     @CCD(
         label = "Did you change your last name when you got married?",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private YesOrNo lastNameChangedWhenMarried;
 
@@ -535,7 +535,7 @@ public class CaseData {
     @JsonProperty("JurisdictionConnections")
     @CCD(
         label = "Jurisdiction connections",
-        access = { PetitionerOwned.class }
+        access = { DefaultAccess.class }
     )
     private Set<JurisdictionConnections> jurisdictionConnections;
 }
--- ccd-definitions/src/test/java/uk/gov/hmcts/reform/divorce/ccd/access/DefaultAccessTest.java
+++ ccd-definitions/src/test/java/uk/gov/hmcts/reform/divorce/ccd/access/DefaultAccessTest.java
@@ -1,12 +1,12 @@
 package uk.gov.hmcts.reform.divorce.ccd.access;
 
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
-import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
-import static uk.gov.hmcts.reform.divorce.ccd.Permissions.CREATE_READ_UPDATE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.gov.hmcts.ccd.sdk.api.Permission.CRU;
 import static uk.gov.hmcts.reform.divorce.ccd.Permissions.READ;
 import static uk.gov.hmcts.reform.divorce.ccd.model.UserRole.CASEWORKER_DIVORCE_COURTADMIN;
 import static uk.gov.hmcts.reform.divorce.ccd.model.UserRole.CASEWORKER_DIVORCE_COURTADMIN_BETA;
@@ -15,17 +15,18 @@ import static uk.gov.hmcts.reform.divorce.ccd.model.UserRole.CASEWORKER_DIVORCE_
 import static uk.gov.hmcts.reform.divorce.ccd.model.UserRole.CASEWORKER_DIVORCE_SUPERUSER;
 import static uk.gov.hmcts.reform.divorce.ccd.model.UserRole.CITIZEN;
 
-public class PetitionerOwned implements HasAccessControl {
-    @Override
-    public SetMultimap<HasRole, Permission> getGrants() {
-        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
-        grants.putAll(CITIZEN, CREATE_READ_UPDATE);
-        grants.putAll(CASEWORKER_DIVORCE_COURTADMIN_BETA, READ);
-        grants.putAll(CASEWORKER_DIVORCE_COURTADMIN, READ);
-        grants.putAll(CASEWORKER_DIVORCE_SOLICITOR, READ);
-        grants.putAll(CASEWORKER_DIVORCE_SUPERUSER, READ);
-        grants.putAll(CASEWORKER_DIVORCE_COURTADMIN_LA, READ);
+public class DefaultAccessTest {
 
-        return grants;
+    @Test
+    void shouldAddGrantsToMultiMap() {
+        DefaultAccess access = new DefaultAccess();
+        SetMultimap<HasRole, Permission> grants = access.getGrants();
+
+        assertEquals(grants.get(CITIZEN), CRU);
+        assertEquals(grants.get(CASEWORKER_DIVORCE_COURTADMIN_BETA), READ);
+        assertEquals(grants.get(CASEWORKER_DIVORCE_COURTADMIN), READ);
+        assertEquals(grants.get(CASEWORKER_DIVORCE_SOLICITOR), READ);
+        assertEquals(grants.get(CASEWORKER_DIVORCE_SUPERUSER), READ);
+        assertEquals(grants.get(CASEWORKER_DIVORCE_COURTADMIN_LA), READ);
     }
 }
