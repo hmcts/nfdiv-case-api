9a77f4eab327ffe9d5513c42ba2c4e3541353029
velimd <duzgunce.vd@gmail.com>
2021-03-31 11:59:03 +0100

    update field name

--- ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/CaseData.java
+++ ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/CaseData.java
@@ -481,5 +481,5 @@ public class CaseData {
         label = "Do you know their email address",
         access = { DefaultAccess.class }
     )
-    private YesOrNo petitionerKnowsRespondentsEmailAddress;
+    private YesOrNo PetitionerKnowsRespondentsAddress;
 }
