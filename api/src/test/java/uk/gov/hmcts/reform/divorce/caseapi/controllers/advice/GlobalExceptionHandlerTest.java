f712ff404291606c3e789cd62445255b9660b880
Shaun Francis <shaun.francis1@hmcts.net>
2021-04-08 10:59:30 +0100

    Fix OrganizationPolicy CCD definition

--- api/src/test/java/uk/gov/hmcts/reform/divorce/caseapi/controllers/advice/GlobalExceptionHandlerTest.java
+++ api/src/test/java/uk/gov/hmcts/reform/divorce/caseapi/controllers/advice/GlobalExceptionHandlerTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class GlobalExceptionCaseDataUpdaterTest {
+public class GlobalExceptionHandlerTest {
 
     @Test
     public void shouldHandleNotificationException() {
--- ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/OrganisationPolicy.java
+++ ccd-definitions/src/main/java/uk/gov/hmcts/reform/divorce/ccd/model/OrganisationPolicy.java
@@ -1,41 +1,32 @@
 package uk.gov.hmcts.reform.divorce.ccd.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.reform.divorce.ccd.access.DefaultAccess;
+import uk.gov.hmcts.ccd.sdk.api.ComplexType;
 
 @Data
-@ToString
-@JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder(toBuilder = true)
+@Builder
+@ComplexType(name = "OrganisationPolicy")
 public class OrganisationPolicy {
 
-    @CCD(
-        label = "Organisation",
-        access = { DefaultAccess.class }
-    )
     @JsonProperty("Organisation")
     private Organisation organisation;
 
-    @CCD(
-        label = "Org Policy Case Assigned Role",
-        access = { DefaultAccess.class }
-    )
     @JsonProperty("OrgPolicyCaseAssignedRole")
     private String orgPolicyCaseAssignedRole;
 
-    @CCD(
-        label = "Org Policy Reference",
-        access = { DefaultAccess.class }
-    )
     @JsonProperty("OrgPolicyReference")
     private String orgPolicyReference;
+
+    @Data
+    @ComplexType(name = "Organisation")
+    public static class Organisation {
+        @JsonProperty("OrganisationID")
+        private final String organisationID;
+    }
 }
