345cfee4a99da56c965fdf1ddc27ebc4b7a0ed13
Shaun Francis <shaun.francis1@hmcts.net>
2021-04-12 15:58:57 +0100

    Add solicitor claim for costs page

---
+++ src/main/java/uk/gov/hmcts/divorce/ccd/event/solicitor/page/ClaimForCosts.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.divorce.ccd.event.solicitor.page;
+
+import uk.gov.hmcts.ccd.sdk.api.Event.EventBuilder;
+import uk.gov.hmcts.ccd.sdk.api.FieldCollection.FieldCollectionBuilder;
+import uk.gov.hmcts.divorce.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.ccd.model.CaseData;
+import uk.gov.hmcts.divorce.ccd.model.State;
+import uk.gov.hmcts.divorce.ccd.model.UserRole;
+
+public class ClaimForCosts implements CcdPageConfiguration {
+
+    @Override
+    public void addTo(final FieldCollectionBuilder<CaseData, EventBuilder<CaseData, UserRole, State>> fieldCollectionBuilder) {
+
+        fieldCollectionBuilder
+            .page("ClaimForCosts")
+            .pageLabel("Claim for costs")
+            .label(
+                "claimForCostsEditMessage",
+                "You can make changes at the end of your application.")
+            .label(
+                "LabelClaimForCostsPara-1",
+                "A claim for costs can include all the fees the petitioner has to pay during the divorce, such as "
+                    + "application fees, solicitor fees and any extra court fees.")
+            .mandatory(CaseData::getDivorceCostsClaim);
+    }
+}
