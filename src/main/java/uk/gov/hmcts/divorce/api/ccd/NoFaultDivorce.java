cc0e04a5d959917d5602002abc072b4f9ac59056
Linus Norton <linusnorton@gmail.com>
2021-04-14 09:39:59 +0100

    remove ccd environment, add coverage

--- src/main/java/uk/gov/hmcts/divorce/api/ccd/NoFaultDivorce.java
+++ src/main/java/uk/gov/hmcts/divorce/api/ccd/NoFaultDivorce.java
@@ -26,7 +26,6 @@ public class NoFaultDivorce implements CCDConfig<CaseData, State, UserRole> {
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        configBuilder.setEnvironment("development");
         configBuilder.setCallbackHost(System.getenv().getOrDefault("CASE_API_URL", "http://nfdiv-case-api:4013"));
 
         configBuilder.caseType(CASE_TYPE, CASE_TYPE, "Handling of the dissolution of marriage");
--- src/test/java/uk/gov/hmcts/divorce/api/ccd/event/SaveAndCloseTest.java
+++ src/test/java/uk/gov/hmcts/divorce/api/ccd/event/SaveAndCloseTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.divorce.api.ccd.event.SaveAndClose;
 import uk.gov.hmcts.divorce.api.ccd.model.CaseData;
 import uk.gov.hmcts.divorce.api.ccd.model.State;
+import uk.gov.hmcts.divorce.api.ccd.model.UserRole;
 import uk.gov.hmcts.divorce.api.notification.handler.SaveAndSignOutNotificationHandler;
 
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.divorce.api.ccd.event.SaveAndClose.SAVE_AND_CLOSE;
 import static uk.gov.hmcts.divorce.api.util.TestDataHelper.caseData;
 
 
@@ -22,6 +28,16 @@ public class SaveAndCloseTest {
 
     @InjectMocks
     private SaveAndClose saveAndClose;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final Set<State> stateSet = Set.of(State.class.getEnumConstants());
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
+
+        saveAndClose.configure(configBuilder);
+
+        assertThat(configBuilder.getEvents().get(0).getEventID(), is(SAVE_AND_CLOSE));
+    }
 
     @Test
     public void givenValidCaseDataWhenCallbackIsInvokedThenSendEmail() throws Exception {
