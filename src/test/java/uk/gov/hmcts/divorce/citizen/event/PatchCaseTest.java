661a1748a6c241cc13fee1669f5bfa23e0f9150e
Shaun Francis <shaun.francis1@hmcts.net>
2021-04-16 13:25:20 +0100

    Add unit tests and exclude model packages

---
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/PatchCaseTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.divorce.citizen.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.divorce.common.model.CaseData;
+import uk.gov.hmcts.divorce.common.model.State;
+import uk.gov.hmcts.divorce.common.model.UserRole;
+
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.gov.hmcts.divorce.citizen.event.PatchCase.PATCH_CASE;
+
+@ExtendWith(MockitoExtension.class)
+class PatchCaseTest {
+
+    @InjectMocks
+    private PatchCase patchCase;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+
+        final Set<State> stateSet = Set.of(State.class.getEnumConstants());
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
+
+        patchCase.configure(configBuilder);
+
+        assertThat(configBuilder.getEvents().get(0).getEventID(), is(PATCH_CASE));
+    }
+}
