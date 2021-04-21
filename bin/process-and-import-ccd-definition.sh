6e57b73bfd320034eb3d7714aa034bdc0b5f46d1
Linus Norton <linusnorton@gmail.com>
2021-04-21 08:12:24 +0100

    fix missing var

--- bin/process-and-import-ccd-definition.sh
+++ bin/process-and-import-ccd-definition.sh
@@ -4,6 +4,7 @@ set -eu
 
 scriptPath=$(dirname $(realpath $0))
 
+root_dir=$(realpath $(dirname ${0})/..)
 build_dir=${root_dir}/build/ccd-config
 definitionOutputFile=${build_dir}/ccd-nfd-${CCD_DEF_NAME:-dev}.xlsx
 
