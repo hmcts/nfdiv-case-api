cfc11af6eef4e6889224e40872868f5acc0d0e3c
Linus Norton <linusnorton@gmail.com>
2021-04-21 07:33:28 +0100

    continue the fight

--- bin/process-and-import-ccd-definition.sh
+++ bin/process-and-import-ccd-definition.sh
@@ -4,14 +4,8 @@ set -eu
 
 scriptPath=$(dirname $(realpath $0))
 
-echo "Script Path ${scriptPath}"
-
-root_dir=$(realpath $(dirname ${0})/..)
-config_dir=${root_dir}/build/definitions
 build_dir=${root_dir}/build/ccd-config
 definitionOutputFile=${build_dir}/ccd-nfd-${CCD_DEF_NAME:-dev}.xlsx
 
-mkdir -p ${build_dir}
-
-${scriptPath}/generate-ccd-definition.sh $config_dir $definitionOutputFile "-e *-nonprod.json,*-testing.json"
+${scriptPath}/build-ccd-definition.sh
 ${scriptPath}/ccd-import-definition.sh $definitionOutputFile
