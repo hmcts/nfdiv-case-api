0c0bc499d600c66c7e2790c9e2c054b795103a38
Tom Elliott <tomelliott@Toms-MBP.home>
2021-04-29 11:36:38 +0100

    Add missing CCD annotation to Payment fields

--- src/main/java/uk/gov/hmcts/divorce/payment/model/Payment.java
+++ src/main/java/uk/gov/hmcts/divorce/payment/model/Payment.java
@@ -1,37 +1,74 @@
 package uk.gov.hmcts.divorce.payment.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.divorce.common.model.access.DefaultAccess;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class Payment {
+
+    @CCD(
+        label = "Payment Date",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentDate")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate paymentDate;
 
+    @CCD(
+        label = "Payment Fee Id",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentFeeId")
     private String paymentFeeId;
 
+    @CCD(
+        label = "Payment Amount",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentAmount")
     private String paymentAmount;
 
+    @CCD(
+        label = "Payment Site Id",
+        access = {DefaultAccess.class}
+    )
     @ApiModelProperty("ID of site the payment was made")
     @JsonProperty("PaymentSiteId")
     private String paymentSiteId;
 
+    @CCD(
+        label = "Payment Status",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentStatus")
     private PaymentStatus paymentStatus;
 
+    @CCD(
+        label = "Payment Channel",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentChannel")
     private String paymentChannel;
 
+    @CCD(
+        label = "Payment Reference",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentReference")
     private String paymentReference;
 
+    @CCD(
+        label = "Payment Transaction Id",
+        access = {DefaultAccess.class}
+    )
     @JsonProperty("PaymentTransactionId")
     private String paymentTransactionId;
 }
