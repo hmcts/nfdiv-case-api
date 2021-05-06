697878e01219c1edd1a27044fedf44275a582568
Linus Norton <linusnorton@gmail.com>
2021-05-06 06:48:54 +0100

    Add more tests

--- src/integrationTest/java/uk/gov/hmcts/divorce/citizen/event/PaymentMadeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/citizen/event/PaymentMadeTest.java
@@ -26,6 +26,7 @@ import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -34,6 +35,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.gov.hmcts.divorce.citizen.event.PaymentMade.PAYMENT_MADE;
 import static uk.gov.hmcts.divorce.common.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.APPLICATION_SUBMITTED;
+import static uk.gov.hmcts.divorce.payment.model.PaymentStatus.DECLINED;
 import static uk.gov.hmcts.divorce.payment.model.PaymentStatus.SUCCESS;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.AUTH_HEADER_VALUE;
@@ -79,7 +81,7 @@ public class PaymentMadeTest {
             .build();
 
         data.put("payments", singletonList(new ListValue<>("1", payment)));
-        System.out.println(objectMapper.writeValueAsString(callbackRequest(data, PAYMENT_MADE)));
+
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
             .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
@@ -91,6 +93,28 @@ public class PaymentMadeTest {
             .sendEmail(eq(TEST_USER_EMAIL), eq(APPLICATION_SUBMITTED), anyMap(), eq(ENGLISH));
 
         verifyNoMoreInteractions(notificationService);
+    }
+
+    @Test
+    public void givenDeclinedPaymentDontSendNotification() throws Exception {
+        Map<String, Object> data = caseDataMap();
+        data.put("dateSubmitted", LocalDateTime.now());
+
+        Payment payment = Payment.builder()
+            .paymentAmount(55000)
+            .paymentStatus(DECLINED)
+            .build();
+
+        data.put("payments", singletonList(new ListValue<>("1", payment)));
+
+        mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
+            .contentType(APPLICATION_JSON)
+            .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
+            .content(objectMapper.writeValueAsString(callbackRequest(data, PAYMENT_MADE)))
+            .accept(APPLICATION_JSON))
+            .andExpect(status().isOk());
+
+        verifyNoInteractions(notificationService);
     }
 
     @Test
--- src/test/java/uk/gov/hmcts/divorce/citizen/event/PaymentMadeTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/PaymentMadeTest.java
@@ -7,17 +7,23 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.citizen.notification.ApplicationSubmittedNotification;
 import uk.gov.hmcts.divorce.common.model.CaseData;
 import uk.gov.hmcts.divorce.common.model.State;
 import uk.gov.hmcts.divorce.common.model.UserRole;
+import uk.gov.hmcts.divorce.payment.model.Payment;
 
 import java.util.Set;
 
+import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static uk.gov.hmcts.divorce.citizen.event.PaymentMade.PAYMENT_MADE;
+import static uk.gov.hmcts.divorce.payment.model.PaymentStatus.DECLINED;
+import static uk.gov.hmcts.divorce.payment.model.PaymentStatus.SUCCESS;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
@@ -41,9 +47,12 @@ public class PaymentMadeTest {
     }
 
     @Test
-    public void givenValidCaseDataWhenCallbackIsInvokedThenSendEmail() throws Exception {
+    public void givenValidCaseDataWhenCallbackIsInvokedThenSendEmail() {
         final CaseData caseData = caseData();
         caseData.setPetitionerEmail(TEST_USER_EMAIL);
+
+        Payment payment = Payment.builder().paymentAmount(55000).paymentStatus(SUCCESS).build();
+        caseData.setPayments(singletonList(new ListValue<>("1", payment)));
 
         final CaseDetails<CaseData, State> details = new CaseDetails<>();
         details.setData(caseData);
@@ -51,5 +60,21 @@ public class PaymentMadeTest {
         paymentMade.aboutToSubmit(details, details);
 
         verify(notification).send(caseData, details.getId());
+    }
+
+    @Test
+    public void givenInvalidPaymentWhenThenDontSendEmail() {
+        final CaseData caseData = caseData();
+        caseData.setPetitionerEmail(TEST_USER_EMAIL);
+
+        Payment payment = Payment.builder().paymentAmount(55000).paymentStatus(DECLINED).build();
+        caseData.setPayments(singletonList(new ListValue<>("1", payment)));
+
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+
+        paymentMade.aboutToSubmit(details, details);
+
+        verifyNoInteractions(notification);
     }
 }
