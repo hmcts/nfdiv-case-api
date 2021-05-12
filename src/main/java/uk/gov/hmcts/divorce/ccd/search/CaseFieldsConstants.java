8c03ce0cce36918db26203782d843a9a1b29a16a
Nitin Prabhu <prabhunitin61@gmail.com>
2021-05-12 15:05:53 +0100

    Added sonar exclusion for org contact information as currently it is not used

--- src/main/java/uk/gov/hmcts/divorce/ccd/search/CaseFieldsConstants.java
+++ src/main/java/uk/gov/hmcts/divorce/ccd/search/CaseFieldsConstants.java
@@ -12,7 +12,7 @@ public final class CaseFieldsConstants {
     public static final String DIVORCE_OR_DISSOLUTION = "divorceOrDissolution";
     public static final String FINANCIAL_ORDER = "financialOrder";
     public static final String PETITIONER_ORGANISATION_POLICY = "petitionerOrganisationPolicy";
-    
+
     // required for Checkstyle
     private CaseFieldsConstants() {
     }
