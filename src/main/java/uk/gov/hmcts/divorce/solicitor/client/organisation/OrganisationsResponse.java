8803ec5f61ca3ca639448ac450f8810f8c6611c2
Nitin Prabhu <prabhunitin61@gmail.com>
2021-05-12 15:22:27 +0100

    Removed unused organisation contact info model

--- src/main/java/uk/gov/hmcts/divorce/solicitor/client/organisation/OrganisationsResponse.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/client/organisation/OrganisationsResponse.java
@@ -4,19 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrganisationsResponse {
-    @JsonProperty(value = "contactInformation")
-    private List<OrganisationContactInformation> contactInformation;
     @JsonProperty(value = "name")
     private String name;
     @JsonProperty(value = "organisationIdentifier")
