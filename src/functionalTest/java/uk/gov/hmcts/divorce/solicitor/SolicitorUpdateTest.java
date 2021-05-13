ee249915149558454f22daeda2da2bfe42846dcc
Shaun Francis <shaun.francis1@hmcts.net>
2021-05-13 13:52:33 +0100

    Tidy up functional test

--- src/functionalTest/java/uk/gov/hmcts/divorce/solicitor/SolicitorUpdateTest.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/solicitor/SolicitorUpdateTest.java
@@ -11,10 +11,6 @@ import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
-import static uk.gov.hmcts.divorce.ccd.search.CaseFieldsConstants.DIVORCE_COSTS_CLAIM;
-import static uk.gov.hmcts.divorce.ccd.search.CaseFieldsConstants.FINANCIAL_ORDER;
 import static uk.gov.hmcts.divorce.solicitor.event.SolicitorUpdate.SOLICITOR_UPDATE;
 import static uk.gov.hmcts.divorce.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.divorce.testutil.TestResourceUtil.ABOUT_TO_SUBMIT_CALLBACK_URL;
@@ -24,15 +20,11 @@ import static uk.gov.hmcts.divorce.testutil.TestResourceUtil.expectedCcdCallback
 public class SolicitorUpdateTest extends FunctionalTestSuite {
 
     private static final String REQUEST = "classpath:request/casedata/ccd-callback-casedata-solicitor-update.json";
-    private static final String LANGUAGE_PREFERENCE_WELSH = "languagePreferenceWelsh";
 
     @Test
     public void shouldUpdateCaseDataWhenAboutToSubmitCallbackIsSuccessful() throws Exception {
 
         final Map<String, Object> caseData = caseData(REQUEST);
-        caseData.put(DIVORCE_COSTS_CLAIM, YES);
-        caseData.put(LANGUAGE_PREFERENCE_WELSH, NO);
-        caseData.put(FINANCIAL_ORDER, NO);
 
         final Response response = triggerCallback(caseData, SOLICITOR_UPDATE, ABOUT_TO_SUBMIT_CALLBACK_URL);
 
