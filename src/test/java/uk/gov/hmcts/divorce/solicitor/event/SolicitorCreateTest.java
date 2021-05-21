56e19f04c335b42f677dcdb94a66771937eff2dd
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2021-05-21 08:45:19 +0100

    Revert "Move service logic to handler as it is more appropriate in handler (#266)"
    This reverts commit 0c7dd4581635cb416b260fa07b5b72d57db32170.

--- src/test/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorCreateTest.java
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorCreateTest.java
@@ -6,46 +6,40 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.divorce.common.model.CaseData;
 import uk.gov.hmcts.divorce.common.model.State;
 import uk.gov.hmcts.divorce.common.model.UserRole;
-import uk.gov.hmcts.divorce.common.updater.CaseDataUpdaterChainFactory;
 import uk.gov.hmcts.divorce.solicitor.event.page.Applicant2ServiceDetails;
 import uk.gov.hmcts.divorce.solicitor.event.page.SolAboutTheSolicitor;
 import uk.gov.hmcts.divorce.solicitor.service.SolicitorCreateApplicationService;
-import uk.gov.hmcts.divorce.solicitor.service.updater.ClaimsCost;
-import uk.gov.hmcts.divorce.solicitor.service.updater.MiniApplicationDraft;
-import uk.gov.hmcts.divorce.solicitor.service.updater.SolicitorCourtDetails;
 
 import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.divorce.solicitor.event.SolicitorCreate.SOLICITOR_CREATE;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class SolicitorCreateTest {
 
     @Mock
-    private SolAboutTheSolicitor solAboutTheSolicitor;
+    private SolicitorCreateApplicationService solicitorCreateApplicationService;
 
     @Mock
     private Applicant2ServiceDetails applicant2ServiceDetails;
 
     @Mock
-    private SolicitorCreateApplicationService solicitorCreateApplicationService;
+    private HttpServletRequest httpServletRequest;
 
     @Mock
-    private ClaimsCost claimsCost;
-
-    @Mock
-    private SolicitorCourtDetails solicitorCourtDetails;
-
-    @Mock
-    private MiniApplicationDraft miniApplicationDraft;
-
-    @Mock
-    private CaseDataUpdaterChainFactory caseDataUpdaterChainFactory;
+    private SolAboutTheSolicitor solAboutTheSolicitor;
 
     @InjectMocks
     private SolicitorCreate solicitorCreate;
@@ -58,5 +52,25 @@ class SolicitorCreateTest {
         solicitorCreate.configure(configBuilder);
 
         assertThat(configBuilder.getEvents().get(0).getId(), is(SOLICITOR_CREATE));
+    }
+
+    @Test
+    public void shouldPopulateMissingRequirementsFieldsInCaseData() throws Exception {
+        final CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(1L);
+        details.setCreatedDate(LOCAL_DATE_TIME);
+
+        final String auth = "authorization";
+        when(httpServletRequest.getHeader(AUTHORIZATION)).thenReturn(auth);
+
+        solicitorCreate.aboutToSubmit(details, details);
+
+        verify(solicitorCreateApplicationService).aboutToSubmit(
+            caseData,
+            details.getId(),
+            details.getCreatedDate().toLocalDate(),
+            auth);
     }
 }
