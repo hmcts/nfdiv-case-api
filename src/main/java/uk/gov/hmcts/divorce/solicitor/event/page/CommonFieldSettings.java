0ab0fb301378fedbc5c8a57a71ce2618f884cfbf
Shaun Francis <shaun.francis1@hmcts.net>
2021-05-24 11:59:23 +0100

    Add layout changes to applicant 1 solictor create page (#274)

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/CommonFieldSettings.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/CommonFieldSettings.java
@@ -10,6 +10,9 @@ public final class CommonFieldSettings {
         "![NFD Preview Joint](https://raw.githubusercontent.com/hmcts/nfdiv-case-api/master/resources/image/SolicitorNfdPreviewJointApplicationBanner.jpg "
             + "\"You are creating a Joint applicant in the NFD preview\")";
 
+    public static final String DARK_HORIZONTAL_RULE =
+        "![Dark Rule](https://raw.githubusercontent.com/hmcts/nfdiv-case-api/master/resources/image/LabelDarkHorizontalRule.png)";
+
     public static final String SOLE_APPLICATION_CONDITION = "applicationType=\"soleApplication\"";
     public static final String JOINT_APPLICATION_CONDITION = "applicationType=\"jointApplication\"";
 
