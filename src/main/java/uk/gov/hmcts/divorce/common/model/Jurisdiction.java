1f5ce91ca8064f5748b47d8d5985b56176292efc
Lara <laratollington@gmail.com>
2021-05-25 15:42:26 +0100

    Add jurisdiction connection

--- src/main/java/uk/gov/hmcts/divorce/common/model/Jurisdiction.java
+++ src/main/java/uk/gov/hmcts/divorce/common/model/Jurisdiction.java
@@ -17,6 +17,7 @@ import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_AP
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_APP_2_LAST_RESIDENT;
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_APP_2_RESIDENT;
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_DOMICILED;
+import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_RESIDENT;
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_RESIDENT_SIX_MONTHS;
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_1_RESIDENT_TWELVE_MONTHS;
 import static uk.gov.hmcts.divorce.common.model.JurisdictionConnections.APP_2_DOMICILED;
@@ -103,6 +104,7 @@ public class Jurisdiction {
             addToErrorList(validateJurisdictionConnectionG(), errorList);
             addToErrorList(validateJurisdictionConnectionH(), errorList);
             addToErrorList(validateJurisdictionConnectionI(), errorList);
+            addToErrorList(validateJurisdictionConnectionJ(), errorList);
         }
 
         return errorList;
@@ -171,6 +173,13 @@ public class Jurisdiction {
     private String validateJurisdictionConnectionI() {
         if (connections.contains(APP_2_DOMICILED) && applicant2Domicile != YesOrNo.YES) {
             return CONNECTION + APP_2_DOMICILED + CANNOT_EXIST;
+        }
+        return null;
+    }
+
+    private String validateJurisdictionConnectionJ() {
+        if (connections.contains(APP_1_RESIDENT) && applicant1Residence != YesOrNo.YES) {
+            return CONNECTION + APP_1_RESIDENT + CANNOT_EXIST;
         }
         return null;
     }
--- src/test/java/uk/gov/hmcts/divorce/common/model/JurisdictionTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/model/JurisdictionTest.java
@@ -114,6 +114,17 @@ public class JurisdictionTest {
     }
 
     @Test
+    public void shouldReturnErrorWhenJurisdictionConnectionIsJ() {
+        Jurisdiction jurisdiction = new Jurisdiction();
+
+        jurisdiction.setConnections(Set.of(JurisdictionConnections.APP_1_RESIDENT));
+
+        List<String> errors = jurisdiction.validate();
+
+        assertThat(errors, contains(CONNECTION + JurisdictionConnections.APP_1_RESIDENT + CANNOT_EXIST));
+    }
+
+    @Test
     public void shouldReturnErrorWhenJurisdictionConnectionsIsNull() {
         Jurisdiction jurisdiction = new Jurisdiction();
 
