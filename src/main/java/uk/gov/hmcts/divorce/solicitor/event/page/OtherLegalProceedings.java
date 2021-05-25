cda12d67d94c5a5f6aeee93e7bf3200ae092e29c
Tom Elliott <tomelliott@Toms-MBP.home>
2021-05-25 15:35:58 +0100

    NFDIV-688: Use new version of sdk and use field method from FieldCollection

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/OtherLegalProceedings.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/OtherLegalProceedings.java
@@ -3,8 +3,9 @@ package uk.gov.hmcts.divorce.solicitor.event.page;
 import uk.gov.hmcts.divorce.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.common.model.CaseData;
-import uk.gov.hmcts.divorce.common.model.LegalProceeding;
 
+import static uk.gov.hmcts.ccd.sdk.api.DisplayContext.Mandatory;
+import static uk.gov.hmcts.ccd.sdk.api.DisplayContext.Optional;
 import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.JOINT_APPLICATION_CONDITION;
 import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLE_APPLICATION_CONDITION;
 import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLICITOR_NFD_JOINT_PREVIEW_BANNER;
@@ -28,9 +29,9 @@ public class OtherLegalProceedings implements CcdPageConfiguration {
                 JOINT_APPLICATION_CONDITION)
             .mandatory(CaseData::getLegalProceedings)
             .complex(CaseData::getLegalProceedingsByCase, "legalProceedings=\"Yes\"")
-                .optional(LegalProceeding::getCaseNumber)
-                .mandatory(LegalProceeding::getCaseRelatesTo)
-                .optional(LegalProceeding::getCaseDetail)
+                .field("caseNumber", Optional)
+                .field("caseRelatesTo", Mandatory)
+                .field("caseDetail", Optional)
                 .done()
             .mandatory(
                 CaseData::getLegalProceedingsOther,
