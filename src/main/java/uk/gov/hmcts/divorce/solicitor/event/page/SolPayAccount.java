1a3b9d983e6bb8cd0e77d857b57a3b12b7035e9c
Shaun Francis <shaun.francis1@hmcts.net>
2021-06-02 16:32:19 +0100

    Add NFD banner to solicitor submit pages (#315)

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolPayAccount.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolPayAccount.java
@@ -4,6 +4,11 @@ import uk.gov.hmcts.divorce.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.common.model.CaseData;
 
+import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.JOINT_APPLICATION_CONDITION;
+import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLE_APPLICATION_CONDITION;
+import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLICITOR_NFD_JOINT_PREVIEW_BANNER;
+import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLICITOR_NFD_PREVIEW_BANNER;
+
 public class SolPayAccount implements CcdPageConfiguration {
 
     @Override
@@ -13,6 +18,14 @@ public class SolPayAccount implements CcdPageConfiguration {
             .page("SolPayAccount")
             .pageLabel("Pay account")
             .showCondition("solPaymentHowToPay=\"feePayByAccount\"")
+            .label(
+                "LabelNFDBanner-SolPayAccount",
+                SOLICITOR_NFD_PREVIEW_BANNER,
+                SOLE_APPLICATION_CONDITION)
+            .label(
+                "LabelNFDJointBanner-SolPayAccount",
+                SOLICITOR_NFD_JOINT_PREVIEW_BANNER,
+                JOINT_APPLICATION_CONDITION)
             .optional(CaseData::getPbaNumbers)
             .mandatory(CaseData::getFeeAccountReference);
     }
