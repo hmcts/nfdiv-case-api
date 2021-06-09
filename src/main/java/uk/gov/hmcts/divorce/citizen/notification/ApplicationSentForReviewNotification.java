2af44c9647e2ad732246e16be36efbcf3e9526e6
Lara <laratollington@gmail.com>
2021-06-09 13:43:04 +0100

    Update template variables

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/ApplicationSentForReviewNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/ApplicationSentForReviewNotification.java
@@ -10,8 +10,8 @@ import uk.gov.hmcts.divorce.notification.NotificationService;
 
 import java.util.Map;
 
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICANT1_ANSWERS_SENT_FOR_REVIEW;
-import static uk.gov.hmcts.divorce.notification.FormatUtil.dateTimeFormatter;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.DIVORCE_OR_DISSOLUTION;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.PARTNER;
 
@@ -25,26 +25,24 @@ public class ApplicationSentForReviewNotification {
     @Autowired
     private CommonContent commonContent;
 
-
     public void send(CaseData caseData, Long id) {
         Map<String, String> templateVars = commonContent.templateVarsFor(caseData);
 
-        templateVars.put("date plus two weeks", caseData.getDateOfSubmissionResponse().format(dateTimeFormatter));
+        templateVars.put("date plus two weeks", caseData.getDueDate().format(ofPattern("d MMMM yyyy")));
 
         if (caseData.getDivorceOrDissolution().isDivorce()) {
             templateVars.put(DIVORCE_OR_DISSOLUTION, "divorce application");
+            templateVars.put(" civilpartnership.case@justice .gov.uk/contactdivorce@justice.gov.uk", "contactdivorce@justice.gov.uk");
             if (caseData.getApplicant2().getGender().equals(Gender.MALE)) {
-                templateVars.put(PARTNER, "Husband");
+                templateVars.put(PARTNER, "husband");
             } else {
-                templateVars.put(PARTNER, "Wife");
+                templateVars.put(PARTNER, "wife");
             }
-            templateVars.put("civilpartnership.case@justice.gov.uk/contactdivorce@justice.gov.uk", "contactdivorce@justice.gov.uk");
         } else {
             templateVars.put(DIVORCE_OR_DISSOLUTION, "application to end your civil partnership");
+            templateVars.put(" civilpartnership.case@justice .gov.uk/contactdivorce@justice.gov.uk", "civilpartnership.case@justice.gov.uk");
             templateVars.put(PARTNER, "civil partner");
-            templateVars.put("civilpartnership.case@justice.gov.uk/contactdivorce@justice.gov.uk", "civilpartnership.case@justice.gov.uk");
         }
-
 
         log.info("Sending application awaiting review for case : {}", id);
 
