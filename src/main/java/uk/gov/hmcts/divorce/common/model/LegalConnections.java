7fc62819f8dd8459f64b3b031b74672466611e42
Nitin Prabhu <prabhunitin61@gmail.com>
2021-06-10 12:35:14 +0100

    Rearranged legal connections and removed residual jurisdiction text

--- src/main/java/uk/gov/hmcts/divorce/common/model/LegalConnections.java
+++ src/main/java/uk/gov/hmcts/divorce/common/model/LegalConnections.java
@@ -9,29 +9,35 @@ import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 @AllArgsConstructor
 public enum LegalConnections implements HasLabel {
 
-    @JsonProperty("A")
-    COURTS_RESIDUAL_JURISDICTION("The courts of England and Wales have residual jurisdiction"),
+    @JsonProperty("I")
+    RESPONDENT_DOMICILED("The respondent is domiciled in England and Wales"),
 
-    @JsonProperty("B")
-    APP_1_APP_2_DOMICILED("The applicant and the respondent are both domiciled in England and Wales"),
+    @JsonProperty("H")
+    APPLICANT_DOMICILED("The applicant is domiciled in England and Wales"),
 
-    @JsonProperty("C")
-    APP_1_DOMICILED_RESIDENT("The applicant is domiciled and habitually resident in England and Wales and has resided "
+    @JsonProperty("G")
+    COURTS_RESIDUAL_JURISDICTION("The courts of England and Wales have residual jurisdiction (same sex married couple)"),
+
+    @JsonProperty("F")
+    APPLICANT_RESPONDENT_DOMICILED("The applicant and respondent are both domiciled in England and Wales"),
+
+    @JsonProperty("E")
+    APPLICANT_DOMICILED_RESIDENT("The applicant is domiciled and habitually resident in England and Wales and has resided "
         + "there for at least six months immediately prior to the application"),
 
     @JsonProperty("D")
-    APP_1_RESIDENT("The applicant is habitually resident in England and Wales and has resided there for at least a year "
+    APPLICANT_RESIDENT("The applicant is habitually resident in England and Wales and has resided there for at least a year "
         + "immediately prior to the presentation of the application"),
 
-    @JsonProperty("E")
-    APP_2_RESIDENT("The respondent is habitually resident in England and Wales"),
+    @JsonProperty("C")
+    RESPONDENT_RESIDENT("The respondent is habitually resident in England and Wales"),
 
-    @JsonProperty("F")
-    APP_1_APP_2_ONE_RESIDENT("The applicant and the respondent were last habitually resident in England and Wales and one "
+    @JsonProperty("B")
+    APPLICANT_RESPONDENT_ONE_RESIDENT("The applicant and respondent were last habitually resident in England and Wales and one "
         + "of them still resides there"),
 
-    @JsonProperty("G")
-    APP_1_APP_2_RESIDENT("The applicant and the respondent are habitually resident in England and Wales");
+    @JsonProperty("A")
+    APPLICANT_RESPONDENT_RESIDENT("The applicant and the respondent are habitually resident in England and Wales");
 
     private final String label;
 }
