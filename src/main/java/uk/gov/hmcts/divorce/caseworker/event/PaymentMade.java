b99783187b010201e620d0da9eb398f17afe23a7
Nitin Prabhu <prabhunitin61@gmail.com>
2021-06-15 16:12:41 +0100

    Added events with starting state transitioning from Awaiting HWF

---
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/PaymentMade.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.common.model.CaseData;
+import uk.gov.hmcts.divorce.common.model.State;
+import uk.gov.hmcts.divorce.common.model.UserRole;
+
+import static uk.gov.hmcts.divorce.common.model.State.AwaitingHWFDecision;
+import static uk.gov.hmcts.divorce.common.model.State.Submitted;
+import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_COURTADMIN;
+import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_COURTADMIN_BETA;
+import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_COURTADMIN_LA;
+import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_SOLICITOR;
+import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_SUPERUSER;
+import static uk.gov.hmcts.divorce.common.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.common.model.access.Permissions.READ;
+
+public class PaymentMade implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String PAYMENT_MADE_FROM_AWAITING_HWF = "paymentMadeFromAwaitingHWFDecision";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        new PageBuilder(configBuilder
+            .event(PAYMENT_MADE_FROM_AWAITING_HWF)
+            .forStateTransition(AwaitingHWFDecision, Submitted)
+            .name("Payment made")
+            .description("Payment made")
+            .explicitGrants()
+            .grant(CREATE_READ_UPDATE, CASEWORKER_DIVORCE_COURTADMIN_BETA, CASEWORKER_DIVORCE_COURTADMIN)
+            .grant(READ, CASEWORKER_DIVORCE_SOLICITOR, CASEWORKER_DIVORCE_SUPERUSER, CASEWORKER_DIVORCE_COURTADMIN_LA));
+    }
+}
