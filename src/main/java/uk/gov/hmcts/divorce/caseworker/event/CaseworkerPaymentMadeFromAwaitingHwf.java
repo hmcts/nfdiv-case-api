b54e8a6fffe2d9cf73cbf7ec1e81f9eda49d101c
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2021-06-17 12:01:39 +0100

    Make event classes as spring components and remove unused annotations (#372)

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPaymentMadeFromAwaitingHwf.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPaymentMadeFromAwaitingHwf.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.divorce.ccd.PageBuilder;
@@ -17,6 +18,7 @@ import static uk.gov.hmcts.divorce.common.model.UserRole.CASEWORKER_DIVORCE_SUPE
 import static uk.gov.hmcts.divorce.common.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.divorce.common.model.access.Permissions.READ;
 
+@Component
 public class CaseworkerPaymentMadeFromAwaitingHwf implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String CASEWORKER_PAYMENT_MADE_FROM_AWAITING_HWF = "caseworker-payment-made-from-awaiting-hwf";
