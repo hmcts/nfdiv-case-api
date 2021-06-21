b20fc2e374cb22102ccac1f3c8429e548bbff060
Nitin Prabhu <prabhunitin61@gmail.com>
2021-06-21 12:15:29 +0100

    Added awaiting payment from awaiting hwf decision event

---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAwaitingPayFromAwaitingHwfTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.divorce.common.model.CaseData;
+import uk.gov.hmcts.divorce.common.model.State;
+import uk.gov.hmcts.divorce.common.model.UserRole;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerAwaitingPayFromAwaitingHwfTest {
+
+    @InjectMocks
+    private CaseworkerAwaitingPayFromAwaitingHwf caseworkerAwaitingPayFromAwaitingHwf;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final Set<State> stateSet = Set.of(State.class.getEnumConstants());
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
+
+        caseworkerAwaitingPayFromAwaitingHwf.configure(configBuilder);
+
+        assertThat(configBuilder.getEvents())
+            .extracting(Event::getId)
+            .contains(CaseworkerAwaitingPayFromAwaitingHwf.CASEWORKER_AWAITING_PAYMENT_FROM_AWAITING_HWF);
+    }
+}
