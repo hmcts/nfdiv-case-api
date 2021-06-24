e641bfe02001a23ca440ee8c9283e86908ec1067
Linus Norton <linusnorton@gmail.com>
2021-06-24 06:33:59 +0100

    Rename caseworker refund event class (#389)

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRefundTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRefundTest.java
@@ -14,19 +14,19 @@ import java.util.EnumSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerRefundFromSubmitted.CASEWORKER_REFUND;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerRefund.CASEWORKER_REFUND;
 
 @ExtendWith(MockitoExtension.class)
-class CaseworkerRefundFromSubmittedTest {
+class CaseworkerRefundTest {
     @InjectMocks
-    private CaseworkerRefundFromSubmitted caseworkerRefundFromSubmitted;
+    private CaseworkerRefund caseworkerRefund;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
         final Set<State> stateSet = EnumSet.allOf(State.class);
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
 
-        caseworkerRefundFromSubmitted.configure(configBuilder);
+        caseworkerRefund.configure(configBuilder);
         assertThat(configBuilder.getEvents())
             .extracting(Event::getId)
             .contains(CASEWORKER_REFUND);
