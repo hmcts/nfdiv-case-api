2e69b196cfda3252500996e9ffd9fd393b3b578b
Shaun Francis <shaun.francis1@hmcts.net>
2021-06-30 14:57:12 +0100

    Fix integration test

--- src/test/java/uk/gov/hmcts/divorce/testutil/TestConstants.java
+++ src/test/java/uk/gov/hmcts/divorce/testutil/TestConstants.java
@@ -46,7 +46,7 @@ public final class TestConstants {
     public static final String WELSH_TEMPLATE_ID = "FL-DIV-GNO-WEL-00256.docx";
     public static final String BEARER = "Bearer ";
     public static final String TEST_ORG_ID = "ABC123";
-    public static final String TEST_ORG_NAME = "TEST ORG";
+    public static final String TEST_ORG_NAME = "Test Organisation";
 
     public static final String SOLICITOR_MID_EVENT_RESPONSE = "classpath:solicitor-mid-event-response.json";
     public static final String SOLICITOR_MID_EVENT_ERROR = "classpath:solicitor-mid-event-error-response.json";
