6ac7a9c655ffb2e5ad48cc78d4c6a46ba927d914
Shaun Francis <shaun.francis1@hmcts.net>
2021-07-01 09:39:41 +0100

    Nfdiv 808 send notice of proceedings (#416)
    * Add notification for applicant and applicant solicitor
    
    * Add notification for respondent solicitor
    
    * Fix integration test
    
    * Add integration and funtional test

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerIssueAos.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerIssueAos.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.caseworker.service.notification.NoticeOfProceedingsNotification;
 import uk.gov.hmcts.divorce.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.common.model.CaseData;
 import uk.gov.hmcts.divorce.common.model.State;
 import uk.gov.hmcts.divorce.common.model.UserRole;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 
 import static uk.gov.hmcts.divorce.common.model.State.AwaitingAos;
 import static uk.gov.hmcts.divorce.common.model.State.Issued;
@@ -25,6 +29,9 @@ public class CaseworkerIssueAos implements CCDConfig<CaseData, State, UserRole> 
 
     public static final String CASEWORKER_ISSUE_AOS = "caseworker-issue-aos";
 
+    @Autowired
+    private NoticeOfProceedingsNotification noticeOfProceedingsNotification;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
@@ -34,6 +41,7 @@ public class CaseworkerIssueAos implements CCDConfig<CaseData, State, UserRole> 
             .description("Issue AOS pack to respondent")
             .displayOrder(1)
             .showSummary()
+            .submittedCallback(this::submitted)
             .explicitGrants()
             .grant(CREATE_READ_UPDATE,
                 CASEWORKER_COURTADMIN_CTSC,
@@ -42,5 +50,16 @@ public class CaseworkerIssueAos implements CCDConfig<CaseData, State, UserRole> 
                 SOLICITOR,
                 CASEWORKER_SUPERUSER,
                 CASEWORKER_LEGAL_ADVISOR));
+    }
+
+    public SubmittedCallbackResponse submitted(final CaseDetails<CaseData, State> details,
+                                               final CaseDetails<CaseData, State> beforeDetails) {
+
+        log.info("Caseworker issue AOS submitted callback invoked");
+
+        final CaseData caseData = details.getData();
+        noticeOfProceedingsNotification.send(caseData, details.getId());
+
+        return SubmittedCallbackResponse.builder().build();
     }
 }
