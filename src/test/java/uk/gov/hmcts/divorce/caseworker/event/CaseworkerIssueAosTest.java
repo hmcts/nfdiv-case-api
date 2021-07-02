369fd8947e952cf1cacea1d7204cc6c8a1dda277
Shaun Francis <shaun.francis1@hmcts.net>
2021-07-02 12:10:14 +0100

    Add personal service notification for issue aos event (#418)
    * Add personal service notification for issue aos event
    
    * Add logging for personal service email sending

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerIssueAosTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerIssueAosTest.java
@@ -9,6 +9,7 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.divorce.caseworker.service.notification.NoticeOfProceedingsNotification;
+import uk.gov.hmcts.divorce.caseworker.service.notification.PersonalServiceNotification;
 import uk.gov.hmcts.divorce.common.model.CaseData;
 import uk.gov.hmcts.divorce.common.model.State;
 import uk.gov.hmcts.divorce.common.model.UserRole;
@@ -17,13 +18,18 @@ import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerIssueAos.CASEWORKER_ISSUE_AOS;
+import static uk.gov.hmcts.divorce.common.model.SolServiceMethod.PERSONAL_SERVICE;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CaseworkerIssueAosTest {
+
+    @Mock
+    private PersonalServiceNotification personalServiceNotification;
 
     @Mock
     private NoticeOfProceedingsNotification noticeOfProceedingsNotification;
@@ -46,7 +52,7 @@ class CaseworkerIssueAosTest {
     @Test
     void shouldSendNoticeOfProceedings() {
 
-        final var caseData = mock(CaseData.class);
+        final var caseData = caseData();
         final var caseDetails = CaseDetails.<CaseData, State>builder()
             .id(TEST_CASE_ID)
             .state(State.AwaitingAos)
@@ -57,5 +63,25 @@ class CaseworkerIssueAosTest {
 
         assertThat(submitted).isNotNull();
         verify(noticeOfProceedingsNotification).send(caseData, TEST_CASE_ID);
+        verifyNoInteractions(personalServiceNotification);
+    }
+
+    @Test
+    void shouldSendPersonalServiceNotification() {
+
+        final var caseData = caseData();
+        caseData.getApplication().setSolServiceMethod(PERSONAL_SERVICE);
+
+        final var caseDetails = CaseDetails.<CaseData, State>builder()
+            .id(TEST_CASE_ID)
+            .state(State.AwaitingAos)
+            .data(caseData)
+            .build();
+
+        final SubmittedCallbackResponse submitted = caseworkerIssueAos.submitted(caseDetails, null);
+
+        assertThat(submitted).isNotNull();
+        verify(personalServiceNotification).send(caseData, TEST_CASE_ID);
+        verifyNoInteractions(noticeOfProceedingsNotification);
     }
 }
