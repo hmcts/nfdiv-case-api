4dd8583e1e8bf8ba94be952cb9406b9c8f53c879
Nitin Prabhu <prabhunitin61@gmail.com>
2021-07-06 15:36:43 +0100

    Refactor scripts so that case types need not be passed from jenkins pipeline

--- bin/ccd-build-definition.sh
+++ bin/ccd-build-definition.sh
@@ -7,10 +7,15 @@ caseType=${1}
 echo "Script Path ${scriptPath}"
 
 root_dir=$(realpath $(dirname ${0})/..)
-config_dir=${root_dir}/build/definitions/${caseType}
 build_dir=${root_dir}/build/ccd-config
-definitionOutputFile=${build_dir}/ccd-${caseType}-${CCD_DEF_NAME:-dev}.xlsx
 
 mkdir -p ${build_dir}
 
-${scriptPath}/generate-ccd-definition.sh $config_dir $definitionOutputFile "-e *-nonprod.json,*-testing.json"
+for dir in $(find ${root_dir}/build/definitions/ -maxdepth 1 -mindepth  1 -type d -exec basename {} \;)
+do
+  caseType=$(echo $dir | tr 'A-Z' 'a-z')
+  config_dir=${root_dir}/build/definitions/${caseType}
+  definitionOutputFile=${build_dir}/ccd-${caseType}-${CCD_DEF_NAME:-dev}.xlsx
+
+  ${scriptPath}/generate-ccd-definition.sh $config_dir $definitionOutputFile "-e *-nonprod.json,*-testing.json"
+done
--- bin/process-and-import-ccd-definition.sh
+++ bin/process-and-import-ccd-definition.sh
@@ -7,10 +7,11 @@ scriptPath=$(dirname $(realpath $0))
 root_dir=$(realpath $(dirname ${0})/..)
 build_dir=${root_dir}/build/ccd-config
 
+ ${scriptPath}/ccd-build-definition.sh
+
 for dir in $(find ${root_dir}/build/definitions/ -maxdepth 1 -mindepth  1 -type d -exec basename {} \;)
 do
   caseType=$(echo $dir | tr 'A-Z' 'a-z')
   definitionOutputFile=${build_dir}/ccd-$caseType-${CCD_DEF_NAME:-dev}.xlsx
-  ${scriptPath}/ccd-build-definition.sh $caseType
   ${scriptPath}/ccd-import-definition.sh $definitionOutputFile
 done
