cde537d96c29784b4f12518cd8c49218affb9aed
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2021-07-08 17:23:08 +0100

    Add schedule task for awaiting conditional order (#445)

---
+++ src/integrationTest/java/uk/gov/hmcts/divorce/caseworker/schedule/CaseworkerAwaitingConditionalOrderTaskTest.java
@@ -0,0 +1,29 @@
+package uk.gov.hmcts.divorce.caseworker.schedule;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CaseworkerAwaitingConditionalOrderTaskTest {
+    @SpyBean
+    private CaseworkerAwaitingConditionalOrderTask awaitingConditionalOrderTask;
+
+    @Test
+    public void givenValidCaseDataWhenCallbackIsInvokedThenOrderSummaryAndSolicitorRolesAreSet() {
+        await(" Awaiting Conditional order task executed atleast once")
+            .atMost(60, TimeUnit.SECONDS)
+            .pollInterval(2, TimeUnit.SECONDS)
+            .until(() -> verify(awaitingConditionalOrderTask, atLeastOnce()).execute());
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/schedule/CaseworkerAwaitingConditionalOrderTaskTest.java
@@ -0,0 +1,133 @@
+package uk.gov.hmcts.divorce.caseworker.schedule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.divorce.caseworker.service.CcdConflictException;
+import uk.gov.hmcts.divorce.caseworker.service.CcdManagementException;
+import uk.gov.hmcts.divorce.caseworker.service.CcdSearchCaseException;
+import uk.gov.hmcts.divorce.caseworker.service.CcdSearchService;
+import uk.gov.hmcts.divorce.caseworker.service.CcdUpdateService;
+import uk.gov.hmcts.divorce.common.model.CaseData;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerAwaitingConditionalOrder.CASEWORKER_AWAITING_CONDITIONAL_ORDER;
+import static uk.gov.hmcts.divorce.common.model.State.Holding;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerAwaitingConditionalOrderTaskTest {
+
+    @Mock
+    private CcdUpdateService ccdUpdateService;
+
+    @Mock
+    private CcdSearchService ccdSearchService;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private CaseworkerAwaitingConditionalOrderTask awaitingConditionalOrderTask;
+
+    @Test
+    void shouldTriggerAwaitingConditionalOrderOnEachCaseWhenCaseIsInHoldingForMoreThan20Weeks() {
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
+
+        CaseData caseData1 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 1)).build();
+        when(objectMapper.convertValue(caseDetails1.getData(), CaseData.class)).thenReturn(caseData1);
+
+        CaseData caseData2 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 2)).build();
+        when(objectMapper.convertValue(caseDetails2.getData(), CaseData.class)).thenReturn(caseData2);
+
+        when(ccdSearchService.searchForAllCasesWithStateOf(Holding)).thenReturn(caseDetailsList);
+
+        awaitingConditionalOrderTask.execute();
+
+        verify(ccdUpdateService).submitEvent(caseDetails1, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+        verify(ccdUpdateService).submitEvent(caseDetails2, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+    }
+
+    @Test
+    void shouldNotTriggerAwaitingConditionalOrderWhenCaseIsInHoldingForLessThan20Weeks() {
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+
+        CaseData caseData1 = CaseData.builder().issueDate(LocalDate.now()).build();
+        when(objectMapper.convertValue(caseDetails1.getData(), CaseData.class)).thenReturn(caseData1);
+
+        when(ccdSearchService.searchForAllCasesWithStateOf(Holding)).thenReturn(singletonList(caseDetails1));
+
+        awaitingConditionalOrderTask.execute();
+
+        verifyNoInteractions(ccdUpdateService);
+    }
+
+    @Test
+    void shouldNotSubmitEventIfSearchFails() {
+        when(ccdSearchService.searchForAllCasesWithStateOf(Holding))
+            .thenThrow(new CcdSearchCaseException("Failed to search cases", mock(FeignException.class)));
+
+        awaitingConditionalOrderTask.execute();
+
+        verifyNoInteractions(ccdUpdateService);
+    }
+
+    @Test
+    void shouldStopProcessingIfThereIsConflictDuringSubmission() {
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
+
+        CaseData caseData1 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 1)).build();
+        when(objectMapper.convertValue(caseDetails1.getData(), CaseData.class)).thenReturn(caseData1);
+
+        CaseData caseData2 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 2)).build();
+        when(objectMapper.convertValue(caseDetails2.getData(), CaseData.class)).thenReturn(caseData2);
+
+        when(ccdSearchService.searchForAllCasesWithStateOf(Holding)).thenReturn(caseDetailsList);
+
+        doThrow(new CcdConflictException("Case is modified by another transaction", mock(FeignException.class)))
+            .when(ccdUpdateService).submitEvent(caseDetails1, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+
+        awaitingConditionalOrderTask.execute();
+
+        verify(ccdUpdateService).submitEvent(caseDetails1, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+    }
+
+    @Test
+    void shouldContinueToNextCaseIfExceptionIsThrownWhileProcessingPreviousCase() {
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
+
+        CaseData caseData1 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 1)).build();
+        when(objectMapper.convertValue(caseDetails1.getData(), CaseData.class)).thenReturn(caseData1);
+
+        CaseData caseData2 = CaseData.builder().issueDate(LocalDate.of(2021, 1, 2)).build();
+        when(objectMapper.convertValue(caseDetails2.getData(), CaseData.class)).thenReturn(caseData2);
+
+        when(ccdSearchService.searchForAllCasesWithStateOf(Holding)).thenReturn(caseDetailsList);
+
+        doThrow(new CcdManagementException("Failed processing of case", mock(FeignException.class)))
+            .when(ccdUpdateService).submitEvent(caseDetails1, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+
+        awaitingConditionalOrderTask.execute();
+
+        verify(ccdUpdateService).submitEvent(caseDetails1, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+        verify(ccdUpdateService).submitEvent(caseDetails2, CASEWORKER_AWAITING_CONDITIONAL_ORDER);
+    }
+}
