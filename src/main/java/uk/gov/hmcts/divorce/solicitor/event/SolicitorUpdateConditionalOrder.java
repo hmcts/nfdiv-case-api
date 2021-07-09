4355223d337b20e1fd31ecf1d9dbebb9c3f36c91
Linus Norton <linusnorton@gmail.com>
2021-07-09 07:42:37 +0100

    NFDIV-957 add skeleton for conditional order events

---
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorUpdateConditionalOrder.java
@@ -0,0 +1,52 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderDrafted;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_CTSC;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_RDU;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_SUPERUSER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
+
+@Component
+public class SolicitorUpdateConditionalOrder implements CCDConfig<CaseData, State, UserRole> {
+    public static final String SOLICITOR_UPDATE_CONDITIONAL_ORDER = "solicitor-update-conditional-order";
+
+    private final List<CcdPageConfiguration> pages = emptyList();
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        final PageBuilder pageBuilder = addEventConfig(configBuilder);
+        pages.forEach(page -> page.addTo(pageBuilder));
+    }
+
+    private PageBuilder addEventConfig(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        return new PageBuilder(configBuilder
+            .event(SOLICITOR_UPDATE_CONDITIONAL_ORDER)
+            .forStateTransition(ConditionalOrderDrafted, ConditionalOrderDrafted)
+            .name("Update Conditional Order")
+            .description("Update Conditional Order")
+            .endButtonLabel("Save Conditional Order")
+            .explicitGrants()
+            .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR)
+            .grant(READ,
+                CASEWORKER_COURTADMIN_CTSC,
+                CASEWORKER_COURTADMIN_RDU,
+                CASEWORKER_SUPERUSER,
+                CASEWORKER_LEGAL_ADVISOR));
+    }
+
+}
