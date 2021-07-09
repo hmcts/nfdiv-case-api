4ac9aa20734132d107e19021284a206e420d5ab3
Nitin Prabhu <prabhunitin61@gmail.com>
2021-07-09 12:08:30 +0100

    Added event to update application type for caseworkers

---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUpdateApplicationTypeTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerUpdateApplicationType.CASEWORKER_UPDATE_APPLICATION_TYPE;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerUpdateApplicationTypeTest {
+    @InjectMocks
+    private CaseworkerUpdateApplicationType updateApplicationType;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final Set<State> stateSet = EnumSet.allOf(State.class);
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
+
+        updateApplicationType.configure(configBuilder);
+        assertThat(configBuilder.getEvents())
+            .extracting(Event::getId)
+            .contains(CASEWORKER_UPDATE_APPLICATION_TYPE);
+    }
+}
