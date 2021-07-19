afbb23e6cd18aee7d1f17a7d3e174ce42acbe5ed
Lara <laratollington@gmail.com>
2021-07-19 17:40:06 +0100

    Update unit test

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/Applicant2ApproveTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/Applicant2ApproveTest.java
@@ -1,19 +1,19 @@
 package uk.gov.hmcts.divorce.citizen.event;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
-import java.util.Set;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.hmcts.divorce.citizen.event.Applicant2Approve.APPLICANT_2_APPROVE;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
 class Applicant2ApproveTest {
@@ -23,12 +23,12 @@ class Applicant2ApproveTest {
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
-
-        final Set<State> stateSet = Set.of(State.class.getEnumConstants());
-        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = new ConfigBuilderImpl<>(CaseData.class, stateSet);
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         applicant2Approve.configure(configBuilder);
 
-        assertThat(configBuilder.getEvents().get(0).getId(), is(APPLICANT_2_APPROVE));
+        Assertions.assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(APPLICANT_2_APPROVE);
     }
 }
