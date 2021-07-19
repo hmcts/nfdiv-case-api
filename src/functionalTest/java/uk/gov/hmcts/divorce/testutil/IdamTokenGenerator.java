1f1474ede74b9cd7c53751e2add73b2e8c451408
Nitin Prabhu <prabhunitin61@gmail.com>
2021-07-19 15:09:58 +0100

    Added functional test for general order midevent and about to submit

--- src/functionalTest/java/uk/gov/hmcts/divorce/testutil/IdamTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/testutil/IdamTokenGenerator.java
@@ -21,7 +21,7 @@ public class IdamTokenGenerator {
     private IdamClient idamClient;
 
     public String generateIdamTokenForSolicitor() {
-        return idamClient.getAccessToken(solicitorUsername, solicitorPassword);
+        return idamClient.getAccessToken("TEST_SOLICITOR@mailinator.com", "genericPassword123");
     }
 
     public UserDetails getUserDetailsFor(final String token) {
