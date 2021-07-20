fa97e049a2bcb25640c24453db9c82b6be1bf1c5
Linus Norton <linusnorton@gmail.com>
2021-07-20 07:24:49 +0100

    update due date event

---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUpdateDueDateTest.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerUpdateDueDate.CASEWORKER_UPDATE_DUE_DATE;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerUpdateDueDateTest {
+
+    @InjectMocks
+    private CaseworkerAddNote caseworkerUpdateDueDate;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() throws Exception {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        caseworkerUpdateDueDate.configure(configBuilder);
+
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(CASEWORKER_UPDATE_DUE_DATE);
+    }
+
+}
