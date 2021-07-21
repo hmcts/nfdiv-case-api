d9aa7e5723f5c63f17579cbcf8ccfadf9b968a3e
Linus Norton <linusnorton@gmail.com>
2021-07-21 08:08:02 +0100

    NFDIV-890 Add update language events

---
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorUpdateApplicant2Language.java
@@ -0,0 +1,49 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2_SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_CTSC;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_RDU;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_SUPERUSER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ_UPDATE;
+import static uk.gov.hmcts.divorce.solicitor.event.page.CommonFieldSettings.SOLICITOR_NFD_PREVIEW_BANNER;
+
+@Component
+public class SolicitorUpdateApplicant2Language implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String SOLICITOR_UPDATE_APPLICANT_2_LANGUAGE = "solicitor-update-applicant2-language";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        new PageBuilder(configBuilder
+            .event(SOLICITOR_UPDATE_APPLICANT_2_LANGUAGE)
+            .forAllStates()
+            .name("Update Language")
+            .description("Update Language")
+            .showSummary()
+            .explicitGrants()
+            .grant(CREATE_READ_UPDATE, APPLICANT_2_SOLICITOR)
+            .grant(READ_UPDATE, CASEWORKER_SUPERUSER)
+            .grant(READ,
+                CASEWORKER_COURTADMIN_CTSC,
+                CASEWORKER_COURTADMIN_RDU,
+                CASEWORKER_LEGAL_ADVISOR))
+            .page("langPref2")
+            .pageLabel("Select Language")
+            .label("LabelNFDBanner-UpdateApplicant2Language", SOLICITOR_NFD_PREVIEW_BANNER)
+            .complex(CaseData::getApplicant2)
+                .mandatory(Applicant::getLanguagePreferenceWelsh)
+                .done();
+    }
+}
