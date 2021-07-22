0e73f93b3c33c2e32afe5a229032e97c5a7f0606
Nitin Prabhu <prabhunitin61@gmail.com>
2021-07-22 13:05:00 +0100

    Fixed pmd and added config test

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUploadConfidentialDocument.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUploadConfidentialDocument.java
@@ -1,11 +1,9 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.divorce.caseworker.service.notification.GeneralEmailNotification;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
@@ -23,9 +21,6 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
 public class CaseworkerUploadConfidentialDocument implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_UPLOAD_CONFIDENTIAL_DOCUMENT = "caseworker-upload-confidential-document";
 
-    @Autowired
-    private GeneralEmailNotification generalEmailNotification;
-
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
@@ -40,6 +35,5 @@ public class CaseworkerUploadConfidentialDocument implements CCDConfig<CaseData,
             .page("uploadConfidentialDocuments")
             .pageLabel("Upload Confidential Documents")
             .optional(CaseData::getConfidentialDocumentsUploaded);
-        ;
     }
 }
