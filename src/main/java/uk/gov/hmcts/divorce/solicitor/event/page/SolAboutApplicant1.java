ba8c12338024ac03134b32d7893959e41569720e
Nitin Prabhu <prabhunitin61@gmail.com>
2021-08-04 16:03:17 +0100

    Reorder applicant 1 details page questions

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant1.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant1.java
@@ -19,44 +19,40 @@ public class SolAboutApplicant1 implements CcdPageConfiguration {
             .page("SolAboutApplicant1")
             .pageLabel("About the applicant")
             .complex(CaseData::getApplicant1)
-                .mandatoryWithLabel(Applicant::getFirstName,
-                    "The applicant's first name")
-                .optionalWithLabel(Applicant::getMiddleName,
-                    "The applicant's middle name")
-                .mandatoryWithLabel(Applicant::getLastName,
-                    "The applicant's last name")
-                .mandatoryWithLabel(Applicant::getNameDifferentToMarriageCertificate,
-                    "Is the applicant's name different to that on their marriage certificate?")
-                .mandatoryWithoutDefaultValue(Applicant::getNameChangedHow,
-                    "applicant1NameDifferentToMarriageCertificate=\"Yes\"",
-                    "How did they change their name?")
-                .mandatoryWithoutDefaultValue(Applicant::getNameChangedHowOtherDetails,
-                    "applicant1NameChangedHow=\"other\"",
-                    "If not through marriage or deed poll, please provide details of how they legally changed they name")
-                .done()
+            .mandatoryWithLabel(Applicant::getFirstName,
+                "The applicant's first name")
+            .optionalWithLabel(Applicant::getMiddleName,
+                "The applicant's middle name")
+            .mandatoryWithLabel(Applicant::getLastName,
+                "The applicant's last name")
+            .mandatoryWithLabel(Applicant::getNameDifferentToMarriageCertificate,
+                "Is the applicant's name different to that on their marriage certificate?")
+            .mandatoryWithoutDefaultValue(Applicant::getNameChangedHow,
+                "applicant1NameDifferentToMarriageCertificate=\"Yes\"",
+                "How did they change their name?")
+            .mandatoryWithoutDefaultValue(Applicant::getNameChangedHowOtherDetails,
+                "applicant1NameChangedHow=\"other\"",
+                "If not through marriage or deed poll, please provide details of how they legally changed they name")
+            .mandatoryWithLabel(Applicant::getGender,
+                "What is the applicant's gender?")
+            .done()
             .complex(CaseData::getApplication)
-                .mandatoryWithLabel(Application::getDivorceWho,
-                    "Their husband or wife")
-                .done()
+            .mandatoryWithLabel(Application::getDivorceWho,
+                "Who are you applying to divorce? (husband/wife)")
+            .complex(Application::getMarriageDetails)
+            .mandatory(MarriageDetails::getIsSameSexCouple)
+            .done()
+            .done()
             .complex(CaseData::getApplicant1)
-                .mandatoryWithLabel(Applicant::getGender,
-                    "What is the applicant's gender?")
-                .done()
-            .complex(CaseData::getApplication)
-                .complex(Application::getMarriageDetails)
-                    .mandatory(MarriageDetails::getIsSameSexCouple)
-                    .done()
-                .done()
-            .complex(CaseData::getApplicant1)
-                .mandatoryWithLabel(Applicant::getEmail,
-                    "The applicant's email address")
-                .optionalWithLabel(Applicant::getPhoneNumber,
-                    "The applicant's phone number")
-                .optionalWithLabel(Applicant::getHomeAddress,
-                    "The applicant's home address")
-                .label("LabelHorizontalLine1-SolAboutApplicant1", DARK_HORIZONTAL_RULE)
-                .mandatoryWithLabel(Applicant::getContactDetailsConfidential,
-                    "Keep the applicant's contact details private from ${labelContentTheApplicant2}?")
-                .done();
+            .mandatoryWithLabel(Applicant::getEmail,
+                "The applicant's email address")
+            .optionalWithLabel(Applicant::getPhoneNumber,
+                "The applicant's phone number")
+            .optionalWithLabel(Applicant::getHomeAddress,
+                "The applicant's home address")
+            .label("LabelHorizontalLine1-SolAboutApplicant1", DARK_HORIZONTAL_RULE)
+            .mandatoryWithLabel(Applicant::getContactDetailsConfidential,
+                "Keep the applicant's contact details private from ${labelContentTheApplicant2}?")
+            .done();
     }
 }
