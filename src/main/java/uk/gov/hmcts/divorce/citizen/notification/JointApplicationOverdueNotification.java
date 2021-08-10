976d9a8e31d93f0c1161f2a93c4e3c0f57bb340d
Tom Elliott <tomelliott@Toms-MBP.home>
2021-08-10 11:37:25 +0100

    NFDIV-978: Implement cron job to remind applicant 1 to complete their application

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/JointApplicationOverdueNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/JointApplicationOverdueNotification.java
@@ -10,9 +10,13 @@ import uk.gov.hmcts.divorce.notification.NotificationService;
 import java.util.Locale;
 import java.util.Map;
 
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICATION_APPROVED_APPLICANT1_REMINDER;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICATION_OVERDUE;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.APPLICATION;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.FOR_DIVORCE;
+import static uk.gov.hmcts.divorce.notification.NotificationConstants.PAID_FOR;
+import static uk.gov.hmcts.divorce.notification.NotificationConstants.PAY_FOR;
+import static uk.gov.hmcts.divorce.notification.NotificationConstants.REMINDER_ACTION_REQUIRED;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.REVIEW_DEADLINE_DATE;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.TO_END_CIVIL_PARTNERSHIP;
 
@@ -26,7 +30,7 @@ public class JointApplicationOverdueNotification {
     @Autowired
     private CommonContent commonContent;
 
-    public void send(CaseData caseData, Long id) {
+    public void sendApplicationNotReviewedEmail(CaseData caseData, Long id) {
         Map<String, String> templateVars = commonContent.templateVarsForApplicant(
             caseData, caseData.getApplicant1(), caseData.getApplicant2());
 
@@ -43,6 +47,36 @@ public class JointApplicationOverdueNotification {
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             JOINT_APPLICATION_OVERDUE,
+            templateVars,
+            caseData.getApplicant1().getLanguagePreference()
+        );
+    }
+
+    public void sendApplicationApprovedReminderToApplicant1(CaseData caseData, Long id) {
+        Map<String, String> templateVars = commonContent.templateVarsForApplicant(
+            caseData, caseData.getApplicant1(), caseData.getApplicant2());
+
+        if (caseData.getDivorceOrDissolution().isDivorce()) {
+            templateVars.put(APPLICATION.toLowerCase(Locale.ROOT), FOR_DIVORCE);
+        } else {
+            templateVars.put(APPLICATION.toLowerCase(Locale.ROOT), TO_END_CIVIL_PARTNERSHIP);
+        }
+
+        templateVars.put(REMINDER_ACTION_REQUIRED, REMINDER_ACTION_REQUIRED);
+
+        if (caseData.getApplication().isHelpWithFeesApplication()) {
+            templateVars.put(PAY_FOR, "");
+            templateVars.put(PAID_FOR, "");
+        } else {
+            templateVars.put(PAY_FOR, PAY_FOR);
+            templateVars.put(PAID_FOR, PAID_FOR);
+        }
+
+        log.info("Sending notification to applicant 1 to them that applicant 2 has reviewed the application: {}", id);
+
+        notificationService.sendEmail(
+            caseData.getApplicant1().getEmail(),
+            JOINT_APPLICATION_APPROVED_APPLICANT1_REMINDER,
             templateVars,
             caseData.getApplicant1().getLanguagePreference()
         );
