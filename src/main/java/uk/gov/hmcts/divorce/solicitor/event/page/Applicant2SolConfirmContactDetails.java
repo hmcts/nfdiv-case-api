0b91117fedf98bcfa01a841ba3f489120793751e
Shaun Francis <shaun.francis1@hmcts.net>
2021-08-18 15:05:37 +0100

    Fix blank dynamic labels in draft oas (#615)

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2SolConfirmContactDetails.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2SolConfirmContactDetails.java
@@ -4,24 +4,34 @@ import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.LabelContent;
 import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
 
 
 public class Applicant2SolConfirmContactDetails implements CcdPageConfiguration {
+
+    private static final String ALWAYS_HIDE = "applicant2SolicitorName=\"ALWAYS_HIDE\"";
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
             .page("Applicant2SolConfirmContactDetails")
             .pageLabel("Confirm contact details")
+            .complex(CaseData::getLabelContent)
+                .readonlyNoSummary(LabelContent::getApplicant2, ALWAYS_HIDE)
+                .readonlyNoSummary(LabelContent::getApplicant2UC, ALWAYS_HIDE)
+                .readonlyNoSummary(LabelContent::getTheApplicant2, ALWAYS_HIDE)
+                .readonlyNoSummary(LabelContent::getTheApplicant2UC, ALWAYS_HIDE)
+                .done()
             .complex(CaseData::getApplicant2)
-            .complex(Applicant::getSolicitor)
-            .mandatoryNoSummary(Solicitor::getName, null, "${labelContentApplicant2UC}'s solicitor's name")
-            .optionalNoSummary(Solicitor::getPhone, null, "${labelContentApplicant2UC}'s solicitor's Phone number")
-            .label("LabelRespSol-EmailHeader", "### Email contact details")
-            .label("LabelRespSol-Email", "Email address will be used to send case updates.")
-            .mandatoryNoSummary(Solicitor::getEmail, null, "${labelContentApplicant2UC}'s solicitor's Email")
-            .mandatoryNoSummary(Solicitor::getAgreeToReceiveEmails)
-            .done();
+                .complex(Applicant::getSolicitor)
+                    .mandatoryNoSummary(Solicitor::getName, null, "${labelContentApplicant2UC}'s solicitor's name")
+                    .optionalNoSummary(Solicitor::getPhone, null, "${labelContentApplicant2UC}'s solicitor's Phone number")
+                    .label("LabelRespSol-EmailHeader", "### Email contact details")
+                    .label("LabelRespSol-Email", "Email address will be used to send case updates.")
+                    .mandatoryNoSummary(Solicitor::getEmail, null, "${labelContentApplicant2UC}'s solicitor's Email")
+                    .mandatoryNoSummary(Solicitor::getAgreeToReceiveEmails)
+                    .done()
+                .done();
     }
 }
--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2SolUpdateAosApplicant1Application.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2SolUpdateAosApplicant1Application.java
@@ -5,23 +5,29 @@ import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.AcknowledgementOfService;
 import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.LabelContent;
 
 public class Applicant2SolUpdateAosApplicant1Application implements CcdPageConfiguration {
+
+    private static final String ALWAYS_HIDE = "miniApplicationLink=\"ALWAYS_HIDE\"";
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
         pageBuilder
             .page("Applicant2SolUpdateAosApplicant1Application")
             .pageLabel("Review the applicant 1's application")
+            .complex(CaseData::getLabelContent)
+                .readonlyNoSummary(LabelContent::getTheApplicant2, ALWAYS_HIDE)
+                .done()
             .complex(CaseData::getApplication)
                 .readonly(Application::getMiniApplicationLink)
                 .done()
             .complex(CaseData::getAcknowledgementOfService)
-            .mandatory(AcknowledgementOfService::getConfirmReadPetition)
-            .label("LabelRespSol-AOSConfirmRead",
-                "### ${labelContentTheApplicant2} has not read the petition\n\n"
-                    + "${labelContentTheApplicant2} must have read the petition in order to respond.",
-              "confirmReadPetition=\"No\"")
-            .done();
+                .mandatory(AcknowledgementOfService::getConfirmReadPetition)
+                .label("LabelRespSol-AOSConfirmRead",
+                    "### ${labelContentTheApplicant2} has not read the petition\n\n"
+                        + "${labelContentTheApplicant2} must have read the petition in order to respond.",
+                    "confirmReadPetition=\"No\"")
+                .done();
     }
 }
