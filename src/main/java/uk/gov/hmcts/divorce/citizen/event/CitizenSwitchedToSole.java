a1a45a39602588d94c4a78c5e4bec84c7e41d62b
Lara <52360278+lara-mpt@users.noreply.github.com>
2021-08-18 12:27:35 +0100

    Remove SwitchedToSole state (#613)

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSole.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSole.java
@@ -11,8 +11,11 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
+import java.util.EnumSet;
+
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant1Response;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.SwitchedToSole;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant2Response;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Draft;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
@@ -20,14 +23,18 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 @Component
 public class CitizenSwitchedToSole implements CCDConfig<CaseData, State, UserRole> {
 
-    public static final String SWITCHED_TO_SOLE = "switched-to-sole";
+    public static final String CITIZEN_SWITCH_TO_SOLE = "citizen-switch-to-sole";
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
 
+        EnumSet<State> stateSet = EnumSet.noneOf(State.class);
+        stateSet.add(AwaitingApplicant1Response);
+        stateSet.add(AwaitingApplicant2Response);
+
         configBuilder
-            .event(SWITCHED_TO_SOLE)
-            .forStateTransition(AwaitingApplicant1Response, SwitchedToSole)
+            .event(CITIZEN_SWITCH_TO_SOLE)
+            .forStateTransition(stateSet, Draft)
             .name("Applicant 1 switched to sole")
             .description("Application type switched to sole")
             .grant(CREATE_READ_UPDATE, CITIZEN)
