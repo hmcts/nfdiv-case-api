ca845fd7de9e4b2b138602105d42a13bd66afd7e
Linus Norton <linusnorton@gmail.com>
2021-08-19 16:34:53 +0100

    Added test

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/service/ScheduledTaskRunner.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/service/ScheduledTaskRunner.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.divorce.common.service;
+package uk.gov.hmcts.divorce.systemupdate.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
