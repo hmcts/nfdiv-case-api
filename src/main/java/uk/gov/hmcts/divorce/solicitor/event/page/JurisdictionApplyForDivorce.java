25c9ada439b0f765ab6fdf1420592342700f017a
Mark Butler <mark.butler@solirius.com>
2021-08-24 15:32:14 +0100

    Remove the line regarding Opposite Sex couples from the Jurisdiction Summary page

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/JurisdictionApplyForDivorce.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/JurisdictionApplyForDivorce.java
@@ -16,9 +16,7 @@ public class JurisdictionApplyForDivorce implements CcdPageConfiguration {
             .pageLabel("Jurisdiction - Apply for a divorce")
             .label(
                 "LabelSolJurisdictionPara-1",
-                "The court has legal power to deal with this application because the following applies:\n\n"
-                    + "Divorce – *Opposite Sex Couple* – Article 3(1) of Council Regulation (EC) No 2201/2003"
-                    + "of 27 November 2003")
+                "The court has legal power to deal with this application because the following applies:\n\n")
             .complex(CaseData::getApplication)
                 .complex(Application::getJurisdiction)
                     .mandatory(Jurisdiction::getLegalConnections)
