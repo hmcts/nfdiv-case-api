cd68318eb9917f34a6d16cba6f3df413278a827b
Shaun Francis <shaun.francis1@hmcts.net>
2021-09-07 16:37:37 +0100

    Add awaiting service state (#676)
    * Add awaiting service state
    
    * Update test names

---
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/SetPostIssueState.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.divorce.caseworker.service.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingAos;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingService;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
+
+@Component
+@Slf4j
+public class SetPostIssueState implements CaseTask {
+
+    @Override
+    public CaseDetails<CaseData, State> apply(final CaseDetails<CaseData, State> caseDetails) {
+
+        final Application application = caseDetails.getData().getApplication();
+
+        if (application.isSolicitorApplication()) {
+            if (application.isSolicitorServiceMethod()) {
+                caseDetails.setState(AwaitingService);
+            } else {
+                caseDetails.setState(AwaitingAos);
+            }
+        } else {
+            caseDetails.setState(Holding);
+        }
+
+        log.info("Setting state to {}.  Case ID: {}", caseDetails.getState(), caseDetails.getId());
+        return caseDetails;
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/service/task/SetPostIssueStateTest.java
@@ -0,0 +1,76 @@
+package uk.gov.hmcts.divorce.caseworker.service.task;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.COURT_SERVICE;
+import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.SOLICITOR_SERVICE;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingAos;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingService;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
+
+@ExtendWith(MockitoExtension.class)
+class SetPostIssueStateTest {
+
+    @InjectMocks
+    private SetPostIssueState setPostIssueState;
+
+    @Test
+    void shouldSetStateToAwaitingServiceWhenApplicationIsSolicitorApplicationAndServiceMethodIsSolicitorService() {
+
+        final CaseData caseData = CaseData.builder()
+            .application(Application.builder()
+                .solSignStatementOfTruth(YES)
+                .solServiceMethod(SOLICITOR_SERVICE)
+                .build())
+            .build();
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+
+        final CaseDetails<CaseData, State> result = setPostIssueState.apply(caseDetails);
+
+        assertThat(result.getState()).isEqualTo(AwaitingService);
+    }
+
+    @Test
+    void shouldSetStateToAwaitingAosWhenApplicationIsSolicitorApplicationAndServiceMethodIsCourtService() {
+
+        final CaseData caseData = CaseData.builder()
+            .application(Application.builder()
+                .solSignStatementOfTruth(YES)
+                .solServiceMethod(COURT_SERVICE)
+                .build())
+            .build();
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+
+        final CaseDetails<CaseData, State> result = setPostIssueState.apply(caseDetails);
+
+        assertThat(result.getState()).isEqualTo(AwaitingAos);
+    }
+
+    @Test
+    void shouldSetStateToHoldingWhenApplicationIsNotSolicitorApplication() {
+
+        final CaseData caseData = CaseData.builder()
+            .application(Application.builder().build())
+            .build();
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+
+        final CaseDetails<CaseData, State> result = setPostIssueState.apply(caseDetails);
+
+        assertThat(result.getState()).isEqualTo(Holding);
+    }
+}
