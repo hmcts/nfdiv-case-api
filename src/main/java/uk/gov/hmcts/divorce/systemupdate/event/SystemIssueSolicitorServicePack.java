c214ae8fd836ae965a5d1d47c9668e4026b9ce07
Shaun Francis <shaun.francis1@hmcts.net>
2021-09-13 08:17:32 +0100

    Fix permissions after master merge

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemIssueSolicitorServicePack.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemIssueSolicitorServicePack.java
@@ -10,12 +10,11 @@ import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingService;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_CTSC;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_COURTADMIN_RDU;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_LEGAL_ADVISOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_SUPERUSER;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASEWORKER_SYSTEMUPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
 
@@ -34,8 +33,8 @@ public class SystemIssueSolicitorServicePack implements CCDConfig<CaseData, Stat
             .name("Issue Solicitor Service Pack")
             .description("Issue Solicitor Service Pack to applicant's solicitor (if service method = Solicitor Service)")
             .explicitGrants()
-            .grant(CREATE_READ_UPDATE, CASEWORKER_SYSTEMUPDATE)
-            .grant(READ, SOLICITOR, CASEWORKER_COURTADMIN_CTSC, CASEWORKER_COURTADMIN_RDU, CASEWORKER_SUPERUSER, CASEWORKER_LEGAL_ADVISOR)
+            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
+            .grant(READ, SOLICITOR, CASE_WORKER, SUPER_USER, LEGAL_ADVISOR)
             .retries(120, 120));
     }
 }
