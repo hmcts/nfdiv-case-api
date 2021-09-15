3d9f661b4d6cde317c812e5cd6cb108ba3a5d5b6
Lara <laratollington@gmail.com>
2021-09-15 12:41:56 +0100

    NFDIV-1001 citizen confirm receipt events

---
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenApplicant2ConfirmReceipt.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.divorce.citizen.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Slf4j
+@Component
+public class CitizenApplicant2ConfirmReceipt implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String APPLICANT_2_CONFIRM_RECEIPT = "applicant2-confirm-receipt";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+
+        configBuilder
+            .event(APPLICANT_2_CONFIRM_RECEIPT)
+            .forStates(Holding)
+            .name("Applicant 2 Confirm Receipt")
+            .description("Applicant 2 confirms receipt for joint application")
+            .grant(CREATE_READ_UPDATE, APPLICANT_2)
+            .retries(120, 120);
+    }
+}
+
