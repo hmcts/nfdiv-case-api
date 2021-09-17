be48b29f03aa00aae73c082132e635297bed7404
Linus Norton <linusnorton@gmail.com>
2021-09-17 16:30:07 +0100

    unused imports

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFields.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFields.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
 import java.util.Map;
 import java.util.function.Consumer;
