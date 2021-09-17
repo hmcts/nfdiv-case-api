fca9c31051fc3224ea1552e651208292202e8f11
Linus Norton <linusnorton@gmail.com>
2021-09-17 14:58:20 +0100

    Add label

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFields.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFields.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
 import java.util.Map;
 import java.util.function.Consumer;
@@ -13,7 +12,7 @@ import java.util.function.Consumer;
 @NoArgsConstructor
 public class RetiredFields {
 
-    @CCD(access = {DefaultAccess.class})
+    @CCD(label = "retired")
     private String exampleRetiredField;
 
     @JsonIgnore
