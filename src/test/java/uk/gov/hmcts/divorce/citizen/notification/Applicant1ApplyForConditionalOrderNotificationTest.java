5f83761074dd9c9c228e6cb67fe209341399084d
Tom Elliott <tomelliott@Toms-MBP.home>
2021-09-29 10:11:41 +0100

    NFDIV-1127: Handle conditional content required for joint applications

--- src/test/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1ApplyForConditionalOrderNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1ApplyForConditionalOrderNotificationTest.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.divorce.citizen.notification.conditionalorder.Applicant1ApplyForConditionalOrderNotification;
+import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
@@ -18,8 +19,10 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
+import static uk.gov.hmcts.divorce.citizen.notification.conditionalorder.Applicant1ApplyForConditionalOrderNotification.JOINT_CONDITIONAL_ORDER;
 import static uk.gov.hmcts.divorce.citizen.notification.conditionalorder.Applicant1ApplyForConditionalOrderNotification.YOUR_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DISSOLUTION;
+import static uk.gov.hmcts.divorce.divorcecase.model.Gender.FEMALE;
 import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.CITIZEN_APPLY_FOR_CONDITIONAL_ORDER;
 import static uk.gov.hmcts.divorce.notification.NotificationConstants.APPLICATION_REFERENCE;
@@ -41,6 +44,7 @@ public class Applicant1ApplyForConditionalOrderNotificationTest {
     @Test
     void shouldSendEmailToApplicant1WithDivorceContent() {
         CaseData data = validJointApplicant1CaseData();
+        data.setApplicationType(ApplicationType.SOLE_APPLICATION);
 
         final HashMap<String, String> templateVars = new HashMap<>();
 
@@ -53,7 +57,8 @@ public class Applicant1ApplyForConditionalOrderNotificationTest {
             eq(CITIZEN_APPLY_FOR_CONDITIONAL_ORDER),
             argThat(allOf(
                 hasEntry(APPLICATION_REFERENCE, String.valueOf(1234567890123456L)),
-                hasEntry(YOUR_APPLICATION, "get a divorce")
+                hasEntry(YOUR_APPLICATION, "get a divorce"),
+                hasEntry(JOINT_CONDITIONAL_ORDER, "")
                 )),
             eq(ENGLISH)
         );
@@ -65,6 +70,7 @@ public class Applicant1ApplyForConditionalOrderNotificationTest {
     void shouldSendEmailToApplicant1WithDissolutionContent() {
         CaseData data = validJointApplicant1CaseData();
         data.setDivorceOrDissolution(DISSOLUTION);
+        data.setApplicationType(ApplicationType.SOLE_APPLICATION);
 
         final HashMap<String, String> templateVars = new HashMap<>();
 
@@ -77,7 +83,88 @@ public class Applicant1ApplyForConditionalOrderNotificationTest {
             eq(CITIZEN_APPLY_FOR_CONDITIONAL_ORDER),
             argThat(allOf(
                 hasEntry(APPLICATION_REFERENCE, String.valueOf(1234567890123456L)),
-                hasEntry(YOUR_APPLICATION, "end your civil partnership")
+                hasEntry(YOUR_APPLICATION, "end your civil partnership"),
+                hasEntry(JOINT_CONDITIONAL_ORDER, "")
+            )),
+            eq(ENGLISH)
+        );
+
+        verify(commonContent).templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2());
+    }
+
+    @Test
+    void shouldSendEmailToApplicant1WithJointDivorceHusbandContent() {
+        CaseData data = validJointApplicant1CaseData();
+        data.setApplicationType(ApplicationType.JOINT_APPLICATION);
+
+        final HashMap<String, String> templateVars = new HashMap<>();
+
+        when(commonContent.templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2())).thenReturn(templateVars);
+        when(commonContent.getTheirPartner(data, data.getApplicant2())).thenReturn("husband");
+
+        notification.sendToApplicant1(data, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(CITIZEN_APPLY_FOR_CONDITIONAL_ORDER),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, String.valueOf(1234567890123456L)),
+                hasEntry(YOUR_APPLICATION, "get a divorce"),
+                hasEntry(JOINT_CONDITIONAL_ORDER, "Your husband has also received this notification. They need to apply for a conditional order too because this is a joint application.")
+            )),
+            eq(ENGLISH)
+        );
+
+        verify(commonContent).templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2());
+    }
+
+    @Test
+    void shouldSendEmailToApplicant1WithJointDivorceWifeContent() {
+        CaseData data = validJointApplicant1CaseData();
+        data.setApplicationType(ApplicationType.JOINT_APPLICATION);
+        data.getApplicant2().setGender(FEMALE);
+
+        final HashMap<String, String> templateVars = new HashMap<>();
+
+        when(commonContent.templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2())).thenReturn(templateVars);
+        when(commonContent.getTheirPartner(data, data.getApplicant2())).thenReturn("wife");
+
+        notification.sendToApplicant1(data, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(CITIZEN_APPLY_FOR_CONDITIONAL_ORDER),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, String.valueOf(1234567890123456L)),
+                hasEntry(YOUR_APPLICATION, "get a divorce"),
+                hasEntry(JOINT_CONDITIONAL_ORDER, "Your wife has also received this notification. They need to apply for a conditional order too because this is a joint application.")
+            )),
+            eq(ENGLISH)
+        );
+
+        verify(commonContent).templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2());
+    }
+
+    @Test
+    void shouldSendEmailToApplicant1WithJointDissolutionContent() {
+        CaseData data = validJointApplicant1CaseData();
+        data.setDivorceOrDissolution(DISSOLUTION);
+        data.setApplicationType(ApplicationType.JOINT_APPLICATION);
+
+        final HashMap<String, String> templateVars = new HashMap<>();
+
+        when(commonContent.templateVarsForApplicant(data, data.getApplicant1(), data.getApplicant2())).thenReturn(templateVars);
+        when(commonContent.getTheirPartner(data, data.getApplicant2())).thenReturn("civil partner");
+
+        notification.sendToApplicant1(data, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(CITIZEN_APPLY_FOR_CONDITIONAL_ORDER),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, String.valueOf(1234567890123456L)),
+                hasEntry(YOUR_APPLICATION, "end your civil partnership"),
+                hasEntry(JOINT_CONDITIONAL_ORDER, "Your civil partner has also received this notification. They need to apply for a conditional order too because this is a joint application.")
             )),
             eq(ENGLISH)
         );
