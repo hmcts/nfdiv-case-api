d165effa00e095f9a0702f809b8328838e588059
Nitin Prabhu <prabhunitin61@gmail.com>
2021-09-29 12:57:31 +0100

    Remove unused imports

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Bailiff.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Bailiff.java
@@ -5,10 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.Document;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Email;
 
 @Data
