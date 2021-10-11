c18864824d8cc3d9ba2e8ff889a0c6fd6c41ba05
Nitin Prabhu <prabhunitin61@gmail.com>
2021-10-11 12:11:07 +0100

    Revert local testing changes

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/service/CcdSearchService.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/service/CcdSearchService.java
@@ -27,7 +27,6 @@ import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
 import static org.elasticsearch.search.sort.SortOrder.ASC;
 import static uk.gov.hmcts.divorce.divorcecase.NoFaultDivorce.CASE_TYPE;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPronouncement;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.Draft;
 
 @Service
 @Slf4j
@@ -118,7 +117,7 @@ public class CcdSearchService {
             int from = 0;
             int totalSearch;
             do {
-                QueryBuilder stateQuery = matchQuery("state", Draft);
+                QueryBuilder stateQuery = matchQuery("state", AwaitingPronouncement);
                 QueryBuilder bulkListingCaseId = existsQuery("data.bulkListCaseReference");
 
                 QueryBuilder query = boolQuery()
