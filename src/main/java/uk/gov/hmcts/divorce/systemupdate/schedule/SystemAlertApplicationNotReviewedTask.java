62cffd8594e060ef7c16490496602172b18dab97
Tom Elliott <tomelliott@Toms-MBP.home>
2021-10-11 16:37:42 +0100

    NFDIV-1252: Keep checks in place

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemAlertApplicationNotReviewedTask.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemAlertApplicationNotReviewedTask.java
@@ -70,7 +70,11 @@ public class SystemAlertApplicationNotReviewedTask implements Runnable {
                             caseDetails.getLastModified()
                         );
                     } else {
-                        notifyApplicant1(caseDetails, caseData, dueDate, user, serviceAuthorization);
+                        if (!dueDate.isAfter(LocalDate.now())
+                            && !caseData.getApplication().isOverdueNotificationSent()
+                        ) {
+                            notifyApplicant1(caseDetails, caseData, dueDate, user, serviceAuthorization);
+                        }
                     }
                 } catch (final CcdManagementException e) {
                     log.error("Submit event failed for case id: {}, continuing to next case", caseDetails.getId());
--- src/test/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemAlertApplicationNotReviewedTaskTest.java
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemAlertApplicationNotReviewedTaskTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.divorce.citizen.notification.JointApplicationOverdueNotification;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.idam.IdamService;
 import uk.gov.hmcts.divorce.systemupdate.service.CcdConflictException;
@@ -26,12 +27,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Collections.singletonList;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant2Response;
 import static uk.gov.hmcts.divorce.systemupdate.event.SystemAlertApplicationNotReviewed.SYSTEM_APPLICATION_NOT_REVIEWED;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.SERVICE_AUTHORIZATION;
@@ -78,15 +82,15 @@ public class SystemAlertApplicationNotReviewedTaskTest {
         final CaseDetails caseDetails2 = mock(CaseDetails.class);
 
         final CaseData caseData1 = CaseData.builder().dueDate(LocalDate.now()).build();
-        final CaseData caseData2 = CaseData.builder().dueDate(LocalDate.now()).build();
+        final CaseData caseData2 = CaseData.builder().dueDate(LocalDate.now().plusDays(5)).build();
 
         when(caseDetails1.getData()).thenReturn(Map.of("dueDate", LocalDate.now()));
         when(caseDetails1.getId()).thenReturn(1L);
-        when(caseDetails2.getData()).thenReturn(Map.of("dueDate", LocalDate.now()));
+        when(caseDetails2.getData()).thenReturn(Map.of("dueDate", LocalDate.now().plusDays(5)));
         when(caseDetails2.getId()).thenReturn(2L);
 
         when(mapper.convertValue(Map.of("dueDate", LocalDate.now()), CaseData.class)).thenReturn(caseData1);
-        when(mapper.convertValue(Map.of("dueDate", LocalDate.now()), CaseData.class)).thenReturn(caseData2);
+        when(mapper.convertValue(Map.of("dueDate", LocalDate.now().plusDays(5)), CaseData.class)).thenReturn(caseData2);
 
         final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
 
@@ -96,9 +100,31 @@ public class SystemAlertApplicationNotReviewedTaskTest {
         systemAlertApplicationNotReviewedTask.run();
 
         verify(jointApplicationOverdueNotification).sendApplicationNotReviewedEmail(caseData1, caseDetails1.getId());
-        verify(jointApplicationOverdueNotification).sendApplicationNotReviewedEmail(caseData2, caseDetails2.getId());
+        verify(jointApplicationOverdueNotification, times(0)).sendApplicationNotReviewedEmail(caseData2, caseDetails2.getId());
         verify(ccdUpdateService).submitEvent(caseDetails1, SYSTEM_APPLICATION_NOT_REVIEWED, user, SERVICE_AUTHORIZATION);
-        verify(ccdUpdateService).submitEvent(caseDetails2, SYSTEM_APPLICATION_NOT_REVIEWED, user, SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldNotSendEmailForOverdueJointApplicationIfOverdueNotificationSent() {
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseData caseData1 = CaseData.builder()
+            .dueDate(LocalDate.now())
+            .application(Application.builder()
+                .overdueNotificationSent(YES)
+                .build())
+            .build();
+
+        when(caseDetails1.getData()).thenReturn(Map.of("dueDate", LocalDate.now()));
+        when(mapper.convertValue(Map.of("dueDate", LocalDate.now()), CaseData.class)).thenReturn(caseData1);
+
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1);
+
+        when(ccdSearchService.searchForAllCasesWithStateOf(AwaitingApplicant2Response, FLAG, user, SERVICE_AUTHORIZATION))
+            .thenReturn(caseDetailsList);
+
+        systemAlertApplicationNotReviewedTask.run();
+
+        verifyNoInteractions(jointApplicationOverdueNotification, ccdUpdateService);
     }
 
     @Test
@@ -119,6 +145,25 @@ public class SystemAlertApplicationNotReviewedTaskTest {
 
         verify(jointApplicationOverdueNotification, never()).sendApplicationNotReviewedEmail(caseData, caseDetails.getId());
         verify(ccdUpdateService, never()).submitEvent(caseDetails, SYSTEM_APPLICATION_NOT_REVIEWED, user, SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldNotTriggerSystemAlertApplicationNotReviewedTaskOnEachCaseWhenCaseDueDateIsAfterCurrentDate() {
+        final CaseDetails caseDetails = mock(CaseDetails.class);
+        final CaseData caseData = CaseData.builder().dueDate(LocalDate.now().plusDays(5)).build();
+
+        Map<String, Object> caseDataMap = new HashMap<>();
+        caseDataMap.put("dueDate", LocalDate.now().plusDays(5));
+
+        when(caseDetails.getData()).thenReturn(Map.of("dueDate", LocalDate.now().plusDays(5)));
+        when(mapper.convertValue(caseDataMap, CaseData.class)).thenReturn(caseData);
+        when(ccdSearchService.searchForAllCasesWithStateOf(AwaitingApplicant2Response, FLAG, user, SERVICE_AUTHORIZATION))
+            .thenReturn(singletonList(caseDetails));
+
+        systemAlertApplicationNotReviewedTask.run();
+
+        verifyNoInteractions(jointApplicationOverdueNotification);
+        verifyNoInteractions(ccdUpdateService);
     }
 
     @Test
