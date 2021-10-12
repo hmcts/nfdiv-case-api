901e6a5f70b92eeb903e58cc21628833623e0fa6
Linus Norton <linusnorton@gmail.com>
2021-10-12 09:51:16 +0100

    NFDIV-1057 update due date logic

--- src/main/java/uk/gov/hmcts/divorce/solicitor/service/task/SetConfirmServiceDueDate.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/service/task/SetConfirmServiceDueDate.java
@@ -1,28 +1,29 @@
 package uk.gov.hmcts.divorce.solicitor.service.task;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
 
-import java.time.Clock;
-import java.time.LocalDate;
-
 @Component
 @Slf4j
 public class SetConfirmServiceDueDate implements CaseTask {
 
-    @Autowired
-    private Clock clock;
+    @Value("${aos_pack.due_date_offset_days}")
+    private long dueDateOffsetDays;
 
+    /**
+     * For solicitor service the dueDate is set to 16 days after the date the solicitor says the served the respondent.
+     */
     @Override
     public CaseDetails<CaseData, State> apply(final CaseDetails<CaseData, State> caseDetails) {
 
         log.info("Setting due date.  Case ID: {}", caseDetails.getId());
-        caseDetails.getData().setDueDate(LocalDate.now(clock).plusDays(14));
+        final var dueDate = caseDetails.getData().getApplication().getSolicitorService().getDateOfService().plusDays(dueDateOffsetDays);
+        caseDetails.getData().setDueDate(dueDate);
 
         return caseDetails;
     }
