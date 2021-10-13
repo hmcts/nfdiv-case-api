8e75a5f98754b45b4fb0762f4079b75344e0d3ba
Lara <laratollington@gmail.com>
2021-10-13 16:50:32 +0100

    Add confirmDisputeApplication field

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/AcknowledgementOfService.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/AcknowledgementOfService.java
@@ -37,6 +37,12 @@ public class AcknowledgementOfService {
     private YesOrNo disputeApplication;
 
     @CCD(
+        label = "Respondent confirms that they want to dispute the application",
+        access = AosAccess.class
+    )
+    private YesOrNo confirmDisputeApplication;
+
+    @CCD(
         label = "Respondent agreed to claimed jurisdiction?",
         access = AosAccess.class
     )
