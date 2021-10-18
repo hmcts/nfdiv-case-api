ee4ffd6ff49d666d65b8d25fd3985c1b45dee631
Lara <52360278+lara-mpt@users.noreply.github.com>
2021-10-18 09:39:36 +0100

    Unset dispute application fields for update Aos if clashing answers (#829)

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateAosTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateAosTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
@@ -30,5 +33,23 @@ public class CitizenUpdateAosTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CITIZEN_UPDATE_AOS);
+    }
+
+    @Test
+    void shouldSetDisputeApplicationFieldsToNullIfConfirmationIsNo() {
+        final long caseId = 1L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseData caseData = CaseData.builder().build();
+        caseData.getAcknowledgementOfService().setDisputeApplication(YesOrNo.YES);
+        caseData.getAcknowledgementOfService().setConfirmDisputeApplication(YesOrNo.NO);
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = citizenUpdateAos.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getData().getAcknowledgementOfService().getDisputeApplication()).isNull();
+        assertThat(response.getData().getAcknowledgementOfService().getConfirmDisputeApplication()).isNull();
+
     }
 }
