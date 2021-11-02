e11876b589b5ae516a9430b936751df70435c3ef
Nitin Prabhu <prabhunitin61@gmail.com>
2021-11-02 13:35:05 +0000

    Fix integration and functional tests

--- src/integrationTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerReIssueApplicationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerReIssueApplicationIT.java
@@ -115,8 +115,6 @@ public class CaseworkerReIssueApplicationIT {
         "classpath:caseworker-reissue-application-about-to-submit-app2-sol-rep-offline-aos-response.json";
     private static final String CASEWORKER_REISSUE_APPLICATION_ABOUT_TO_SUBMIT_APP_2_SOL_REP_REISSUE_CASE_TYPE =
         "classpath:caseworker-reissue-application-about-to-submit-app2-sol-rep-reissue-response.json";
-    private static final String CASEWORKER_REISSUE_APPLICATION_ABOUT_TO_SUBMIT_SOLICITOR_SERVICE =
-        "classpath:caseworker-issue-application-about-to-submit-solicitor-service-response.json";
     private static final String REISSUE_APPLICATION_ABOUT_TO_SUBMIT_APP_2_NOT_SOL_REP =
         "classpath:caseworker-reissue-application-about-to-submit-app2-not-sol-rep-response.json";
     private static final String REISSUE_APPLICATION_ABOUT_TO_SUBMIT_APP_2_NOT_SOL_REP_OFFLINE_AOS =
