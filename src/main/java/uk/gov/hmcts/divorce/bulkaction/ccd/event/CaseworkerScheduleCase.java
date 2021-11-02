4b296ea3b9dabdc46244659a9d465d6f8b93a376
Nitin Prabhu <prabhunitin61@gmail.com>
2021-11-02 16:09:40 +0000

    Added validation for hearing date to check hearing date is not in past

--- src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCase.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCase.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionPageBuilder;
 import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
 import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
@@ -14,6 +15,9 @@ import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Created;
@@ -33,6 +37,9 @@ public class CaseworkerScheduleCase implements CCDConfig<BulkActionCaseData, Bul
     @Autowired
     private HttpServletRequest request;
 
+    @Autowired
+    private Clock clock;
+
     @Override
     public void configure(final ConfigBuilder<BulkActionCaseData, BulkActionState, UserRole> configBuilder) {
         new BulkActionPageBuilder(configBuilder
@@ -42,6 +49,7 @@ public class CaseworkerScheduleCase implements CCDConfig<BulkActionCaseData, Bul
             .description("Schedule cases for listing")
             .showSummary()
             .showEventNotes()
+            .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::submitted)
             .explicitGrants()
             .grant(CREATE_READ_UPDATE, CASE_WORKER, SYSTEMUPDATE))
@@ -50,6 +58,24 @@ public class CaseworkerScheduleCase implements CCDConfig<BulkActionCaseData, Bul
             .mandatory(BulkActionCaseData::getCourtName)
             .mandatory(BulkActionCaseData::getDateAndTimeOfHearing)
             .mandatoryNoSummary(BulkActionCaseData::getBulkListCaseDetails);
+    }
+
+    private AboutToStartOrSubmitResponse<BulkActionCaseData, BulkActionState> aboutToSubmit(final CaseDetails<BulkActionCaseData, BulkActionState> bulkCaseDetails,
+                                                                                            final CaseDetails<BulkActionCaseData, BulkActionState> beforeDetails) {
+        String errorMessage = null;
+        if (bulkCaseDetails.getData().getDateAndTimeOfHearing().isBefore(LocalDateTime.now(clock))) {
+            errorMessage = "Please enter hearing date and time which is not in past";
+            return AboutToStartOrSubmitResponse
+                .<BulkActionCaseData, BulkActionState>builder()
+                .errors(List.of(errorMessage))
+                .data(bulkCaseDetails.getData())
+                .build();
+        }
+
+        return AboutToStartOrSubmitResponse
+            .<BulkActionCaseData, BulkActionState>builder()
+            .data(bulkCaseDetails.getData())
+            .build();
     }
 
     public SubmittedCallbackResponse submitted(
