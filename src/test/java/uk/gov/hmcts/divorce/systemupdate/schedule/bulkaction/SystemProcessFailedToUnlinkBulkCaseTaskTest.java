24d7e7ef061b1c0c5550641be59a82f489eec51c
Nitin Prabhu <prabhunitin61@gmail.com>
2021-11-04 11:45:12 +0000

    Added unit test for cron

---
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/schedule/bulkaction/SystemProcessFailedToUnlinkBulkCaseTaskTest.java
@@ -0,0 +1,114 @@
+package uk.gov.hmcts.divorce.systemupdate.schedule.bulkaction;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
+import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
+import uk.gov.hmcts.divorce.bulkaction.service.BulkCaseProcessingService;
+import uk.gov.hmcts.divorce.bulkaction.task.BulkCaseCaseTaskFactory;
+import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
+import uk.gov.hmcts.divorce.idam.IdamService;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchCaseException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.idam.client.models.User;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Dropped;
+import static uk.gov.hmcts.divorce.systemupdate.event.SystemUnlinkBulkCase.SYSTEM_UNLINK_BULK_CASE;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SERVICE_AUTHORIZATION;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SYSTEM_UPDATE_AUTH_TOKEN;
+
+@ExtendWith(MockitoExtension.class)
+public class SystemProcessFailedToUnlinkBulkCaseTaskTest {
+    @Mock
+    private CcdSearchService ccdSearchService;
+
+    @Mock
+    private IdamService idamService;
+
+    @Mock
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    private BulkCaseCaseTaskFactory bulkCaseCaseTaskFactory;
+
+    @Mock
+    private BulkCaseProcessingService bulkCaseProcessingService;
+
+    @InjectMocks
+    private SystemProcessFailedToUnlinkBulkCaseTask processFailedToUnlinkBulkCaseTask;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User(SYSTEM_UPDATE_AUTH_TOKEN, UserDetails.builder().build());
+        when(idamService.retrieveSystemUpdateUserDetails()).thenReturn(user);
+        when(authTokenGenerator.generate()).thenReturn(SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldProcessFailedDroppedBulkCases() {
+
+        final var bulkActionCaseData = BulkActionCaseData.builder().build();
+
+        CaseDetails<BulkActionCaseData, BulkActionState> caseDetails1 = new CaseDetails<>();
+        caseDetails1.setId(1L);
+        caseDetails1.setData(bulkActionCaseData);
+
+        CaseDetails<BulkActionCaseData, BulkActionState> caseDetails2 = new CaseDetails<>();
+        caseDetails2.setId(2L);
+        caseDetails2.setData(bulkActionCaseData);
+
+        final List<CaseDetails<BulkActionCaseData, BulkActionState>> caseDetailsList = asList(caseDetails1, caseDetails2);
+
+        when(ccdSearchService.searchForUnprocessedOrErroredBulkCases(Dropped, user, SERVICE_AUTHORIZATION))
+            .thenReturn(caseDetailsList);
+
+        final CaseTask caseTask = mock(CaseTask.class);
+        when(bulkCaseCaseTaskFactory.getCaseTask(bulkActionCaseData, SYSTEM_UNLINK_BULK_CASE)).thenReturn(caseTask);
+
+        processFailedToUnlinkBulkCaseTask.run();
+
+        verify(bulkCaseProcessingService)
+            .updateUnprocessedBulkCases(
+                caseDetails1,
+                SYSTEM_UNLINK_BULK_CASE,
+                caseTask,
+                user,
+                SERVICE_AUTHORIZATION);
+        verify(bulkCaseProcessingService)
+            .updateUnprocessedBulkCases(
+                caseDetails2,
+                SYSTEM_UNLINK_BULK_CASE,
+                caseTask,
+                user,
+                SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldStopProcessingIfCcdSearchCaseExceptionIsThrown() {
+        doThrow(new CcdSearchCaseException("message", null))
+            .when(ccdSearchService).searchForUnprocessedOrErroredBulkCases(Dropped, user, SERVICE_AUTHORIZATION);
+
+        processFailedToUnlinkBulkCaseTask.run();
+
+        verifyNoInteractions(bulkCaseProcessingService);
+    }
+}
+
+
