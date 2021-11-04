65702eba003cf692c8a9f9d24b0a427a0e2d7eb7
Mark Butler <mark.butler@solirius.com>
2021-11-04 11:46:04 +0000

    Unused import

--- src/test/java/uk/gov/hmcts/divorce/document/content/PronouncementListTemplateContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/PronouncementListTemplateContentTest.java
@@ -20,7 +20,6 @@ import uk.gov.hmcts.reform.idam.client.models.User;
 import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
