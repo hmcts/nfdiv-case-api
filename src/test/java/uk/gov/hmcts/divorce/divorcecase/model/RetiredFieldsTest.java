416142393e3f5ed52b5e951244567f569ea4ed72
Shaun Francis <shaun.francis1@hmcts.net>
2021-11-11 09:39:41 +0000

    Add unit tests for coverage

--- src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
@@ -11,25 +11,55 @@ class RetiredFieldsTest {
 
     @Test
     void migrateShouldMigrateSomeFieldsAndLeaveOthersAlone() {
-        var data = new HashMap<String, Object>();
+        final var data = new HashMap<String, Object>();
         data.put("applicant1FirstName", "This will be overwritten");
         data.put("exampleRetiredField", "This will be nulled");
         data.put("applicant1LastName", "This will be left alone");
         data.put("applicant1ContactDetailsConfidential", "keep");
         data.put("applicant2ContactDetailsConfidential", "share");
+        data.put("applicant1FinancialOrderForRemoved", "value");
+        data.put("applicant2FinancialOrderForRemoved", "value");
+        data.put("dateConditionalOrderSubmitted", "2021-11-11");
+        data.put("legalProceedingsExist", "YES");
+        data.put("legalProceedingsDescription", "value");
+        data.put("doYouAgreeCourtHasJurisdiction", "YES");
+        data.put("serviceApplicationType", "type");
         data.put("coCourtName", "serviceCentre");
         data.put("courtName", "serviceCentre");
 
-        var result = RetiredFields.migrate(data);
+        final var result = RetiredFields.migrate(data);
 
         assertThat(result.get("applicant1FirstName")).isEqualTo("This will be nulled");
         assertThat(result.get("exampleRetiredField")).isNull();
+        assertThat(result.get("applicant1LastName")).isEqualTo("This will be left alone");
         assertThat(result.get("applicant1ContactDetailsConfidential")).isNull();
         assertThat(result.get("applicant2ContactDetailsConfidential")).isNull();
-        assertThat(result.get("applicant1LastName")).isEqualTo("This will be left alone");
+        assertThat(result.get("applicant1FinancialOrderForRemoved")).isNull();
+        assertThat(result.get("applicant2FinancialOrderForRemoved")).isNull();
+        assertThat(result.get("dateConditionalOrderSubmitted")).isNull();
+        assertThat(result.get("coDateSubmitted")).isEqualTo("2021-11-11");
+        assertThat(result.get("legalProceedingsExist")).isNull();
+        assertThat(result.get("applicant2LegalProceedings")).isEqualTo("YES");
+        assertThat(result.get("legalProceedingsDescription")).isNull();
+        assertThat(result.get("applicant2LegalProceedingsDetails")).isEqualTo("value");
+        assertThat(result.get("doYouAgreeCourtHasJurisdiction")).isNull();
+        assertThat(result.get("jurisdictionAgree")).isEqualTo("YES");
+        assertThat(result.get("serviceApplicationType")).isNull();
+        assertThat(result.get("alternativeServiceType")).isEqualTo("type");
         assertThat(result.get("coCourtName")).isNull();
         assertThat(result.get("courtName")).isNull();
         assertThat(result.get("coCourt")).isEqualTo(BURY_ST_EDMUNDS.getCourtId());
         assertThat(result.get("court")).isEqualTo(BURY_ST_EDMUNDS.getCourtId());
+    }
+
+    @Test
+    void shouldIgnoreFieldIfPresentAndSetToNull() {
+        final var data = new HashMap<String, Object>();
+        data.put("courtName", null);
+
+        final var result = RetiredFields.migrate(data);
+
+        assertThat(result.get("courtName")).isNull();
+        assertThat(result.get("court")).isNull();
     }
 }
--- src/test/java/uk/gov/hmcts/divorce/document/content/CertificateOfEntitlementContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/CertificateOfEntitlementContentTest.java
@@ -9,6 +9,7 @@ import uk.gov.hmcts.divorce.common.ConditionalOrderCourtDetails;
 import uk.gov.hmcts.divorce.common.config.ConditionalOrderCourtDetailsConfig;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 
@@ -20,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderCourt.BURY_ST_EDMUNDS;
 import static uk.gov.hmcts.divorce.divorcecase.model.WhoDivorcing.HUSBAND;
@@ -44,40 +46,10 @@ public class CertificateOfEntitlementContentTest {
     private CertificateOfEntitlementContent certificateOfEntitlementContent;
 
     @Test
-    void shouldReturnTemplateContent() {
+    void shouldReturnTemplateContentForSole() {
 
-        final LocalDateTime localDateTime = LocalDateTime.of(2021, 11, 8, 14, 56);
-        final LocalDate localDate = LocalDate.of(2021, 11, 8);
-        final CaseData caseData = CaseData.builder()
-            .applicationType(SOLE_APPLICATION)
-            .applicant1(Applicant.builder()
-                .firstName("John")
-                .lastName("Smith")
-                .financialOrder(YES)
-                .build())
-            .applicant2(Applicant.builder()
-                .firstName("Jane")
-                .lastName("Jones")
-                .build())
-            .conditionalOrder(ConditionalOrder.builder()
-                .court(BURY_ST_EDMUNDS)
-                .dateAndTimeOfHearing(localDateTime)
-                .decisionDate(localDate)
-                .claimsGranted(YES)
-                .claimsCostsOrderInformation("info")
-                .build())
-            .application(Application.builder()
-                .divorceWho(HUSBAND)
-                .build())
-            .build();
-
-        final ConditionalOrderCourtDetails expectedDetails = new ConditionalOrderCourtDetails();
-        expectedDetails.setName("Bury St. Edmunds Regional Divorce Centre");
-        expectedDetails.setAddress("2nd Floor\nTriton House\nSt. Andrews Street North\nBury St. Edmunds\nIP33 1TR");
-        expectedDetails.setEmail("divorcecase@justice.gov.uk");
-        expectedDetails.setPhone("0300 303 0642");
-
-        when(conditionalOrderCourtDetailsConfig.get(BURY_ST_EDMUNDS.getCourtId())).thenReturn(expectedDetails);
+        final CaseData caseData = getCaseDataFor(SOLE_APPLICATION);
+        final ConditionalOrderCourtDetails expectedDetails = setupConditionalOrderCourtDetailsConfig();
 
         final Map<String, Object> contentMap = certificateOfEntitlementContent.apply(caseData, TEST_CASE_ID);
 
@@ -98,5 +70,70 @@ public class CertificateOfEntitlementContentTest {
             entry("claimsCostsOrderInformation", "info"),
             entry("divorceWho", "husband")
         );
+    }
+
+    @Test
+    void shouldReturnTemplateContentForJoint() {
+
+        final CaseData caseData = getCaseDataFor(JOINT_APPLICATION);
+        final ConditionalOrderCourtDetails expectedDetails = setupConditionalOrderCourtDetailsConfig();
+
+        final Map<String, Object> contentMap = certificateOfEntitlementContent.apply(caseData, TEST_CASE_ID);
+
+        assertThat(contentMap).contains(
+            entry(CCD_CASE_REFERENCE, "1616-5914-0147-3378"),
+            entry("courtDetails", expectedDetails),
+            entry("approvalDate", "8 November 2021"),
+            entry(APPLICANT_1_FIRST_NAME, "John"),
+            entry(APPLICANT_1_LAST_NAME, "Smith"),
+            entry(APPLICANT_2_FIRST_NAME, "Jane"),
+            entry(APPLICANT_2_LAST_NAME, "Jones"),
+            entry("isSole", false),
+            entry("isJoint", true),
+            entry(DATE_OF_HEARING, "8 November 2021"),
+            entry(TIME_OF_HEARING, "14:56 pm"),
+            entry(HAS_FINANCIAL_ORDERS, true),
+            entry(COSTS_GRANTED, true),
+            entry("claimsCostsOrderInformation", "info"),
+            entry("divorceWho", "husband")
+        );
+    }
+
+    private CaseData getCaseDataFor(final ApplicationType soleApplication) {
+        final LocalDateTime localDateTime = LocalDateTime.of(2021, 11, 8, 14, 56);
+        final LocalDate localDate = LocalDate.of(2021, 11, 8);
+        return CaseData.builder()
+            .applicationType(soleApplication)
+            .applicant1(Applicant.builder()
+                .firstName("John")
+                .lastName("Smith")
+                .financialOrder(YES)
+                .build())
+            .applicant2(Applicant.builder()
+                .firstName("Jane")
+                .lastName("Jones")
+                .build())
+            .conditionalOrder(ConditionalOrder.builder()
+                .court(BURY_ST_EDMUNDS)
+                .dateAndTimeOfHearing(localDateTime)
+                .decisionDate(localDate)
+                .claimsGranted(YES)
+                .claimsCostsOrderInformation("info")
+                .build())
+            .application(Application.builder()
+                .divorceWho(HUSBAND)
+                .build())
+            .build();
+    }
+
+    private ConditionalOrderCourtDetails setupConditionalOrderCourtDetailsConfig() {
+        final ConditionalOrderCourtDetails expectedDetails = new ConditionalOrderCourtDetails();
+        expectedDetails.setName("Bury St. Edmunds Regional Divorce Centre");
+        expectedDetails.setAddress("2nd Floor\nTriton House\nSt. Andrews Street North\nBury St. Edmunds\nIP33 1TR");
+        expectedDetails.setEmail("divorcecase@justice.gov.uk");
+        expectedDetails.setPhone("0300 303 0642");
+
+        when(conditionalOrderCourtDetailsConfig.get(BURY_ST_EDMUNDS.getCourtId())).thenReturn(expectedDetails);
+        return expectedDetails;
     }
 }
