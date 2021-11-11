6ff256e3569713c168af11dc5b9656d49edf3d78
Lara <52360278+lara-mpt@users.noreply.github.com>
2021-11-11 09:15:55 +0000

    nfdiv-1214 remove notification for app2 if marriage not broken selected (#903)
    * nfdiv-1214 remove notification for app2 if marriage not broken selected
    
    * Update CitizenSwitchedToSole.java
    
    * Update after dev review

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSoleTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSoleTest.java
@@ -63,7 +63,7 @@ class CitizenSwitchedToSoleTest {
     }
 
     @Test
-    void givenEventStartedWithValidJointCaseShouldSetApplicationTypeToSoleAndSendNotifications() {
+    void givenEventStartedWithValidJointCaseAndApplicant2ScreenHasMarriageBrokenIsNullShouldSetApplicationTypeToSoleAndSendNotifications() {
         final long caseId = 1L;
         final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
         CaseData caseData = validJointApplicant1CaseData();
@@ -76,6 +76,25 @@ class CitizenSwitchedToSoleTest {
 
         verify(applicant1SwitchToSoleNotification).sendToApplicant1(caseData, caseDetails.getId());
         verify(applicant1SwitchToSoleNotification).sendToApplicant2(caseData, caseDetails.getId());
+        verifyNoMoreInteractions(applicant1SwitchToSoleNotification);
+
+        assertThat(response.getData().getApplicationType()).isEqualTo(SOLE_APPLICATION);
+    }
+
+    @Test
+    void givenApplicant2ScreenHasMarriageBrokenIsNoThenOnlyApplicant1NotificationIsSent() {
+        final long caseId = 1L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        CaseData caseData = validJointApplicant1CaseData();
+        caseData.getApplication().setApplicant2ScreenHasMarriageBroken(NO);
+        caseData.setApplicationType(JOINT_APPLICATION);
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = citizenSwitchedToSole.aboutToSubmit(caseDetails, caseDetails);
+
+        verify(applicant1SwitchToSoleNotification).sendToApplicant1(caseData, caseDetails.getId());
         verifyNoMoreInteractions(applicant1SwitchToSoleNotification);
 
         assertThat(response.getData().getApplicationType()).isEqualTo(SOLE_APPLICATION);
