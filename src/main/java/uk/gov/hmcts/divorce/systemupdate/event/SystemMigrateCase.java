8dab2a97fb16018e9f72521c563017830bfb3496
Linus Norton <linusnorton@gmail.com>
2021-11-16 09:27:30 +0000

    Migrate cases without using a callback, add check that cases can be deserialized (#933)
    * Migrate cases without using a callback, add check that cases can be deserialized
    
    * update tests
    
    * Add another test

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemMigrateCase.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemMigrateCase.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.divorce.systemupdate.event;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
@@ -24,17 +22,7 @@ public class SystemMigrateCase implements CCDConfig<CaseData, State, UserRole> {
             .forAllStates()
             .name("Migrate case data")
             .description("Migrate case data to the latest version")
-            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
-            .aboutToSubmitCallback(this::aboutToSubmit);
+            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE);
     }
 
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
-                                                                       CaseDetails<CaseData, State> beforeDetails) {
-
-        // the RetiredFields::migrate method runs as a pre-event hook to all events so all we need to do is return the migrated data.
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(details.getData())
-            .build();
-    }
 }
