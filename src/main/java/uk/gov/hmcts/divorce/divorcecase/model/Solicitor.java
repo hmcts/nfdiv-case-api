b9fb9165397e501d8b659b217df2a8eaef4c800f
Tom Elliott <tomelliott@Toms-MBP.home>
2021-11-26 14:08:02 +0000

    Add field access to solicitor fields as does not appear to be cascading

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Solicitor.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Solicitor.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 import uk.gov.hmcts.divorce.divorcecase.model.access.OrganisationPolicyAccess;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
@@ -23,33 +24,43 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 @Builder
 public class Solicitor {
 
-    @CCD(label = "Solicitor’s name")
+    @CCD(
+        label = "Solicitor’s name",
+        access = {DefaultAccess.class}
+    )
     private String name;
 
-    @CCD(label = "Solicitor’s reference number")
+    @CCD(
+        label = "Solicitor’s reference number",
+        access = {DefaultAccess.class}
+    )
     private String reference;
 
     @CCD(
         label = "Solicitor’s Phone number",
-        regex = "^[0-9 +().-]{9,}$"
+        regex = "^[0-9 +().-]{9,}$",
+        access = {DefaultAccess.class}
     )
     private String phone;
 
     @CCD(
         label = "Solicitor’s Email",
-        typeOverride = Email
+        typeOverride = Email,
+        access = {DefaultAccess.class}
     )
     private String email;
 
     @CCD(
         label = "Solicitor’s firm/ DX address",
-        typeOverride = TextArea
+        typeOverride = TextArea,
+        access = {DefaultAccess.class}
     )
     private String address;
 
     @CCD(
         label = "I confirm I am willing to accept service of all correspondence and orders by email at the email address "
-            + "stated above."
+            + "stated above.",
+        access = {DefaultAccess.class}
     )
     private YesOrNo agreeToReceiveEmails;
 
