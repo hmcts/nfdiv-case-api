036f10611e193317a496a3cb0439b1de906a2330
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2021-12-07 16:27:10 +0000

    NFDIV-1457: Move case reference and applicant names into pageLabel

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolPaymentSummary.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolPaymentSummary.java
@@ -12,9 +12,8 @@ public class SolPaymentSummary implements CcdPageConfiguration {
 
         pageBuilder
             .page("SolPaymentSummary")
-            .label("LabelSolPaySummaryFeeAccount-CaseRef", "## Case number: ${[CASE_REFERENCE]}")
-            .label("LabelSolPaySummaryFeeAccount-Joint",
-                "## ${applicant1FirstName} ${applicant1LastName} and ${applicant2FirstName} ${applicant2LastName}")
+            .pageLabel("Case number: ${[CASE_REFERENCE]}\n"
+                + "${applicant1FirstName} ${applicant1LastName} and ${applicant2FirstName} ${applicant2LastName}")
             .complex(CaseData::getApplication)
                 .mandatory(Application::getApplicationFeeOrderSummary)
                 .done()
