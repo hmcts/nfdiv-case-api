fb486064a6b2ad2d97bf77ec048204e806343bf8
Tom Elliott <tomelliott@Toms-MBP.home>
2021-12-15 09:10:50 +0000

    NFDIV-1053: Remove unused imports

--- src/functionalTest/java/uk/gov/hmcts/divorce/common/SubmitConditionalOrderFT.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/common/SubmitConditionalOrderFT.java
@@ -8,16 +8,11 @@ import uk.gov.hmcts.divorce.testutil.FunctionalTestSuite;
 import java.io.IOException;
 import java.util.Map;
 
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
-import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_ARRAY_ORDER;
-import static net.javacrumbs.jsonunit.core.Option.IGNORING_EXTRA_FIELDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 import static uk.gov.hmcts.divorce.common.event.SubmitConditionalOrder.SUBMIT_CONDITIONAL_ORDER;
 import static uk.gov.hmcts.divorce.testutil.CaseDataUtil.caseData;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.ABOUT_TO_SUBMIT_URL;
-import static uk.gov.hmcts.divorce.testutil.TestResourceUtil.expectedResponse;
 
 @SpringBootTest
 public class SubmitConditionalOrderFT extends FunctionalTestSuite {
