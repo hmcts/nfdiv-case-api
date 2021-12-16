100edc522a3042d726f2dc136f8c68da358207df
Mark Butler <mark.butler@solirius.com>
2021-12-16 10:39:09 +0000

    Remove unnecessary SuppressWarnings

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
@@ -38,7 +38,6 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
-@SuppressWarnings("PMD")
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
