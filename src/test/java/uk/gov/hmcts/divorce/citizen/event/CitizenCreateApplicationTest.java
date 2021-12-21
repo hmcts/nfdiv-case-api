d120447f355c74e8f47a4654e039054c4154b470
Nitin Prabhu <prabhunitin61@gmail.com>
2021-12-21 10:05:58 +0000

    Set formatted(hyphenated) case reference for citizen create app event

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenCreateApplicationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenCreateApplicationTest.java
@@ -8,8 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.AddSystemUpdateRole;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
@@ -29,6 +31,9 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CitizenCreateApplicationTest {
@@ -90,5 +95,19 @@ class CitizenCreateApplicationTest {
             .containsExactlyInAnyOrder(expectedRolesAndPermissions);
 
         verify(addSystemUpdateRole).addIfConfiguredForEnvironment(anyList());
+    }
+
+    @Test
+    void shouldSetFormattedCaseReferenceWhenAboutToSubmitCallbackIsTriggered() {
+        final CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> beforeDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+        details.setCreatedDate(LOCAL_DATE_TIME);
+
+        AboutToStartOrSubmitResponse<CaseData, State> response = citizenCreateApplication.aboutToSubmit(details, beforeDetails);
+
+        assertThat(response.getData().getHyphenatedCaseRef()).isEqualTo(caseData.formatCaseRef(TEST_CASE_ID));
     }
 }
