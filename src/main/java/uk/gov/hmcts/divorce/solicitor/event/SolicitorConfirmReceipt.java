72b4d066feae393b532a762d2d1118822e2397a5
Nitin Prabhu <prabhunitin61@gmail.com>
2021-12-23 10:39:31 +0000

    Added solicitor confirm receipt

---
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorConfirmReceipt.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
+
+@Component
+public class SolicitorConfirmReceipt implements CCDConfig<CaseData, State, UserRole> {
+    public static final String SOLICITOR_VERIFY_CERTIFICATE = "solicitor-confirm-receipt";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        new PageBuilder(configBuilder
+            .event(SOLICITOR_VERIFY_CERTIFICATE)
+            .forStates(Holding)
+            .showCondition("applicationType=\"jointApplication\"")
+            .name("Confirm Receipt")
+            .description("Confirm Receipt")
+            .showSummary()
+            .showEventNotes()
+            .grant(CREATE_READ_UPDATE, SOLICITOR)
+            .grant(READ,
+                SUPER_USER,
+                CASE_WORKER,
+                LEGAL_ADVISOR));
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorConfirmReceiptTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+
+@ExtendWith(MockitoExtension.class)
+public class SolicitorConfirmReceiptTest {
+
+    @InjectMocks
+    private SolicitorConfirmReceipt solicitorConfirmReceipt;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        solicitorConfirmReceipt.configure(configBuilder);
+
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(SolicitorConfirmReceipt.SOLICITOR_VERIFY_CERTIFICATE);
+    }
+}
