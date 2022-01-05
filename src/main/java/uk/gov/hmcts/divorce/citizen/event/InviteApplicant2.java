ab7cb0de94777a9af221979cdc317f9be8d4d590
Nitin Prabhu <prabhunitin61@gmail.com>
2022-01-05 16:36:38 +0000

    Fix role

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/InviteApplicant2.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/InviteApplicant2.java
@@ -19,10 +19,10 @@ import java.util.List;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant2Response;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.Draft;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
@@ -52,7 +52,7 @@ public class InviteApplicant2 implements CCDConfig<CaseData, State, UserRole> {
             .showCondition("applicationType=\"jointApplication\"")
             .showSummary()
             .showEventNotes()
-            .grant(CREATE_READ_UPDATE, CITIZEN, APPONESOLICITOR)
+            .grant(CREATE_READ_UPDATE, CITIZEN, APPLICANT_1_SOLICITOR)
             .grant(READ,
                 SUPER_USER,
                 CASE_WORKER,
