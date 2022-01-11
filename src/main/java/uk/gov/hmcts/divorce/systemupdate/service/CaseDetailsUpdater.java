7b432dc724729e97776ffeb717648b0d0d44698b
Shaun Francis <shaun.francis1@hmcts.net>
2022-01-11 16:02:14 +0000

    Add more logging to trace document field issue

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/service/CaseDetailsUpdater.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/service/CaseDetailsUpdater.java
@@ -2,15 +2,18 @@ package uk.gov.hmcts.divorce.systemupdate.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
+import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 
 @Component
+@Slf4j
 public class CaseDetailsUpdater {
 
     @Autowired
@@ -19,9 +22,16 @@ public class CaseDetailsUpdater {
     public CaseDetails<CaseData, State> updateCaseData(final CaseTask caseTask,
                                                        final StartEventResponse startEventResponse) {
 
+        final uk.gov.hmcts.reform.ccd.client.model.CaseDetails initCaseDetails = startEventResponse.getCaseDetails();
         final CaseDetails<CaseData, State> caseDetails = objectMapper
-            .convertValue(startEventResponse.getCaseDetails(), new TypeReference<>() {
+            .convertValue(initCaseDetails, new TypeReference<>() {
             });
+
+        //TODO: Remove temp logging for tracking certificate of entitlement
+        log.info(
+            "****** Converted CaseData for case id: {}, conditional order: {}",
+            initCaseDetails.getId(),
+            caseDetails.getData().getConditionalOrder());
 
         return caseTask.apply(caseDetails);
     }
