e40a822f50c5914f86830d187bfa34d5cc7237be
taleb <taleb.benouaer@hmcts.net>
2022-01-12 12:53:44 +0000

    nfdiv-1050: add notification for ConditionalOrder pronouncement

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemPronounceCase.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemPronounceCase.java
@@ -1,13 +1,18 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.citizen.notification.conditionalorder.ConditionalOrderPronouncedNotification;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPronouncement;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderPronounced;
@@ -25,15 +30,35 @@ public class SystemPronounceCase implements CCDConfig<CaseData, State, UserRole>
 
     public static final String SYSTEM_PRONOUNCE_CASE = "system-pronounce-case";
 
+    @Autowired
+    private ConditionalOrderPronouncedNotification conditionalOrderPronouncedNotification;
+
+    @Autowired
+    private NotificationDispatcher notificationDispatcher;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
 
-        new PageBuilder(configBuilder
-            .event(SYSTEM_PRONOUNCE_CASE)
-            .forStateTransition(AwaitingPronouncement, ConditionalOrderPronounced)
-            .name("System pronounce case")
-            .description("System pronounce case")
-            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
-            .grant(READ, SOLICITOR, CASE_WORKER, SUPER_USER, LEGAL_ADVISOR));
+        new PageBuilder(
+            configBuilder
+                .event(SYSTEM_PRONOUNCE_CASE)
+                .forStateTransition(AwaitingPronouncement, ConditionalOrderPronounced)
+                .name("System pronounce case")
+                .description("System pronounce case")
+                .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
+                .grant(READ, SOLICITOR, CASE_WORKER, SUPER_USER, LEGAL_ADVISOR)
+                .aboutToSubmitCallback(this::aboutToSubmit)
+        );
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                       CaseDetails<CaseData, State> beforeDetails) {
+        CaseData data = details.getData();
+
+        notificationDispatcher.send(conditionalOrderPronouncedNotification, data, details.getId());
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .build();
     }
 }
