5027ce003de0c546eb58a22df2bc4fce401d2a33
danielbe11 <daniel.bell@solirius.com>
2022-01-18 10:25:03 +0000

    Add sendToApplicant1 method for LegalAdvisorClarificationSubmittedNotification

--- src/main/java/uk/gov/hmcts/divorce/legaladvisor/notification/LegalAdvisorClarificationSubmittedNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/legaladvisor/notification/LegalAdvisorClarificationSubmittedNotification.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.divorce.legaladvisor.notification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.divorce.notification.NotificationService;
 import java.util.Map;
 
 import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_NAME;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.CITIZEN_CONDITIONAL_ORDER_REFUSED;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_CLARIFICATION_SUBMITTED;
 
 @Component
@@ -22,6 +24,24 @@ public class LegalAdvisorClarificationSubmittedNotification implements Applicant
 
     @Autowired
     private CommonContent commonContent;
+
+    @Override
+    public void sendToApplicant1(CaseData caseData, Long caseId) {
+
+        final Applicant applicant1 = caseData.getApplicant1();
+        final Applicant applicant2 = caseData.getApplicant2();
+
+        final Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, caseId, applicant1, applicant2);
+
+        log.info("Sending Conditional order refused notification to applicant 1 for case : {}", caseId);
+
+        notificationService.sendEmail(
+            caseData.getApplicant1().getEmail(),
+            CITIZEN_CONDITIONAL_ORDER_REFUSED,
+            templateVars,
+            caseData.getApplicant1().getLanguagePreference()
+        );
+    }
 
     @Override
     public void sendToApplicant1Solicitor(final CaseData caseData, final Long caseId) {
