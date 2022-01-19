00685950a9eb00dd30538cd39d2c0077a3a265d0
Lara <laratollington@gmail.com>
2022-01-19 10:04:42 +0000

    Update states for submit-conditional-order

--- src/main/java/uk/gov/hmcts/divorce/common/event/SubmitConditionalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/SubmitConditionalOrder.java
@@ -18,6 +18,7 @@ import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
+import java.util.EnumSet;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingLegalAdvisorReferral;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderDrafted;
@@ -49,7 +50,7 @@ public class SubmitConditionalOrder implements CCDConfig<CaseData, State, UserRo
     public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
             .event(SUBMIT_CONDITIONAL_ORDER)
-            .forStateTransition(ConditionalOrderDrafted, AwaitingLegalAdvisorReferral)
+            .forStateTransition(EnumSet.of(ConditionalOrderDrafted, ConditionalOrderPending), AwaitingLegalAdvisorReferral)
             .name("Submit Conditional Order")
             .description("Submit Conditional Order")
             .endButtonLabel("Save Conditional Order")
