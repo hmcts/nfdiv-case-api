03953824505be23dfb17d09318d3aebcae95852f
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-01-24 08:01:41 +0000

    Add unit tests for Caseworker progress paper case aboutToSubmit method.
    Confirm that it correctly updates to the selected state when the
    user selects one in the dropdown on the frontend.

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerProgressPaperCaseTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerProgressPaperCaseTest.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.ProgressPaperCase;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
@@ -30,5 +33,50 @@ public class CaseworkerProgressPaperCaseTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_PROGRESS_PAPER_CASE);
+    }
+
+    @Test
+    void shouldUpdateCaseStateWhenCaseworkerSelectsAwaitingPayment() {
+        final long caseId = 1L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseData caseData = CaseData.builder().build();
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+        caseData.setProgressPaperCase(ProgressPaperCase.AWAITING_PAYMENT);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerProgressPaperCase.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(State.AwaitingPayment);
+    }
+
+    @Test
+    void shouldUpdateCaseStateWhenCaseworkerSelectsAwaitingDocuments() {
+        final long caseId = 1L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseData caseData = CaseData.builder().build();
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+        caseData.setProgressPaperCase(ProgressPaperCase.AWAITING_DOCUMENT);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerProgressPaperCase.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(State.AwaitingDocuments);
+    }
+
+    @Test
+    void shouldUpdateCaseStateWhenCaseworkerSelectsSubmitted() {
+        final long caseId = 1L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseData caseData = CaseData.builder().build();
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+        caseData.setProgressPaperCase(ProgressPaperCase.SUBMITTED);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerProgressPaperCase.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(State.Submitted);
     }
 }
