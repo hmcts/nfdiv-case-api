9d91f3d7d498a3be1f043c9a0bfea159ac383a22
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2022-01-25 15:47:23 +0000

    Use correct roles for system update and fail build when uploads fails (#1254)

--- src/main/java/uk/gov/hmcts/divorce/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/divorce/common/ccd/HighLevelDataSetupApp.java
@@ -46,7 +46,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
 
     @Override
     protected boolean shouldTolerateDataSetupFailure() {
-        return true;
+        return false;
     }
 
     @Override
