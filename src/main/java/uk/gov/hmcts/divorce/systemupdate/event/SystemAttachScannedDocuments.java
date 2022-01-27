d891708602dcf04bf7f168c6cbe6007901aa8384
Nitin Prabhu <prabhunitin61@gmail.com>
2022-01-27 16:57:46 +0000

    Added delete access to system update user role used by bulkscan

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemAttachScannedDocuments.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemAttachScannedDocuments.java
@@ -1,6 +1,13 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
+import static uk.gov.hmcts.divorce.divorcecase.model.State.OfflineDocumentReceived;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ_UPDATE_DELETE;
+
 import org.springframework.stereotype.Component;
+
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
@@ -9,12 +16,6 @@ import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-
-import static uk.gov.hmcts.divorce.divorcecase.model.State.OfflineDocumentReceived;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.UPDATE_DELETE;
 
 @Component
 public class SystemAttachScannedDocuments implements CCDConfig<CaseData, State, UserRole> {
@@ -25,8 +26,8 @@ public class SystemAttachScannedDocuments implements CCDConfig<CaseData, State, 
             .attachScannedDocEvent()
             .forAllStates()
             .aboutToSubmitCallback(this::aboutToSubmit)
-            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
-            .grant(UPDATE_DELETE, CASE_WORKER))
+            .grant(CREATE_READ_UPDATE_DELETE, SYSTEMUPDATE)
+            .grant(READ_UPDATE_DELETE, CASE_WORKER))
             .page("attachScannedDocs")
             .pageLabel("Correspondence")
             .mandatory(CaseData::getScannedDocuments)
--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemHandleSupplementaryEvidence.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemHandleSupplementaryEvidence.java
@@ -1,17 +1,18 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ_UPDATE_DELETE;
+
 import org.springframework.stereotype.Component;
+
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.UPDATE_DELETE;
 
 @Component
 public class SystemHandleSupplementaryEvidence implements CCDConfig<CaseData, State, UserRole> {
@@ -21,8 +22,8 @@ public class SystemHandleSupplementaryEvidence implements CCDConfig<CaseData, St
         new PageBuilder(configBuilder
             .handleSupplementaryEvent()
             .forAllStates()
-            .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
-            .grant(UPDATE_DELETE, CASE_WORKER))
+            .grant(CREATE_READ_UPDATE_DELETE, SYSTEMUPDATE)
+            .grant(READ_UPDATE_DELETE, CASE_WORKER))
             .page("handleEvidence")
             .pageLabel("Correspondence")
             .mandatory(CaseData::getEvidenceHandled);
