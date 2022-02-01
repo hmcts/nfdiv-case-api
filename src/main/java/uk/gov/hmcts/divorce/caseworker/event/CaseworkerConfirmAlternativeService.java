929818ecd92463c6f5a44eb251ffaa2e83db2e63
Shaun Francis <shaun.francis1@hmcts.net>
2022-02-01 17:38:55 +0000

    Remove due date setting for alternative service event

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerConfirmAlternativeService.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerConfirmAlternativeService.java
@@ -1,20 +1,13 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-
-import java.time.Clock;
-import java.time.LocalDate;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingAlternativeService;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
@@ -32,12 +25,6 @@ public class CaseworkerConfirmAlternativeService implements CCDConfig<CaseData, 
 
     public static final String CASEWORKER_CONFIRM_ALTERNATIVE_SERVICE = "caseworker-confirm-alternative-service";
 
-    @Value("${aos_pack.due_date_offset_days}")
-    private long dueDateOffsetDays;
-
-    @Autowired
-    private Clock clock;
-
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
@@ -46,23 +33,11 @@ public class CaseworkerConfirmAlternativeService implements CCDConfig<CaseData, 
             .name("Confirm alternative service")
             .description("Confirm alternative service")
             .showEventNotes()
-            .aboutToSubmitCallback(this::aboutToSubmit)
             .grant(CREATE_READ_UPDATE, CASE_WORKER)
             .grant(READ,
                 SUPER_USER,
                 SOLICITOR,
                 LEGAL_ADVISOR,
                 CITIZEN));
-    }
-
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
-                                                                       final CaseDetails<CaseData, State> beforeDetails) {
-        final CaseData caseData = details.getData();
-
-        caseData.setDueDate(LocalDate.now(clock).plusDays(dueDateOffsetDays));
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .build();
     }
 }
