df327c526e12014aa13582444be76616ef1216c9
danielbe11 <daniel.bell@solirius.com>
2022-02-01 19:15:52 +0000

    Make code changes

---
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/AwaitingFinalOrderNotification.java
@@ -0,0 +1,69 @@
+package uk.gov.hmcts.divorce.common.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.notification.ApplicantNotification;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_FINAL_ORDER_ELIGIBLE_FROM_PLUS_3_MONTHS;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_REMINDER;
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.APPLICANT_APPLY_FOR_FINAL_ORDER;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
+
+@Component
+@Slf4j
+public class AwaitingFinalOrderNotification implements ApplicantNotification {
+
+    @Autowired
+    private CommonContent commonContent;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Override
+    public void sendToApplicant1(final CaseData caseData, final Long id) {
+        log.info("Notifying applicant 1 that they can apply for a final order: {}", id);
+        final Applicant applicant1 = caseData.getApplicant1();
+        final Applicant applicant2 = caseData.getApplicant2();
+
+        notificationService.sendEmail(
+            applicant1.getEmail(),
+            APPLICANT_APPLY_FOR_FINAL_ORDER,
+            templateVars(caseData, id, applicant1, applicant2, false),
+            applicant1.getLanguagePreference()
+        );
+    }
+
+    @Override
+    public void sendToApplicant2(final CaseData caseData, final Long id) {
+        final Applicant applicant1 = caseData.getApplicant1();
+        final Applicant applicant2 = caseData.getApplicant2();
+
+        if (!caseData.getApplicationType().isSole()) {
+            log.info("Notifying applicant 2 (joint application) that they can apply for a final order: {}", id);
+
+            notificationService.sendEmail(
+                caseData.getApplicant2EmailAddress(),
+                APPLICANT_APPLY_FOR_FINAL_ORDER,
+                templateVars(caseData, id, applicant2, applicant1, false),
+                applicant2.getLanguagePreference()
+            );
+        }
+    }
+
+    private Map<String, String> templateVars(CaseData caseData, Long id, Applicant applicant, Applicant partner, boolean isReminder) {
+        Map<String, String> templateVars = commonContent.conditionalOrderTemplateVars(caseData, id, applicant, partner);
+        templateVars.put(IS_REMINDER, isReminder ? YES :  NO);
+        templateVars.put(DATE_FINAL_ORDER_ELIGIBLE_FROM_PLUS_3_MONTHS,
+            caseData.getFinalOrder().getDateFinalOrderEligibleToRespondent().format(DATE_TIME_FORMATTER));
+        return templateVars;
+    }
+}
--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemProgressCaseToAwaitingFinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemProgressCaseToAwaitingFinalOrder.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.common.notification.AwaitingFinalOrderNotification;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrder;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderPronounced;
@@ -19,9 +25,16 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
 
 @Component
+@Slf4j
 public class SystemProgressCaseToAwaitingFinalOrder implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String SYSTEM_PROGRESS_CASE_TO_AWAITING_FINAL_ORDER = "system-progress-case-awaiting-final-order";
+
+    @Autowired
+    private NotificationDispatcher notificationDispatcher;
+
+    @Autowired
+    private AwaitingFinalOrderNotification awaitingFinalOrderNotification;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -32,5 +45,18 @@ public class SystemProgressCaseToAwaitingFinalOrder implements CCDConfig<CaseDat
             .description("Progress case to Awaiting Final Order")
             .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
             .grant(READ, SOLICITOR, CASE_WORKER, SUPER_USER, LEGAL_ADVISOR));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
+                                                                       final CaseDetails<CaseData, State> beforeDetails) {
+        final CaseData caseData = details.getData();
+        final Long caseId = details.getId();
+        log.info("6 week 1 day period elapsed for Case({}), notifying applicant(s) that they can apply for final order", caseId);
+
+        notificationDispatcher.send(awaitingFinalOrderNotification, caseData, caseId);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
     }
 }
