df9ff65d43caf2deb63ac9047b88a0cd32f0d1a0
Linus Norton <linusnorton@gmail.com>
2022-02-01 06:43:09 +0000

    unused import

--- src/integrationTest/java/uk/gov/hmcts/divorce/systemupdate/service/CcdUpdateServiceIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/systemupdate/service/CcdUpdateServiceIT.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.divorce.systemupdate.service;
 
-import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
