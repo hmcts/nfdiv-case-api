3bbdc46fa8149a086bf7da1dab42646aedf126e7
Linus Norton <linusnorton@gmail.com>
2022-02-02 08:51:32 +0000

    Clean up CaseInvite example

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseInvite.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseInvite.java
@@ -2,29 +2,13 @@ package uk.gov.hmcts.divorce.divorcecase.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
-import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 import uk.gov.hmcts.divorce.divorcecase.util.AccessCodeGenerator;
 
 import static java.util.Objects.isNull;
 
 public record CaseInvite(
-    @CCD(
-        label = "The respondent's email address",
-        access = {DefaultAccess.class}
-    )
     String applicant2InviteEmailAddress,
-
-    @CCD(
-        label = "The respondent invite access code",
-        access = {DefaultAccess.class}
-    )
     String accessCode,
-
-    @CCD(
-        label = "The respondent's user id",
-        access = {DefaultAccess.class}
-    )
     String applicant2UserId) {
 
     @Builder()
@@ -35,7 +19,6 @@ public record CaseInvite(
         return !isNull(applicant2UserId) && userId.equals(applicant2UserId);
     }
 
-    @JsonIgnore
     public CaseInvite generateAccessCode() {
         return new CaseInvite(applicant2InviteEmailAddress, AccessCodeGenerator.generateAccessCode(), applicant2UserId);
     }
