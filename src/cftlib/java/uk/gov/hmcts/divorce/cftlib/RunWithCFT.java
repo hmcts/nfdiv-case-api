1e9ac47ad09a38b7182c2691048fb43efe2e7cd9
Alex McAusland <alex.mcausland@gmail.com>
2022-02-02 20:43:39 +0000

    0.8.0

--- src/cftlib/java/uk/gov/hmcts/divorce/cftlib/RunWithCFT.java
+++ src/cftlib/java/uk/gov/hmcts/divorce/cftlib/RunWithCFT.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.divorce.cftlib;
 
-import java.util.Map;
 import uk.gov.hmcts.divorce.CaseApiApplication;
 import uk.gov.hmcts.rse.ccd.lib.api.LibRunner;
 
 public class RunWithCFT {
 
     public static void main(final String[] args) {
-      new LibRunner(CaseApiApplication.class).run(Map.of(
-         "management.health.case-document-am-api.enabled", "false"
-      ));
+      new LibRunner(CaseApiApplication.class).run();
     }
 }
