46536585d3108f10faf97e80adb848dffba02f4a
Linus Norton <linusnorton@gmail.com>
2022-02-03 11:52:52 +0000

    Library based local development environment (#1305)
    * Remove spring contract runner from test configuration
    
    * add config
    
    * working ccd lib test
    
    * remove obsolete config
    
    * revert dependency changes
    
    * comment
    
    * remove obsolete exclude
    
    * tidy tests
    
    * hookup test to generated definition
    
    * style fixes
    
    * comment
    
    * fix test on preview
    
    * rename test
    
    * formatting
    
    * configure import script to use embedded ccd
    
    * add xui boot task
    
    * introduce Cftlib task type
    
    * remove nonfunctional task
    
    * booting locally
    
    * add am role assignment json
    
    * set callback url
    
    * add cftlib sourceset
    
    * remove ccdTest configurations
    
    * bump cft lib
    
    * fix linux host lookup
    
    * use aat rd
    
    * case creation working
    
    * Add spring based cftlib config
    
    * update services
    
    * import role assignments
    
    * use es cftlib
    
    * clean up gradle
    
    * tidy up
    
    * undo change
    
    * undo formatting
    
    * cftlib 0.5.0
    
    * lightweight init script
    
    * Revert "lightweight init script"
    
    This reverts commit bf5ab8a42cd072e36227686c35efa0af3157f213.
    
    * update service auth client
    
    * 0.5.6
    
    * load env file from vault
    
    * Revert "load env file from vault"
    
    This reverts commit 01330c21a7a5bc8fd6678a70b343653d5746593b.
    
    * Revert "Revert "load env file from vault""
    
    This reverts commit 37216e34950a6f68afb4bd4fde432967656e92f0.
    
    * better splitting
    
    * add task to reload secrets
    
    * fix issue with bootXui
    
    * fix bootrun
    
    * bump cft lib to 0.6.0
    
    * tmp
    
    * revert unnecessary changes
    
    * 0.7.0
    
    * fix cftlib on jdk 17
    
    * fix cftlib on java 17
    
    * 0.8.0
    
    * rebuild xlsx on boot
    
    * increase code cache size
    
    * clean up
    
    * update FT
    
    * change default CASE_API_URL
    
    * update README
    
    Co-authored-by: Alex McAusland <alex.mcausland@gmail.com>

---
+++ src/cftlib/java/uk/gov/hmcts/divorce/cftlib/CftLibConfig.java
@@ -0,0 +1,53 @@
+package uk.gov.hmcts.divorce.cftlib;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.rse.ccd.lib.api.CFTLib;
+import uk.gov.hmcts.rse.ccd.lib.api.CFTLibConfigurer;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@Component
+public class CftLibConfig implements CFTLibConfigurer {
+    @Override
+    public void configure(CFTLib lib) throws Exception {
+        for (String p : List.of(
+            "DivCaseWorkerUser@AAT.com",
+            "TEST_CASE_WORKER_USER@mailinator.com",
+            "TEST_SOLICITOR@mailinator.com",
+            "divorce_as_caseworker_admin@mailinator.com")) {
+            lib.createProfile(p, "DIVORCE", "NO_FAULT_DIVORCE", "Submitted");
+        }
+
+        lib.createRoles(
+            "caseworker-divorce-courtadmin_beta",
+            "caseworker-divorce-superuser",
+            "caseworker-divorce-courtadmin-la",
+            "caseworker-divorce-courtadmin",
+            "caseworker-divorce-solicitor",
+            "caseworker-divorce-pcqextractor",
+            "caseworker-divorce-systemupdate",
+            "caseworker-divorce-bulkscan",
+            "caseworker-caa",
+            "citizen",
+            "caseworker-divorce",
+            "caseworker",
+            "pui-case-manager",
+            "pui-finance-manager",
+            "pui-organisation-manager",
+            "pui-user-manager"
+        );
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        var json = IOUtils.toString(resourceLoader.getResource("classpath:cftlib-am-role-assignments.json")
+            .getInputStream(), Charset.defaultCharset());
+        lib.configureRoleAssignments(json);
+
+        var def = Files.readAllBytes(Path.of("build/ccd-config/ccd-NFD-dev.xlsx"));
+        lib.importDefinition(def);
+    }
+}
