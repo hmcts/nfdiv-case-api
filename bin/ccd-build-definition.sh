3b046ebadb40c36bd8170c02b9a9860653cbcb4d
Linus Norton <linusnorton@gmail.com>
2022-02-04 14:27:24 +0000

    update

--- bin/ccd-build-definition.sh
+++ bin/ccd-build-definition.sh
@@ -10,7 +10,7 @@ build_dir=${root_dir}/build/ccd-config
 
 mkdir -p ${build_dir}
 
-az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
+docker logout hmctspublic.azurecr.io
 
 for dir in $(find ${root_dir}/build/definitions/ -maxdepth 1 -mindepth  1 -type d -exec basename {} \;)
 do
