b91d7ef8b1d7499922f00b1fec3dd49753c936df
Linus Norton <linusnorton@gmail.com>
2022-02-04 14:22:53 +0000

    Add az login to hmctspublic

--- bin/ccd-build-definition.sh
+++ bin/ccd-build-definition.sh
@@ -10,6 +10,8 @@ build_dir=${root_dir}/build/ccd-config
 
 mkdir -p ${build_dir}
 
+az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
+
 for dir in $(find ${root_dir}/build/definitions/ -maxdepth 1 -mindepth  1 -type d -exec basename {} \;)
 do
   config_dir=${root_dir}/build/definitions/${dir}
