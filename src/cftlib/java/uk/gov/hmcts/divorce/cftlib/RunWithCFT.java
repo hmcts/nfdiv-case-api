42023fb96171888c9235a9ae8aa04d4b102f355e
Alex McAusland <alex.mcausland@gmail.com>
2022-02-07 16:17:15 +0000

    Update cftlib to 0.9.1
    Which features a breaking change to the LibRunner API.

--- src/cftlib/java/uk/gov/hmcts/divorce/cftlib/RunWithCFT.java
+++ src/cftlib/java/uk/gov/hmcts/divorce/cftlib/RunWithCFT.java
@@ -7,6 +7,6 @@ import uk.gov.hmcts.rse.ccd.lib.api.LibRunner;
 public class RunWithCFT {
 
     public static void main(final String[] args) {
-        new LibRunner(CaseApiApplication.class).run();
+        LibRunner.run(CaseApiApplication.class);
     }
 }
