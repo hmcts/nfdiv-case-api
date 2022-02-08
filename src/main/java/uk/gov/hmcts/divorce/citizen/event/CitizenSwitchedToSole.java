c3691cc1caf5838d4b8ca8aef9bdb401a8d745b4
taleb <taleb.benouaer@hmcts.net>
2022-02-08 15:21:24 +0000

    nfdiv-1133: add applicant2 role

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSole.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenSwitchedToSole.java
@@ -28,6 +28,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant1Res
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant2Response;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPayment;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.Draft;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
@@ -63,7 +64,7 @@ public class CitizenSwitchedToSole implements CCDConfig<CaseData, State, UserRol
             .forStates(AwaitingApplicant1Response, AwaitingApplicant2Response, Applicant2Approved, AwaitingPayment)
             .name("Application switched to sole")
             .description("Application type switched to sole")
-            .grant(CREATE_READ_UPDATE, CITIZEN)
+            .grant(CREATE_READ_UPDATE, CITIZEN, APPLICANT_2)
             .retries(120, 120)
             .aboutToSubmitCallback(this::aboutToSubmit);
     }
