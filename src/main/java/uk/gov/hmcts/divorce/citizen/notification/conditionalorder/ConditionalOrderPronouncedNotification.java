d75c2ca58158f2ce2586bb76be1909b60fcfc83e
Shaun Francis <shaun.francis1@hmcts.net>
2022-02-08 10:34:37 +0000

    Add check for pronounce notification template content

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/ConditionalOrderPronouncedNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/ConditionalOrderPronouncedNotification.java
@@ -5,13 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.EmailTemplateName;
 import uk.gov.hmcts.divorce.notification.NotificationService;
+import uk.gov.hmcts.divorce.notification.exception.NotificationTemplateException;
 
 import java.util.Map;
 
+import static java.lang.String.format;
+import static java.util.Objects.isNull;
 import static uk.gov.hmcts.divorce.notification.CommonContent.COURT_NAME;
 import static uk.gov.hmcts.divorce.notification.CommonContent.CO_PRONOUNCEMENT_DATE_PLUS_43;
 import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_OF_HEARING;
@@ -23,6 +27,8 @@ import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
 @Slf4j
 public class ConditionalOrderPronouncedNotification implements ApplicantNotification {
 
+    private static final String MISSING_FIELD_MESSAGE = "Notification failed with missing field '%s' for Case Id: %s";
+
     @Autowired
     private NotificationService notificationService;
 
@@ -30,37 +36,53 @@ public class ConditionalOrderPronouncedNotification implements ApplicantNotifica
     private CommonContent commonContent;
 
     @Override
-    public void sendToApplicant1(final CaseData caseData, final Long id) {
-        log.info("Notifying applicant 1 that their conditional order application has been pronounced: {}", id);
+    public void sendToApplicant1(final CaseData caseData, final Long caseId) {
+        log.info("Notifying applicant 1 that their conditional order application has been pronounced: {}", caseId);
 
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             CITIZEN_CONDITIONAL_ORDER_PRONOUNCED,
-            templateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
+            templateVars(caseData, caseId, caseData.getApplicant1(), caseData.getApplicant2()),
             caseData.getApplicant1().getLanguagePreference()
         );
     }
 
     @Override
-    public void sendToApplicant2(final CaseData caseData, final Long id) {
-        log.info("Notifying applicant 2 that their conditional order application has been granted: {}", id);
+    public void sendToApplicant2(final CaseData caseData, final Long caseId) {
+        log.info("Notifying applicant 2 that their conditional order application has been granted: {}", caseId);
 
         EmailTemplateName emailTemplateName =
             caseData.getApplicationType().isSole() ? SOLE_RESPONDENT_CONDITIONAL_ORDER_PRONOUNCED : CITIZEN_CONDITIONAL_ORDER_PRONOUNCED;
         notificationService.sendEmail(
             caseData.getApplicant2EmailAddress(),
             emailTemplateName,
-            templateVars(caseData, id, caseData.getApplicant2(), caseData.getApplicant1()),
+            templateVars(caseData, caseId, caseData.getApplicant2(), caseData.getApplicant1()),
             caseData.getApplicant2().getLanguagePreference()
         );
     }
 
-    private Map<String, String> templateVars(CaseData caseData, Long id, Applicant applicant, Applicant partner) {
-        Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, applicant, partner);
-        templateVars.put(COURT_NAME, caseData.getConditionalOrder().getCourt().getLabel());
-        templateVars.put(DATE_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(DATE_TIME_FORMATTER));
+    private Map<String, String> templateVars(final CaseData caseData,
+                                             final Long caseId,
+                                             final Applicant applicant,
+                                             final Applicant partner) {
+
+        final ConditionalOrder conditionalOrder = caseData.getConditionalOrder();
+
+        if (isNull(conditionalOrder.getCourt())) {
+            throw new NotificationTemplateException(format(MISSING_FIELD_MESSAGE, "coCourt", caseId));
+        }
+        if (isNull(conditionalOrder.getDateAndTimeOfHearing())) {
+            throw new NotificationTemplateException(format(MISSING_FIELD_MESSAGE, "coDateAndTimeOfHearing", caseId));
+        }
+        if (isNull(conditionalOrder.getGrantedDate())) {
+            throw new NotificationTemplateException(format(MISSING_FIELD_MESSAGE, "coGrantedDate", caseId));
+        }
+
+        final Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, caseId, applicant, partner);
+        templateVars.put(COURT_NAME, conditionalOrder.getCourt().getLabel());
+        templateVars.put(DATE_OF_HEARING, conditionalOrder.getDateAndTimeOfHearing().format(DATE_TIME_FORMATTER));
         templateVars.put(CO_PRONOUNCEMENT_DATE_PLUS_43,
-            caseData.getConditionalOrder().getGrantedDate().plusDays(43).format(DATE_TIME_FORMATTER));
+            conditionalOrder.getGrantedDate().plusDays(43).format(DATE_TIME_FORMATTER));
         return templateVars;
     }
 }
