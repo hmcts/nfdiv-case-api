fccdf3b30524315dcf9bce593b06414b199f4f98
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-02-09 09:01:17 +0000

    Implement validation to prevent user continuing if they select no
    Added a mid event to the ConditionalOrderReviewAoS page which
    checks if the user selects no on the question for whether they wish
    to continue, and prevents them continuing until they select yes.
    
    Added unit tests for mid event and cleaned up spacing

--- src/main/java/uk/gov/hmcts/divorce/common/event/page/ConditionalOrderReviewAoS.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/page/ConditionalOrderReviewAoS.java
@@ -1,18 +1,28 @@
 package uk.gov.hmcts.divorce.common.event.page;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderQuestions;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
 public class ConditionalOrderReviewAoS implements CcdPageConfiguration {
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
 
         pageBuilder
-            .page("ConditionalOrderReviewAoS")
+            .page("ConditionalOrderReviewAoS", this::midEvent)
             .pageLabel("Review Acknowledgement of Service - Draft Conditional Order Application")
             .complex(CaseData::getConditionalOrder)
                 .readonly(ConditionalOrder::getRespondentAnswersLink)
@@ -21,6 +31,36 @@ public class ConditionalOrderReviewAoS implements CcdPageConfiguration {
                 .complex(ConditionalOrder::getConditionalOrderApplicant1Questions)
                 .mandatory(ConditionalOrderQuestions::getApplyForConditionalOrder)
                 .done()
+                .label(
+                    "ConditionalOrderReviewAoSNo",
+                    "You must select yes to be able to draft a conditional order",
+                    "coApplicant1ApplyForConditionalOrder=\"No\""
+                )
             .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+        log.info("Mid-event callback triggered for ConditionalOrderReviewAoS");
+
+        CaseData data = details.getData();
+        List<String> errors = new ArrayList<>();
+        ConditionalOrder conditionalOrder = data.getConditionalOrder();
+
+        // TODO: Review error message to get actual text
+        if (!conditionalOrder.getConditionalOrderApplicant1Questions().getApplyForConditionalOrder().toBoolean()) {
+            errors.add("To continue, applicant 1 must select Yes");
+        }
+
+        if (conditionalOrder.getConditionalOrderApplicant2Questions().getApplyForConditionalOrder() != null &&
+            !conditionalOrder.getConditionalOrderApplicant2Questions().getApplyForConditionalOrder().toBoolean()) {
+            errors.add("To continue, applicant 2 must select Yes");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .errors(errors)
+            .build();
     }
 }
