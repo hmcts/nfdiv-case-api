580a714c2f29f62d71a4fee7b9ce5cda848a4086
danielbe11 <daniel.bell@solirius.com>
2022-02-15 14:43:40 +0000

    Adding code for SystemNotifyRespondentApplyFinalOrder

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemNotifyRespondentApplyFinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemNotifyRespondentApplyFinalOrder.java
@@ -1,23 +1,34 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.divorce.common.notification.RespondentApplyForFinalOrderNotification;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrder;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
+@Slf4j
 public class SystemNotifyRespondentApplyFinalOrder implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String SYSTEM_NOTIFY_RESPONDENT_APPLY_FINAL_ORDER = "system-notify-respondent-apply-final-order";
+
+    @Autowired
+    private NotificationDispatcher notificationDispatcher;
+
+    @Autowired
+    private RespondentApplyForFinalOrderNotification respondentApplyForFinalOrderNotification;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -25,8 +36,8 @@ public class SystemNotifyRespondentApplyFinalOrder implements CCDConfig<CaseData
         configBuilder
             .event(SYSTEM_NOTIFY_RESPONDENT_APPLY_FINAL_ORDER)
             .forState(AwaitingFinalOrder)
-            .name("Notify respondent overdue")
-            .description("Notify respondent that Final Order application is overdue")
+            .name("Notify respondent to apply for final order")
+            .description("Notify respondent that they can make a Final Order application")
             .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
             .retries(120, 120)
             .aboutToSubmitCallback(this::aboutToSubmit);
@@ -34,11 +45,16 @@ public class SystemNotifyRespondentApplyFinalOrder implements CCDConfig<CaseData
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
                                                                        CaseDetails<CaseData, State> beforeDetails) {
-        // TODO send respondent apply reminder notification
-        CaseData data = details.getData();
-        data.getFinalOrder().setFinalOrderReminderSentApplicant2(YesOrNo.YES);
+        final CaseData caseData = details.getData();
+        final Long caseId = details.getId();
+        log.info("A period of 3 months has elapsed since applicant became eligible to apply for final order for case {}. "
+            + "Triggering notificationDispatcher...", caseId);
+
+        notificationDispatcher.send(respondentApplyForFinalOrderNotification, caseData, caseId);
+
+        caseData.getFinalOrder().setFinalOrderReminderSentApplicant2(YesOrNo.YES);
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(data)
+            .data(caseData)
             .build();
     }
 }
