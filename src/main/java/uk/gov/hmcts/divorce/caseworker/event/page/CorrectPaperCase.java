e1822da4eeeca140cd23e9ee611fbb4cb3c7492d
Tom Elliott <tomelliott@Toms-MBP.home>
2022-02-16 17:56:52 +0000

    NFDIV-1376: Add form validation and associated tests for correct paper case

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/page/CorrectPaperCase.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/page/CorrectPaperCase.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.divorce.caseworker.event.page;
 
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
@@ -9,6 +11,14 @@ import uk.gov.hmcts.divorce.divorcecase.model.HelpWithFees;
 import uk.gov.hmcts.divorce.divorcecase.model.Jurisdiction;
 import uk.gov.hmcts.divorce.divorcecase.model.MarriageDetails;
 import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
+import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.SOT_REQUIRED;
 
 public class CorrectPaperCase implements CcdPageConfiguration {
 
@@ -25,7 +35,7 @@ public class CorrectPaperCase implements CcdPageConfiguration {
                 "### ${labelContentApplicantsOrApplicant1s} details")
             .complex(CaseData::getApplicant1)
                 .mandatory(Applicant::getFirstName)
-                .mandatory(Applicant::getMiddleName)
+                .optional(Applicant::getMiddleName)
                 .mandatory(Applicant::getLastName)
                 .mandatory(Applicant::getNameDifferentToMarriageCertificate)
                 .mandatory(Applicant::getNameChangedHowOtherDetails,
@@ -64,7 +74,7 @@ public class CorrectPaperCase implements CcdPageConfiguration {
                 "### ${labelContentRespondentsOrApplicant2s} details")
             .complex(CaseData::getApplicant2)
                 .mandatory(Applicant::getFirstName)
-                .mandatory(Applicant::getMiddleName)
+                .optional(Applicant::getMiddleName)
                 .mandatory(Applicant::getLastName)
                 .mandatory(Applicant::getNameDifferentToMarriageCertificate)
                 .mandatory(Applicant::getNameChangedHowOtherDetails,
@@ -88,12 +98,13 @@ public class CorrectPaperCase implements CcdPageConfiguration {
                 .mandatory(Applicant::getFinancialOrdersFor, "applicant2FinancialOrder=\"Yes\"")
             .done()
             .label("Label-CorrectApplicant2SOTDetails",
-                "### ${labelContentRespondentsOrApplicant2s} statement of truth details")
+                "### ${labelContentRespondentsOrApplicant2s} statement of truth details",
+                "applicationType=\"jointApplication\"")
             .complex(CaseData::getApplication)
-                .mandatory(Application::getApplicant2StatementOfTruth)
-                .mandatory(Application::getApplicant2SolSignStatementOfTruth)
-                .mandatory(Application::getApplicant2SolStatementOfReconciliationName)
-                .mandatory(Application::getApplicant2SolStatementOfReconciliationFirm)
+                .mandatory(Application::getApplicant2StatementOfTruth, "applicationType=\"jointApplication\"")
+                .mandatory(Application::getApplicant2SolSignStatementOfTruth, "applicationType=\"jointApplication\"")
+                .mandatory(Application::getApplicant2SolStatementOfReconciliationName, "applicationType=\"jointApplication\"")
+                .mandatory(Application::getApplicant2SolStatementOfReconciliationFirm, "applicationType=\"jointApplication\"")
                 .label("Label-CorrectJurisdictionDetails", "### Jurisdiction connection details")
                 .complex(Application::getJurisdiction)
                     .mandatory(Jurisdiction::getConnections)
@@ -104,11 +115,15 @@ public class CorrectPaperCase implements CcdPageConfiguration {
                 .mandatory(Application::getApplicant2ScreenHasMarriageBroken)
                 .mandatory(Application::getApplicant1PrayerHasBeenGivenCheckbox)
                 .mandatory(Application::getApplicant2PrayerHasBeenGivenCheckbox)
+                .label("Label-CorrectApp1HWFDetails",
+                    "### ${labelContentApplicantsOrApplicant1s} Help With Fees details")
                 .complex(Application::getApplicant1HelpWithFees)
                     .optional(HelpWithFees::getReferenceNumber)
                     .mandatory(HelpWithFees::getAppliedForFees)
                     .mandatory(HelpWithFees::getNeedHelp)
                 .done()
+                .label("Label-CorrectApp2HWFDetails",
+                    "### ${labelContentRespondentsOrApplicant2s} Help With Fees details")
                 .complex(Application::getApplicant2HelpWithFees)
                     .optional(HelpWithFees::getReferenceNumber)
                     .mandatory(HelpWithFees::getAppliedForFees)
@@ -132,5 +147,56 @@ public class CorrectPaperCase implements CcdPageConfiguration {
                         "marriageCertifyMarriageCertificateIsCorrect=\"No\"")
                 .done()
             .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+        final CaseData data = details.getData();
+        final Application application = data.getApplication();
+        final List<String> errors = new ArrayList<>();
+
+        data.getLabelContent().setApplicationType(data.getApplicationType());
+        data.getLabelContent().setUnionType(data.getDivorceOrDissolution());
+        data.getConditionalOrder().getConditionalOrderApplicant1Questions().setIsSubmitted(NO);
+        data.getConditionalOrder().getConditionalOrderApplicant1Questions().setIsDrafted(NO);
+        if (!data.getApplicationType().isSole()) {
+            data.getConditionalOrder().getConditionalOrderApplicant2Questions().setIsSubmitted(NO);
+            data.getConditionalOrder().getConditionalOrderApplicant2Questions().setIsDrafted(NO);
+        }
+
+        if (!application.getApplicant1ScreenHasMarriageBroken().toBoolean()) {
+            errors.add("To continue, applicant 1 must believe and declare that their marriage has irrevocably broken");
+        }
+
+        if (application.getApplicant2ScreenHasMarriageBroken() != null && !application.getApplicant2ScreenHasMarriageBroken().toBoolean()) {
+            errors.add("To continue, applicant 2 must believe and declare that their marriage has irrevocably broken");
+        }
+
+        if (Objects.isNull(application.getApplicant1PrayerHasBeenGivenCheckbox())) {
+            errors.add("Applicant 1 prayer must not be empty");
+        } else if (application.getApplicant1PrayerHasBeenGivenCheckbox().isEmpty()) {
+            errors.add("Applicant 1 prayer must be yes");
+        }
+
+        if (Objects.isNull(application.getApplicant2PrayerHasBeenGivenCheckbox())) {
+            errors.add("Applicant 2 prayer must not be empty");
+        } else if (application.getApplicant2PrayerHasBeenGivenCheckbox().isEmpty()) {
+            errors.add("Applicant 2 prayer must be yes");
+        }
+
+        if (!application.hasStatementOfTruth()) {
+            errors.add(SOT_REQUIRED);
+        }
+
+        if (!data.getApplicationType().isSole() && !application.hasApplicant2StatementOfTruth()) {
+            errors.add("Statement of truth must be accepted by Applicant 2 for joint applications");
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .errors(errors)
+            .build();
     }
 }
