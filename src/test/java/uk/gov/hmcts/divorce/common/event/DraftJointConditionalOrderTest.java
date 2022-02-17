45026e5790b55b3db208e40a1ae0e7fc5adfe649
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-02-17 12:32:01 +0000

    Fix link to application for draft joint conditional order
    Also add unit tests to cover new code for both applicant 1
    and applicant 2 draft conditional order fixes.

--- src/test/java/uk/gov/hmcts/divorce/common/event/DraftJointConditionalOrderTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/event/DraftJointConditionalOrderTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.divorce.common.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -15,8 +16,11 @@ import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderQuestions;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.solicitor.service.task.AddMiniApplicationLink;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.common.event.DraftJointConditionalOrder.DRAFT_JOINT_CONDITIONAL_ORDER;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderDrafted;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
@@ -24,6 +28,9 @@ import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
 class DraftJointConditionalOrderTest {
+
+    @Mock
+    private AddMiniApplicationLink addMiniApplicationLink;
 
     @InjectMocks
     private DraftJointConditionalOrder draftJointConditionalOrder;
@@ -68,5 +75,22 @@ class DraftJointConditionalOrderTest {
 
         assertThat(response.getData().getConditionalOrder().getConditionalOrderApplicant2Questions().getIsDrafted())
             .isEqualTo(YesOrNo.YES);
+    }
+
+    @Test
+    void shouldCallAddMiniApplicationAndReturnCaseDataOnAboutToStart() {
+
+        final CaseData expectedCaseData = CaseData.builder().build();
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        final CaseDetails<CaseData, State> updateCaseDetails = new CaseDetails<>();
+        updateCaseDetails.setData(expectedCaseData);
+
+        when(addMiniApplicationLink.apply(caseDetails)).thenReturn(updateCaseDetails);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = draftJointConditionalOrder.aboutToStart(caseDetails);
+
+        assertThat(response.getData()).isSameAs(expectedCaseData);
+
+        verify(addMiniApplicationLink).apply(caseDetails);
     }
 }
