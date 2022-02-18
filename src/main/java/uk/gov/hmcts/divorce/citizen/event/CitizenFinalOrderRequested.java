1722ef2228b790f5d21739678692b51f68f8e593
Lara <laratollington@gmail.com>
2022-02-18 14:57:42 +0000

    remove unnecessary state change from event

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderRequested.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderRequested.java
@@ -3,18 +3,13 @@ package uk.gov.hmcts.divorce.citizen.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
-import java.time.LocalDate;
-
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrder;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderOverdue;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderRequested;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
@@ -37,28 +32,7 @@ public class CitizenFinalOrderRequested implements CCDConfig<CaseData, State, Us
             .name("Citizen FO requested")
             .description("Citizen final order requested")
             .grant(CREATE_READ_UPDATE, CREATOR)
-            .grant(READ, SUPER_USER, LEGAL_ADVISOR, CASE_WORKER)
-            .aboutToSubmitCallback(this::aboutToSubmit);
-    }
-
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
-                                                                       CaseDetails<CaseData, State> beforeDetails) {
-
-        log.info("Citizen final order requested about to submit callback invoked");
-
-        final CaseData caseData = details.getData();
-        final State state;
-
-        if (caseData.getFinalOrder().getDateFinalOrderNoLongerEligible().isBefore(LocalDate.now())) {
-            state = FinalOrderOverdue;
-        } else {
-            state = FinalOrderRequested;
-        }
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseData)
-            .state(state)
-            .build();
+            .grant(READ, SUPER_USER, LEGAL_ADVISOR, CASE_WORKER);
     }
 }
 
--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderRequestedTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderRequestedTest.java
@@ -5,19 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.divorce.citizen.event.CitizenFinalOrderRequested.CITIZEN_FINAL_ORDER_REQUESTED;
-import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.getExpectedLocalDate;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.validCaseDataForAwaitingFinalOrder;
 
 @ExtendWith(MockitoExtension.class)
 class CitizenFinalOrderRequestedTest {
@@ -34,35 +30,5 @@ class CitizenFinalOrderRequestedTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CITIZEN_FINAL_ORDER_REQUESTED);
-    }
-
-    @Test
-    void shouldSetStateToFinalOrderOverdueIfDateFinalOrderNoLongerEligibleIsPast() {
-        final long caseId = 2L;
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        CaseData caseData = validCaseDataForAwaitingFinalOrder();
-        caseData.getFinalOrder().setDateFinalOrderNoLongerEligible(getExpectedLocalDate().minusWeeks(1L));
-
-        caseDetails.setData(caseData);
-        caseDetails.setId(caseId);
-
-        final AboutToStartOrSubmitResponse<CaseData, State> response = citizenFinalOrderRequested.aboutToSubmit(caseDetails, caseDetails);
-
-        assertThat(response.getState()).isEqualTo(State.FinalOrderOverdue);
-    }
-
-    @Test
-    void shouldSetStateToFinalOrderRequestedIfDateFinalOrderNoLongerEligibleIsPast() {
-        final long caseId = 2L;
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        CaseData caseData = validCaseDataForAwaitingFinalOrder();
-        caseData.getFinalOrder().setDateFinalOrderNoLongerEligible(getExpectedLocalDate().plusWeeks(1L));
-
-        caseDetails.setData(caseData);
-        caseDetails.setId(caseId);
-
-        final AboutToStartOrSubmitResponse<CaseData, State> response = citizenFinalOrderRequested.aboutToSubmit(caseDetails, caseDetails);
-
-        assertThat(response.getState()).isEqualTo(State.FinalOrderRequested);
     }
 }
