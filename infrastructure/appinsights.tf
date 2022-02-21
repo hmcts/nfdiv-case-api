17d90ce704d24ef05e35f256a30a20957cc4994a
Linus Norton <linusnorton@gmail.com>
2022-02-21 08:52:51 +0000

    Maybe it's here

--- infrastructure/appinsights.tf
+++ infrastructure/appinsights.tf
@@ -10,6 +10,6 @@ data "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = data.ai.connection_string
+  value        = data.azurerm_application_insights.ai.connection_string
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
