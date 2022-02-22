50b9936d040d863ce665abe6c2fb67399eedb6bd
danielbe11 <daniel.bell@solirius.com>
2022-02-22 11:49:49 +0000

    Merging apply for final order events into one

---
+++ src/main/java/uk/gov/hmcts/divorce/common/event/ApplyForFinalOrder.java
@@ -0,0 +1,73 @@
+package uk.gov.hmcts.divorce.common.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.common.event.page.ApplyForFinalOrderDetails;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import java.util.EnumSet;
+import java.util.List;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrder;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderOverdue;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderRequested;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
+
+@Component
+@Slf4j
+public class ApplyForFinalOrder implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String FINAL_ORDER_REQUESTED = "final-order-requested";
+
+    public static final String APPLY_FOR_FINAL_ORDER = "Apply for final order";
+
+    private static final List<CcdPageConfiguration> pages = List.of(
+        new ApplyForFinalOrderDetails()
+    );
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        final PageBuilder pageBuilder = addEventConfig(configBuilder);
+        pages.forEach(page -> page.addTo(pageBuilder));
+    }
+
+    private PageBuilder addEventConfig(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        return new PageBuilder(configBuilder
+            .event(FINAL_ORDER_REQUESTED)
+            .forStateTransition(EnumSet.of(AwaitingFinalOrder, FinalOrderOverdue), FinalOrderRequested)
+            .name(APPLY_FOR_FINAL_ORDER)
+            .description(APPLY_FOR_FINAL_ORDER)
+            .showSummary()
+            .showEventNotes()
+            .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR, CREATOR)
+            .grant(READ,
+                CASE_WORKER,
+                SUPER_USER,
+                LEGAL_ADVISOR));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
+                                                                       final CaseDetails<CaseData, State> beforeDetails) {
+
+        log.info("Apply for final order about to submit callback invoked for case id: {}", details.getId());
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .state(FinalOrderRequested)
+            .build();
+    }
+}
---
+++ src/main/java/uk/gov/hmcts/divorce/common/event/page/ApplyForFinalOrderDetails.java
@@ -0,0 +1,67 @@
+package uk.gov.hmcts.divorce.common.event.page;
+
+import lombok.extern.slf4j.Slf4j;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.FinalOrder;
+import uk.gov.hmcts.divorce.divorcecase.model.LabelContent;
+import uk.gov.hmcts.divorce.divorcecase.model.MarriageDetails;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+
+import static java.util.Collections.singletonList;
+import static uk.gov.hmcts.divorce.common.event.ApplyForFinalOrder.APPLY_FOR_FINAL_ORDER;
+
+@Slf4j
+public class ApplyForFinalOrderDetails implements CcdPageConfiguration {
+
+    private static final String ALWAYS_HIDE = "doesApplicantWantToApplyForFinalOrder=\"ALWAYS_HIDE\"";
+
+    @Override
+    public void addTo(PageBuilder pageBuilder) {
+        pageBuilder
+            .page("SolicitorApplyForFinalOrder", this::midEvent)
+            .pageLabel(APPLY_FOR_FINAL_ORDER)
+            .complex(CaseData::getLabelContent)
+            .readonlyNoSummary(LabelContent::getDivorceOrCivilPartnership, ALWAYS_HIDE)
+            .readonlyNoSummary(LabelContent::getFinaliseDivorceOrEndCivilPartnership, ALWAYS_HIDE)
+            .done()
+            .complex(CaseData::getApplication)
+            .complex(Application::getMarriageDetails)
+            .readonlyNoSummary(MarriageDetails::getApplicant1Name, ALWAYS_HIDE)
+            .readonlyNoSummary(MarriageDetails::getApplicant2Name, ALWAYS_HIDE)
+            .done()
+            .done()
+            .label("label-ApplyForFinalOrder",
+                "Once the court has checked that there are no outstanding applications "
+                    + "or other matters which need to be resolved first, the final order will be made absolute"
+                    + " and the ${labelContentDivorceOrCivilPartnership} between ${marriageApplicant1Name} and "
+                    + " ${marriageApplicant2Name} will be ended.")
+            .complex(CaseData::getFinalOrder)
+            .mandatory(FinalOrder::getDoesApplicantWantToApplyForFinalOrder)
+            .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> midEvent(
+        CaseDetails<CaseData, State> details,
+        CaseDetails<CaseData, State> detailsBefore
+    ) {
+        log.info("Mid-event callback triggered for SolicitorApplyForFinalOrder");
+
+        final CaseData data = details.getData();
+
+        if (!data.getFinalOrder().getDoesApplicantWantToApplyForFinalOrder().toBoolean()) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .data(data)
+                .errors(singletonList("You must select 'Yes' to apply for Final Order"))
+                .build();
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .build();
+    }
+}
