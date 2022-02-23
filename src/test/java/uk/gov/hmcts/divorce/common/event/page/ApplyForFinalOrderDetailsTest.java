6ef7e99f0243532521ed8a30dd4ff122ea44f8fb
danielbe11 <70265071+danielbe11@users.noreply.github.com>
2022-02-23 09:26:07 +0000

    Merging apply for final order events into one "apply-for-final-order" event (#1409)
    * Merging apply for final order events into one
    
    * Remove abouttosubmit callback for now
    
    * Fix checkstyle

--- src/test/java/uk/gov/hmcts/divorce/common/event/page/ApplyForFinalOrderDetailsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/event/page/ApplyForFinalOrderDetailsTest.java
@@ -1,42 +1,20 @@
-package uk.gov.hmcts.divorce.solicitor.event;
+package uk.gov.hmcts.divorce.common.event.page;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
-import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
-import static uk.gov.hmcts.divorce.solicitor.event.SolicitorApplyForFinalOrder.SOLICITOR_FINAL_ORDER_REQUESTED;
-import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
-import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
-@ExtendWith(MockitoExtension.class)
-class SolicitorApplyForFinalOrderTest {
+class ApplyForFinalOrderDetailsTest {
 
-    @InjectMocks
-    private SolicitorApplyForFinalOrder solicitorApplyForFinalOrder;
-
-    @Test
-    void shouldAddConfigurationToConfigBuilder() {
-        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
-
-        solicitorApplyForFinalOrder.configure(configBuilder);
-
-        assertThat(getEventsFrom(configBuilder).values())
-            .extracting(Event::getId)
-            .contains(SOLICITOR_FINAL_ORDER_REQUESTED);
-    }
+    private final ApplyForFinalOrderDetails page = new ApplyForFinalOrderDetails();
 
     @Test
     void shouldReturnErrorWhenApplyForFinalOrderIsNoAndMidEventIsInvoked() {
@@ -48,7 +26,7 @@ class SolicitorApplyForFinalOrderTest {
         details.setData(caseData);
         details.setId(TEST_CASE_ID);
 
-        AboutToStartOrSubmitResponse<CaseData, State> response = solicitorApplyForFinalOrder.midEvent(details, beforeDetails);
+        AboutToStartOrSubmitResponse<CaseData, State> response = page.midEvent(details, beforeDetails);
 
         assertThat(response.getErrors()).containsExactly("You must select 'Yes' to apply for Final Order");
     }
@@ -63,7 +41,7 @@ class SolicitorApplyForFinalOrderTest {
         details.setData(caseData);
         details.setId(TEST_CASE_ID);
 
-        AboutToStartOrSubmitResponse<CaseData, State> response = solicitorApplyForFinalOrder.midEvent(details, beforeDetails);
+        AboutToStartOrSubmitResponse<CaseData, State> response = page.midEvent(details, beforeDetails);
 
         assertThat(response.getErrors()).isNull();
     }
