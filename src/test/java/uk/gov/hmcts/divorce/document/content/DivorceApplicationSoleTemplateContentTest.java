e023c82056855d977523644f0f33c3acc3d3ed26
velimd <duzgunce.vd@gmail.com>
2022-03-03 10:34:45 +0000

    add additional unit test for sol address only

--- src/test/java/uk/gov/hmcts/divorce/document/content/DivorceApplicationSoleTemplateContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/DivorceApplicationSoleTemplateContentTest.java
@@ -65,6 +65,7 @@ import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_FIRST_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_LAST_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_MIDDLE_NAME;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_ADDRESS;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
@@ -153,9 +154,6 @@ public class DivorceApplicationSoleTemplateContentTest {
 
     @Test
     public void shouldSuccessfullyApplyContentFromCaseDataForSoleApplicationWithTypeDissolution() {
-        final String solAddressWithNewLine = "10 Solicitor Road\ntown\n\npostcode\n";
-        final String solAddressWithCleanUp = "10 Solicitor Road\ntown\npostcode";
-
         final Applicant applicant1 = Applicant.builder()
             .firstName(TEST_FIRST_NAME)
             .middleName(TEST_MIDDLE_NAME)
@@ -175,7 +173,7 @@ public class DivorceApplicationSoleTemplateContentTest {
                 .name(TEST_SOLICITOR_NAME)
                 .email(TEST_SOLICITOR_EMAIL)
                 .firmName(TEST_SOLICITOR_NAME)
-                .address(solAddressWithNewLine)
+                .address(TEST_SOLICITOR_ADDRESS)
                 .build())
             .build();
 
@@ -225,6 +223,83 @@ public class DivorceApplicationSoleTemplateContentTest {
             entry(APPLICANT_2_SOLICITOR_NAME, TEST_SOLICITOR_NAME),
             entry(APPLICANT_2_SOLICITOR_EMAIL, TEST_SOLICITOR_EMAIL),
             entry(APPLICANT_2_SOLICITOR_FIRM_NAME, TEST_SOLICITOR_NAME),
+            entry(APPLICANT_2_SOLICITOR_ADDRESS, TEST_SOLICITOR_ADDRESS)
+        );
+
+        verify(applicantTemplateDataProvider).deriveSoleFinancialOrder(any(Applicant.class));
+        verify(applicantTemplateDataProvider).deriveApplicantPostalAddress(any(Applicant.class));
+        verify(applicantTemplateDataProvider).deriveApplicant2PostalAddress(any(Applicant.class), any(Application.class));
+        verify(applicationTemplateDataProvider).deriveSoleJurisdictionList(any(Application.class), eq(TEST_CASE_ID));
+    }
+
+    @Test
+    public void shouldSuccessfullyApplyContentFromCaseDataForSoleApplicationWithSolicitorAddressRepresented() {
+        final String solAddressWithNewLine = "10 Solicitor Road\ntown\n\npostcode\n";
+        final String solAddressWithCleanUp = "10 Solicitor Road\ntown\npostcode";
+
+        final Applicant applicant1 = Applicant.builder()
+            .firstName(TEST_FIRST_NAME)
+            .middleName(TEST_MIDDLE_NAME)
+            .lastName(TEST_LAST_NAME)
+            .financialOrder(NO)
+            .legalProceedings(NO)
+            .email(TEST_USER_EMAIL)
+            .contactDetailsType(PUBLIC)
+            .build();
+        final Applicant applicant2 = Applicant.builder()
+            .firstName(TEST_FIRST_NAME)
+            .middleName(TEST_MIDDLE_NAME)
+            .lastName(TEST_LAST_NAME)
+            .email(TEST_USER_EMAIL)
+            .contactDetailsType(PUBLIC)
+            .solicitor(Solicitor.builder()
+                .address(solAddressWithNewLine)
+                .build())
+            .build();
+
+        final CaseData caseData = CaseData.builder()
+            .applicationType(SOLE_APPLICATION)
+            .divorceOrDissolution(DISSOLUTION)
+            .application(Application.builder()
+                .issueDate(LocalDate.of(2021, 4, 28))
+                .applicant1IsApplicant2Represented(Applicant2Represented.YES)
+                .build())
+            .applicant1(applicant1)
+            .applicant2(applicant2)
+            .build();
+
+        caseData.getApplication().getMarriageDetails().setApplicant1Name(TEST_LAST_NAME);
+        caseData.getApplication().getMarriageDetails().setApplicant2Name(TEST_LAST_NAME);
+
+        final Map<String, Object> result = templateContent.apply(caseData, TEST_CASE_ID);
+
+        assertThat(result).contains(
+            entry(CONDITIONAL_ORDER_DIVORCE_OR_CIVIL_PARTNERSHIP, DISSOLUTION_OF_THE_CIVIL_PARTNERSHIP_WITH),
+            entry(DIVORCE_OR_DISSOLUTION, "application to end your civil partnership"),
+            entry(MARRIAGE_OR_CIVIL_PARTNERSHIP, CIVIL_PARTNERSHIP),
+            entry(MARRIAGE_OR_RELATIONSHIP, RELATIONSHIP),
+            entry(DIVORCE_OR_END_CIVIL_PARTNERSHIP, "ending the civil partnership"),
+            entry(CCD_CASE_REFERENCE, FORMATTED_TEST_CASE_ID),
+            entry(ISSUE_DATE, "28 April 2021"),
+            entry(APPLICANT_1_FIRST_NAME, TEST_FIRST_NAME),
+            entry(APPLICANT_1_MIDDLE_NAME, TEST_MIDDLE_NAME),
+            entry(APPLICANT_1_LAST_NAME, TEST_LAST_NAME),
+            entry(APPLICANT_1_FULL_NAME, TEST_LAST_NAME),
+            entry(APPLICANT_1_POSTAL_ADDRESS, null),
+            entry(APPLICANT_1_EMAIL, TEST_USER_EMAIL),
+            entry(HAS_FINANCIAL_ORDER_APPLICANT_1, false),
+            entry(APPLICANT_1_FINANCIAL_ORDER, null),
+            entry(HAS_OTHER_COURT_CASES_APPLICANT_1, false),
+            entry(APPLICANT_1_COURT_CASE_DETAILS, null),
+            entry(APPLICANT_2_FIRST_NAME, TEST_FIRST_NAME),
+            entry(APPLICANT_2_MIDDLE_NAME, TEST_MIDDLE_NAME),
+            entry(APPLICANT_2_LAST_NAME, TEST_LAST_NAME),
+            entry(APPLICANT_2_FULL_NAME, TEST_LAST_NAME),
+            entry(APPLICANT_2_POSTAL_ADDRESS, null),
+            entry(APPLICANT_2_EMAIL, TEST_USER_EMAIL),
+            entry(PLACE_OF_MARRIAGE, null),
+            entry(MARRIAGE_DATE, null),
+            entry(APPLICANT_1_HAS_ENTERED_RESPONDENTS_SOLICITOR_DETAILS, true),
             entry(APPLICANT_2_SOLICITOR_ADDRESS, solAddressWithCleanUp)
         );
 
