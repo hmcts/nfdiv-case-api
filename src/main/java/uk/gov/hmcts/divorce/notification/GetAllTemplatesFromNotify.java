039f066d9ae5887262d9f2467bf98955d3912f67
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-03-07 12:05:55 +0000

    Add new util class to get all notify email templates that match a given search criteria
    With this util class we can now search a templates values for a dynamic content variable
    or we can search the body of the template for static text.
    
    Useful for when changes are needed to notify email templates we can now find which templates
    from the list will need changing.

---
+++ src/main/java/uk/gov/hmcts/divorce/notification/GetAllTemplatesFromNotify.java
@@ -0,0 +1,103 @@
+package uk.gov.hmcts.divorce.notification;
+
+import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClientException;
+import uk.gov.service.notify.Template;
+import uk.gov.service.notify.TemplateList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// GetAllTemplatesFromNotify is a standalone util class which allows you to get all templates from Notify
+// which contain a given search criteria.
+public final class GetAllTemplatesFromNotify {
+
+    private static final String searchCriteria = "Enter search criteria here";
+    private static final String notificationType = "email";
+
+    private static final String apiKey = "";
+
+    private GetAllTemplatesFromNotify() {
+    }
+
+    public static void main(String[] args) {
+
+        NotificationClient client = getNotificationClient();
+
+        try {
+            TemplateList templates = client.getAllTemplates(notificationType);
+
+            System.out.println("Found " + templates.getTemplates().size() + " templates in total");
+
+            // Uncomment if searching template values.
+//            searchTemplateValues(templates);
+
+            // uncomment if searching template body contents.
+//            searchTemplateBody(templates);
+
+        } catch (NotificationClientException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public static NotificationClient getNotificationClient() {
+        return new NotificationClient(apiKey);
+    }
+
+    private static void searchTemplateValues(TemplateList templates) {
+
+        int count = 0;
+
+        for (Template template: templates.getTemplates()) {
+
+            Map<String, Object> personalisation = template.getPersonalisation().orElse(new HashMap<>());
+
+            if (personalisation.containsKey(searchCriteria)) {
+                System.out.println("************************************************************");
+
+                if (template.getName().contains("COR") || template.getName().contains("NOT USED") || template.getName().contains("OLD")) {
+                    System.out.println("Excluding: " + template.getName());
+                    continue;
+                }
+                printFoundTemplate(template);
+                count++;
+            }
+        }
+
+        System.out.println("\nThere were " + count + " templates found with key " + searchCriteria);
+    }
+
+    private static void searchTemplateBody(TemplateList templates) {
+
+        int count = 0;
+
+        for (Template template: templates.getTemplates()) {
+
+            String body = template.getBody();
+
+            if (body.contains(searchCriteria)) {
+                System.out.println("************************************************************");
+
+                if (template.getName().contains("COR") || template.getName().contains("NOT USED") || template.getName().contains("OLD")) {
+                    System.out.println("Excluding: " + template.getName());
+                    continue;
+                }
+                printFoundTemplate(template);
+                count++;
+            }
+        }
+
+        System.out.println("\nThere were " + count + " templates found with text '" + searchCriteria + "'");
+    }
+
+    private static void printFoundTemplate(Template template) {
+
+        System.out.println("Name: " + template.getName());
+        System.out.println("ID: " + template.getId());
+        System.out.println("Type: " + template.getTemplateType());
+
+//        Optional<String> subject = template.getSubject();
+//        subject.ifPresent(s -> System.out.println("Subject: " + s));
+//        System.out.println("Body is: " + template.getBody());
+    }
+}
