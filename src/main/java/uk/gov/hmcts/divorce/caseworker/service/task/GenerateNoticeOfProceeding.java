793285c6bb75c53dd29847ea6bd2226546d2d804
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-03-10 11:36:07 +0000

    remove unused imports

--- src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateNoticeOfProceeding.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateNoticeOfProceeding.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
