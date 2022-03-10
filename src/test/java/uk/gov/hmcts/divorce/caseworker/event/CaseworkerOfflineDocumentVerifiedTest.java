d4caa07b92f3d91831a178db0eef66b6a5fb3704
Linus Norton <linusnorton@gmail.com>
2022-03-10 09:07:34 +0000

    changes to various event and state permissions (#1515)
    * changes to various event and state permissions
    
    * clean up imports
    
    * revert change

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerOfflineDocumentVerifiedTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerOfflineDocumentVerifiedTest.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.divorce.caseworker.event.CaseworkerOfflineDocumentVerified;
 import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
