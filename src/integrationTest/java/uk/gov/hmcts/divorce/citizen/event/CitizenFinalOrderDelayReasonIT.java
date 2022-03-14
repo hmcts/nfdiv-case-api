70693ff7431f432adfcd448808997156fc30b3fb
Lara <52360278+lara-mpt@users.noreply.github.com>
2022-03-14 13:34:05 +0000

    Nfdiv-1572 updates to citizen final order notifications (#1525)
    * Initial implementation
    
    * Delete ApplyForFinalOrderFT.java
    
    * update after dev review

--- src/integrationTest/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderDelayReasonIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/citizen/event/CitizenFinalOrderDelayReasonIT.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.divorce.common;
+package uk.gov.hmcts.divorce.citizen.event;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -29,12 +29,8 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
-import static uk.gov.hmcts.divorce.common.event.ApplyForFinalOrder.FINAL_ORDER_REQUESTED;
+import static uk.gov.hmcts.divorce.citizen.event.CitizenFinalOrderDelayReason.CITIZEN_FINAL_ORDER_DELAY_REASON;
 import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_APPLIED_FOR_FINAL_ORDER;
 import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.getExpectedLocalDate;
@@ -54,12 +50,11 @@ import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SYSTEM_AUTHORISAT
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.callbackRequest;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.validApplicant2CaseData;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class ApplyForFinalOrderIT {
+public class CitizenFinalOrderDelayReasonIT {
 
     @Autowired
     private MockMvc mockMvc;
@@ -83,46 +78,7 @@ public class ApplyForFinalOrderIT {
     private NotificationService notificationService;
 
     @Test
-    void shouldReturnErrorWhenApplyForFinalOrderIsNoAndMidEventIsInvoked() throws Exception {
-
-        final CaseData caseData = caseData();
-        caseData.getFinalOrder().setDoesApplicant1WantToApplyForFinalOrder(NO);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/callbacks/mid-event?page=SolicitorApplyForFinalOrder")
-                .contentType(APPLICATION_JSON)
-                .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
-                .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
-                .content(objectMapper.writeValueAsString(callbackRequest(caseData, FINAL_ORDER_REQUESTED)))
-                .accept(APPLICATION_JSON))
-            .andDo(print())
-            .andExpect(
-                status().isOk()
-            )
-            .andExpect(jsonPath("$.errors").value("You must select 'Yes' to apply for Final Order"));
-    }
-
-    @Test
-    void shouldNotReturnErrorWhenApplyForFinalOrderIsYesAndMidEventIsInvoked() throws Exception {
-
-        final CaseData caseData = caseData();
-        caseData.getFinalOrder().setDoesApplicant1WantToApplyForFinalOrder(YES);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/callbacks/mid-event?page=SolicitorApplyForFinalOrder")
-                .contentType(APPLICATION_JSON)
-                .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
-                .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
-                .content(objectMapper.writeValueAsString(callbackRequest(caseData, FINAL_ORDER_REQUESTED)))
-                .accept(APPLICATION_JSON))
-            .andDo(print())
-            .andExpect(
-                status().isOk()
-            )
-            .andExpect(jsonPath("$.errors").doesNotExist());
-    }
-
-    @Test
-    void shouldSendEmailToApplicant1InAwaitingFinalOrderState() throws Exception {
-
+    void givenValidCaseDataWhenCallbackIsInvokedThenSendEmailToApplicant1() throws Exception {
         setMockClock(clock);
 
         when(serviceTokenGenerator.generate()).thenReturn(TEST_SERVICE_AUTH_TOKEN);
@@ -140,7 +96,7 @@ public class ApplyForFinalOrderIT {
             .contentType(APPLICATION_JSON)
             .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
             .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
-            .content(objectMapper.writeValueAsString(callbackRequest(data, FINAL_ORDER_REQUESTED, "AwaitingFinalOrder")))
+            .content(objectMapper.writeValueAsString(callbackRequest(data, CITIZEN_FINAL_ORDER_DELAY_REASON, "finalOrderOverdue")))
             .accept(APPLICATION_JSON))
             .andExpect(
                 status().isOk()
@@ -152,8 +108,7 @@ public class ApplyForFinalOrderIT {
     }
 
     @Test
-    void shouldSendEmailToApplicant2InAwaitingFinalOrderState() throws Exception {
-
+    void givenValidCaseDataWhenCallbackIsInvokedThenSendEmailToApplicant2() throws Exception {
         setMockClock(clock);
 
         when(serviceTokenGenerator.generate()).thenReturn(TEST_SERVICE_AUTH_TOKEN);
@@ -163,21 +118,20 @@ public class ApplyForFinalOrderIT {
 
         when(ccdAccessService.isApplicant1(anyString(), anyLong())).thenReturn(false);
 
-        final CaseData data = validApplicant2CaseData();
-        data.getApplicant2().setEmail(TEST_APPLICANT_2_USER_EMAIL);
+        final CaseData data = caseData();
         data.setApplicationType(ApplicationType.SOLE_APPLICATION);
         data.setFinalOrder(FinalOrder.builder().dateFinalOrderNoLongerEligible(getExpectedLocalDate().plusDays(30)).build());
+        data.getApplicant2().setEmail(TEST_APPLICANT_2_USER_EMAIL);
 
         mockMvc.perform(MockMvcRequestBuilders.post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
             .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
             .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
-            .content(objectMapper.writeValueAsString(callbackRequest(data, FINAL_ORDER_REQUESTED, "AwaitingFinalOrder")))
+            .content(objectMapper.writeValueAsString(callbackRequest(data, CITIZEN_FINAL_ORDER_DELAY_REASON, "finalOrderOverdue")))
             .accept(APPLICATION_JSON))
             .andExpect(
                 status().isOk()
             );
-
 
         verify(notificationService)
             .sendEmail(eq(TEST_APPLICANT_2_USER_EMAIL), eq(SOLE_APPLIED_FOR_FINAL_ORDER), anyMap(), eq(ENGLISH));
@@ -185,16 +139,16 @@ public class ApplyForFinalOrderIT {
     }
 
     @Test
-    void shouldNotSendEmailsToEitherApplicantInFinalOrderOverdueState() throws Exception {
+    void shouldNotSendEmailsToEitherApplicantForJointApplications() throws Exception {
         final CaseData data = caseData();
-        data.setApplicationType(ApplicationType.SOLE_APPLICATION);
+        data.setApplicationType(ApplicationType.JOINT_APPLICATION);
         data.setFinalOrder(FinalOrder.builder().dateFinalOrderNoLongerEligible(getExpectedLocalDate().minusDays(30)).build());
 
         mockMvc.perform(MockMvcRequestBuilders.post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
             .header(SERVICE_AUTHORIZATION, AUTH_HEADER_VALUE)
             .header(AUTHORIZATION, TEST_AUTHORIZATION_TOKEN)
-            .content(objectMapper.writeValueAsString(callbackRequest(data, FINAL_ORDER_REQUESTED, "finalOrderOverdue")))
+            .content(objectMapper.writeValueAsString(callbackRequest(data, CITIZEN_FINAL_ORDER_DELAY_REASON, "finalOrderOverdue")))
             .accept(APPLICATION_JSON))
             .andExpect(
                 status().isOk()
