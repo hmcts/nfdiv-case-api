eb1b95416e2945595c0cb6af0bc5d8b7048a6a46
Nitin Prabhu <prabhunitin61@gmail.com>
2022-03-16 16:02:29 +0000

    Updated condition to set applicant 2 status offline to include org policy

--- src/main/java/uk/gov/hmcts/divorce/common/service/task/SetApplicantOfflineStatus.java
+++ src/main/java/uk/gov/hmcts/divorce/common/service/task/SetApplicantOfflineStatus.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.divorce.common.service.task;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
@@ -12,12 +13,21 @@ import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 
 @Component
 public class SetApplicantOfflineStatus implements CaseTask {
+
     @Override
     public CaseDetails<CaseData, State> apply(CaseDetails<CaseData, State> details) {
-        details.getData().getApplicant1().setOffline(NO);
+        CaseData data = details.getData();
+        data.getApplicant1().setOffline(NO);
 
-        final var applicant2 = details.getData().getApplicant2();
-        final var applicant2Offline = !applicant2.isRepresented() && isBlank(applicant2.getEmail()) ? YES : NO;
+        final var applicant2 = data.getApplicant2();
+        final YesOrNo applicant2Offline;
+        if (!applicant2.isRepresented() && isBlank(applicant2.getEmail())
+            || applicant2.isRepresented() && !applicant2.getSolicitor().hasOrgId()
+        ) {
+            applicant2Offline = YES;
+        } else {
+            applicant2Offline = NO;
+        }
 
         applicant2.setOffline(applicant2Offline);
 
