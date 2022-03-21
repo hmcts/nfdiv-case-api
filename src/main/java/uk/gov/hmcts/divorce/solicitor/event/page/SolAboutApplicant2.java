18d58a8d3f6e17f1ec44d24b9b30d8d56c3df739
Nitin Prabhu <prabhunitin61@gmail.com>
2022-03-21 11:41:03 +0000

    Remove unused import

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant2.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant2.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.solicitor.event.page;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
-import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 
 public class SolAboutApplicant2 implements CcdPageConfiguration {
