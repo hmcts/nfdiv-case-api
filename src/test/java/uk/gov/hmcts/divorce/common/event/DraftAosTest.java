0266d6d0eb81d19d3c0acb796735ee8b494a2452
velimd <duzgunce.vd@gmail.com>
2022-03-23 11:21:56 +0000

    clean up test

--- src/test/java/uk/gov/hmcts/divorce/common/event/DraftAosTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/event/DraftAosTest.java
@@ -18,6 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.common.event.DraftAos.DRAFT_AOS;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AosDrafted;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingAos;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingConditionalOrder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
@@ -57,6 +59,20 @@ class DraftAosTest {
         assertThat(response.getData()).isSameAs(expectedCaseData);
 
         verify(addMiniApplicationLink).apply(caseDetails);
+    }
+
+    @Test
+    void shouldChangeTheStateAndReturnCaseDataOnAboutToStart() {
+        final CaseData expectedCaseData = CaseData.builder().build();
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(expectedCaseData);
+        caseDetails.setState(AwaitingAos);
+
+        when(addMiniApplicationLink.apply(caseDetails)).thenReturn(caseDetails);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = draftAos.aboutToStart(caseDetails);
+
+        assertThat(response.getState()).isEqualTo(AosDrafted);
     }
 
     @Test
