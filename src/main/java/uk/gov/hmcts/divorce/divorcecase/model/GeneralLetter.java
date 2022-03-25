8b377359a54f15c7eb2aa601b5e50eaca2fc3851
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-03-25 13:22:30 +0000

    updated ccd attributes

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralLetter.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralLetter.java
@@ -7,10 +7,12 @@ import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
 import java.util.List;
 
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
@@ -33,7 +35,12 @@ public class GeneralLetter {
     @CCD(label = "Recipient's address")
     private AddressGlobalUK otherRecipientAddress;
 
-    @CCD(label = "Add attachments")
+    @CCD(
+        label = "Add attachments",
+        typeOverride = Collection,
+        typeParameterOverride = "DivorceDocument",
+        access = {DefaultAccess.class}
+    )
     private List<ListValue<DivorceDocument>> generalLetterAttachments;
 
     @CCD(
