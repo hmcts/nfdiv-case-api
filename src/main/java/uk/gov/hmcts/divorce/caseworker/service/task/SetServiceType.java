e40b081b11eabec02324d47ba9208bd6282ee016
Shaun Francis <shaun.francis1@hmcts.net>
2022-03-31 14:42:43 +0100

    Create SetServiceType and SetIssueDate for issue service

---
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/SetServiceType.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.divorce.caseworker.service.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.PERSONAL_SERVICE;
+
+@Component
+@Slf4j
+public class SetServiceType implements CaseTask {
+
+    @Override
+    public CaseDetails<CaseData, State> apply(final CaseDetails<CaseData, State> details) {
+
+        final CaseData caseData = details.getData();
+        final Applicant applicant1 = caseData.getApplicant1();
+        final Applicant applicant2 = caseData.getApplicant2();
+
+        if (!applicant1.isRepresented() && !applicant2.isRepresented() && applicant2.isBasedOverseas()) {
+            caseData.getApplication().setSolServiceMethod(PERSONAL_SERVICE);
+        }
+
+        return details;
+    }
+}
