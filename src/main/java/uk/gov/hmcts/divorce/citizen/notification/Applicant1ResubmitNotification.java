01534ed3cb96eb216e1b2c4a8601bfecc9548a55
taleb <taleb.benouaer@hmcts.net>
2022-04-02 14:36:40 +0100

    nfdiv-1946: notify applicant2solicitor of applicant1 changes

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1ResubmitNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1ResubmitNotification.java
@@ -12,12 +12,18 @@ import uk.gov.hmcts.divorce.notification.NotificationService;
 
 import java.util.Map;
 
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DISSOLUTION;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DIVORCE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_DISSOLUTION_URL;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_DIVORCE_URL;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_URL;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_NAME;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SUBMISSION_RESPONSE_DATE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICANT1_APPLICANT1_CHANGES_MADE;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICANT2_APPLICANT1_CHANGES_MADE;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICANT2_SOLICITOR_APPLICANT1_CHANGES_MADE;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
 
 @Component
@@ -59,6 +65,21 @@ public class Applicant1ResubmitNotification implements ApplicantNotification {
         );
     }
 
+    @Override
+    public void sendToApplicant2Solicitor(final CaseData caseData, final Long id) {
+
+        if (!caseData.getApplicationType().isSole()) {
+            log.info("Notifying Applicant2Solicitor that Applicant 1 has made changes : {}", id);
+
+            notificationService.sendEmail(
+                caseData.getApplicant2().getSolicitor().getEmail(),
+                JOINT_APPLICANT2_SOLICITOR_APPLICANT1_CHANGES_MADE,
+                applicant2SolicitorTemplateVars(caseData, id),
+                caseData.getApplicant2().getLanguagePreference()
+            );
+        }
+    }
+
     private Map<String, String> applicant1TemplateVars(CaseData caseData, Long id) {
         Map<String, String> templateVars = resubmitTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2());
         templateVars.put(THEIR_EMAIL_ADDRESS, caseData.getApplicant2EmailAddress());
@@ -75,6 +96,17 @@ public class Applicant1ResubmitNotification implements ApplicantNotification {
     private Map<String, String> resubmitTemplateVars(CaseData caseData, Long id, Applicant applicant, Applicant partner) {
         Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, applicant, partner);
         templateVars.put(SUBMISSION_RESPONSE_DATE, caseData.getDueDate().format(DATE_TIME_FORMATTER));
+        return templateVars;
+    }
+
+    private Map<String, String> applicant2SolicitorTemplateVars(CaseData caseData, Long id) {
+        var templateVars = commonContent.basicTemplateVars(caseData, id);
+
+        templateVars.put(IS_DIVORCE, caseData.isDivorce() ? YES : NO);
+        templateVars.put(IS_DISSOLUTION, !caseData.isDivorce() ? YES : NO);
+        templateVars.put(SOLICITOR_NAME, caseData.getApplicant2().getSolicitor().getName());
+        templateVars.put(SIGN_IN_URL, commonContent.getProfessionalUsersSignInUrl());
+
         return templateVars;
     }
 }
