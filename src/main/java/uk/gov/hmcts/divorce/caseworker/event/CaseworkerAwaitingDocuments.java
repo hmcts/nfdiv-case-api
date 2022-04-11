2e82a0f2c86ee905586bdd6320c037883435c2e3
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-04-11 12:44:18 +0100

    remove unused imports

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAwaitingDocuments.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAwaitingDocuments.java
@@ -14,11 +14,9 @@ import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingHWFDecision;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPayment;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.Submitted;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
 public class CaseworkerAwaitingDocuments implements CCDConfig<CaseData, State, UserRole> {
