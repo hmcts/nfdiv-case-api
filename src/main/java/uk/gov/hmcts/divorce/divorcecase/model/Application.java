869c9b2c8098463c6754e962133fbddbcc34a22f
danielbe11 <daniel.bell@solirius.com>
2022-04-12 16:27:42 +0100

    Other code changes to add new fields

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Application.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Application.java
@@ -261,25 +261,25 @@ public class Application {
     private YesOrNo app2ContactMethodIsDigital;
 
     @CCD(
-        label = "Applicant needs to post in supporting documents",
+        label = "Supporting documents need posting?",
         access = {DefaultAccess.class}
     )
     private YesOrNo applicant1CannotUpload;
 
     @CCD(
-        label = "Outstanding documents which need to be posted by applicant",
+        label = "Which documents need posting",
         access = {DefaultAccess.class}
     )
     private Set<DocumentType> applicant1CannotUploadSupportingDocument;
 
     @CCD(
-        label = "Applicant needs to post in supporting documents",
+        label = "Supporting documents need posting?",
         access = {DefaultAccess.class, Applicant2Access.class}
     )
     private YesOrNo applicant2CannotUpload;
 
     @CCD(
-        label = "Outstanding documents which need to be posted by applicant",
+        label = "Which documents need posting",
         access = {DefaultAccess.class, Applicant2Access.class}
     )
     private Set<DocumentType> applicant2CannotUploadSupportingDocument;
