1df6cd0490a92647c6b9955b0d1fdd102cd9cb3f
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2022-04-27 08:43:17 +0100

    Added new fields to display on unassigned cases tab for CAA (#1828)

---
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerWithCAAAccess.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.divorce.divorcecase.model.access;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.ORGANISATION_CASE_ACCESS_ADMINISTRATOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.READ;
+
+public class CaseworkerWithCAAAccess implements HasAccessControl {
+
+    @Override
+    public SetMultimap<HasRole, Permission> getGrants() {
+        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
+        grants.putAll(CITIZEN, READ);
+        grants.putAll(SOLICITOR, READ);
+        grants.putAll(SUPER_USER, READ);
+
+        grants.putAll(CASE_WORKER, CREATE_READ_UPDATE);
+        grants.putAll(LEGAL_ADVISOR, CREATE_READ_UPDATE);
+        grants.putAll(SYSTEMUPDATE, CREATE_READ_UPDATE_DELETE);
+        grants.putAll(ORGANISATION_CASE_ACCESS_ADMINISTRATOR, CREATE_READ_UPDATE);
+        return grants;
+    }
+}
