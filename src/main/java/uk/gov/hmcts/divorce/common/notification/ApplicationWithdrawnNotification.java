922c638a7d625752d56d00221cdd8886a53824a2
Lara <laratollington@gmail.com>
2022-04-28 10:03:58 +0100

    notification initial implementation

---
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/ApplicationWithdrawnNotification.java
@@ -0,0 +1,64 @@
+package uk.gov.hmcts.divorce.common.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.notification.ApplicantNotification;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.CITIZEN_APPLICATION_WITHDRAWN;
+
+@Component
+@Slf4j
+public class ApplicationWithdrawnNotification implements ApplicantNotification {
+    private static final String IS_RESPONDENT = "isRespondent";
+    private static final String RESPONDENT_PARTNER = "respondentPartner";
+
+    @Autowired
+    private CommonContent commonContent;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Override
+    public void sendToApplicant1(final CaseData caseData, final Long id) {
+        log.info("Sending application withdrawn notification to applicant 1 for: {}", id);
+        final Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2());
+        templateVars.put(IS_RESPONDENT, NO);
+        templateVars.put(RESPONDENT_PARTNER, "");
+
+        notificationService.sendEmail(
+            caseData.getApplicant1().getEmail(),
+            CITIZEN_APPLICATION_WITHDRAWN,
+            templateVars,
+            caseData.getApplicant1().getLanguagePreference()
+        );
+    }
+
+    @Override
+    public void sendToApplicant2(final CaseData caseData, final Long id) {
+        log.info("Sending application withdrawn notification to applicant 2 for: {}", id);
+        final Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, caseData.getApplicant2(), caseData.getApplicant1());
+
+        if (caseData.getApplicationType().isSole()) {
+            templateVars.put(IS_RESPONDENT, YES);
+            templateVars.put(RESPONDENT_PARTNER, commonContent.getPartner(caseData, caseData.getApplicant2()));
+        } else {
+            templateVars.put(IS_RESPONDENT, NO);
+            templateVars.put(RESPONDENT_PARTNER, "");
+        }
+
+        notificationService.sendEmail(
+            caseData.getApplicant2EmailAddress(),
+            CITIZEN_APPLICATION_WITHDRAWN,
+            templateVars,
+            caseData.getApplicant1().getLanguagePreference()
+        );
+    }
+}
