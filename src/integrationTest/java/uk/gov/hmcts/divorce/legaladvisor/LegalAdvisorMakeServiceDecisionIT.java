eea278fe1f9a83164f02bfb6b84413eeb805021d
danielbe11 <daniel.bell@solirius.com>
2022-04-29 09:08:58 +0100

    Change NFD_Refusal_Order_Deemed_Dispensed_Service reference

--- src/integrationTest/java/uk/gov/hmcts/divorce/legaladvisor/LegalAdvisorMakeServiceDecisionIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/legaladvisor/LegalAdvisorMakeServiceDecisionIT.java
@@ -76,7 +76,7 @@ public class LegalAdvisorMakeServiceDecisionIT {
 
     private static final String UUID = "5cd725e8-f053-4493-9cbe-bb69d1905ae3";
     private static final String SERVICE_ORDER_TEMPLATE_FILE = "NFD_Service_Order_V2.docx";
-    private static final String SERVICE_ORDER_REFUSAL_TEMPLATE_FILE = "NFD_Refusal_Order_Deemed_Dispensed_Service.docx";
+    private static final String SERVICE_ORDER_REFUSAL_TEMPLATE_FILE = "NFD_Refusal_Order_Deemed_Dispensed_Service_V2.docx";
 
     @Autowired
     private MockMvc mockMvc;
