d7f011ed76c51d69dc85c20cad9b075e0b0d63d9
Nitin Prabhu <prabhunitin61@gmail.com>
2022-05-05 09:58:18 +0100

    Add show condition on scanned documents and not on state transition dropdown

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUploadDocument.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUploadDocument.java
@@ -41,6 +41,7 @@ public class CaseworkerUploadDocument implements CCDConfig<CaseData, State, User
             .pageLabel("Upload document")
             .complex(CaseData::getDocuments)
                 .optional(CaseDocuments::getDocumentsUploaded)
+                .optional(CaseDocuments::getScannedDocuments)
                 .done();
     }
 
