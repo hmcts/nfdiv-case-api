fce30b5f6910d9bb57c52a9a984dfaa782234b06
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-05-11 10:59:32 +0100

    NFDIV-1867 : initial commit

---
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/RespondentSolicitorReminderNotification.java
@@ -0,0 +1,65 @@
+package uk.gov.hmcts.divorce.citizen.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.notification.ApplicantNotification;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import java.util.Map;
+
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.ISSUE_DATE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.NOT_PROVIDED;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DISSOLUTION;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DIVORCE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_URL;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SUBMISSION_RESPONSE_DATE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.RESPONDENT_SOLICITOR_HAS_NOT_RESPONDED;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
+
+@Slf4j
+@Component
+public class RespondentSolicitorReminderNotification implements ApplicantNotification {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @Autowired
+    private CommonContent commonContent;
+
+    @Override
+    public void sendToApplicant2Solicitor(CaseData caseData, Long caseId) {
+        log.info("Sending the respondent solicitor has not responded notification to the solicitor for case : {}", caseId);
+
+        notificationService.sendEmail(
+            caseData.getApplicant2().getSolicitor().getEmail(),
+            RESPONDENT_SOLICITOR_HAS_NOT_RESPONDED,
+            respondentSolicitorTemplateVars(caseData, caseId),
+            caseData.getApplicant2().getLanguagePreference()
+        );
+    }
+
+    private Map<String, String> respondentSolicitorTemplateVars(CaseData caseData, Long id) {
+        var templateVars = commonContent.basicTemplateVars(caseData, id);
+
+        templateVars.put(ISSUE_DATE, caseData.getApplication().getIssueDate().format(DATE_TIME_FORMATTER));
+        templateVars.put(SUBMISSION_RESPONSE_DATE, caseData.getDueDate().format(DATE_TIME_FORMATTER));
+        templateVars.put(IS_DIVORCE, caseData.isDivorce() ? YES : NO);
+        templateVars.put(IS_DISSOLUTION, !caseData.isDivorce() ? YES : NO);
+        templateVars.put(SOLICITOR_NAME, caseData.getApplicant2().getSolicitor().getName());
+        templateVars.put(SOLICITOR_REFERENCE,
+            isNotEmpty(caseData.getApplicant1().getSolicitor().getReference())
+                ? caseData.getApplicant1().getSolicitor().getReference()
+                : NOT_PROVIDED);
+        templateVars.put(SIGN_IN_URL, commonContent.getProfessionalUsersSignInUrl(id));
+
+        return templateVars;
+    }
+}
