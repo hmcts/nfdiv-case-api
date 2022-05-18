f9de3a749110f4166550557e963d7ff7d1e6f1d0
danielbe11 <daniel.bell@solirius.com>
2022-05-18 13:22:15 +0100

    Add unit tests

--- src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
@@ -7,6 +7,7 @@ import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 import java.util.HashMap;
 import java.util.List;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.withEnvironmentVariable;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.PERSONAL_SERVICE;
@@ -58,6 +59,77 @@ class RetiredFieldsTest {
                 .build()
             )),
             entry("d11Document", null)
+        );
+    }
+
+    @Test
+    void shouldMigrateSOTWhenEnvFlagIsTrueAndDataIsAppropriate() throws Exception {
+        final var data = new HashMap<String, Object>();
+        data.put("applicationType", "soleApplication");
+        data.put("applicant2StatementOfTruth", "Yes");
+
+        final var result = withEnvironmentVariable("SOT_MIGRATION_ENABLED", "true")
+            .execute(() -> RetiredFields.migrate(data));
+
+        assertThat(result).contains(
+            entry("applicationType", "soleApplication"),
+            entry("applicant2StatementOfTruth", null),
+            entry("statementOfTruth", "Yes")
+        );
+    }
+
+    @Test
+    void shouldNotMigrateSOTWhenEnvFlagIsFalse() throws Exception {
+        final var data = new HashMap<String, Object>();
+        data.put("applicationType", "soleApplication");
+        data.put("applicant2StatementOfTruth", "Yes");
+
+        final var result = withEnvironmentVariable("SOT_MIGRATION_ENABLED", "false")
+            .execute(() -> RetiredFields.migrate(data));
+
+        assertThat(result).contains(
+            entry("applicationType", "soleApplication"),
+            entry("applicant2StatementOfTruth", "Yes")
+        );
+
+        assertThat(result).doesNotContain(
+            entry("statementOfTruth", "Yes")
+        );
+    }
+
+    @Test
+    void shouldNotMigrateSOTWhenEnvFlagIsTrueAndApplicationIsJoint() throws Exception {
+        final var data = new HashMap<String, Object>();
+        data.put("applicationType", "jointApplication");
+        data.put("applicant2StatementOfTruth", "Yes");
+
+        final var result = withEnvironmentVariable("SOT_MIGRATION_ENABLED", "true")
+            .execute(() -> RetiredFields.migrate(data));
+
+        assertThat(result).contains(
+            entry("applicationType", "jointApplication"),
+            entry("applicant2StatementOfTruth", "Yes")
+        );
+
+        assertThat(result).doesNotContain(
+            entry("statementOfTruth", "Yes")
+        );
+    }
+
+    @Test
+    void shouldNotMigrateSOTWhenEnvFlagIsTrueAndApplicant2StatementOfTruthNotSet() throws Exception {
+        final var data = new HashMap<String, Object>();
+        data.put("applicationType", "soleApplication");
+
+        final var result = withEnvironmentVariable("SOT_MIGRATION_ENABLED", "true")
+            .execute(() -> RetiredFields.migrate(data));
+
+        assertThat(result).contains(
+            entry("applicationType", "soleApplication")
+        );
+
+        assertThat(result).doesNotContain(
+            entry("statementOfTruth", "Yes")
         );
     }
 }
