7cadc0de4e48c4a926aa5f3c94f3c99aaa391a79
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-05-23 13:01:51 +0100

    changes

--- src/main/java/uk/gov/hmcts/divorce/common/service/task/SetStateAfterSubmission.java
+++ src/main/java/uk/gov/hmcts/divorce/common/service/task/SetStateAfterSubmission.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.common.service.task;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
