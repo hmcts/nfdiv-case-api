a983fc9e9f93deaa3fa1c0e8da3b9c87077c386b
Tom Elliott <tomelliott@Toms-MBP.home>
2022-05-23 16:26:24 +0100

    NFDIV-1266: Add Solicitor Certificate of Entitlement Email notifications

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/EntitlementGrantedConditionalOrderNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/EntitlementGrantedConditionalOrderNotification.java
@@ -13,15 +13,27 @@ import uk.gov.hmcts.divorce.notification.NotificationService;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 
+import static java.lang.String.join;
 import static java.time.temporal.ChronoUnit.DAYS;
+import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICANT_NAME;
 import static uk.gov.hmcts.divorce.notification.CommonContent.COURT_NAME;
 import static uk.gov.hmcts.divorce.notification.CommonContent.CO_PRONOUNCEMENT_DATE_PLUS_43;
 import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_OF_HEARING;
 import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_OF_HEARING_MINUS_SEVEN_DAYS;
+import static uk.gov.hmcts.divorce.notification.CommonContent.ISSUE_DATE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_JOINT;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_SOLE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
+import static uk.gov.hmcts.divorce.notification.CommonContent.RESPONDENT_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_REFERENCE;
 import static uk.gov.hmcts.divorce.notification.CommonContent.TIME_OF_HEARING;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.CITIZEN_CONDITIONAL_ORDER_ENTITLEMENT_GRANTED;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_RESPONDENT_CONDITIONAL_ORDER_ENTITLEMENT_GRANTED;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_CONDITIONAL_ORDER_ENTITLEMENT_GRANTED;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.TIME_FORMATTER;
 
@@ -47,6 +59,17 @@ public class EntitlementGrantedConditionalOrderNotification implements Applicant
     }
 
     @Override
+    public void sendToApplicant1Solicitor(final CaseData caseData, final Long id) {
+        log.info("Sending entitlement granted on conditional order notification to applicant 1 solicitor for case : {}", id);
+
+        notificationService.sendEmail(
+            caseData.getApplicant1().getSolicitor().getEmail(),
+            SOLICITOR_CONDITIONAL_ORDER_ENTITLEMENT_GRANTED,
+            templateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
+            caseData.getApplicant1().getLanguagePreference());
+    }
+
+    @Override
     public void sendToApplicant2(final CaseData caseData, final Long id) {
         log.info("Sending entitlement granted on conditional order notification to applicant 2 for case : {}", id);
 
@@ -61,17 +84,42 @@ public class EntitlementGrantedConditionalOrderNotification implements Applicant
             caseData.getApplicant2().getLanguagePreference());
     }
 
+    @Override
+    public void sendToApplicant2Solicitor(final CaseData caseData, final Long id) {
+        log.info("Sending entitlement granted on conditional order notification to applicant 2 solicitor for case : {}", id);
+
+        notificationService.sendEmail(
+            caseData.getApplicant2().getSolicitor().getEmail(),
+            SOLICITOR_CONDITIONAL_ORDER_ENTITLEMENT_GRANTED,
+            templateVars(caseData, id, caseData.getApplicant2(), caseData.getApplicant1()),
+            caseData.getApplicant2().getLanguagePreference());
+    }
+
     private Map<String, String> templateVars(CaseData caseData, Long id, Applicant applicant, Applicant partner) {
         Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, applicant, partner);
 
         final ConditionalOrder conditionalOrder = caseData.getConditionalOrder();
         final LocalDateTime dateAndTimeOfHearing = conditionalOrder.getDateAndTimeOfHearing();
 
+        templateVars.put(IS_SOLE, caseData.getApplicationType().isSole() ? YES : NO);
+        templateVars.put(IS_JOINT, !caseData.getApplicationType().isSole() ? YES : NO);
+        templateVars.put(ISSUE_DATE, caseData.getApplication().getIssueDate().format(DATE_TIME_FORMATTER));
+
         templateVars.put(COURT_NAME, conditionalOrder.getCourt().getLabel());
         templateVars.put(DATE_OF_HEARING, dateAndTimeOfHearing.format(DATE_TIME_FORMATTER));
         templateVars.put(TIME_OF_HEARING, dateAndTimeOfHearing.format(TIME_FORMATTER));
         templateVars.put(CO_PRONOUNCEMENT_DATE_PLUS_43, dateAndTimeOfHearing.plusDays(43).format(DATE_TIME_FORMATTER));
         templateVars.put(DATE_OF_HEARING_MINUS_SEVEN_DAYS, dateAndTimeOfHearing.minus(7, DAYS).format(DATE_TIME_FORMATTER));
+
+        if (applicant.isRepresented()) {
+            templateVars.put(APPLICANT_NAME, join(" ", caseData.getApplicant1().getFirstName(), caseData.getApplicant1().getLastName()));
+            templateVars.put(RESPONDENT_NAME, join(" ", caseData.getApplicant2().getFirstName(), caseData.getApplicant2().getLastName()));
+
+            templateVars.put(SOLICITOR_NAME, applicant.getSolicitor().getName());
+            templateVars.put(SOLICITOR_REFERENCE, Objects.nonNull(applicant.getSolicitor().getReference())
+                ? applicant.getSolicitor().getReference()
+                : "not provided");
+        }
 
         return templateVars;
     }
