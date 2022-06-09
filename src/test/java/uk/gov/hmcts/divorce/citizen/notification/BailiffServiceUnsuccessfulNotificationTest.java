d54c3a91515a5676a8dac0441798e875419f329f
Nitin Prabhu <prabhunitin61@gmail.com>
2022-06-09 14:14:30 +0100

    Added welsh translation for unsuccessful bailiff application

--- src/test/java/uk/gov/hmcts/divorce/citizen/notification/BailiffServiceUnsuccessfulNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/notification/BailiffServiceUnsuccessfulNotificationTest.java
@@ -17,12 +17,14 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
 import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICATION_REFERENCE;
 import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DISSOLUTION;
 import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DIVORCE;
 import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
-import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
+import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.BAILIFF_SERVICE_UNSUCCESSFUL;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
@@ -54,7 +56,7 @@ class BailiffServiceUnsuccessfulNotificationTest {
             eq(BAILIFF_SERVICE_UNSUCCESSFUL),
             argThat(allOf(
                 hasEntry(APPLICATION_REFERENCE, formatId(1234567890123456L)),
-                hasEntry(IS_DIVORCE, YES),
+                hasEntry(IS_DIVORCE, CommonContent.YES),
                 hasEntry(IS_DISSOLUTION, NO)
             )),
             eq(ENGLISH)
@@ -63,10 +65,36 @@ class BailiffServiceUnsuccessfulNotificationTest {
     }
 
     @Test
+    void shouldSendNotificationToApplicantWithDivorceContentWhenLanguagePrefIsWelsh() {
+        CaseData caseData = validApplicant1CaseData();
+        caseData.getApplicant1().setLanguagePreferenceWelsh(YES);
+
+        when(commonContent.mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getPartnerWelshContent(caseData, caseData.getApplicant2())).thenReturn("gŵr");
+
+        notification.sendToApplicant1(caseData, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(BAILIFF_SERVICE_UNSUCCESSFUL),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, formatId(1234567890123456L)),
+                hasEntry(IS_DIVORCE, CommonContent.YES),
+                hasEntry(IS_DISSOLUTION, NO),
+                hasEntry(PARTNER, "gŵr")
+            )),
+            eq(WELSH)
+        );
+        verify(commonContent).mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2());
+    }
+
+    @Test
     void shouldSendNotificationToApplicantWithDissolutionContent() {
         CaseData caseData = validApplicant1CaseData();
         Map<String, String> templateVars = getMainTemplateVars();
-        templateVars.put(IS_DISSOLUTION, YES);
+        templateVars.put(IS_DISSOLUTION, CommonContent.YES);
         templateVars.put(IS_DIVORCE, NO);
         when(commonContent.mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2()))
             .thenReturn(templateVars);
@@ -79,9 +107,39 @@ class BailiffServiceUnsuccessfulNotificationTest {
             argThat(allOf(
                 hasEntry(APPLICATION_REFERENCE, formatId(1234567890123456L)),
                 hasEntry(IS_DIVORCE, NO),
-                hasEntry(IS_DISSOLUTION, YES)
+                hasEntry(IS_DISSOLUTION, CommonContent.YES)
             )),
             eq(ENGLISH)
+        );
+        verify(commonContent).mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2());
+    }
+
+    @Test
+    void shouldSendNotificationToApplicantWithDissolutionContentWhenLangPrefIsWelsh() {
+        CaseData caseData = validApplicant1CaseData();
+        caseData.getApplicant1().setLanguagePreferenceWelsh(YES);
+
+        Map<String, String> templateVars = getMainTemplateVars();
+        templateVars.put(IS_DISSOLUTION, CommonContent.YES);
+        templateVars.put(IS_DIVORCE, NO);
+
+        when(commonContent.mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(templateVars);
+
+        when(commonContent.getPartnerWelshContent(caseData, caseData.getApplicant2())).thenReturn("partner sifil");
+
+        notification.sendToApplicant1(caseData, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(BAILIFF_SERVICE_UNSUCCESSFUL),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, formatId(1234567890123456L)),
+                hasEntry(IS_DIVORCE, NO),
+                hasEntry(IS_DISSOLUTION, CommonContent.YES),
+                hasEntry(PARTNER, "partner sifil")
+            )),
+            eq(WELSH)
         );
         verify(commonContent).mainTemplateVars(caseData, 1234567890123456L, caseData.getApplicant1(), caseData.getApplicant2());
     }
