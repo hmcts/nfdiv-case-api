a66b3f44838fe2bbba77076ee1c29f81cdd1140a
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-06-09 12:56:50 +0100

    NFDIV-1984 : added Welsh notification to applicant when dispute form not submitted

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/DisputeFormOverdueOverdueNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/DisputeFormOverdueOverdueNotification.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
 import java.util.Map;
 
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
+import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SUBMISSION_RESPONSE_DATE;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.DISPUTE_FORM_OVERDUE;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
@@ -32,11 +35,17 @@ public class DisputeFormOverdueOverdueNotification implements ApplicantNotificat
             commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2());
         templateVars.put(SUBMISSION_RESPONSE_DATE, caseData.getDueDate().format(DATE_TIME_FORMATTER));
 
+        LanguagePreference languagePreference = caseData.getApplicant1().getLanguagePreference();
+
+        if (WELSH.equals(languagePreference)) {
+            templateVars.put(PARTNER, commonContent.getPartnerWelshContent(caseData, caseData.getApplicant2()));
+        }
+
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             DISPUTE_FORM_OVERDUE,
             templateVars,
-            caseData.getApplicant1().getLanguagePreference()
+            languagePreference
         );
     }
 }
--- src/test/java/uk/gov/hmcts/divorce/citizen/notification/DisputeFormOverdueOverdueNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/notification/DisputeFormOverdueOverdueNotificationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
@@ -16,6 +17,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
 import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
+import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SUBMISSION_RESPONSE_DATE;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.DISPUTE_FORM_OVERDUE;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
@@ -37,7 +40,7 @@ class DisputeFormOverdueOverdueNotificationTest {
     private DisputeFormOverdueOverdueNotification notification;
 
     @Test
-    void shouldSendEmailToApplicant2WhileInAwaitingApplicant2ResponseState() {
+    void shouldSendEmailToApplicant1WhileInAwaitingApplicant2ResponseState() {
         CaseData data = caseData();
         data.setDueDate(LOCAL_DATE);
         when(commonContent.mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2()))
@@ -54,5 +57,31 @@ class DisputeFormOverdueOverdueNotificationTest {
             eq(ENGLISH)
         );
         verify(commonContent).mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2());
+    }
+
+    @Test
+    void shouldSendWelshEmailToApplicant1WhileInAwaitingApplicant2ResponseState() {
+        CaseData data = caseData();
+        data.setDueDate(LOCAL_DATE);
+        data.getApplicant1().setLanguagePreferenceWelsh(YesOrNo.YES);
+        when(commonContent.mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getPartnerWelshContent(data, data.getApplicant2())).thenReturn("gŵr");
+
+        notification.sendToApplicant1(data, 1234567890123456L);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(DISPUTE_FORM_OVERDUE),
+            argThat(allOf(
+                hasEntry(SUBMISSION_RESPONSE_DATE, LOCAL_DATE.format(DATE_TIME_FORMATTER)),
+                hasEntry(PARTNER, "gŵr")
+            )),
+            eq(WELSH)
+        );
+
+        verify(commonContent).mainTemplateVars(data, 1234567890123456L, data.getApplicant1(), data.getApplicant2());
+        verify(commonContent).getPartnerWelshContent(data, data.getApplicant2());
     }
 }
