c02cb4656dc01ced33684887ea4489c76f4215c4
Linus Norton <linusnorton@gmail.com>
2022-06-10 08:25:47 +0100

    keep PMD happy

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DateAndHour.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DateAndHour.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
+import lombok.Data;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 import java.time.LocalDate;
 
 @CCD
+@Data
 public class DateAndHour {
+    @CCD(label = "date")
     private LocalDate date;
+    @CCD(label = "hour")
     private String hour;
 }
