64aa1ef517128f4ff4382eebc0f869e329523615
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-06-13 14:05:53 +0100

    remove unused imports

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant2SwitchToSoleNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant2SwitchToSoleNotification.java
@@ -8,8 +8,6 @@ import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
-import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
-import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.APPLICANT_SWITCH_TO_SOLE;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICATION_ENDED;
 
