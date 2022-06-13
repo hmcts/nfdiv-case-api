6851701215370b6949884f385a702bdc250eed2c
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-06-13 08:59:04 +0100

    add welsh template var and rework getPartner logic

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1SwitchToSoleNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1SwitchToSoleNotification.java
@@ -9,8 +9,6 @@ import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
-import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
-import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.APPLICANT_SWITCH_TO_SOLE;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.JOINT_APPLICATION_ENDED;
 
@@ -28,15 +26,10 @@ public class Applicant1SwitchToSoleNotification implements ApplicantNotification
     public void sendToApplicant1(final CaseData caseData, final Long id) {
         log.info("Sending applicant 1 switch to sole notification to applicant 1 for case : {}", id);
 
-        final var templateContent = commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2());
-        if (WELSH.equals(caseData.getApplicant1().getLanguagePreference())) {
-            templateContent.put(PARTNER, commonContent.getPartnerWelshContent(caseData, caseData.getApplicant2()));
-        }
-
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             APPLICANT_SWITCH_TO_SOLE,
-            templateContent,
+            commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
             caseData.getApplicant1().getLanguagePreference()
         );
     }
