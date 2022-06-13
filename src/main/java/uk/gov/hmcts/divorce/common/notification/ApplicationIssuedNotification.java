42399452af3148a9307bcc6ab61959034f961344
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-06-13 16:05:20 +0100

    removed unused import

--- src/main/java/uk/gov/hmcts/divorce/common/notification/ApplicationIssuedNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/ApplicationIssuedNotification.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.divorce.common.notification;
 
-import io.cucumber.java.jv.Lan;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
