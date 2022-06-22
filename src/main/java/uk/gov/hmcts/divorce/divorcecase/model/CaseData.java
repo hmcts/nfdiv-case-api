9ca5672f064069be229b74bfc724b1cbbd8484ce
Linus Norton <linusnorton@gmail.com>
2022-06-22 12:50:59 +0100

    example change

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.OrderSummary;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.caseworker.model.CaseNote;
-import uk.gov.hmcts.divorce.divorcecase.model.access.Applicant2Access;
 import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAccess;
 import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAccessOnlyAccess;
 import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAndSuperUserAccess;
@@ -78,7 +77,7 @@ public class CaseData {
 
     @JsonUnwrapped(prefix = "applicant2")
     @Builder.Default
-    @CCD(access = {DefaultAccess.class, Applicant2Access.class})
+    @CCD(access = {DefaultAccess.class})
     private Applicant applicant2 = new Applicant();
 
     @JsonUnwrapped()
