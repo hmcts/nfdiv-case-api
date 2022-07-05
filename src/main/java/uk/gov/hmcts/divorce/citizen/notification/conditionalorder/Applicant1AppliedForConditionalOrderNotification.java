c5d3b3f69f99f5dc494275d0c45e5b6f644eca30
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-07-05 09:29:14 +0100

    updated as per review comments

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/Applicant1AppliedForConditionalOrderNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/conditionalorder/Applicant1AppliedForConditionalOrderNotification.java
@@ -25,44 +25,49 @@ public class Applicant1AppliedForConditionalOrderNotification
     private NotificationService notificationService;
 
     @Override
-    public void sendToApplicant1(final CaseData caseData, final Long id) {
+    public void sendToApplicant1(final CaseData caseData, final Long caseId) {
 
         EmailTemplateName templateName;
 
         if (caseData.getApplicationType().isSole()) {
-            log.info("Notifying applicant that their conditional order application has been submitted: {}", id);
             templateName = CITIZEN_APPLIED_FOR_CONDITIONAL_ORDER;
+            log.info("Notifying applicant that their conditional order application has been submitted for case: {}, with template: {}",
+                caseId, templateName);
         } else if (alreadyApplied(caseData, APPLICANT2)) {
-            log.info("Notifying applicant 1 that both applicants has submitted their conditional order application: {}", id);
             templateName = JOINT_BOTH_APPLIED_FOR_CONDITIONAL_ORDER;
+            log.info("Notifying applicant 1 that both applicants have submitted their conditional order applications for case: {}, "
+                + "with template: {}", caseId, templateName);
         } else {
-            log.info("Notifying applicant 1 that their conditional order application has been submitted: {}", id);
             templateName = JOINT_APPLIED_FOR_CONDITIONAL_ORDER;
+            log.info("Notifying applicant 1 that their conditional order application has been submitted for case: {}, with template: {}",
+                caseId, templateName);
         }
 
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             templateName,
-            templateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2(), APPLICANT1),
+            templateVars(caseData, caseId, caseData.getApplicant1(), caseData.getApplicant2(), APPLICANT1),
             caseData.getApplicant1().getLanguagePreference()
         );
     }
 
     @Override
-    public void sendToApplicant2(final CaseData caseData, final Long id) {
+    public void sendToApplicant2(final CaseData caseData, final Long caseId) {
         if (!caseData.getApplicationType().isSole()) {
 
             EmailTemplateName templateName;
             Map<String, String> templateMap;
 
             if (alreadyApplied(caseData, APPLICANT2)) {
-                log.info("Notifying applicant 2 that both applicants has submitted their conditional order application: {}", id);
                 templateName = JOINT_BOTH_APPLIED_FOR_CONDITIONAL_ORDER;
-                templateMap = templateVars(caseData, id, caseData.getApplicant2(), caseData.getApplicant1(), APPLICANT1);
+                templateMap = templateVars(caseData, caseId, caseData.getApplicant2(), caseData.getApplicant1(), APPLICANT1);
+                log.info("Notifying applicant 2 that both applicants have submitted their conditional order applications for case: {}, "
+                    + "with template: {}", caseId, templateName);
             } else {
-                log.info("Notifying applicant 2 that their partner has submitted a conditional order application: {}", id);
                 templateName = JOINT_PARTNER_APPLIED_FOR_CONDITIONAL_ORDER;
-                templateMap = partnerTemplateVars(caseData, id, caseData.getApplicant2(), caseData.getApplicant1(), APPLICANT1);
+                templateMap = partnerTemplateVars(caseData, caseId, caseData.getApplicant2(), caseData.getApplicant1(), APPLICANT1);
+                log.info("Notifying applicant 2 that their partner has submitted a conditional order application for case: {}, "
+                    + "with template: {}", caseId, templateName);
             }
 
             notificationService.sendEmail(
