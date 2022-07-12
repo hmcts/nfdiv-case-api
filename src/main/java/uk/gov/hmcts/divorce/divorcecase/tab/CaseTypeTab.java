610694e66732b9b58faa3dc3e8aa113cd53f5ef1
Nitin Prabhu <prabhunitin61@gmail.com>
2022-07-12 13:11:39 +0100

    Add missing field on payment tab

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
@@ -118,6 +118,7 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
             .forRoles(CASE_WORKER, LEGAL_ADVISOR, SUPER_USER)
             .label("LabelApplicant1-PaymentHeading", IS_JOINT, "### The applicant")
             .field("applicant2HWFNeedHelp", NEVER_SHOW)
+            .field("applicationType", NEVER_SHOW)
             .field("applicant1HWFReferenceNumber", SOLE_APPLICATION_OR_APPLICATION_TYPE_JOINT_APPLICATION_AND_APPLICANT_2_HWFNEED_HELP_YES)
             .label("LabelApplicant2-PaymentHeading", IS_JOINT_AND_HWF_ENTERED, "### ${labelContentTheApplicant2UC}")
             .field("applicant2HWFReferenceNumber", IS_JOINT_AND_HWF_ENTERED)
