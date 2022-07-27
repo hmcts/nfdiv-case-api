db49df041eacf389934fb44f95be7a56ae80751c
Sunil Natikar <98022094+sunilnatikar@users.noreply.github.com>
2022-07-27 14:42:58 +0100

    NFDIV-2390: Corrected confidential documents flow with bulk print issue (#2108)
    * NFDIV-2390 : corrected confidential documents flow
    
    * removed unneccessory logs
    
    * NFDIV-2390 : updated as per review comments
    
    * removed unused import

--- src/main/java/uk/gov/hmcts/divorce/common/service/ProcessConfidentialDocumentsService.java
+++ src/main/java/uk/gov/hmcts/divorce/common/service/ProcessConfidentialDocumentsService.java
@@ -7,16 +7,20 @@ import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments;
+import uk.gov.hmcts.divorce.divorcecase.model.GeneralParties;
 import uk.gov.hmcts.divorce.document.model.ConfidentialDivorceDocument;
-import uk.gov.hmcts.divorce.document.model.ConfidentialDocumentsReceived;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
+import uk.gov.hmcts.divorce.document.model.DocumentType;
 
 import java.util.Collection;
 import java.util.List;
 
 import static java.util.stream.Stream.ofNullable;
 import static uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments.addDocumentToTop;
-import static uk.gov.hmcts.divorce.document.DocumentUtil.isApplicableForConfidentiality;
+import static uk.gov.hmcts.divorce.divorcecase.model.GeneralParties.APPLICANT;
+import static uk.gov.hmcts.divorce.divorcecase.model.GeneralParties.RESPONDENT;
+import static uk.gov.hmcts.divorce.document.DocumentUtil.getConfidentialDocumentType;
+import static uk.gov.hmcts.divorce.document.DocumentUtil.isDocumentApplicableForConfidentiality;
 
 @Service
 @Slf4j
@@ -34,17 +38,19 @@ public class ProcessConfidentialDocumentsService {
     public void processDocuments(final CaseData caseData, final Applicant applicant,
                                                          final boolean isApplicant1) {
         if (applicant.isConfidentialContactDetails()) {
-            moveToConfidentialDocumentsTab(caseData.getDocuments(), isApplicant1);
+            moveToConfidentialDocumentsTab(caseData, isApplicant1);
         }
     }
 
-    private void moveToConfidentialDocumentsTab(final CaseDocuments caseDocuments, final Boolean isApplicant1) {
+    private void moveToConfidentialDocumentsTab(final CaseData caseData, final Boolean isApplicant1) {
         log.info("Moving documents to confidential list for : applicant {}", isApplicant1 ? "1" : "2");
+
+        CaseDocuments caseDocuments = caseData.getDocuments();
 
         List<ListValue<DivorceDocument>> documentsToMove
             = ofNullable(caseDocuments.getDocumentsGenerated())
             .flatMap(Collection::stream)
-            .filter(document -> isApplicableForConfidentiality(document.getValue().getDocumentType(), isApplicant1))
+            .filter(document -> filterDocumentBasedOnConfidentialityForGivenApplicant(caseData, document.getValue(), isApplicant1))
             .toList();
 
         if (!CollectionUtils.isEmpty(documentsToMove)) {
@@ -52,7 +58,7 @@ public class ProcessConfidentialDocumentsService {
             documentsToMove.forEach(documentListValue ->
                 caseDocuments.setConfidentialDocumentsGenerated(addDocumentToTop(
                     caseDocuments.getConfidentialDocumentsGenerated(),
-                    mapToConfidentialDivorceDocument(documentListValue.getValue(), isApplicant1),
+                    mapToConfidentialDivorceDocument(documentListValue.getValue()),
                     documentListValue.getId()
                 )));
 
@@ -60,16 +66,38 @@ public class ProcessConfidentialDocumentsService {
         }
     }
 
-    private ConfidentialDivorceDocument mapToConfidentialDivorceDocument(final DivorceDocument divorceDocument,
-                                                                         final boolean isApplicant1) {
+    private ConfidentialDivorceDocument mapToConfidentialDivorceDocument(final DivorceDocument divorceDocument) {
         return ConfidentialDivorceDocument.builder()
             .documentLink(divorceDocument.getDocumentLink())
             .documentComment(divorceDocument.getDocumentComment())
             .documentFileName(divorceDocument.getDocumentFileName())
             .documentDateAdded(divorceDocument.getDocumentDateAdded())
             .documentEmailContent(divorceDocument.getDocumentEmailContent())
-            .confidentialDocumentsReceived(isApplicant1 ? ConfidentialDocumentsReceived.NOTICE_OF_PROCEEDINGS_APP_1
-                : ConfidentialDocumentsReceived.NOTICE_OF_PROCEEDINGS_APP_2)
+            .confidentialDocumentsReceived(getConfidentialDocumentType(divorceDocument.getDocumentType()))
             .build();
     }
+
+    private boolean filterDocumentBasedOnConfidentialityForGivenApplicant(final CaseData caseData, final DivorceDocument document,
+                                                                          final boolean isApplicant1) {
+        boolean applicable = isDocumentApplicableForConfidentiality(document.getDocumentType(), isApplicant1);
+
+        if (applicable && DocumentType.GENERAL_LETTER.equals(document.getDocumentType())) {
+            return generalLetterBelongsToGivenApplicant(caseData, document, isApplicant1);
+        }
+
+        return applicable;
+    }
+
+    private boolean generalLetterBelongsToGivenApplicant(final CaseData caseData, final DivorceDocument generalLetterDocument,
+                                                               final boolean isApplicant1) {
+
+        GeneralParties party = isApplicant1 ? APPLICANT : RESPONDENT;
+
+        return ofNullable(caseData.getGeneralLetters())
+            .flatMap(Collection::stream)
+            .map(ListValue::getValue)
+            .anyMatch(generalLetterDetail -> party.equals(generalLetterDetail.getGeneralLetterParties())
+                && generalLetterDetail.getGeneralLetterLink().getUrl().equals(generalLetterDocument.getDocumentLink().getUrl()));
+    }
+
 }
