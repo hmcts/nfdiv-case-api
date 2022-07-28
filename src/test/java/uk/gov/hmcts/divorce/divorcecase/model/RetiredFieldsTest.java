13f85b4e2175b44f44eb306521093c84fa9ff999
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-07-28 20:47:17 +0100

    remove unused imports

--- src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/model/RetiredFieldsTest.java
@@ -8,7 +8,6 @@ import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
