a0e862a6c64d49dbb0d409d16d333bdab184d38f
Tom Elliott <tomelliott@Toms-MBP.home>
2022-07-28 12:33:39 +0100

    NFDIV-1743: Remove print ln

--- src/functionalTest/java/uk/gov/hmcts/divorce/systemupdate/event/SystemUpdateCaseWithCourtHearingFT.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/systemupdate/event/SystemUpdateCaseWithCourtHearingFT.java
@@ -88,8 +88,6 @@ public class SystemUpdateCaseWithCourtHearingFT extends FunctionalTestSuite {
 
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
 
-        System.out.println(response.asString());
-
         assertThatJson(response.asString())
             .when(IGNORING_EXTRA_FIELDS)
             .when(IGNORING_ARRAY_ORDER)
