c248ffb184beede28ead68ef0b6d4c8c3325463b
Tom Elliott <tomelliott@Toms-MBP.home>
2022-07-29 10:34:35 +0100

    NFDIV-2422: Move common html into static method

--- src/main/java/uk/gov/hmcts/divorce/common/event/page/WithdrawingJointApplicationApplicant2.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/page/WithdrawingJointApplicationApplicant2.java
@@ -6,6 +6,10 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderQuestions;
 
+import static uk.gov.hmcts.divorce.common.event.page.WithdrawingJointApplicationApplicant1.getFirstInTimeLabel;
+import static uk.gov.hmcts.divorce.common.event.page.WithdrawingJointApplicationApplicant1.getSecondInTimeLabel;
+import static uk.gov.hmcts.divorce.common.event.page.WithdrawingJointApplicationApplicant1.getWithdrawLabel;
+
 public class WithdrawingJointApplicationApplicant2 implements CcdPageConfiguration {
 
     private static final String NEVER_SHOW = "coApplicant2ApplyForConditionalOrder=\"NEVER_SHOW\"";
@@ -23,45 +27,9 @@ public class WithdrawingJointApplicationApplicant2 implements CcdPageConfigurati
                     .readonlyNoSummary(ConditionalOrderQuestions::getConfirmInformationStillCorrect, NEVER_SHOW)
                 .done()
             .done()
-            .label("withdrawLabelApp2", "If you want to withdraw this joint application then both parties need to jointly fill out a <a href=\"https://www.gov.uk/government/publications/form-d11-application-notice\" target=\"_blank\" rel=\"noopener noreferrer\">D11 form</a> and send it to the court. Details of where to send it are on the form.")
-            .label("firstInTimeApp2",
-                "<details>"
-                    + "<summary>"
-                    + "<span>"
-                    + "Making a sole application for a conditional order"
-                    + "</span>"
-                    + "</summary>"
-                    + "<div>"
-                    + "<p>If you want to progress the application but you do not think the other party will confirm the joint application "
-                    + "for a conditional order, then you can change to a sole application. "
-                    + "This means the other party will become the respondent.</p>"
-                    + "<p>You should still continue to draft and submit a joint conditional order application. "
-                    + "If the other party does not confirm the application then you will be able to change to a sole application online."
-                    + "</p>"
-                    + "<p>If you want to apply for a conditional order as a sole applicant now, then you need to <a href=\"https://www.gov.uk/government/publications/form-d84-application-for-a-decree-nisi-conditional-order-or-judicial-separation-decreeorder\" target=\"_blank\" rel=\"noopener noreferrer\">download and fill out a paper D84 form</a>. Details of where to post it are on the form. Your application will be lodged as soon as the form is received by the court but it could take up to 3 weeks to process the application.</p>"
-                    + "</div>"
-                    + "</details>"
-                    + "<br>", "coApplicant1ConfirmInformationStillCorrect!=\"*\"")
-            .label("secondInTimeApp2",
-                "<details>"
-                    + "<summary>"
-                    + "<span>"
-                    + "If you want to continue as a sole applicant"
-                    + "</span>"
-                    + "</summary>"
-                    + "<div>"
-                    + "<p>The other applicant has already confirmed they want to continue with the joint application. "
-                    + "The quickest way for you to progress the ${labelContentDivorceOrEndingCivilPartnership} is for you to also confirm "
-                    + "you want to continue with the joint application.</p>"
-                    + "<p>"
-                    + "If you want to change to a sole application then it will delay the ${labelContentDivorceOrEndingCivilPartnership}. "
-                    + "You will have to make a separate application by <a href=\"https://www.gov.uk/government/publications/form-d84-application-for-a-decree-nisi-conditional-order-or-judicial-separation-decreeorder\" target=\"_blank\" rel=\"noopener noreferrer\">downloading and filling out a D84 paper form.</a> Details of where to send it are on the form. If you want to change to a sole application then you should go back to ‘manage cases’, sign out and send in the form.</p>"
-                    + "<p>"
-                    + "The quickest way to progress the ${labelContentDivorceOrEndingCivilPartnership} "
-                    + "is to confirm you want to continue with the joint application.</p>"
-                    + "</div>"
-                    + "</details>"
-                    + "<br>", "coApplicant1ConfirmInformationStillCorrect=\"*\"")
+            .label("withdrawLabelApp2", getWithdrawLabel())
+            .label("firstInTimeApp2", getFirstInTimeLabel(), "coApplicant1ConfirmInformationStillCorrect!=\"*\"")
+            .label("secondInTimeApp2", getSecondInTimeLabel(), "coApplicant1ConfirmInformationStillCorrect=\"*\"")
             .done()
         ;
     }
