7345fb9632898abffce9ac43f9bcd84f3a5f2913
Linus Norton <linusnorton@gmail.com>
2022-08-01 21:34:12 +0100

    Add another test

---
+++ src/cftlibTest/java/uk/gov/hmcts/divorce/cftlib/CaseworkerAddNoteTest.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.divorce.cftlib;
+
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static java.util.regex.Pattern.compile;
+
+public class CaseworkerAddNoteTest extends XuiTest {
+
+    @Test
+    public void addNote() {
+        signInWith("DivCaseWorkerUser@AAT.com");
+        page.locator("text=Create case").click();
+        assertThat(page).hasURL("http://localhost:3000/cases/case-filter");
+
+        page.locator("select[name=\"case-type\"]").selectOption("NFD");
+        page.locator("text=Start").click();
+        assertThat(page).hasURL("http://localhost:3000/cases/case-create/DIVORCE/NFD/create-paper-case/submit");
+
+        page.locator("text=Save and continue").click();
+        assertThat(page).hasURL(compile("http://localhost:3000/cases/case-details/*"));
+
+        page.locator("select").selectOption("1: Object");
+        page.locator("text=Go").click();
+        page.locator("textarea").click();
+        page.locator("textarea").fill("Note");
+        page.locator("text=Continue").click();
+        page.locator("text=Save and continue").click();
+
+        assertThat(page).hasURL(compile("http://localhost:3000/cases/case-details/*"));
+    }
+}
--- src/cftlibTest/java/uk/gov/hmcts/divorce/cftlib/XuiTest.java
+++ src/cftlibTest/java/uk/gov/hmcts/divorce/cftlib/XuiTest.java
@@ -1,55 +1,60 @@
 package uk.gov.hmcts.divorce.cftlib;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.rse.ccd.lib.test.CftlibTest;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
-import static java.util.regex.Pattern.compile;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class XuiTest extends CftlibTest {
 
+    Playwright playwright;
+    Browser browser;
 
-    @Test
-    public void logsIn() {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
-            // Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
-            Page page = browser.newPage();
-            page.navigate("http://localhost:3000");
+    @BeforeAll
+    void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch();
+        // browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+    }
 
-            // page.pause();
+    @AfterAll
+    void closeBrowser() {
+        playwright.close();
+    }
 
-            page.locator("[placeholder=\"Enter Username\"]").fill("DivCaseWorkerUser@AAT.com");
-            page.locator("[placeholder=\"Enter Password\"]").fill("anythingWillWork");
-            page.locator("text=Sign in").click();
-            assertThat(page).hasURL("http://localhost:3000/cases");
+    BrowserContext context;
+    Page page;
 
-            page.locator("text=Create case").click();
-            assertThat(page).hasURL("http://localhost:3000/cases/case-filter");
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
 
-            page.locator("select[name=\"case-type\"]").selectOption("NFD");
-            page.locator("text=Start").click();
-            assertThat(page).hasURL("http://localhost:3000/cases/case-create/DIVORCE/NFD/create-paper-case/submit");
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
 
-            page.locator("text=Save and continue").click();
-            assertThat(page).hasURL(compile("http://localhost:3000/cases/case-details/*"));
+    void signInWith(String username) {
+        page.navigate("http://localhost:3000");
 
-            page.locator("select").selectOption("1: Object");
-            page.locator("text=Go").click();
-            page.locator("textarea").click();
-            page.locator("textarea").fill("Note");
-            page.locator("text=Continue").click();
-            page.locator("text=Save and continue").click();
-
-            assertThat(page).hasURL(compile("http://localhost:3000/cases/case-details/*"));
-        }
+        page.locator("[placeholder=\"Enter Username\"]").fill(username);
+        page.locator("[placeholder=\"Enter Password\"]").fill("anythingWillWork");
+        page.locator("text=Sign in").click();
+        assertThat(page).hasURL("http://localhost:3000/cases");
+        // page.pause();
     }
 
 }
