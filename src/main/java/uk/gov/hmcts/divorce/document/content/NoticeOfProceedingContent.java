aa9c2780d26ab7ded0ac69072099b5c853e5ba36
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-08-02 10:56:21 +0100

    NFDIV-2484: Update R1 and R2 documents and use respondent link

--- src/main/java/uk/gov/hmcts/divorce/document/content/NoticeOfProceedingContent.java
+++ src/main/java/uk/gov/hmcts/divorce/document/content/NoticeOfProceedingContent.java
@@ -123,8 +123,8 @@ public class NoticeOfProceedingContent {
     private static final int PAPER_SERVE_OFFSET_DAYS = 28;
     private static final int RESPONDENT_SOLICITOR_RESPONSE_OFFSET_DAYS = 16;
 
-    private static final String APPLICANT_2_SIGN_IN_DIVORCE_URL = "applicant2SignInDivorceUrl";
-    private static final String APPLICANT_2_SIGN_IN_DISSOLUTION_URL = "applicant2SignInDissolutionUrl";
+    private static final String APPLICANT_2_SIGN_IN_DIVORCE_URL = "respondentSignInDivorceUrl";
+    private static final String APPLICANT_2_SIGN_IN_DISSOLUTION_URL = "respondentSignInDissolutionUrl";
 
     @Autowired
     private HoldingPeriodService holdingPeriodService;
