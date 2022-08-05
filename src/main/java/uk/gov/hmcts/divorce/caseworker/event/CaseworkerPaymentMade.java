f622c16b2d8f3edf6318ec5cd6534c7979e337b6
Sunil Natikar <98022094+sunilnatikar@users.noreply.github.com>
2022-08-05 13:38:06 +0100

    Changes added (#2228)

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPaymentMade.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPaymentMade.java
@@ -2,13 +2,14 @@ package uk.gov.hmcts.divorce.caseworker.event;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-
-import java.util.EnumSet;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingHWFDecision;
@@ -28,12 +29,31 @@ public class CaseworkerPaymentMade implements CCDConfig<CaseData, State, UserRol
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
             .event(CASEWORKER_PAYMENT_MADE)
-            .forStateTransition(EnumSet.of(AwaitingPayment, AwaitingHWFDecision, AwaitingDocuments),
-                Submitted)
+            .forStates(AwaitingPayment, AwaitingHWFDecision, AwaitingDocuments)
             .name("Payment made")
             .description("Payment made")
+            .aboutToSubmitCallback(this::aboutToSubmit)
             .showEventNotes()
             .grant(CREATE_READ_UPDATE, CASE_WORKER)
             .grantHistoryOnly(SUPER_USER, LEGAL_ADVISOR));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        CaseDetails<CaseData, State> details, CaseDetails<CaseData, State> beforeDetails) {
+
+        CaseData caseData = details.getData();
+
+        if (caseData.getApplicationType().isSole()
+            && YesOrNo.NO.equals(caseData.getApplication().getApplicant1KnowsApplicant2Address())
+            && YesOrNo.YES.equals(caseData.getApplication().getApplicant1WantsToHavePapersServedAnotherWay())) {
+            details.setState(AwaitingDocuments);
+        } else {
+            details.setState(Submitted);
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .state(details.getState())
+            .build();
     }
 }
