c82218f64984bcf54d8fee3e43f0a17f6d38ea22
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-08-05 12:53:39 +0100

    Changes added

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerHwfApplicationAcceptedTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerHwfApplicationAcceptedTest.java
@@ -5,13 +5,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerHwfApplicationAccepted.CASEWORKER_HWF_APPLICATION_ACCEPTED;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Submitted;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 
@@ -30,5 +39,78 @@ class CaseworkerHwfApplicationAcceptedTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_HWF_APPLICATION_ACCEPTED);
+    }
+
+    @Test
+    void shouldSetCaseStatusToAwaitingDocumentsWhenSoleCaseAndApplicantWantToServeByAlternativeMeansToRespondent() {
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+
+        final CaseData caseData = CaseData
+            .builder()
+            .applicationType(ApplicationType.SOLE_APPLICATION)
+            .application(
+                Application
+                    .builder()
+                    .applicant1WantsToHavePapersServedAnotherWay(YesOrNo.YES)
+                    .applicant1KnowsApplicant2Address(YesOrNo.NO)
+                    .build()
+            )
+            .build();
+
+        caseDetails.setId(1L);
+        caseDetails.setData(caseData);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerHwfApplicationAccepted
+            .aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(AwaitingDocuments);
+    }
+
+    @Test
+    void shouldSetCaseStatusToSubmittedWhenSoleCaseAndRespondentAddressIsKnown() {
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+
+        final CaseData caseData = CaseData
+            .builder()
+            .applicationType(ApplicationType.SOLE_APPLICATION)
+            .applicant2(Applicant.builder()
+                .address(AddressGlobalUK.builder()
+                    .addressLine1("line1")
+                    .country("UK")
+                    .build())
+                .build())
+            .application(
+                Application
+                    .builder()
+                    .applicant1WantsToHavePapersServedAnotherWay(YesOrNo.NO)
+                    .build()
+            )
+            .build();
+
+        caseDetails.setId(1L);
+        caseDetails.setData(caseData);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerHwfApplicationAccepted
+            .aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(Submitted);
+    }
+
+    @Test
+    void shouldSetCaseStatusToSubmittedWhenJointCase() {
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+
+        final CaseData caseData = CaseData
+            .builder()
+            .applicationType(ApplicationType.JOINT_APPLICATION)
+            .build();
+
+        caseDetails.setId(1L);
+        caseDetails.setData(caseData);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerHwfApplicationAccepted
+            .aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(Submitted);
     }
 }
