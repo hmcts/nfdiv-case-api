dd65c09d73ed0aa6974bfd2ffb3ec217f95e72a8
velimd <duzgunce.vd@gmail.com>
2022-08-08 14:22:26 +0100

    clean up

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenAddPaymentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenAddPaymentTest.java
@@ -11,7 +11,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static uk.gov.hmcts.divorce.citizen.event.CitizenPaymentMade.CITIZEN_PAYMENT_MADE;
+import static uk.gov.hmcts.divorce.citizen.event.CitizenAddPayment.CITIZEN_ADD_PAYMENT;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 
@@ -19,16 +19,16 @@ import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 public class CitizenAddPaymentTest {
 
     @InjectMocks
-    private CitizenPaymentMade citizenPaymentMade;
+    private CitizenAddPayment citizenAddPayment;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
-        citizenPaymentMade.configure(configBuilder);
+        citizenAddPayment.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CITIZEN_PAYMENT_MADE);
+            .contains(CITIZEN_ADD_PAYMENT);
     }
 }
