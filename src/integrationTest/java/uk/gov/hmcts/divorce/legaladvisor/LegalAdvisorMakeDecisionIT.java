583174d5ba1636b9bdb2a76ed1b32ffff42e79fa
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-08-10 14:06:11 +0100

    NFDIV-1742: Point at new versions of refusal orders for online and offline

--- src/integrationTest/java/uk/gov/hmcts/divorce/legaladvisor/LegalAdvisorMakeDecisionIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/legaladvisor/LegalAdvisorMakeDecisionIT.java
@@ -212,7 +212,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
@@ -263,7 +263,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
@@ -360,7 +360,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
                 .contentType(APPLICATION_JSON)
@@ -400,7 +400,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         String actualResponse = mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
             .contentType(APPLICATION_JSON)
@@ -455,7 +455,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
                 .contentType(APPLICATION_JSON)
@@ -527,7 +527,7 @@ public class LegalAdvisorMakeDecisionIT {
 
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, CASEWORKER_USER_ID, CASEWORKER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
-        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "NFD_Refusal_Order_V2.docx");
+        stubForDocAssemblyWith("49fa338b-1955-41c2-8e05-1df710a8ffaa", "FL-NFD-GOR-ENG-Conditional-Order-Refusal-Order-Online.docx");
 
         mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
                 .contentType(APPLICATION_JSON)
