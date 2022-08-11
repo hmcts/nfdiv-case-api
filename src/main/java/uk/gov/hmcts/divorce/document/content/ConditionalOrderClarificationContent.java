acd6795f00c44f83973b41b885d73bef33cf9915
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-08-11 14:25:59 +0100

    NFDIV-1741 - CO Clarification docs sent to offline applicants
    Rework LegalAdvisorMakeDecision event to account for new clarification
    refusal order.
    
    Implement basic version of ConditionalOrderClarificationContent class. WIP.
    
    Add offline notification methods to moreInfo notification class.
    
    Implement basic version of clarification printer. WIP.

---
+++ src/main/java/uk/gov/hmcts/divorce/document/content/ConditionalOrderClarificationContent.java
@@ -0,0 +1,141 @@
+package uk.gov.hmcts.divorce.document.content;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.ClarificationReason;
+import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
+import uk.gov.hmcts.divorce.divorcecase.model.CtscContactDetails;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_1_FULL_NAME;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_2_FULL_NAME;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CCD_CASE_REFERENCE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CIVIL_PARTNERSHIP;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CIVIL_PARTNERSHIP_CASE_JUSTICE_GOV_UK;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_DIVORCE_JUSTICE_GOV_UK;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CTSC_CONTACT_DETAILS;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DATE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_OR_CIVIL_PARTNERSHIP;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE_OR_CIVIL_PARTNERSHIP;
+import static uk.gov.hmcts.divorce.document.content.NoticeOfProceedingContent.DIVORCE_OR_CIVIL_PARTNERSHIP_EMAIL;
+import static uk.gov.hmcts.divorce.notification.CommonContent.DIVORCE;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
+
+@Component
+public class ConditionalOrderClarificationContent {
+
+    public static final String LEGAL_ADVISOR_COMMENTS = "legalAdvisorComments";
+    private static final String IS_SOLE = "isSole";
+    private static final String IS_JOINT = "isJoint";
+    public static final String JUDICIAL_SEPARATION = "judicialSeparation";
+
+    @Autowired
+    private Clock clock;
+
+    @Value("${court.locations.serviceCentre.serviceCentreName}")
+    private String serviceCentre;
+
+    @Value("${court.locations.serviceCentre.centreName}")
+    private String centreName;
+
+    @Value("${court.locations.serviceCentre.poBox}")
+    private String poBox;
+
+    @Value("${court.locations.serviceCentre.town}")
+    private String town;
+
+    @Value("${court.locations.serviceCentre.postCode}")
+    private String postcode;
+
+    @Value("${court.locations.serviceCentre.phoneNumber}")
+    private String phoneNumber;
+
+    public Map<String, Object> apply(final CaseData caseData, final Long ccdCaseReference) {
+
+        Map<String, Object> templateContent = new HashMap<>();
+        final ConditionalOrder conditionalOrder = caseData.getConditionalOrder();
+
+        templateContent.put(CCD_CASE_REFERENCE, formatId(ccdCaseReference));
+        templateContent.put(DATE, LocalDate.now(clock).format(DATE_TIME_FORMATTER));
+
+        templateContent.put(IS_SOLE, caseData.getApplicationType().isSole());
+        templateContent.put(IS_JOINT, !caseData.getApplicationType().isSole());
+
+        templateContent.put(APPLICANT_1_FULL_NAME, caseData.getApplicant1().getFullName());
+        templateContent.put(APPLICANT_2_FULL_NAME, caseData.getApplicant2().getFullName());
+
+        templateContent.put(LEGAL_ADVISOR_COMMENTS, generateLegalAdvisorComments(conditionalOrder));
+
+
+        if (caseData.getDivorceOrDissolution().isDivorce()) {
+            templateContent.put(MARRIAGE_OR_CIVIL_PARTNERSHIP, MARRIAGE);
+            templateContent.put(DIVORCE_OR_CIVIL_PARTNERSHIP, DIVORCE);
+            templateContent.put(DIVORCE_OR_CIVIL_PARTNERSHIP_EMAIL, CONTACT_DIVORCE_JUSTICE_GOV_UK);
+            templateContent.put(JUDICIAL_SEPARATION, caseData.getIsJudicialSeparation());
+
+        } else {
+            templateContent.put(MARRIAGE_OR_CIVIL_PARTNERSHIP, CIVIL_PARTNERSHIP);
+            templateContent.put(DIVORCE_OR_CIVIL_PARTNERSHIP, CIVIL_PARTNERSHIP);
+            templateContent.put(DIVORCE_OR_CIVIL_PARTNERSHIP_EMAIL, CIVIL_PARTNERSHIP_CASE_JUSTICE_GOV_UK);
+        }
+
+        final var ctscContactDetails = CtscContactDetails
+            .builder()
+            .centreName(centreName)
+            .serviceCentre(serviceCentre)
+            .poBox(poBox)
+            .town(town)
+            .postcode(postcode)
+            .phoneNumber(phoneNumber)
+            .build();
+
+        templateContent.put(CTSC_CONTACT_DETAILS, ctscContactDetails);
+
+        return templateContent;
+    }
+
+    public List<RefusalReason> generateLegalAdvisorComments(ConditionalOrder conditionalOrder) {
+
+
+        Set<ClarificationReason> refusalClarificationReason = conditionalOrder.getRefusalClarificationReason();
+        if (isEmpty(refusalClarificationReason)) {
+            return emptyList();
+        }
+
+        List<RefusalReason> legalAdvisorComments = refusalClarificationReason.stream()
+            .filter(clarificationReason -> !clarificationReason.equals(ClarificationReason.OTHER))
+            .map(reason -> new RefusalReason(reason.getLabel()))
+            .collect(Collectors.toList());
+
+        String refusalClarificationAdditionalInfo = conditionalOrder.getRefusalClarificationAdditionalInfo();
+        if (isNotEmpty(refusalClarificationAdditionalInfo)) {
+            legalAdvisorComments.add(new RefusalReason(refusalClarificationAdditionalInfo));
+        }
+
+        return legalAdvisorComments;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    public static class RefusalReason {
+        private String comment;
+    }
+}
