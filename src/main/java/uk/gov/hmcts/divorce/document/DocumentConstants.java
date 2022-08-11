e6b50fa3895e722a935d59e5562b31e4b430899d
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-08-11 15:58:53 +0100

    initial changes

--- src/main/java/uk/gov/hmcts/divorce/document/DocumentConstants.java
+++ src/main/java/uk/gov/hmcts/divorce/document/DocumentConstants.java
@@ -24,6 +24,7 @@ public final class DocumentConstants {
 
     public static final String NFD_NOP_R1_SOLE_APP2_CIT_ONLINE = "NFD_NOP_R1_SOLE_APP2_CIT_ONLINE";
     public static final String NFD_NOP_R2_SOLE_APP2_CIT_OFFLINE = "NFD_NOP_R2_SOLE_APP2_CIT_OFFLINE";
+    public static final String NFD_NOP_R2_SOLE_APP2_CIT_OFFLINE_REISSUE = "NFD_NOP_R2_SOLE_APP2_CIT_OFFLINE_REISSUE";
     public static final String COVERSHEET_DOCUMENT_NAME = "coversheet";
     public static final String CONDITIONAL_ORDER_GRANTED_COVERSHEET_DOCUMENT_NAME = "conditionalOrderGrantedCoversheet";
 
