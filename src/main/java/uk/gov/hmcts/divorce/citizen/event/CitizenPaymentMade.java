75788a9e348688e9da305b9c5c2f71b4d55e722d
velimd <duzgunce.vd@gmail.com>
2022-08-19 11:12:49 +0100

    merge clean up

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenPaymentMade.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenPaymentMade.java
@@ -22,6 +22,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPayment;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.validation.ApplicationValidation.validateSubmission;
@@ -46,7 +47,7 @@ public class CitizenPaymentMade implements CCDConfig<CaseData, State, UserRole> 
             .description("Payment made")
             .retries(120, 120)
             .grant(CREATE_READ_UPDATE, CITIZEN)
-            .grantHistoryOnly(SUPER_USER, CASE_WORKER)
+            .grantHistoryOnly(SUPER_USER, CASE_WORKER, LEGAL_ADVISOR)
             .aboutToSubmitCallback(this::aboutToSubmit);
     }
 
