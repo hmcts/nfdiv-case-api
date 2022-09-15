55bfd90dadd3668bef23c5f752b17aeebfe35a8b
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-09-15 18:34:35 +0100

    NFDIV-2691: Fix indentation

--- src/main/java/uk/gov/hmcts/divorce/common/event/Applicant2ApplyForFinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/Applicant2ApplyForFinalOrder.java
@@ -61,19 +61,19 @@ public class Applicant2ApplyForFinalOrder implements CCDConfig<CaseData, State, 
 
     private PageBuilder addEventConfig(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         return new PageBuilder(configBuilder
-                .event(APPLICANT2_FINAL_ORDER_REQUESTED)
-                .forStates(AwaitingFinalOrder, AwaitingJointFinalOrder, FinalOrderOverdue)
-                .name(APPLICANT2_APPLY_FOR_FINAL_ORDER)
-                .description(APPLICANT2_APPLY_FOR_FINAL_ORDER)
-                .showSummary()
-                .showEventNotes()
-                .grant(CREATE_READ_UPDATE, APPLICANT_2, APPLICANT_2_SOLICITOR)
-                .aboutToSubmitCallback(this::aboutToSubmit)
-                .grantHistoryOnly(
-                        CASE_WORKER,
-                        SUPER_USER,
-                        LEGAL_ADVISOR,
-                        APPLICANT_1_SOLICITOR));
+            .event(APPLICANT2_FINAL_ORDER_REQUESTED)
+            .forStates(AwaitingFinalOrder, AwaitingJointFinalOrder, FinalOrderOverdue)
+            .name(APPLICANT2_APPLY_FOR_FINAL_ORDER)
+            .description(APPLICANT2_APPLY_FOR_FINAL_ORDER)
+            .showSummary()
+            .showEventNotes()
+            .grant(CREATE_READ_UPDATE, APPLICANT_2, APPLICANT_2_SOLICITOR)
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .grantHistoryOnly(
+                    CASE_WORKER,
+                    SUPER_USER,
+                    LEGAL_ADVISOR,
+                    APPLICANT_1_SOLICITOR));
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
