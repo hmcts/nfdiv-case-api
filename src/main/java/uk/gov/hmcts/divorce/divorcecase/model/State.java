c80fe162b3f785a9221b0c7a2786e1334e430982
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2022-09-15 10:03:45 +0100

    Revert "Allow AwaitingApplicant2Response as pre state for return to previous state (#2366)"
    This reverts commit 790a840bc94c358f6b88a60b0815636b1bdde2c5.

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/State.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/State.java
@@ -389,6 +389,7 @@ public enum State {
     public static final EnumSet<State> PRE_RETURN_TO_PREVIOUS_STATES = EnumSet.complementOf(EnumSet.of(
         Draft,
         AwaitingApplicant1Response,
+        AwaitingApplicant2Response,
         Applicant2Approved,
         AwaitingPronouncement,
         Withdrawn,
