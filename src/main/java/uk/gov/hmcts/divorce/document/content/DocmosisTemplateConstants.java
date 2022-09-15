0ceda6507c201ee749a30c10ea0c7c2bfd2a709b
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-09-15 13:21:02 +0100

    Add function to set common content for docmosis templates

--- src/main/java/uk/gov/hmcts/divorce/document/content/DocmosisTemplateConstants.java
+++ src/main/java/uk/gov/hmcts/divorce/document/content/DocmosisTemplateConstants.java
@@ -1,5 +1,12 @@
 package uk.gov.hmcts.divorce.document.content;
 
+import uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
+
 public final class DocmosisTemplateConstants {
 
     public static final String DIVORCE_OR_DISSOLUTION = "divorceOrDissolution";
@@ -169,6 +176,38 @@ public final class DocmosisTemplateConstants {
     public static final String CO_PRONOUNCED_DATE = "coPronouncedDate";
     public static final String DATE_FO_ELIGIBLE_FROM = "dateFinalOrderEligibleFrom";
 
+    public static final String DIVORCE_AND_DISSOLUTION_HEADER = "divorceAndDissolutionHeader";
+    public static final String DIVORCE_AND_DISSOLUTION_HEADER_TEXT = "Divorce and Dissolution";
+    public static final String DIVORCE_AND_DISSOLUTION_HEADER_TEXT_CY = "Gwasanaeth Ysgariadau a Diddymiadau";
+
+    public static final String COURTS_AND_TRIBUNALS_SERVICE_HEADER = "courtsAndTribunalsServiceHeader";
+    public static final String COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT = "HM Courts & Tribunals Service";
+    public static final String COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT_CY = "Gwasanaeth Llysoedd a Thribiwnlysoedd EM";
+
+    public static final String CONTACT_EMAIL = "contactEmail";
+
+    public static final String PHONE_AND_OPENING_TIMES = "phoneAndOpeningTimes";
+    public static final String PHONE_AND_OPENING_TIMES_TEXT = "Phone: 0300 303 0642 (Monday to Friday, 8am to 6pm)";
+    public static final String PHONE_AND_OPENING_TIMES_TEXT_CY = "Phone: 0300 303 0642 (Dydd Llun i Ddydd Gwener, 8am to 6pm)";
+
     private DocmosisTemplateConstants() {
+    }
+
+    public Map<String, Object> getBasicDocmosisTemplateContent(LanguagePreference languagePreference) {
+        Map<String, Object> templateContent = new HashMap<>();
+
+        if (ENGLISH.equals(languagePreference)) {
+            templateContent.put(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT);
+            templateContent.put(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT);
+            templateContent.put(CONTACT_EMAIL, CONTACT_DIVORCE_JUSTICE_GOV_UK);
+            templateContent.put(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT);
+        } else {
+            templateContent.put(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT_CY);
+            templateContent.put(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT_CY);
+            templateContent.put(CONTACT_EMAIL, CONTACT_JUSTICE_GOV_UK_CY);
+            templateContent.put(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT_CY);
+        }
+
+        return templateContent;
     }
 }
