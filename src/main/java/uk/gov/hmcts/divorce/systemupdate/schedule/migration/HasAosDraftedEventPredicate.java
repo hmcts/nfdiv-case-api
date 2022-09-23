7c439509fcf0564191921a90ffc58c2a14186b43
Shaun Francis <shaun.francis1@hmcts.net>
2022-09-23 10:51:30 +0100

    Add comments HasAosDraftedEventPredicate

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/migration/HasAosDraftedEventPredicate.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/migration/HasAosDraftedEventPredicate.java
@@ -27,12 +27,23 @@ public class HasAosDraftedEventPredicate {
     @Autowired
     private CaseEventsApi caseEventsApi;
 
+    /**
+     * isAosDrafted is set to Yes when the DRAFT_AOS event occurs, but it is reset on CASEWORKER_REISSUE_APPLICATION event.
+     * <p>
+     * This predicate checks for the DRAFT_AOS event in the Case event history, but It will only return true if a
+     * CASEWORKER_REISSUE_APPLICATION event does not occur after the DRAFT_AOS event.
+     *
+     * @param user                 - for authentication to CCD
+     * @param serviceAuthorization - for authorization to CCD
+     * @return true - if DRAFT_AOS event is present and CASEWORKER_REISSUE_APPLICATION event does not occur after DRAFT_AOS event.
+     */
     public Predicate<CaseDetails> hasAosDraftedEvent(final User user, final String serviceAuthorization) {
 
         return caseDetails -> {
             final Long caseId = caseDetails.getId();
 
             try {
+                //Get list of events ordered by created date
                 final List<CaseEventDetail> eventDetailsForCase = caseEventsApi.findEventDetailsForCase(
                         user.getAuthToken(),
                         serviceAuthorization,
@@ -47,18 +58,23 @@ public class HasAosDraftedEventPredicate {
                 final Deque<CaseEventDetail> stack = new LinkedList<>();
                 for (CaseEventDetail caseEventDetail : eventDetailsForCase) {
                     final String eventId = caseEventDetail.getId();
+
+                    //Push DRAFT_AOS and CASEWORKER_REISSUE_APPLICATION onto stack to check which comes last.
+                    //First event on the stack will be the last event in history
                     if (eventId.equals(DRAFT_AOS) || eventId.equals(CASEWORKER_REISSUE_APPLICATION)) {
                         stack.push(caseEventDetail);
                     }
                 }
 
                 if (!stack.isEmpty()) {
+                    //Only return true if DRAFT_AOS is the first event on the stack
                     return stack.pop().getId().equals(DRAFT_AOS);
                 }
             } catch (final FeignException e) {
                 log.error("Failed to retrieve event history, skipping Case Id {}", caseId);
             }
 
+            //Return false if there is no DRAFT_AOS event or CASEWORKER_REISSUE_APPLICATION occurs after DRAFT_AOS event
             return false;
         };
     }
