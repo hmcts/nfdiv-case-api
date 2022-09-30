c3cb82dc098829b42e244262e594b3f7647cd186
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-09-30 08:59:40 +0100

    Fix whitespace issue

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2ServiceDetails.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/Applicant2ServiceDetails.java
@@ -116,7 +116,7 @@ public class Applicant2ServiceDetails implements CcdPageConfiguration {
         List<String> validationErrors = new ArrayList<>();
 
         boolean validApplicantEmail;
-        if (isNotEmpty(applicant2.getEmail()) ) {
+        if (isNotEmpty(applicant2.getEmail())) {
             validApplicantEmail = EmailValidator.getInstance().isValid(applicant2.getEmail());
             if (!validApplicantEmail) {
                 validationErrors.add(INVALID_APPLICANT_EMAIL_ERROR);
