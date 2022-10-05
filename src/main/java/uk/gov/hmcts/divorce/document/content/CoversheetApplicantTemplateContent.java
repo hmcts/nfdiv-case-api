9bd9fa0cd93b3ba1bbe6337eaa44dc1dfdef9f05
Barry Weeks <barry.weeks@justice.gov.uk>
2022-10-05 15:22:21 +0100

    NFDiv-2607 Update Can-Apply-Final-Order Cover Letter

--- src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
+++ src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.divorce.document.content;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CASE_REFERENCE;
@@ -15,10 +15,16 @@ import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
 @Slf4j
 public class CoversheetApplicantTemplateContent {
 
+    @Autowired
+    private DocmosisCommonContent docmosisCommonContent;
+
     public Map<String, Object> apply(final CaseData caseData,
                                      final Long ccdCaseReference,
                                      final Applicant applicant) {
-        final Map<String, Object> templateContent = new HashMap<>();
+
+        final Map<String, Object> templateContent = docmosisCommonContent.getBasicDocmosisTemplateContent(
+            applicant.getLanguagePreference());
+
         templateContent.put(CASE_REFERENCE, formatId(ccdCaseReference));
         templateContent.put("applicantFirstName", applicant.getFirstName());
         templateContent.put("applicantLastName", applicant.getLastName());
--- src/test/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContentTest.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.divorce.document.content;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
@@ -11,14 +12,28 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_2_FIRST_NAME;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_2_LAST_NAME;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_DIVORCE_JUSTICE_GOV_UK;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_EMAIL;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER_TEXT;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES_TEXT;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getBasicDocmosisTemplateContent;
 
 @ExtendWith(MockitoExtension.class)
 class CoversheetApplicantTemplateContentTest {
+
+    @Mock
+    private DocmosisCommonContent docmosisCommonContent;
 
     @InjectMocks
     private CoversheetApplicantTemplateContent coversheetApplicantTemplateContent;
@@ -36,6 +51,10 @@ class CoversheetApplicantTemplateContentTest {
                 .postCode("postcode")
                 .build()
         );
+
+        when(docmosisCommonContent.getBasicDocmosisTemplateContent(ENGLISH)).thenReturn(getBasicDocmosisTemplateContent(
+            caseData.getApplicant1().getLanguagePreference()));
+
         final Map<String, Object> result = coversheetApplicantTemplateContent.apply(caseData, TEST_CASE_ID, caseData.getApplicant2());
 
         Map<String, Object> expectedEntries = new LinkedHashMap<>();
@@ -43,6 +62,10 @@ class CoversheetApplicantTemplateContentTest {
         expectedEntries.put("applicantFirstName", APPLICANT_2_FIRST_NAME);
         expectedEntries.put("applicantLastName", APPLICANT_2_LAST_NAME);
         expectedEntries.put("applicantAddress", "line 1\npostcode");
+        expectedEntries.put(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT);
+        expectedEntries.put(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT);
+        expectedEntries.put(CONTACT_EMAIL, CONTACT_DIVORCE_JUSTICE_GOV_UK);
+        expectedEntries.put(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT);
 
         assertThat(result).containsExactlyInAnyOrderEntriesOf(expectedEntries);
     }
