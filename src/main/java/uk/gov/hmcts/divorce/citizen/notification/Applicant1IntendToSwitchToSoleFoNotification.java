7e0ec6acded03c37769dc358102c7197d2d6e75b
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-10-12 15:26:44 +0100

    NFDIV-2657: Remove duplicate code

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1IntendToSwitchToSoleFoNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1IntendToSwitchToSoleFoNotification.java
@@ -5,42 +5,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
-import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.util.Map;
-
-import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_PLUS_14_DAYS;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.INTEND_TO_SWITCH_TO_SOLE_FO;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.PARTNER_INTENDS_TO_SWITCH_TO_SOLE_FO;
-import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
 
 @Component
 @Slf4j
-public class Applicant1IntendToSwitchToSoleFoNotification implements ApplicantNotification {
+public class Applicant1IntendToSwitchToSoleFoNotification extends IntendToSwitchToSoleFoNotification implements ApplicantNotification {
 
     @Autowired
     private NotificationService notificationService;
-
-    @Autowired
-    private CommonContent commonContent;
-
-    @Autowired
-    private Clock clock;
 
     @Override
     public void sendToApplicant1(final CaseData caseData, final Long id) {
         log.info("Notifying applicant 1 that the court has received their intention to switch to sole fo : {}", id);
 
-        Map<String, String> templateVars = commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2());
-        templateVars.put(DATE_PLUS_14_DAYS, LocalDate.now(clock).plusDays(14).format(DATE_TIME_FORMATTER));
-
         notificationService.sendEmail(
             caseData.getApplicant1().getEmail(),
             INTEND_TO_SWITCH_TO_SOLE_FO,
-            templateVars,
+            templateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
             caseData.getApplicant1().getLanguagePreference()
         );
     }
@@ -49,13 +33,10 @@ public class Applicant1IntendToSwitchToSoleFoNotification implements ApplicantNo
     public void sendToApplicant2(CaseData data, Long id) {
         log.info("Notifying applicant 2 that applicant 1 intends to switch to sole fo : {}", id);
 
-        Map<String, String> templateVars = commonContent.mainTemplateVars(data, id, data.getApplicant2(), data.getApplicant1());
-        templateVars.put(DATE_PLUS_14_DAYS, LocalDate.now(clock).plusDays(14).format(DATE_TIME_FORMATTER));
-
         notificationService.sendEmail(
             data.getApplicant2EmailAddress(),
             PARTNER_INTENDS_TO_SWITCH_TO_SOLE_FO,
-            templateVars,
+            templateVars(data, id, data.getApplicant2(), data.getApplicant1()),
             data.getApplicant2().getLanguagePreference()
         );
     }
