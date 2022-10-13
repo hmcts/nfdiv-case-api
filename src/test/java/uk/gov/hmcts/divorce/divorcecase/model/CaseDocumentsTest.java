b0ac11d813785878a151c65747442ef16b901f25
Shaun Francis <shaun.francis1@hmcts.net>
2022-10-13 16:49:07 +0100

    Remove duplicate test

--- src/test/java/uk/gov/hmcts/divorce/divorcecase/model/CaseDocumentsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/model/CaseDocumentsTest.java
@@ -415,31 +415,6 @@ class CaseDocumentsTest {
         assertTrue(conditionalOrderGrantedDoc.isEmpty());
     }
 
-    @Test
-    public void shouldRemoveGivenDocumentType() {
-        final Map<String, Object> templateContent = new HashMap<>();
-        final CaseDocuments caseDocuments = CaseDocuments.builder()
-            .documentsGenerated(Lists.newArrayList(
-                ListValue.<DivorceDocument>builder()
-                    .id("1")
-                    .value(DivorceDocument.builder()
-                        .documentType(APPLICATION)
-                        .build())
-                    .build(),
-                ListValue.<DivorceDocument>builder()
-                    .id("2")
-                    .value(DivorceDocument.builder()
-                        .documentType(AMENDED_APPLICATION)
-                        .build()).build()
-            ))
-            .build();
-
-        caseDocuments.removeDocumentGeneratedWithType(APPLICATION);
-
-        assertEquals(1, caseDocuments.getDocumentsGenerated().size());
-        assertEquals(AMENDED_APPLICATION, caseDocuments.getDocumentsGenerated().get(0).getValue().getDocumentType());
-    }
-
     private ListValue<ScannedDocument> getDocumentListValue(final String url,
                                                             final String filename,
                                                             final ScannedDocumentType scannedDocumentType) {
