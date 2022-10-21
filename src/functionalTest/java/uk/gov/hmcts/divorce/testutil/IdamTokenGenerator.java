f7445826f2463570ef34da1176222738615d9d2d
Nitin Prabhu <prabhunitin61@gmail.com>
2022-10-21 12:05:11 +0100

    Use expiring caffeine cache to remove expired tokens

--- src/functionalTest/java/uk/gov/hmcts/divorce/testutil/IdamTokenGenerator.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/testutil/IdamTokenGenerator.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.divorce.testutil;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @TestPropertySource("classpath:application.yaml")
 @Service
@@ -31,15 +34,19 @@ public class IdamTokenGenerator {
     @Autowired
     private IdamClient idamClient;
 
+    private final Cache<String, String> cache = Caffeine.newBuilder().expireAfterWrite(2, TimeUnit.HOURS).build();
+
     public String generateIdamTokenForSolicitor() {
         return tokensMap.computeIfAbsent(solicitorUsername, token -> idamClient.getAccessToken(solicitorUsername, solicitorPassword));
     }
 
     public String generateIdamTokenForSystem() {
-        return tokensMap.computeIfAbsent(
-            systemUpdateUsername,
-            token -> idamClient.getAccessToken(systemUpdateUsername, systemUpdatePassword)
-        );
+        String systemUserToken = cache.getIfPresent(systemUpdateUsername);
+        if (systemUserToken == null) {
+            systemUserToken = idamClient.getAccessToken(systemUpdateUsername, systemUpdatePassword);
+            cache.put(systemUpdateUsername, systemUserToken);
+        }
+        return systemUserToken;
     }
 
     public UserDetails getUserDetailsFor(final String token) {
