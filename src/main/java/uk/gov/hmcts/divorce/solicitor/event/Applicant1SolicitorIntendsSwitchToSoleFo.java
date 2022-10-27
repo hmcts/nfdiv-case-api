fec19a5d34c614296c0660943d98627bdc6dcc84
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-10-27 11:23:14 +0100

    NFDIV-2761: Set up app1 and app2 solicitor intends to switch to sole events

---
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/Applicant1SolicitorIntendsSwitchToSoleFo.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingJointFinalOrder;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Component
+public class Applicant1SolicitorIntendsSwitchToSoleFo implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String APPLICANT_1_INTENDS_TO_SWITCH_TO_SOLE_FO = "applicant1-intends-switch-to-sole-fo";
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+
+        configBuilder
+            .event(APPLICANT_1_INTENDS_TO_SWITCH_TO_SOLE_FO)
+            .forStates(AwaitingJointFinalOrder)
+            .name("Intends to switch to sole FO")
+            .description("Applicant 1 intends to switch to sole FO")
+            .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR)
+            .grantHistoryOnly(CITIZEN, CASE_WORKER, SUPER_USER)
+            .retries(120, 120)
+            .aboutToSubmitCallback(this::aboutToSubmit);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                       CaseDetails<CaseData, State> beforeDetails) {
+        CaseData data = details.getData();
+
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .build();
+    }
+}
