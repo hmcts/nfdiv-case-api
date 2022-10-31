98830f46730ee09bdf2295b9bb75907b2f664237
Barry Weeks <barry.weeks@justice.gov.uk>
2022-10-31 14:59:59 +0000

    NFDiv-2609 Resolve checkStyle issue

--- src/main/java/uk/gov/hmcts/divorce/common/service/task/GenerateSwitchToSoleConditionalOrderLetter.java
+++ src/main/java/uk/gov/hmcts/divorce/common/service/task/GenerateSwitchToSoleConditionalOrderLetter.java
@@ -3,10 +3,10 @@ package uk.gov.hmcts.divorce.common.service.task;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.divorce.document.content.DocmosisCommonContent;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.document.CaseDataDocumentService;
+import uk.gov.hmcts.divorce.document.content.DocmosisCommonContent;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 
 import java.time.Clock;
