a0610dbcc9047f0515777db629cbeb2480e2a723
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-11-03 15:04:02 +0000

    NFDIV-2860: updated TestConstants

--- src/test/java/uk/gov/hmcts/divorce/testutil/TestConstants.java
+++ src/test/java/uk/gov/hmcts/divorce/testutil/TestConstants.java
@@ -86,7 +86,7 @@ public final class TestConstants {
 
     public static final String EMPTY_STRING = "";
 
-    public static final String TEST_DIVORCE_APPLICATION_SOLE_TEMPLATE_ID = "NFD_CP_Application_Sole_V6.docx";
+    public static final String TEST_DIVORCE_APPLICATION_SOLE_TEMPLATE_ID = "FL-NFD-GOR-ENG-Application-Sole_V6.docx";
     public static final String TEST_DIVORCE_APPLICATION_JOINT_TEMPLATE_ID = "NFD_CP_Application_Joint_V5.docx";
 
     public static final String TEST_FINANCIAL_ORDER_POLICY_HEADER = "The applicant wants to apply for a financial order.";
