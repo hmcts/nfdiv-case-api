a63305181d50062deb7cb3dbcc7eeddb20dae167
Sunil Natikar <98022094+sunilnatikar@users.noreply.github.com>
2022-11-04 16:38:09 +0000

    NFDIV-2969 : confidential co granted cover letter (#2445)
    * updated new email template
    
    * NFDIV-2883 : changes for confidential CoE cover letter
    
    * initial commit
    
    * refactoring
    
    * NFDIV-2883 : changes to handle confidential co granted cover letter
    
    * refactor
    
    * refactor
    
    * removed log
    
    * removed changes related to AC1 as that is covered in another PR
    
    * removed blank line
    
    * corrected
    
    Co-authored-by: Shaun Francis <shaun.francis1@hmcts.net>

--- src/main/java/uk/gov/hmcts/divorce/document/DocumentUtil.java
+++ src/main/java/uk/gov/hmcts/divorce/document/DocumentUtil.java
@@ -25,6 +25,8 @@ import static uk.gov.hmcts.divorce.document.DocumentConstants.APPLICANT2;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.AOS_RESPONSE_LETTER;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP1;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP2;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_2;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.GENERAL_LETTER;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.NOTICE_OF_PROCEEDINGS_APP_1;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.NOTICE_OF_PROCEEDINGS_APP_2;
@@ -154,6 +156,8 @@ public final class DocumentUtil {
             case AOS_RESPONSE_LETTER -> ConfidentialDocumentsReceived.AOS_RESPONSE_LETTER;
             case CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP1 -> ConfidentialDocumentsReceived.CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP1;
             case CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP2 -> ConfidentialDocumentsReceived.CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP2;
+            case CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1 -> ConfidentialDocumentsReceived.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1;
+            case CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_2 -> ConfidentialDocumentsReceived.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_2;
             default -> ConfidentialDocumentsReceived.OTHER;
         };
     }
@@ -164,12 +168,14 @@ public final class DocumentUtil {
                 NOTICE_OF_PROCEEDINGS_APP_1,
                 GENERAL_LETTER,
                 AOS_RESPONSE_LETTER,
-                CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP1
+                CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP1,
+                CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1
             ),
             APPLICANT2, Lists.newArrayList(
                 NOTICE_OF_PROCEEDINGS_APP_2,
                 GENERAL_LETTER,
-                CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP2
+                CERTIFICATE_OF_ENTITLEMENT_COVER_LETTER_APP2,
+                CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_2
             )
         );
     }
