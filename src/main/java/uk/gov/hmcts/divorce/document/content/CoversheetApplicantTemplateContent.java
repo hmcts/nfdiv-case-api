2d499403fed25a27a3c7d8b1f57b94df7c4c28ae
Barry Weeks <barry.weeks@justice.gov.uk>
2022-11-08 09:56:47 +0000

    NFDiv-2607 Fix checkstyle issue

--- src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
+++ src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
@@ -8,7 +8,15 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 
 import java.util.Map;
 
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.*;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CASE_REFERENCE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_DIVORCE_JUSTICE_GOV_UK;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_EMAIL;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER_TEXT;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES_TEXT;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
 
 @Component
