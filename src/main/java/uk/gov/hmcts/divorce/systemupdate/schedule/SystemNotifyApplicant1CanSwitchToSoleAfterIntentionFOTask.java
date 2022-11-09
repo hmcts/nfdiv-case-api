33e2fa30957b9b2c637541bcfe70f4bfa12dfebe
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2022-11-09 09:01:37 +0000

    NFDIV-2822 - Solicitor notification to apply switch to sole 14 days after intention
    - Create a cron job task to find cases where applicant1 has intended to switch to sole
    over 14 days ago, to notify them they can no continue to switch to sole.
    
    - Unit test task.

---
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTask.java
@@ -0,0 +1,118 @@
+package uk.gov.hmcts.divorce.systemupdate.schedule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.idam.IdamService;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdConflictException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdManagementException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchCaseException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdUpdateService;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.idam.client.models.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingJointFinalOrder;
+import static uk.gov.hmcts.divorce.systemupdate.event.SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFO.SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION;
+import static uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService.DATA;
+import static uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService.STATE;
+
+@Component
+@Slf4j
+public class SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTask implements Runnable {
+
+    public static final String NOTIFICATION_SENT_FLAG = "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention";
+    public static final String APP_1_INTENDED_TO_SWITCH_TO_SOLE = "doesApplicant1IntendToSwitchToSole";
+    private static final int FOURTEEN_DAYS = 14;
+
+    @Autowired
+    private CcdSearchService ccdSearchService;
+
+    @Autowired
+    private CcdUpdateService ccdUpdateService;
+
+    @Autowired
+    private IdamService idamService;
+
+    @Autowired
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void run() {
+        log.info("Notify applicant who intended to switch to sole that they can now do so");
+
+        final User user = idamService.retrieveSystemUpdateUserDetails();
+        final String serviceAuthorization = authTokenGenerator.generate();
+
+        try {
+            final BoolQueryBuilder query = boolQuery()
+                .must(matchQuery(STATE, AwaitingJointFinalOrder))
+                .must(matchQuery(String.format(DATA, APP_1_INTENDED_TO_SWITCH_TO_SOLE), YES))
+                .mustNot(matchQuery(String.format(DATA, NOTIFICATION_SENT_FLAG), YES));
+
+            final List<CaseDetails> casesWithApplicant1IntendedSwitchToSole =
+                ccdSearchService.searchForAllCasesWithQuery(query, user, serviceAuthorization, AwaitingJointFinalOrder);
+
+            for (final CaseDetails caseDetails : casesWithApplicant1IntendedSwitchToSole) {
+                try {
+                    final CaseData caseData = objectMapper.convertValue(caseDetails.getData(), CaseData.class);
+                    final LocalDate dateFinalOrderIntendsSwitchToSoleNowEligible =
+                        caseData.getFinalOrder().getDateApplicant1DeclaredIntentionToSwitchToSoleFo().plusDays(FOURTEEN_DAYS);
+
+                    if (dateFinalOrderIntendsSwitchToSoleNowEligible.isBefore(LocalDate.now())
+                        && !caseData.getFinalOrder().hasApplicant1BeenNotifiedTheyCanContinueSwitchToSoleFO()) {
+                        notifyApplicantCanNowSwitchToSole(
+                            caseDetails,
+                            caseData.getFinalOrder().getDateApplicant1DeclaredIntentionToSwitchToSoleFo(),
+                            user,
+                            serviceAuthorization
+                        );
+                    }
+                } catch (final CcdManagementException e) {
+                    log.error("Submit event failed for case id: {}, continuing to next case", caseDetails.getId());
+                } catch (final IllegalArgumentException e) {
+                    log.error("Deserialization failed for case id: {}, continuing to next case", caseDetails.getId());
+                }
+            }
+
+            log.info("Notify applicant they can switch to sole after intention final order task complete.");
+
+        } catch (final CcdSearchCaseException e) {
+            log.error("SystemNotifyWhenSecondInTimeNotAppliedForFO schedule task stopped after search error", e);
+        } catch (final CcdConflictException e) {
+            log.info("SystemNotifyWhenSecondInTimeNotAppliedForFO schedule task stopping "
+                + "due to conflict with another running task"
+            );
+        }
+    }
+
+    private void notifyApplicantCanNowSwitchToSole(CaseDetails caseDetails,
+                                                   LocalDate dateApplicant1DeclaredIntentionToSwitchToSoleFo,
+                                                   User user,
+                                                   String serviceAuth) {
+
+        log.info(
+            """
+            14 days has passed since applicant intended to switch to sole for final order on {} for Case id {}
+            - notifying them they can continue and switch to sole.
+            """,
+            dateApplicant1DeclaredIntentionToSwitchToSoleFo,
+            caseDetails.getId()
+        );
+
+        ccdUpdateService.submitEvent(caseDetails, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, serviceAuth);
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTaskTest.java
@@ -0,0 +1,281 @@
+package uk.gov.hmcts.divorce.systemupdate.schedule;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.FinalOrder;
+import uk.gov.hmcts.divorce.idam.IdamService;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdConflictException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdManagementException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchCaseException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdUpdateService;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
+import uk.gov.hmcts.reform.idam.client.models.User;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Collections.singletonList;
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.cloud.contract.spec.internal.HttpStatus.REQUEST_TIMEOUT;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingJointFinalOrder;
+import static uk.gov.hmcts.divorce.systemupdate.event.SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFO.SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION;
+import static uk.gov.hmcts.divorce.systemupdate.schedule.SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTask.APP_1_INTENDED_TO_SWITCH_TO_SOLE;
+import static uk.gov.hmcts.divorce.systemupdate.schedule.SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTask.NOTIFICATION_SENT_FLAG;
+import static uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService.DATA;
+import static uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService.STATE;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SERVICE_AUTHORIZATION;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SYSTEM_UPDATE_AUTH_TOKEN;
+
+@ExtendWith(MockitoExtension.class)
+public class SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTaskTest {
+
+    @Mock
+    private CcdUpdateService ccdUpdateService;
+
+    @Mock
+    private CcdSearchService ccdSearchService;
+
+    @Mock
+    private IdamService idamService;
+
+    @Mock
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @InjectMocks
+    private SystemNotifyApplicant1CanSwitchToSoleAfterIntentionFOTask task;
+
+    private User user;
+
+    private static final BoolQueryBuilder query =
+        boolQuery()
+            .must(matchQuery(STATE, AwaitingJointFinalOrder))
+            .must(matchQuery(String.format(DATA, APP_1_INTENDED_TO_SWITCH_TO_SOLE), YES))
+            .mustNot(matchQuery(String.format(DATA, NOTIFICATION_SENT_FLAG), YES));
+
+    @BeforeEach
+    void setUp() {
+        user = new User(SYSTEM_UPDATE_AUTH_TOKEN, UserDetails.builder().build());
+        when(idamService.retrieveSystemUpdateUserDetails()).thenReturn(user);
+        when(authTokenGenerator.generate()).thenReturn(SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldTriggerSystemUpdateTaskOnEachCaseWhenFinalOrderApp1DateDeclaredIntentionSwitchToSoleIsPastFourteenDays() {
+        final LocalDate datePast14Days = LocalDate.now().minusDays(15);
+        final LocalDate dateNotPast14Days = LocalDate.now().minusDays(6);
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final CaseDetails caseDetails3 = mock(CaseDetails.class);
+        final CaseData caseData1 = CaseData.builder()
+            .finalOrder(
+                FinalOrder.builder()
+                    .dateApplicant1DeclaredIntentionToSwitchToSoleFo(datePast14Days)
+                    .finalOrderApplicant1NotifiedCanSwitchToSoleAfterIntention(NO)
+                    .build()
+            )
+            .build();
+        final CaseData caseData2 = CaseData.builder()
+            .finalOrder(
+                FinalOrder.builder()
+                    .dateApplicant1DeclaredIntentionToSwitchToSoleFo(dateNotPast14Days)
+                    .finalOrderApplicant1NotifiedCanSwitchToSoleAfterIntention(NO)
+                    .build()
+            )
+            .build();
+
+        when(caseDetails1.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", datePast14Days,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+        when(caseDetails2.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", datePast14Days,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+        when(caseDetails3.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", dateNotPast14Days,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+
+        when(objectMapper.convertValue(Map.of(
+            "dateApplicant1DeclaredIntentionToSwitchToSoleFo", datePast14Days,
+            "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO), CaseData.class)).thenReturn(caseData1);
+        when(objectMapper.convertValue(Map.of(
+            "dateApplicant1DeclaredIntentionToSwitchToSoleFo", dateNotPast14Days,
+            "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO), CaseData.class)).thenReturn(caseData2);
+
+
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2, caseDetails3);
+
+        when(ccdSearchService.searchForAllCasesWithQuery(query, user, SERVICE_AUTHORIZATION, AwaitingJointFinalOrder))
+            .thenReturn(caseDetailsList);
+
+        task.run();
+
+        verify(ccdUpdateService).submitEvent(caseDetails1, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+        verify(ccdUpdateService).submitEvent(caseDetails2, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+        verifyNoMoreInteractions(ccdUpdateService);
+    }
+
+    @Test
+    void shouldNotTriggerSystemUpdateTaskOnCaseWhenCaseApp1DateDeclaredIntentionIsNotPastFourteenDays() {
+        final LocalDate date = LocalDate.now().minusDays(6);
+        final CaseDetails caseDetails = mock(CaseDetails.class);
+        final CaseData caseData = CaseData.builder()
+            .finalOrder(
+                FinalOrder.builder()
+                    .dateApplicant1DeclaredIntentionToSwitchToSoleFo(date)
+                    .finalOrderApplicant1NotifiedCanSwitchToSoleAfterIntention(NO)
+                    .build()
+            )
+            .build();
+
+        when(caseDetails.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+
+        when(objectMapper.convertValue(Map.of(
+            "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+            "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO), CaseData.class)).thenReturn(caseData);
+
+        when(ccdSearchService.searchForAllCasesWithQuery(query, user, SERVICE_AUTHORIZATION, AwaitingJointFinalOrder))
+            .thenReturn(singletonList(caseDetails));
+
+        task.run();
+
+        verifyNoInteractions(ccdUpdateService);
+    }
+
+    @Test
+    void shouldNotSubmitEventIfSearchFails() {
+        when(ccdSearchService.searchForAllCasesWithQuery(query, user, SERVICE_AUTHORIZATION, AwaitingJointFinalOrder))
+            .thenThrow(new CcdSearchCaseException("Failed to search cases", mock(FeignException.class)));
+
+        task.run();
+
+        verifyNoInteractions(ccdUpdateService);
+    }
+
+    @Test
+    void shouldStopProcessingIfThereIsConflictDuringSubmission() {
+        final LocalDate date = LocalDate.now().minusDays(15);
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
+        final CaseData caseData = CaseData.builder()
+            .finalOrder(
+                FinalOrder.builder()
+                    .dateApplicant1DeclaredIntentionToSwitchToSoleFo(date)
+                    .finalOrderApplicant1NotifiedCanSwitchToSoleAfterIntention(NO)
+                    .build()
+            )
+            .build();
+
+        when(caseDetails1.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+
+        when(objectMapper.convertValue(Map.of(
+            "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+            "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO), CaseData.class)).thenReturn(caseData);
+
+        when(ccdSearchService.searchForAllCasesWithQuery(query, user, SERVICE_AUTHORIZATION, AwaitingJointFinalOrder))
+            .thenReturn(caseDetailsList);
+
+        doThrow(new CcdConflictException("Case is modified by another transaction", mock(FeignException.class)))
+            .when(ccdUpdateService).submitEvent(caseDetails1, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+
+        task.run();
+
+        verify(ccdUpdateService)
+            .submitEvent(caseDetails1, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+        verify(ccdUpdateService, never())
+            .submitEvent(caseDetails2, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+    }
+
+    @Test
+    void shouldContinueToNextCaseIfExceptionIsThrownWhileProcessingPreviousCase() {
+        final LocalDate date = LocalDate.now().minusDays(15);
+        final CaseDetails caseDetails1 = mock(CaseDetails.class);
+        final CaseDetails caseDetails2 = mock(CaseDetails.class);
+        final CaseData caseData = CaseData.builder()
+            .finalOrder(
+                FinalOrder.builder()
+                    .dateApplicant1DeclaredIntentionToSwitchToSoleFo(date)
+                    .finalOrderApplicant1NotifiedCanSwitchToSoleAfterIntention(NO)
+                    .build()
+            )
+            .build();
+
+        when(caseDetails1.getData()).thenReturn(Map.of("dueDate", LocalDate.now().toString()));
+        when(caseDetails2.getData()).thenReturn(Map.of("dueDate", LocalDate.now().minusDays(5).toString()));
+
+        final List<CaseDetails> caseDetailsList = List.of(caseDetails1, caseDetails2);
+
+        when(caseDetails1.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+
+        when(caseDetails2.getData()).thenReturn(
+            Map.of(
+                "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+                "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO
+            )
+        );
+
+        when(objectMapper.convertValue(Map.of(
+            "dateApplicant1DeclaredIntentionToSwitchToSoleFo", date,
+            "finalOrderApplicantNotifiedCanSwitchToSoleAfterIntention", NO), CaseData.class)).thenReturn(caseData);
+
+
+        when(ccdSearchService.searchForAllCasesWithQuery(query, user, SERVICE_AUTHORIZATION, AwaitingJointFinalOrder))
+            .thenReturn(caseDetailsList);
+
+        doThrow(new CcdManagementException(REQUEST_TIMEOUT, "Failed processing of case", mock(FeignException.class)))
+            .when(ccdUpdateService).submitEvent(caseDetails1, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+
+        task.run();
+
+        verify(ccdUpdateService).submitEvent(caseDetails2, SYSTEM_APPLICANT_SWITCH_TO_SOLE_AFTER_INTENTION, user, SERVICE_AUTHORIZATION);
+        verifyNoMoreInteractions(ccdUpdateService);
+    }
+}
