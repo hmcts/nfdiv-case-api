eefe28265306e3437e09adb4876266fd132184f5
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-11-10 17:09:55 +0000

    NFDIV-2657: Set intends fields for citizen users

--- src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenIntendToSwitchToSoleFOTest.java
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenIntendToSwitchToSoleFOTest.java
@@ -8,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.citizen.notification.Applicant1IntendToSwitchToSoleFoNotification;
 import uk.gov.hmcts.divorce.citizen.notification.Applicant2IntendToSwitchToSoleFoNotification;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
@@ -18,13 +19,18 @@ import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.time.Clock;
+import java.time.LocalDate;
+
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.citizen.event.CitizenIntendToSwitchToSoleFO.INTEND_SWITCH_TO_SOLE_FO;
+import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.setMockClock;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
@@ -49,6 +55,9 @@ public class CitizenIntendToSwitchToSoleFOTest {
     @Mock
     private Applicant2IntendToSwitchToSoleFoNotification applicant2IntendToSwitchToSoleFoNotification;
 
+    @Mock
+    private Clock clock;
+
     @InjectMocks
     private CitizenIntendToSwitchToSoleFO citizenIntendToSwitchToSoleFO;
 
@@ -61,6 +70,45 @@ public class CitizenIntendToSwitchToSoleFOTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(INTEND_SWITCH_TO_SOLE_FO);
+    }
+
+    @Test
+    void shouldSetApplicant1IntendsFieldsIfTriggeredByApplicant1() {
+
+        setMockClock(clock);
+
+        final CaseData caseData = CaseData.builder().build();
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+
+        when(request.getHeader(eq(AUTHORIZATION))).thenReturn(DUMMY_AUTH_TOKEN);
+        when(ccdAccessService.isApplicant1(DUMMY_AUTH_TOKEN, TEST_CASE_ID)).thenReturn(true);
+
+        AboutToStartOrSubmitResponse<CaseData, State> response = citizenIntendToSwitchToSoleFO.aboutToSubmit(details, details);
+
+        assertThat(response.getData().getFinalOrder().getDoesApplicant1IntendToSwitchToSole()).isEqualTo(YES);
+        assertThat(response.getData().getFinalOrder().getDateApplicant1DeclaredIntentionToSwitchToSoleFo()).isEqualTo(LocalDate.now(clock));
+    }
+
+    @Test
+    void shouldSetApplicant2IntendsFieldsIfTriggeredByApplicant2() {
+
+        setMockClock(clock);
+
+        final CaseData caseData = CaseData.builder().build();
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+
+        when(request.getHeader(eq(AUTHORIZATION))).thenReturn(DUMMY_AUTH_TOKEN);
+        when(ccdAccessService.isApplicant1(DUMMY_AUTH_TOKEN, TEST_CASE_ID)).thenReturn(false);
+        when(ccdAccessService.isApplicant2(DUMMY_AUTH_TOKEN, TEST_CASE_ID)).thenReturn(true);
+
+        AboutToStartOrSubmitResponse<CaseData, State> response = citizenIntendToSwitchToSoleFO.aboutToSubmit(details, details);
+
+        assertThat(response.getData().getFinalOrder().getDoesApplicant2IntendToSwitchToSole()).isEqualTo(YES);
+        assertThat(response.getData().getFinalOrder().getDateApplicant2DeclaredIntentionToSwitchToSoleFo()).isEqualTo(LocalDate.now(clock));
     }
 
     @Test
