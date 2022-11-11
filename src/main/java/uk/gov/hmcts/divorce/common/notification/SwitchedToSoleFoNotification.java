614a69c8f94cb32bdb3831e7983f804587f5e40d
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-11-11 12:06:49 +0000

    NFDIV-2666 & NFDIV-2753: Add new notifications after switch to sole completed by citizen

---
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/SwitchedToSoleFoNotification.java
@@ -0,0 +1,55 @@
+package uk.gov.hmcts.divorce.common.notification;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.notification.ApplicantNotification;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import java.util.Map;
+
+import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICANT_NAME;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.PARTNER_HAS_SWITCHED_TO_SOLE_FINAL_ORDER;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_APPLIED_FOR_FINAL_ORDER;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_SOLE_APPLICATION_FOR_CONDITIONAL_ORDER;
+
+@Component
+@Slf4j
+public class SwitchedToSoleFoNotification implements ApplicantNotification {
+
+    @Autowired
+    private CommonContent commonContent;
+
+    @Autowired
+    private NotificationService notificationService;
+
+
+    @Override
+    public void sendToApplicant1(final CaseData caseData, final Long id) {
+
+        log.info("Notifying solicitor that they made a sole application for a final order: {}", id);
+
+        notificationService.sendEmail(
+            caseData.getApplicant1().getEmail(),
+            SOLE_APPLIED_FOR_FINAL_ORDER,
+            commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
+            caseData.getApplicant1().getLanguagePreference()
+        );
+    }
+
+    @Override
+    public void sendToApplicant2(final CaseData caseData, final Long id) {
+
+        log.info("Notifying solicitor that they made a sole application for a final order: {}", id);
+
+        notificationService.sendEmail(
+            caseData.getApplicant2().getEmail(),
+            PARTNER_HAS_SWITCHED_TO_SOLE_FINAL_ORDER,
+            commonContent.mainTemplateVars(caseData, id, caseData.getApplicant1(), caseData.getApplicant2()),
+            caseData.getApplicant2().getLanguagePreference()
+        );
+    }
+}
