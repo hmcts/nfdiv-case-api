80c8a12cdd40b6e19696cfdc2f94aea2c59c128a
Tom Elliott <tomelliott@Toms-MacBook-Pro.local>
2022-11-11 14:07:37 +0000

    NFDIV-2666 & 2753: Resolve checkstyle

--- src/main/java/uk/gov/hmcts/divorce/common/notification/SwitchedToSoleFoNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/SwitchedToSoleFoNotification.java
@@ -3,18 +3,13 @@ package uk.gov.hmcts.divorce.common.notification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
-import java.util.Map;
-
-import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICANT_NAME;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.PARTNER_HAS_SWITCHED_TO_SOLE_FINAL_ORDER;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_APPLIED_FOR_FINAL_ORDER;
-import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_SOLE_APPLICATION_FOR_CONDITIONAL_ORDER;
 
 @Component
 @Slf4j
