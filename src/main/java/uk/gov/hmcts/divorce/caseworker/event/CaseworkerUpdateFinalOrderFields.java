29c0d8f7936d34f62896031dd32254d2eaf47dc5
Nitin Prabhu <nitinprabhu@users.noreply.github.com>
2022-11-15 16:36:37 +0000

    Remove date final order eligible from update fo event (#2635)

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUpdateFinalOrderFields.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerUpdateFinalOrderFields.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
-import uk.gov.hmcts.divorce.divorcecase.model.FinalOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
@@ -42,9 +41,6 @@ public class CaseworkerUpdateFinalOrderFields implements CCDConfig<CaseData, Sta
             .grantHistoryOnly(CASE_WORKER, LEGAL_ADVISOR))
             .page("updateFinalOrderFields")
             .pageLabel("Update FO fields")
-            .complex(CaseData::getFinalOrder)
-                .mandatory(FinalOrder::getDateFinalOrderEligibleFrom)
-                .done()
             .complex(CaseData::getConditionalOrder)
                 .mandatory(ConditionalOrder::getDateAndTimeOfHearing)
             .done();
