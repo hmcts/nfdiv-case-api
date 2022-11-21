8c338fd4fdc6c98198859000e43ea50c60b30f7e
AbdullahShahHMCTS <Abdullah.shah@hmcts.net>
2022-11-21 15:29:03 +0000

    NFDIV-3071 fix checkstyles

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendApplicationType.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendApplicationType.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -12,25 +11,13 @@ import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.document.content.CoversheetApplicantTemplateContent;
 
-import java.time.Clock;
-import java.time.LocalDate;
-
-import static java.time.LocalDateTime.now;
-import static java.util.Collections.singletonList;
 import static java.util.Objects.isNull;
-import static org.springframework.util.CollectionUtils.isEmpty;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
-import static uk.gov.hmcts.divorce.caseworker.service.task.util.FileNameUtil.formatDocumentName;
 import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DISSOLUTION;
 import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DIVORCE;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.*;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.*;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
-import static uk.gov.hmcts.divorce.document.DocumentConstants.AMENDED_APPLICATION_COVERSHEET_DOCUMENT_NAME;
-import static uk.gov.hmcts.divorce.document.DocumentConstants.COVERSHEET_APPLICANT;
 
 @Component
 @Slf4j
@@ -56,14 +43,14 @@ public class CaseworkerAmendApplicationType implements CCDConfig<CaseData, State
         log.info("Caseworker Amend Application Type about to submit callback invoked for case id: {}", details.getId());
         CaseData caseData = details.getData();
 
-        if(!isNull(caseData.getApplicationType())) {
-            if (caseData.getDivorceOrDissolution().equals(DIVORCE)) {
+        if(!isNull(caseData.getApplicationType())){
+            if(caseData.getDivorceOrDissolution().equals(DIVORCE)){
                 caseData.setDivorceOrDissolution(DISSOLUTION);
             } else {
                 caseData.setDivorceOrDissolution(DIVORCE);
             }
         }
-        
+
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(caseData)
             .build();
