4e2e0bd820eabce2ed3b52a1a3fc0c97ca6b2284
Sunil Natikar <sunil.natikar@justice.gov.uk>
2022-11-23 13:59:16 +0000

    corrected test

--- src/integrationTest/java/uk/gov/hmcts/divorce/document/content/NoticeOfProceedingContentIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/document/content/NoticeOfProceedingContentIT.java
@@ -8,13 +8,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
 import uk.gov.hmcts.ccd.sdk.type.Organisation;
 import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
-import uk.gov.hmcts.divorce.common.config.DocmosisTemplatesConfig;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseInvite;
 import uk.gov.hmcts.divorce.divorcecase.model.CtscContactDetails;
 import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.notification.CommonContent;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -143,12 +141,6 @@ import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class NoticeOfProceedingContentIT {
-
-    @Autowired
-    private CommonContent commonContent;
-
-    @Autowired
-    private DocmosisTemplatesConfig config;
 
     @Autowired
     private NoticeOfProceedingContent noticeOfProceedingContent;
