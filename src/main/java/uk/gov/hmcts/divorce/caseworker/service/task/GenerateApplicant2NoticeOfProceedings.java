15d799e5d4c58583b65ce56449837baf710aafa2
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-11-25 12:47:21 +0000

    NFDIV-2654: fix checkstyle

--- src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateApplicant2NoticeOfProceedings.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateApplicant2NoticeOfProceedings.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference;
