424f6e6855edbcc0de6550f8626b39e88453de63
Nitin Prabhu <prabhunitin61@gmail.com>
2022-11-29 09:56:43 +0000

    Fix checkstyle

--- src/main/java/uk/gov/hmcts/divorce/common/event/SwitchedToSoleFinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/SwitchedToSoleFinalOrder.java
@@ -17,8 +17,8 @@ import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.EnumSet;
+import javax.servlet.http.HttpServletRequest;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
