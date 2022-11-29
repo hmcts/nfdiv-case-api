690bce54d64e172359b7abaca691343b61f1ef6f
Nitin Prabhu <prabhunitin61@gmail.com>
2022-11-29 15:30:37 +0000

    Fix code smell

--- src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerRetryPronounceList.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerRetryPronounceList.java
@@ -26,6 +26,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class CaseworkerRetryPronounceList implements CCDConfig<BulkActionCaseData, BulkActionState, UserRole> {
 
     public static final String CASEWORKER_RETRY_PRONOUNCE_LIST = "caseworker-retry-pronounce-list";
+    private static final String RETRY_PRONOUNCE_LIST = "Retry Pronounce list";
 
     @Autowired
     private CasePronouncementService casePronouncementService;
@@ -35,15 +36,15 @@ public class CaseworkerRetryPronounceList implements CCDConfig<BulkActionCaseDat
         new BulkActionPageBuilder(configBuilder
             .event(CASEWORKER_RETRY_PRONOUNCE_LIST)
             .forStates(Pronounced)
-            .name("Retry Pronounce list")
-            .description("Retry Pronounce list")
+            .name(RETRY_PRONOUNCE_LIST)
+            .description(RETRY_PRONOUNCE_LIST)
             .showSummary()
             .showEventNotes()
             .submittedCallback(this::submitted)
             .grant(CREATE_READ_UPDATE, SUPER_USER)
             .grantHistoryOnly(CASE_WORKER, LEGAL_ADVISOR, SOLICITOR, CITIZEN))
             .page("retryPronounceList")
-            .pageLabel("Retry Pronounce list")
+            .pageLabel(RETRY_PRONOUNCE_LIST)
             .mandatory(BulkActionCaseData::getPronouncementJudge, null, "District Judge");
     }
 
