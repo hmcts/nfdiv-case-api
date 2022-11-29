f21328001fc4904f515d90eef710b364b48b5480
Nitin Prabhu <prabhunitin61@gmail.com>
2022-11-29 14:59:56 +0000

    Make pronouncement judge editable in retry pronouncement list for super users

--- src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerRetryPronounceList.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerRetryPronounceList.java
@@ -41,7 +41,10 @@ public class CaseworkerRetryPronounceList implements CCDConfig<BulkActionCaseDat
             .showEventNotes()
             .submittedCallback(this::submitted)
             .grant(CREATE_READ_UPDATE, SUPER_USER)
-            .grantHistoryOnly(CASE_WORKER, LEGAL_ADVISOR, SOLICITOR, CITIZEN));
+            .grantHistoryOnly(CASE_WORKER, LEGAL_ADVISOR, SOLICITOR, CITIZEN))
+            .page("retryPronounceList")
+            .pageLabel("Retry Pronounce list")
+            .mandatory(BulkActionCaseData::getPronouncementJudge, null, "District Judge");
     }
 
     public SubmittedCallbackResponse submitted(
