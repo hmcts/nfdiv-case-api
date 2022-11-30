81c7b736f32b704633cb96cc50589e9947ffba8a
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-11-30 10:45:30 +0000

    NFDIV-2654: improved test coverage

--- src/test/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateD10FormTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateD10FormTest.java
@@ -1,33 +1,48 @@
 package uk.gov.hmcts.divorce.caseworker.service.task;
 
+import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.ccd.sdk.type.Organisation;
+import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
 import uk.gov.hmcts.divorce.common.service.task.GenerateFormHelper;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
-import uk.gov.hmcts.reform.document.domain.Document;
+import uk.gov.hmcts.reform.ccd.client.model.CaseAssignmentUserRolesRequest;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
+import static reactor.core.publisher.Mono.when;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.COURT_SERVICE;
 import static uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod.SOLICITOR_SERVICE;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.D10;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SYSTEM_UPDATE_AUTH_TOKEN;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.feignException;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.validApplicant1CaseData;
 
 @ExtendWith(MockitoExtension.class)
@@ -50,13 +65,35 @@ public class GenerateD10FormTest {
         caseDetails.setData(caseData);
         caseDetails.setId(TEST_CASE_ID);
 
-        final Document document = new Document();
-        document.links = new Document.Links();
-        document.links.self = new Document.Link();
-        document.links.binary = new Document.Link();
-        document.links.self.href = "/";
-        document.links.binary.href = "/binary";
-        document.originalDocumentName = "D10";
+        generateD10Form.apply(caseDetails);
+        verify(generateFormHelper).addFormToGeneratedDocuments(
+            caseData,
+            D10,
+            "D10",
+            "D10.pdf",
+            "/D10.pdf");
+    }
+
+    @Test
+    void shouldGenerateD10DocumentAndAddToListOfDocumentsGeneratedForApp2Offline() throws IOException {
+        final CaseData caseData = validApplicant1CaseData();
+        caseData.setApplicationType(SOLE_APPLICATION);
+        caseData.getApplication().setServiceMethod(COURT_SERVICE);
+        caseData.getApplicant2().setSolicitorRepresented(YES);
+        caseData.getApplicant2().setOffline(YES);
+        caseData.setIsJudicialSeparation(NO);
+        Solicitor solicitor = Solicitor.builder()
+            .organisationPolicy(OrganisationPolicy.<UserRole>builder()
+                .organisation(Organisation.builder()
+                    .build())
+                .build())
+            .build();
+        caseData.getApplicant2().setSolicitor(solicitor);
+        caseData.getDocuments().setDocumentsGenerated(new ArrayList<>());
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
 
         generateD10Form.apply(caseDetails);
         verify(generateFormHelper).addFormToGeneratedDocuments(
@@ -148,6 +185,22 @@ public class GenerateD10FormTest {
         final var result = generateD10Form.apply(caseDetails);
 
         verifyNoInteractions(generateFormHelper);
+        assertThat(result.getData()).isEqualTo(caseData);
+    }
+    @Test
+    void shouldCatchExceptionForGenerateFormError() throws IOException {
+        final CaseData caseData = validApplicant1CaseData();
+        caseData.setApplicationType(SOLE_APPLICATION);
+        caseData.getApplication().setServiceMethod(SOLICITOR_SERVICE);
+        caseData.getDocuments().setDocumentsGenerated(new ArrayList<>());
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        doThrow(IOException.class).when(generateFormHelper).addFormToGeneratedDocuments(any(), any(), any(), any(), any());
+
+        final var result = generateD10Form.apply(caseDetails);
         assertThat(result.getData()).isEqualTo(caseData);
     }
 }
