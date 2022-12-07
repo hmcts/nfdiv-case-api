2978ca80e5bac1d2b30e9709d2f85f652f303af5
Barry Weeks <barry.weeks@justice.gov.uk>
2022-12-07 10:30:15 +0000

    NFDiv-2607 Remove duplicate docmosis content addition

--- src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
+++ src/main/java/uk/gov/hmcts/divorce/document/content/CoversheetApplicantTemplateContent.java
@@ -9,14 +9,6 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import java.util.Map;
 
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CASE_REFERENCE;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_DIVORCE_JUSTICE_GOV_UK;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_EMAIL;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER_TEXT;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES_TEXT;
 import static uk.gov.hmcts.divorce.notification.FormatUtil.formatId;
 
 @Component
@@ -37,10 +29,6 @@ public class CoversheetApplicantTemplateContent {
         templateContent.put("applicantFirstName", applicant.getFirstName());
         templateContent.put("applicantLastName", applicant.getLastName());
         templateContent.put("applicantAddress", applicant.getPostalAddress());
-        templateContent.put(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT);
-        templateContent.put(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT);
-        templateContent.put(CONTACT_EMAIL, CONTACT_DIVORCE_JUSTICE_GOV_UK);
-        templateContent.put(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT);
         return templateContent;
     }
 }
