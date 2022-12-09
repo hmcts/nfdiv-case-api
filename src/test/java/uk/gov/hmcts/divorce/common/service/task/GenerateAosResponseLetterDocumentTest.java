22e43b9e840adc7350daa19986eeb4fb89af2181
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-12-09 12:02:29 +0000

    NFDIV-2900: added response letter code

--- src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAosResponseLetterDocumentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAosResponseLetterDocumentTest.java
@@ -26,6 +26,7 @@ import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.HowToRespondApplication.DISPUTE_DIVORCE;
 import static uk.gov.hmcts.divorce.divorcecase.model.HowToRespondApplication.WITHOUT_DISPUTE_DIVORCE;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.AOS_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.NFD_NOP_APP1_JS_SOLE_DISPUTED;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.RESPONDENT_RESPONDED_DISPUTED_TEMPLATE_ID;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.RESPONDENT_RESPONDED_UNDEFENDED_TEMPLATE_ID;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.AOS_RESPONSE_LETTER;
@@ -123,6 +124,105 @@ class GenerateAosResponseLetterDocumentTest {
 
         verifyNoMoreInteractions(caseDataDocumentService);
         verifyNoInteractions(aosResponseLetterTemplateContent);
+    }
+
+    @Test
+    void shouldGenerateRespondentRespondedDocWhenApplicant1IsOfflineAndDisputedAndJS() {
+
+        final CaseData caseData = caseData();
+        caseData.setIsJudicialSeparation(YES);
+        caseData.getApplicant1().setOffline(YES);
+        caseData.getAcknowledgementOfService().setHowToRespondApplication(DISPUTE_DIVORCE);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final Map<String, Object> templateContent = new HashMap<>();
+
+        when(aosResponseLetterTemplateContent.apply(caseData, TEST_CASE_ID))
+            .thenReturn(templateContent);
+
+        generateAosResponseLetterDocument.apply(caseDetails);
+
+        verify(caseDataDocumentService)
+            .renderDocumentAndUpdateCaseData(
+                caseData,
+                AOS_RESPONSE_LETTER,
+                templateContent,
+                TEST_CASE_ID,
+                NFD_NOP_APP1_JS_SOLE_DISPUTED,
+                caseData.getApplicant1().getLanguagePreference(),
+                AOS_RESPONSE_LETTER_DOCUMENT_NAME
+            );
+
+        verifyNoMoreInteractions(caseDataDocumentService);
+    }
+
+    @Test
+    void shouldNotGenerateRespondentRespondedDocWhenApplicant1IsOfflineAndDisputedAndJS() {
+
+        final CaseData caseData = caseData();
+        caseData.setIsJudicialSeparation(YES);
+        caseData.getApplicant1().setOffline(YES);
+        caseData.getAcknowledgementOfService().setHowToRespondApplication(WITHOUT_DISPUTE_DIVORCE);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final Map<String, Object> templateContent = new HashMap<>();
+
+        generateAosResponseLetterDocument.apply(caseDetails);
+
+        verifyNoMoreInteractions(caseDataDocumentService);
+    }
+
+    @Test
+    void shouldNotGenerateRespondentAnswerDocWhenApplicant1IsOfflineAndIsDisputedAndNotJS() {
+
+        final CaseData caseData = caseData();
+        caseData.getApplicant1().setOffline(YES);
+        caseData.getAcknowledgementOfService().setHowToRespondApplication(DISPUTE_DIVORCE);
+        caseData.setIsJudicialSeparation(NO);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final Map<String, Object> templateContent = new HashMap<>();
+
+        when(aosResponseLetterTemplateContent.apply(caseData, TEST_CASE_ID))
+            .thenReturn(templateContent);
+
+        doNothing()
+            .when(caseDataDocumentService).renderDocumentAndUpdateCaseData(
+                caseData,
+                AOS_RESPONSE_LETTER,
+                templateContent,
+                TEST_CASE_ID,
+                RESPONDENT_RESPONDED_DISPUTED_TEMPLATE_ID,
+                caseData.getApplicant1().getLanguagePreference(),
+                AOS_RESPONSE_LETTER_DOCUMENT_NAME
+            );
+
+        final CaseDetails<CaseData, State> result = generateAosResponseLetterDocument.apply(caseDetails);
+
+        verify(caseDataDocumentService)
+            .renderDocumentAndUpdateCaseData(
+                caseData,
+                AOS_RESPONSE_LETTER,
+                templateContent,
+                TEST_CASE_ID,
+                RESPONDENT_RESPONDED_DISPUTED_TEMPLATE_ID,
+                caseData.getApplicant1().getLanguagePreference(),
+                AOS_RESPONSE_LETTER_DOCUMENT_NAME
+            );
+
+        verifyNoMoreInteractions(caseDataDocumentService);
+        verifyNoInteractions(aosUndefendedResponseLetterTemplateContent);
+
+        assertThat(result.getData()).isEqualTo(caseData);
     }
 
     @Test
