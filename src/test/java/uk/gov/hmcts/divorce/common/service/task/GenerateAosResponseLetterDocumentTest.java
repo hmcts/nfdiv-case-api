327db6d244810217884fcfa4bf77afbf39523bfe
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2022-12-15 10:36:58 +0000

    NFDIV-2903: added new template parms

--- src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAosResponseLetterDocumentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAosResponseLetterDocumentTest.java
@@ -26,6 +26,7 @@ import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.HowToRespondApplication.DISPUTE_DIVORCE;
 import static uk.gov.hmcts.divorce.divorcecase.model.HowToRespondApplication.WITHOUT_DISPUTE_DIVORCE;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.AOS_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.NFD_NOP_APP1_SOL_JS_SOLE_DISPUTED;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.RESPONDENT_RESPONDED_DISPUTED_TEMPLATE_ID;
 import static uk.gov.hmcts.divorce.document.DocumentConstants.RESPONDENT_RESPONDED_UNDEFENDED_TEMPLATE_ID;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.AOS_RESPONSE_LETTER;
@@ -117,6 +118,40 @@ class GenerateAosResponseLetterDocumentTest {
                 templateContent,
                 TEST_CASE_ID,
                 RESPONDENT_RESPONDED_UNDEFENDED_TEMPLATE_ID,
+                caseData.getApplicant1().getLanguagePreference(),
+                AOS_RESPONSE_LETTER_DOCUMENT_NAME
+            );
+
+        verifyNoMoreInteractions(caseDataDocumentService);
+        verifyNoInteractions(aosResponseLetterTemplateContent);
+    }
+
+    @Test
+    void shouldGenerateRespondentRespondedDocWhenApplicant1IsOfflineAndUndisputedAndJS() {
+
+        final CaseData caseData = caseData();
+        caseData.getApplicant1().setOffline(YES);
+        caseData.getAcknowledgementOfService().setHowToRespondApplication(WITHOUT_DISPUTE_DIVORCE);
+        caseData.setIsJudicialSeparation(YES);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final Map<String, Object> templateContent = new HashMap<>();
+
+        when(aosUndefendedResponseLetterTemplateContent.apply(caseData, TEST_CASE_ID))
+            .thenReturn(templateContent);
+
+        generateAosResponseLetterDocument.apply(caseDetails);
+
+        verify(caseDataDocumentService)
+            .renderDocumentAndUpdateCaseData(
+                caseData,
+                AOS_RESPONSE_LETTER,
+                templateContent,
+                TEST_CASE_ID,
+                NFD_NOP_APP1_SOL_JS_SOLE_DISPUTED,
                 caseData.getApplicant1().getLanguagePreference(),
                 AOS_RESPONSE_LETTER_DOCUMENT_NAME
             );
