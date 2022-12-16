49e500781b7b070c37cb4ebcced7cf0d07598a8b
Barry Weeks <barry.weeks@justice.gov.uk>
2022-12-16 12:40:35 +0000

    NFDiv-2917 and NFDiv-2956 Add JS CO Clarification Refusal content generator class

---
+++ src/main/java/uk/gov/hmcts/divorce/document/content/GenerateJudicialSeparationCORefusedForClarificationCoverLetter.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.divorce.document.content;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.document.model.DocumentType;
+
+@Component
+@Slf4j
+public class GenerateJudicialSeparationCORefusedForClarificationCoverLetter extends
+    GenerateJudicialSeparationCORefusedForAmendmentCoverLetter {
+
+    @Autowired
+    private ConditionalOrderCommonContent conditionalOrderCommonContent;
+
+    public DocumentType getDocumentType(final CaseData caseData, final Applicant applicant) {
+        return conditionalOrderCommonContent.getDocumentType(caseData, applicant, true);
+    }
+}
