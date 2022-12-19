86ad4aa5fffd6d7f6ac42a80849ad76650dfc38a
danielbe11 <daniel.bell@solirius.com>
2022-12-19 15:10:04 +0000

    Unused import

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
@@ -35,7 +35,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 import java.util.stream.Stream;
 
 import static java.lang.Integer.parseInt;
