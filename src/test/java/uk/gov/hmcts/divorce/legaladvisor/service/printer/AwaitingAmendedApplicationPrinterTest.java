9de13527b55a90370291bb963746b29d2bdc4913
Barry Weeks <barry.weeks@justice.gov.uk>
2023-01-23 23:43:55 +0000

    NFDiv-2917 Refactor Tests

--- src/test/java/uk/gov/hmcts/divorce/legaladvisor/service/printer/AwaitingAmendedApplicationPrinterTest.java
+++ src/test/java/uk/gov/hmcts/divorce/legaladvisor/service/printer/AwaitingAmendedApplicationPrinterTest.java
@@ -7,36 +7,18 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
-import uk.gov.hmcts.divorce.caseworker.service.task.GenerateCoversheet;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
-import uk.gov.hmcts.divorce.divorcecase.model.Application;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments;
-import uk.gov.hmcts.divorce.document.content.CoversheetApplicantTemplateContent;
-import uk.gov.hmcts.divorce.document.content.CoversheetSolicitorTemplateContent;
-import uk.gov.hmcts.divorce.document.content.GenerateJudicialSeparationCORefusedForAmendmentCoverLetter;
-import uk.gov.hmcts.divorce.document.model.DivorceDocument;
-import uk.gov.hmcts.divorce.document.print.BulkPrintService;
-import uk.gov.hmcts.divorce.document.print.model.Print;
-import uk.gov.hmcts.divorce.legaladvisor.service.task.GenerateCoRefusedCoverLetter;
 
-import static java.util.Arrays.asList;
-import static java.util.Collections.emptyList;
-import static java.util.UUID.randomUUID;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
-import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
-import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
-import static uk.gov.hmcts.divorce.document.DocumentConstants.COVERSHEET_APPLICANT;
-import static uk.gov.hmcts.divorce.document.DocumentConstants.COVERSHEET_APPLICANT2_SOLICITOR;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.APPLICATION;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_REFUSAL;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_REFUSAL_COVER_LETTER;
@@ -49,71 +31,23 @@ import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 public class AwaitingAmendedApplicationPrinterTest {
 
     @Mock
-    private BulkPrintService bulkPrintService;
-
-    @Mock
-    private GenerateCoversheet generateCoversheet;
-
-    @Mock
-    private CoversheetApplicantTemplateContent coversheetApplicantTemplateContent;
-
-    @Mock
-    private CoversheetSolicitorTemplateContent coversheetSolicitorTemplateContent;
-
-    @Mock
-    private GenerateCoRefusedCoverLetter generateCoRefusedCoverLetter;
-
-    @Mock
-    private GenerateJudicialSeparationCORefusedForAmendmentCoverLetter generateJudicialSeparationCORefusedForAmendmentCoverLetter;
+    private AwaitingAmendedOrClarificationApplicationCommonPrinter awaitingAmendedOrClarificationApplicationCommonPrinter;
 
     @InjectMocks
     private AwaitingAmendedApplicationPrinter awaitingAmendedApplicationPrinter;
 
     @Captor
-    ArgumentCaptor<Print> printCaptor;
+    ArgumentCaptor<MissingDocumentsValidation> missingDocumentsValidationCaptor;
 
     @Test
     void shouldPrintAwaitingAmendedApplicationPack() {
-
-        final ListValue<DivorceDocument> coversheetDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(COVERSHEET)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coCanApplyDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(CONDITIONAL_ORDER_REFUSAL_COVER_LETTER)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coRefusalDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(CONDITIONAL_ORDER_REFUSAL)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> applicationDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(APPLICATION)
-                .build())
-            .build();
 
         final CaseData caseData = CaseData.builder()
             .applicationType(SOLE_APPLICATION)
             .applicant1(Applicant.builder().languagePreferenceWelsh(NO).build())
             .applicant2(Applicant.builder().build())
-            .documents(
-                CaseDocuments.builder()
-                    .documentsGenerated(asList(coversheetDoc, coCanApplyDoc, coRefusalDoc, applicationDoc))
-                    .build()
-            )
             .isJudicialSeparation(YesOrNo.NO)
             .build();
-
-        when(bulkPrintService.print(printCaptor.capture())).thenReturn(randomUUID());
-        when(generateJudicialSeparationCORefusedForAmendmentCoverLetter.getCoverLetterDocumentType(caseData, caseData.getApplicant1()))
-            .thenReturn(CONDITIONAL_ORDER_REFUSAL_COVER_LETTER);
 
         awaitingAmendedApplicationPrinter.sendLetters(
             caseData,
@@ -121,75 +55,33 @@ public class AwaitingAmendedApplicationPrinterTest {
             caseData.getApplicant1()
         );
 
-        verify(generateCoversheet).generateCoversheet(
+        verify(awaitingAmendedOrClarificationApplicationCommonPrinter).sendLetters(
             eq(caseData),
             eq(TEST_CASE_ID),
-            eq(COVERSHEET_APPLICANT),
-            anyMap(),
-            eq(ENGLISH)
+            eq(caseData.getApplicant1()),
+            missingDocumentsValidationCaptor.capture(),
+            eq(false)
         );
 
-        verify(generateCoRefusedCoverLetter).generateAndUpdateCaseData(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
+        final MissingDocumentsValidation missingDocumentsValidation = missingDocumentsValidationCaptor.getValue();
+        assertThat(missingDocumentsValidation.message).isEqualTo(
+            "Awaiting Amended Application Letter pack has missing documents. Expected documents with type {} , for Case ID: {}"
         );
-
-        final Print print = printCaptor.getValue();
-        assertThat(print.getCaseId()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getCaseRef()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getLetterType()).isEqualTo("awaiting-amended-application-letter");
-        assertThat(print.getLetters().size()).isEqualTo(4);
-        assertThat(print.getLetters().get(0).getDivorceDocument()).isSameAs(coversheetDoc.getValue());
-        assertThat(print.getLetters().get(1).getDivorceDocument()).isSameAs(coCanApplyDoc.getValue());
-        assertThat(print.getLetters().get(2).getDivorceDocument()).isSameAs(coRefusalDoc.getValue());
-        assertThat(print.getLetters().get(3).getDivorceDocument()).isSameAs(applicationDoc.getValue());
-
-        verify(bulkPrintService).print(print);
+        assertThat(missingDocumentsValidation.documentTypeList).isEqualTo(
+            List.of(COVERSHEET, CONDITIONAL_ORDER_REFUSAL_COVER_LETTER, CONDITIONAL_ORDER_REFUSAL, APPLICATION)
+        );
+        assertThat(missingDocumentsValidation.expectedDocumentsSize).isEqualTo(4);
     }
 
     @Test
     void shouldPrintAwaitingAmendedApplicationPackForJudicialSeparation() {
 
-        final ListValue<DivorceDocument> coversheetDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(COVERSHEET)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coCanApplyDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_COVER_LETTER)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coRefusalDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(CONDITIONAL_ORDER_REFUSAL)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> applicationDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(APPLICATION)
-                .build())
-            .build();
-
         final CaseData caseData = CaseData.builder()
-            .applicationType(JOINT_APPLICATION)
-            .applicant1(Applicant.builder().languagePreferenceWelsh(NO).build())
+            .applicationType(SOLE_APPLICATION)
+            .applicant1(Applicant.builder().languagePreferenceWelsh(NO).solicitorRepresented(NO).build())
             .applicant2(Applicant.builder().build())
-            .documents(
-                CaseDocuments.builder()
-                    .documentsGenerated(asList(coversheetDoc, coCanApplyDoc, coRefusalDoc, applicationDoc))
-                    .build()
-            )
-            .isJudicialSeparation(YesOrNo.YES)
+            .isJudicialSeparation(YES)
             .build();
-
-        when(bulkPrintService.print(printCaptor.capture())).thenReturn(randomUUID());
-        when(generateJudicialSeparationCORefusedForAmendmentCoverLetter.getCoverLetterDocumentType(caseData, caseData.getApplicant1()))
-            .thenReturn(JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_COVER_LETTER);
 
         awaitingAmendedApplicationPrinter.sendLetters(
             caseData,
@@ -197,123 +89,37 @@ public class AwaitingAmendedApplicationPrinterTest {
             caseData.getApplicant1()
         );
 
-        verify(generateCoversheet).generateCoversheet(
+        verify(awaitingAmendedOrClarificationApplicationCommonPrinter).sendLetters(
             eq(caseData),
             eq(TEST_CASE_ID),
-            eq(COVERSHEET_APPLICANT),
-            anyMap(),
-            eq(ENGLISH)
+            eq(caseData.getApplicant1()),
+            missingDocumentsValidationCaptor.capture(),
+            eq(false)
         );
 
-        verify(generateJudicialSeparationCORefusedForAmendmentCoverLetter).generateAndUpdateCaseData(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
+        final MissingDocumentsValidation missingDocumentsValidation = missingDocumentsValidationCaptor.getValue();
+        assertThat(missingDocumentsValidation.message).isEqualTo(
+            "Awaiting Amended JS Application Letter pack has missing documents. Expected documents with type {} , for Case ID: {}"
         );
-
-        final Print print = printCaptor.getValue();
-        assertThat(print.getCaseId()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getCaseRef()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getLetterType()).isEqualTo("awaiting-amended-application-letter");
-        assertThat(print.getLetters()).hasSize(4);
-        assertThat(print.getLetters().get(0).getDivorceDocument()).isSameAs(coversheetDoc.getValue());
-        assertThat(print.getLetters().get(1).getDivorceDocument()).isSameAs(coCanApplyDoc.getValue());
-        assertThat(print.getLetters().get(2).getDivorceDocument()).isSameAs(coRefusalDoc.getValue());
-        assertThat(print.getLetters().get(3).getDivorceDocument()).isSameAs(applicationDoc.getValue());
-
-        verify(bulkPrintService).print(print);
+        assertThat(missingDocumentsValidation.documentTypeList).isEqualTo(
+            List.of(
+                COVERSHEET,
+                JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_COVER_LETTER,
+                CONDITIONAL_ORDER_REFUSAL,
+                APPLICATION
+            )
+        );
+        assertThat(missingDocumentsValidation.expectedDocumentsSize).isEqualTo(4);
     }
 
     @Test
     void shouldPrintAwaitingAmendedApplicationSolicitorPackForJudicialSeparation() {
 
-        final ListValue<DivorceDocument> coversheetDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(COVERSHEET)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coCanApplyDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_SOLICITOR_COVER_LETTER)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coRefusalDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(CONDITIONAL_ORDER_REFUSAL)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> applicationDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(APPLICATION)
-                .build())
-            .build();
-
-        final CaseData caseData = CaseData.builder()
-            .applicationType(JOINT_APPLICATION)
-            .applicant1(Applicant.builder().solicitorRepresented(YesOrNo.YES).languagePreferenceWelsh(NO).build())
-            .applicant2(Applicant.builder().build())
-            .documents(
-                CaseDocuments.builder()
-                    .documentsGenerated(asList(coversheetDoc, coCanApplyDoc, coRefusalDoc, applicationDoc))
-                    .build()
-            )
-            .application(Application.builder().newPaperCase(YesOrNo.YES).build())
-            .isJudicialSeparation(YesOrNo.YES)
-            .build();
-
-        when(bulkPrintService.print(printCaptor.capture())).thenReturn(randomUUID());
-        when(generateJudicialSeparationCORefusedForAmendmentCoverLetter.getCoverLetterDocumentType(caseData, caseData.getApplicant1()))
-            .thenReturn(JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_SOLICITOR_COVER_LETTER);
-
-        awaitingAmendedApplicationPrinter.sendLetters(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
-        );
-
-        verify(generateCoversheet).generateCoversheet(
-            eq(caseData),
-            eq(TEST_CASE_ID),
-            eq(COVERSHEET_APPLICANT2_SOLICITOR),
-            anyMap(),
-            eq(ENGLISH)
-        );
-
-        verify(generateJudicialSeparationCORefusedForAmendmentCoverLetter).generateAndUpdateCaseData(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
-        );
-
-        final Print print = printCaptor.getValue();
-        assertThat(print.getCaseId()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getCaseRef()).isEqualTo(TEST_CASE_ID.toString());
-        assertThat(print.getLetterType()).isEqualTo("awaiting-amended-application-letter");
-        assertThat(print.getLetters()).hasSize(4);
-        assertThat(print.getLetters().get(0).getDivorceDocument()).isSameAs(coversheetDoc.getValue());
-        assertThat(print.getLetters().get(1).getDivorceDocument()).isSameAs(coCanApplyDoc.getValue());
-        assertThat(print.getLetters().get(2).getDivorceDocument()).isSameAs(coRefusalDoc.getValue());
-        assertThat(print.getLetters().get(3).getDivorceDocument()).isSameAs(applicationDoc.getValue());
-
-        verify(bulkPrintService).print(print);
-    }
-
-    @Test
-    void shouldNotPrintIfAwaitingAmendedApplicationLettersNotFound() {
-
         final CaseData caseData = CaseData.builder()
             .applicationType(SOLE_APPLICATION)
-            .applicant1(Applicant.builder().languagePreferenceWelsh(NO).build())
+            .applicant1(Applicant.builder().languagePreferenceWelsh(NO).solicitorRepresented(YES).build())
             .applicant2(Applicant.builder().build())
-            .documents(
-                CaseDocuments.builder()
-                    .documentsGenerated(emptyList())
-                    .build()
-            )
-            .isJudicialSeparation(YesOrNo.NO)
+            .isJudicialSeparation(YES)
             .build();
 
         awaitingAmendedApplicationPrinter.sendLetters(
@@ -322,59 +128,26 @@ public class AwaitingAmendedApplicationPrinterTest {
             caseData.getApplicant1()
         );
 
-        verifyNoInteractions(bulkPrintService);
-    }
-
-    @Test
-    void shouldNotPrintIfNumberOfAwaitingAmendedApplicationLettersDoesNotMatchExpectedDocumentsSize() {
-
-        final ListValue<DivorceDocument> coversheetDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(COVERSHEET)
-                .build())
-            .build();
-
-        final ListValue<DivorceDocument> coCanApplyDoc = ListValue.<DivorceDocument>builder()
-            .value(DivorceDocument.builder()
-                .documentType(CONDITIONAL_ORDER_REFUSAL_COVER_LETTER)
-                .build())
-            .build();
-
-        final CaseData caseData = CaseData.builder()
-            .applicationType(SOLE_APPLICATION)
-            .applicant1(Applicant.builder().languagePreferenceWelsh(NO).build())
-            .applicant2(Applicant.builder().build())
-            .documents(
-                CaseDocuments.builder()
-                    .documentsGenerated(asList(coversheetDoc, coCanApplyDoc))
-                    .build()
-            )
-            .isJudicialSeparation(YesOrNo.NO)
-            .build();
-
-        when(generateJudicialSeparationCORefusedForAmendmentCoverLetter.getCoverLetterDocumentType(caseData, caseData.getApplicant1()))
-            .thenReturn(CONDITIONAL_ORDER_REFUSAL_COVER_LETTER);
-
-        awaitingAmendedApplicationPrinter.sendLetters(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
-        );
-
-        verify(generateCoversheet).generateCoversheet(
+        verify(awaitingAmendedOrClarificationApplicationCommonPrinter).sendLetters(
             eq(caseData),
             eq(TEST_CASE_ID),
-            eq(COVERSHEET_APPLICANT),
-            anyMap(),
-            eq(ENGLISH)
+            eq(caseData.getApplicant1()),
+            missingDocumentsValidationCaptor.capture(),
+            eq(false)
         );
 
-        verify(generateCoRefusedCoverLetter).generateAndUpdateCaseData(
-            caseData,
-            TEST_CASE_ID,
-            caseData.getApplicant1()
+        final MissingDocumentsValidation missingDocumentsValidation = missingDocumentsValidationCaptor.getValue();
+        assertThat(missingDocumentsValidation.message).isEqualTo(
+            "Awaiting Amended JS Application Letter pack has missing documents. Expected documents with type {} , for Case ID: {}"
         );
-
-        verifyNoInteractions(bulkPrintService);
+        assertThat(missingDocumentsValidation.documentTypeList).isEqualTo(
+            List.of(
+                COVERSHEET,
+                JUDICIAL_SEPARATION_CONDITIONAL_ORDER_REFUSAL_SOLICITOR_COVER_LETTER,
+                CONDITIONAL_ORDER_REFUSAL,
+                APPLICATION
+            )
+        );
+        assertThat(missingDocumentsValidation.expectedDocumentsSize).isEqualTo(4);
     }
 }
