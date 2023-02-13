41c58a40b30d97af9751f420ead9112b869074f3
FaisalMoJ <faisal.amin@justice.gov.uk>
2023-02-13 13:11:56 +0000

    NFDIV-3284: Add common header and footer and remove family court logo

--- src/test/java/uk/gov/hmcts/divorce/document/content/CertificateOfEntitlementContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/CertificateOfEntitlementContentTest.java
@@ -16,6 +16,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
+import uk.gov.hmcts.divorce.divorcecase.model.CtscContactDetails;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
@@ -28,6 +29,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DISSOL
 import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DIVORCE;
 import static uk.gov.hmcts.divorce.divorcecase.model.Gender.FEMALE;
 import static uk.gov.hmcts.divorce.divorcecase.model.Gender.MALE;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.divorcecase.model.WhoDivorcing.HUSBAND;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_1_FULL_NAME;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.APPLICANT_2_FULL_NAME;
@@ -35,19 +37,28 @@ import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.BE
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CCD_CASE_REFERENCE;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CIVIL_PARTNERSHIP;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CIVIL_PARTNERSHIP_CY;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_EMAIL;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURTS_AND_TRIBUNALS_SERVICE_HEADER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CTSC_CONTACT_DETAILS;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DATE_OF_HEARING;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.HAS_FINANCIAL_ORDERS;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE_CY;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE_OR_CIVIL_PARTNERSHIP;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.PHONE_AND_OPENING_TIMES;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.TIME_OF_HEARING;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getBasicDocmosisTemplateContentWithCtscContactDetails;
 
 @ExtendWith(MockitoExtension.class)
 public class CertificateOfEntitlementContentTest {
 
     @Mock
     private ConditionalOrderCourtDetailsConfig conditionalOrderCourtDetailsConfig;
+
+    @Mock
+    private DocmosisCommonContent docmosisCommonContent;
 
     @InjectMocks
     private CertificateOfEntitlementContent certificateOfEntitlementContent;
@@ -58,6 +69,9 @@ public class CertificateOfEntitlementContentTest {
         final CaseData caseData = getCaseDataFor(SOLE_APPLICATION);
         caseData.setDivorceOrDissolution(DIVORCE);
         final ConditionalOrderCourtDetails expectedDetails = setupConditionalOrderCourtDetailsConfig();
+
+        when(docmosisCommonContent.getBasicDocmosisTemplateContent(
+                caseData.getApplicant1().getLanguagePreference())).thenReturn(getBasicDocmosisTemplateContentWithCtscContactDetails(ENGLISH));
 
         final Map<String, Object> contentMap = certificateOfEntitlementContent.apply(caseData, TEST_CASE_ID);
 
@@ -73,7 +87,12 @@ public class CertificateOfEntitlementContentTest {
             entry(DATE_OF_HEARING, "8 November 2021"),
             entry(TIME_OF_HEARING, "14:56 pm"),
             entry(BEFORE_DATE_OF_HEARING, "1 November 2021"),
-            entry(HAS_FINANCIAL_ORDERS, true)
+            entry(HAS_FINANCIAL_ORDERS, true),
+            entry(CTSC_CONTACT_DETAILS, buildCtscContactDetails()),
+            entry(DIVORCE_AND_DISSOLUTION_HEADER, "Divorce and Dissolution"),
+            entry(COURTS_AND_TRIBUNALS_SERVICE_HEADER, "HM Courts & Tribunals Service"),
+            entry(CONTACT_EMAIL, "contactdivorce@justice.gov.uk"),
+            entry(PHONE_AND_OPENING_TIMES, "Phone: 0300 303 0642 (Monday to Friday, 8am to 6pm)")
         );
     }
 
@@ -83,6 +102,9 @@ public class CertificateOfEntitlementContentTest {
         final CaseData caseData = getCaseDataFor(JOINT_APPLICATION);
         caseData.setDivorceOrDissolution(DIVORCE);
         final ConditionalOrderCourtDetails expectedDetails = setupConditionalOrderCourtDetailsConfig();
+
+        when(docmosisCommonContent.getBasicDocmosisTemplateContent(
+                caseData.getApplicant1().getLanguagePreference())).thenReturn(getBasicDocmosisTemplateContentWithCtscContactDetails(ENGLISH));
 
         final Map<String, Object> contentMap = certificateOfEntitlementContent.apply(caseData, TEST_CASE_ID);
 
@@ -98,7 +120,12 @@ public class CertificateOfEntitlementContentTest {
             entry(DATE_OF_HEARING, "8 November 2021"),
             entry(TIME_OF_HEARING, "14:56 pm"),
             entry(BEFORE_DATE_OF_HEARING, "1 November 2021"),
-            entry(HAS_FINANCIAL_ORDERS, true)
+            entry(HAS_FINANCIAL_ORDERS, true),
+            entry(CTSC_CONTACT_DETAILS, buildCtscContactDetails()),
+            entry(DIVORCE_AND_DISSOLUTION_HEADER, "Divorce and Dissolution"),
+            entry(COURTS_AND_TRIBUNALS_SERVICE_HEADER, "HM Courts & Tribunals Service"),
+            entry(CONTACT_EMAIL, "contactdivorce@justice.gov.uk"),
+            entry(PHONE_AND_OPENING_TIMES, "Phone: 0300 303 0642 (Monday to Friday, 8am to 6pm)")
         );
     }
 
@@ -237,5 +264,18 @@ public class CertificateOfEntitlementContentTest {
 
         when(conditionalOrderCourtDetailsConfig.get(BURY_ST_EDMUNDS.getCourtId())).thenReturn(expectedDetails);
         return expectedDetails;
+    }
+
+    private CtscContactDetails buildCtscContactDetails() {
+        return CtscContactDetails
+                .builder()
+                .centreName("HMCTS Digital Divorce and Dissolution")
+                .serviceCentre("Courts and Tribunals Service Centre")
+                .poBox("PO Box 13226")
+                .town("Harlow")
+                .postcode("CM20 9UG")
+                .phoneNumber("0300 303 0642")
+                .emailAddress("contactdivorce@justice.gov.uk")
+                .build();
     }
 }
