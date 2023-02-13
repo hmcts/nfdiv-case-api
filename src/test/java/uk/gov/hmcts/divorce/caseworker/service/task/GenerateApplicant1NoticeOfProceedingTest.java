a12163080099e5fec0d18067f54a86c5829b7905
aramchurn21 <Adesh.Ramchurn@justice.gov.uk>
2023-02-13 11:52:34 +0000

    NFDIV 2898: removed unused import from GenerateApplicant1NoP unit test

--- src/test/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateApplicant1NoticeOfProceedingTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/service/task/GenerateApplicant1NoticeOfProceedingTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
