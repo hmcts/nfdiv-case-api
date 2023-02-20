86056b7d1ffdb47a6547ef79c3103629b69ae599
Barry Weeks <barry.weeks@justice.gov.uk>
2023-02-20 15:07:19 +0000

    NFDiv-3253 Remove unused import

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/CaseData.java
@@ -17,7 +17,6 @@ import uk.gov.hmcts.ccd.sdk.type.FieldType;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.OrderSummary;
 import uk.gov.hmcts.ccd.sdk.type.ScannedDocument;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.caseworker.model.CaseNote;
 import uk.gov.hmcts.divorce.divorcecase.model.access.Applicant2Access;
 import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAccess;
