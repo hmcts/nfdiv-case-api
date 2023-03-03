452dfc09e90a294036cd965b3ad8da0aefc214b6
Barry Weeks <barry.weeks@justice.gov.uk>
2023-03-03 16:36:50 +0000

    NFDiv-3285 Fix integration testa

--- src/integrationTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerRegenerateCourtOrdersIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerRegenerateCourtOrdersIT.java
@@ -126,7 +126,7 @@ public class CaseworkerRegenerateCourtOrdersIT {
         stubForDocAssemblyWith("5cd725e8-f053-4493-9cbe-bb69d1905ae3", "FL-NFD-GOR-ENG-Certificate_Of_Entitlement.docx");
         stubForDocAssemblyWith("ea114af6-ed73-476b-8cc6-41ec8eb4f0b3", "FL-NFD-GOR-ENG-Conditional-Order-Granted-Cover-Letter.docx");
         stubForDocAssemblyWith("b3d8d9de-8706-4b6e-881c-d8b400d6c533", "FL-NFD-GOR-ENG-CO-Pronounced-Cover-Letter-Offline-Respondent.docx");
-        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced-V2.docx");
+        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced_V3.docx");
         stubForDocAssemblyWith("959ddaf2-75d8-4d49-8a2d-bc29d451f921", "FL-NFD-GOR-ENG-Final-Order-Cover-Letter.docx");
         stubForDocAssemblyWith("7aa5c8bb-1177-4b3e-af83-841c20b572c2", "FL-NFD-GOR-ENG-Final-Order-Granted.docx");
 
@@ -200,7 +200,7 @@ public class CaseworkerRegenerateCourtOrdersIT {
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, SYSTEM_USER_USER_ID, SYSTEM_USER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
         stubForDocAssemblyWith("5cd725e8-f053-4493-9cbe-bb69d1905ae3", "FL-NFD-GOR-ENG-Certificate_Of_Entitlement.docx");
-        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced-V2.docx");
+        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced_V3.docx");
         stubForDocAssemblyWith("7aa5c8bb-1177-4b3e-af83-841c20b572c2", "FL-NFD-GOR-ENG-Final-Order-Granted.docx");
 
         final ListValue<DivorceDocument> coGrantedDoc =
@@ -271,7 +271,7 @@ public class CaseworkerRegenerateCourtOrdersIT {
         stubForIdamDetails(TEST_SYSTEM_AUTHORISATION_TOKEN, SYSTEM_USER_USER_ID, SYSTEM_USER_ROLE);
         stubForIdamToken(TEST_SYSTEM_AUTHORISATION_TOKEN);
         stubForDocAssemblyWith("5cd725e8-f053-4493-9cbe-bb69d1905ae3", "FL-NFD-GOR-ENG-Certificate_Of_Entitlement.docx");
-        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced-V2.docx");
+        stubForDocAssemblyWith("d2fcd6f7-5365-4b8a-af15-ce3c949173aa", "FL-NFD-GOR-ENG-Conditional-Order-Pronounced_V3.docx");
         stubForDocAssemblyWith("7aa5c8bb-1177-4b3e-af83-841c20b572c2", "FL-NFD-GOR-ENG-Final-Order-Granted.docx");
 
         final ListValue<DivorceDocument> coGrantedDoc =
