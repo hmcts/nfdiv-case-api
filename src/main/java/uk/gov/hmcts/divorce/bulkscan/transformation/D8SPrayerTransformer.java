2f95e815679d7458445a66747216d6184415cc4d
Barry Weeks <barry.weeks@justice.gov.uk>
2023-04-12 15:50:59 +0100

    NFDiv-3396 Uncomment prayerDissolveDivorce setter

--- src/main/java/uk/gov/hmcts/divorce/bulkscan/transformation/D8SPrayerTransformer.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkscan/transformation/D8SPrayerTransformer.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.function.Function;
 
 import static org.apache.commons.lang3.BooleanUtils.toBoolean;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicantPrayer.DissolveDivorce.DISSOLVE_DIVORCE;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicantPrayer.FinancialOrdersChild.FINANCIAL_ORDERS_CHILD;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicantPrayer.FinancialOrdersThemselves.FINANCIAL_ORDERS_THEMSELVES;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicantPrayer.JudicialSeparation.JUDICIAL_SEPARATION;
@@ -28,7 +29,7 @@ public class D8SPrayerTransformer implements Function<TransformationDetails, Tra
         OcrDataFields ocrDataFields = transformationDetails.getOcrDataFields();
 
         if (toBoolean(ocrDataFields.getPrayerApplicant1JudiciallySeparated())) {
-            //caseData.getApplicant1().getApplicantPrayer().setPrayerDissolveDivorce(Set.of(DISSOLVE_DIVORCE));
+            caseData.getApplicant1().getApplicantPrayer().setPrayerDissolveDivorce(Set.of(DISSOLVE_DIVORCE));
             caseData.getApplicant1().getApplicantPrayer().setPrayerJudicialSeparation(Set.of(JUDICIAL_SEPARATION));
         } else {
             transformationDetails.getTransformationWarnings().add("Please review prayer in the scanned form");
