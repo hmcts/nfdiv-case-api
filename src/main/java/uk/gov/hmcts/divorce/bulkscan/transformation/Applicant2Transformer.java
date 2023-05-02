78c2acd8240424613a0e22492a198579bdf87d91
Barry Weeks <barry.weeks@justice.gov.uk>
2023-05-02 17:06:32 +0100

    NFDiv-3422 Fix ternary (logic was backwards)

--- src/main/java/uk/gov/hmcts/divorce/bulkscan/transformation/Applicant2Transformer.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkscan/transformation/Applicant2Transformer.java
@@ -81,7 +81,7 @@ public class Applicant2Transformer implements Function<TransformationDetails, Tr
                     .postTown(ocrDataFields.getRespondentOrApplicant2TownOrCity())
                     .county(ocrDataFields.getRespondentOrApplicant2County())
                     // set country to UK if not overseas
-                    .country(toBoolean(ocrDataFields.getServeOutOfUK()) ? "UK" : ocrDataFields.getRespondentOrApplicant2Country())
+                    .country(toBoolean(ocrDataFields.getServeOutOfUK()) ? ocrDataFields.getRespondentOrApplicant2Country() : "UK")
                     .postCode(ocrDataFields.getRespondentOrApplicant2Postcode())
                     .build()
             )
