a4d0ce54c16c898be78515901daddd1422604fa6
Barry Weeks <barry.weeks@justice.gov.uk>
2023-05-19 12:46:15 +0100

    Update ServiceOrderTemplateContentTest.java

--- src/test/java/uk/gov/hmcts/divorce/document/content/ServiceOrderTemplateContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/ServiceOrderTemplateContentTest.java
@@ -19,8 +19,6 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
