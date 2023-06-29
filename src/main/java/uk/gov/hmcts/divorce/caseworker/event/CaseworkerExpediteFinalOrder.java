70d145db7b1391513919220509c0383c7aaf4321
Barry Weeks <barry.weeks@justice.gov.uk>
2023-06-29 12:03:45 +0100

    NFDIV-3463 Add CaseworkerExpediteFinalOrder event and unit tests

---
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerExpediteFinalOrder.java
@@ -0,0 +1,107 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.caseworker.service.notification.FinalOrderGrantedNotification;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateFinalOrder;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateFinalOrderCoverLetter;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.FinalOrder;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
+import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderComplete;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderPending;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.FinalOrderRequested;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.GeneralConsiderationComplete;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Slf4j
+@Component
+public class CaseworkerExpediteFinalOrder implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String CASEWORKER_EXPEDITE_FINAL_ORDER = "caseworker-expedite-final-order";
+
+    @Autowired
+    private Clock clock;
+
+    @Autowired
+    private GenerateFinalOrder generateFinalOrder;
+
+    @Autowired
+    private GenerateFinalOrderCoverLetter generateFinalOrderCoverLetter;
+
+    @Autowired
+    private FinalOrderGrantedNotification finalOrderGrantedNotification;
+
+    @Autowired
+    private NotificationDispatcher notificationDispatcher;
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        new PageBuilder(configBuilder
+            .event(CASEWORKER_EXPEDITE_FINAL_ORDER)
+            .forStates(FinalOrderRequested, FinalOrderPending, GeneralConsiderationComplete)
+            .name("Expedite Final order")
+            .description("Expedite Final order")
+            .showSummary()
+            .showEventNotes()
+            .endButtonLabel("Submit")
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .submittedCallback(this::submitted)
+            .grant(CREATE_READ_UPDATE, CASE_WORKER)
+            .grantHistoryOnly(SOLICITOR, SUPER_USER, LEGAL_ADVISOR, JUDGE))
+            .page("expediteFinalOrder")
+            .pageLabel("Expedite Final Order")
+            .complex(CaseData::getFinalOrder)
+            .mandatory(FinalOrder::getGranted)
+            .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
+                                                                       CaseDetails<CaseData, State> beforeDetails) {
+
+        log.info("{} about to submit callback invoked for Case Id: {}", CASEWORKER_EXPEDITE_FINAL_ORDER, details.getId());
+
+        CaseData caseData = details.getData();
+
+        caseData.getFinalOrder().setGrantedDate(LocalDateTime.now(clock));
+
+        generateFinalOrderCoverLetter.apply(details);
+        generateFinalOrder.apply(details);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .state(FinalOrderComplete)
+            .build();
+    }
+
+    public SubmittedCallbackResponse submitted(CaseDetails<CaseData, State> details,
+                                               CaseDetails<CaseData, State> beforeDetails) {
+
+        final Long caseId = details.getId();
+        final CaseData caseData = details.getData();
+
+        log.info("CitizenSaveAndClose submitted callback invoked for case id: {}", details.getId());
+
+        notificationDispatcher.send(finalOrderGrantedNotification, caseData, caseId);
+
+        return SubmittedCallbackResponse.builder().build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerExpediteFinalOrderTest.java
@@ -0,0 +1,110 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.caseworker.service.notification.FinalOrderGrantedNotification;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateFinalOrder;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateFinalOrderCoverLetter;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.FinalOrder;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerGrantFinalOrder.CASEWORKER_GRANT_FINAL_ORDER;
+import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.getExpectedLocalDateTime;
+import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.setMockClock;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
+
+@ExtendWith(MockitoExtension.class)
+class CaseworkerExpediteFinalOrderTest {
+
+    @Mock
+    private Clock clock;
+
+    @Mock
+    private GenerateFinalOrder generateFinalOrder;
+
+    @Mock
+    private GenerateFinalOrderCoverLetter generateFinalOrderCoverLetter;
+
+    @Mock
+    private FinalOrderGrantedNotification finalOrderGrantedNotification;
+
+    @Mock
+    private NotificationDispatcher notificationDispatcher;
+
+    @InjectMocks
+    private CaseworkerExpediteFinalOrder caseworkerExpediteFinalOrder;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        caseworkerExpediteFinalOrder.configure(configBuilder);
+
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(CASEWORKER_GRANT_FINAL_ORDER);
+    }
+
+    @Test
+    void shouldPopulateFinalOrderGrantedDateAndSendEmail() {
+        final CaseData caseData = caseData();
+        caseData.setFinalOrder(
+            FinalOrder.builder()
+                .granted(Set.of(FinalOrder.Granted.YES))
+                .dateFinalOrderEligibleFrom(LocalDate.now())
+                .build()
+        );
+
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+
+        setMockClock(clock);
+
+        AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerExpediteFinalOrder.aboutToSubmit(details, details);
+
+        assertThat(response.getData().getFinalOrder().getGrantedDate()).isNotNull();
+        assertThat(response.getData().getFinalOrder().getGrantedDate()).isEqualTo(getExpectedLocalDateTime());
+
+        verify(generateFinalOrderCoverLetter).apply(details);
+        verify(generateFinalOrder).apply(details);
+    }
+
+    @Test
+    void shouldSendNotificationWhenSubmittedCallbackIsInvoked() {
+        final CaseData caseData = caseData();
+        caseData.setFinalOrder(
+            FinalOrder.builder()
+                .granted(Set.of(FinalOrder.Granted.YES))
+                .dateFinalOrderEligibleFrom(LocalDate.now())
+                .build()
+        );
+
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+
+        caseworkerExpediteFinalOrder.submitted(details, details);
+
+        verify(notificationDispatcher).send(finalOrderGrantedNotification, caseData, TEST_CASE_ID);
+    }
+}
