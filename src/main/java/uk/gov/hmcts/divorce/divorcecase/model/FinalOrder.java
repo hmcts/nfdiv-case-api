59c85a82f9ae39146990485b5f787e0b017ee5dd
Barry Weeks <barry.weeks@justice.gov.uk>
2023-07-07 11:56:28 +0100

    NFDiv-3463 Avoid unwrapping expeditedFinalOrderAuthorisation

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.divorcecase.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -272,7 +271,6 @@ public class FinalOrder {
         label = "Expedited Final Order Authorisation",
         access = {DefaultAccess.class}
     )
-    @JsonUnwrapped
     private ExpeditedFinalOrderAuthorisation expeditedFinalOrderAuthorisation;
 
     @JsonIgnore
