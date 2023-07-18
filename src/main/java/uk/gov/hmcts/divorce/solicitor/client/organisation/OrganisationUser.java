2cd784265e39116d3f8b3019d32e0acde6226bbf
EllisD-B <ellis.domengerboyce@hmcts.net>
2023-07-18 10:04:06 +0100

    Add Json ignore

--- src/main/java/uk/gov/hmcts/divorce/solicitor/client/organisation/OrganisationUser.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/client/organisation/OrganisationUser.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.divorce.solicitor.client.organisation;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OrganisationUser {
     private String userIdentifier;
 }
