19f4562f0921be24ac7fe8d11610f4974dc7bb39
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2023-07-20 11:23:19 +0100

    correct line violation

--- src/main/java/uk/gov/hmcts/divorce/document/print/model/Print.java
+++ src/main/java/uk/gov/hmcts/divorce/document/print/model/Print.java
@@ -15,8 +15,8 @@ public class Print {
     private final String caseRef;
     private final String letterType;
 
-    // TODO: NFDIV-3567 - recipients will have a value of case reference + applicant/recipient's full name + the letter pack type as a List<String>.
-    //  E.g. "recipients": ['123412341234', 'John Smith', 'aos-overdue']).
+    // TODO: NFDIV-3567 - recipients will have a value of case reference + applicant/recipient's full name + the letter pack type
+    //  as a List<String>. E.g. "recipients": ['123412341234', 'John Smith', 'aos-overdue']).
     //  Which is unique per request & repeatable as required.
     private final List<String> recipients;
 
