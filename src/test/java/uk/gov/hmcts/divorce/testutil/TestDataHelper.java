025374827947f3d5bc9782564fd4d1ea1d95a3c6
Andrew McMahon <andrew.mcmahon@solirius.com>
2023-07-27 15:42:37 +0100

    Add solicitor to Applicant2 builder to fix error

--- src/test/java/uk/gov/hmcts/divorce/testutil/TestDataHelper.java
+++ src/test/java/uk/gov/hmcts/divorce/testutil/TestDataHelper.java
@@ -261,6 +261,9 @@ public class TestDataHelper {
             .firstName(TEST_APP2_FIRST_NAME)
             .middleName(TEST_APP2_MIDDLE_NAME)
             .lastName(TEST_APP2_LAST_NAME)
+            .solicitor(
+                Solicitor.builder().build()
+            )
             .build();
     }
 
