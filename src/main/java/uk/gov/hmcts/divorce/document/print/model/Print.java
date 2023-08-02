574de2b37951f6bd78a42c00054c7138feaab1a3
Andrew McMahon <andrew.mcmahon@solirius.com>
2023-08-02 12:10:56 +0100

    Checkstyle fixes

--- src/main/java/uk/gov/hmcts/divorce/document/print/model/Print.java
+++ src/main/java/uk/gov/hmcts/divorce/document/print/model/Print.java
@@ -13,17 +13,17 @@ public class Print {
     private final List<String> recipients;
 
     public Print(
-            final List<Letter> letters,
-            final String caseId,
-            final String caseRef,
-            final String letterType,
-            final String recipientName
-        ) {
-            this.letters = letters;
-            this.caseId = caseId;
-            this.caseRef = caseRef;
-            this.letterType = letterType;
-            this.recipients = List.of(caseId, recipientName, letterType);
-        }
+        final List<Letter> letters,
+        final String caseId,
+        final String caseRef,
+        final String letterType,
+        final String recipientName
+    ) {
+        this.letters = letters;
+        this.caseId = caseId;
+        this.caseRef = caseRef;
+        this.letterType = letterType;
+        this.recipients = List.of(caseId, recipientName, letterType);
+    }
 
 }
