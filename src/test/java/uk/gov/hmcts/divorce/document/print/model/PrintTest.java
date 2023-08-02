640d7a1a2a8cb76d884cec364fc263116c26f9bd
Andrew McMahon <andrew.mcmahon@solirius.com>
2023-08-02 12:45:04 +0100

    Checkstyle

--- src/test/java/uk/gov/hmcts/divorce/document/print/model/PrintTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/print/model/PrintTest.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.divorce.document.print.model;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 import java.util.UUID;
 
