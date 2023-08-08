9488fc15b9bd9345170d63604f3c7b1a635e0f2d
Andrew McMahon <andrew.mcmahon@solirius.com>
2023-08-08 09:59:20 +0100

    Update applicant2FinalOrderLateExplanation and show on Final Order Tab

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
@@ -446,7 +446,8 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
                 "applicationType=\"soleApplication\" AND doesApplicant2WantToApplyForFinalOrder=\"*\"",
                 "### Respondent")
             .field("doesApplicant2WantToApplyForFinalOrder")
-            .field("applicant2FinalOrderExplanation");
+            .field("applicant2FinalOrderExplanation")
+            .field("applicant2FinalOrderLateExplanation");
     }
 
     private void buildAmendedApplicationTab(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
