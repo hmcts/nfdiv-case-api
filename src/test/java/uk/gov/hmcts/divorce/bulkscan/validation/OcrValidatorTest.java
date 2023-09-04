7b25f73e1f8f009a12ce9f9d716b13ef8b67d04c
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-09-04 12:25:37 +0100

    NFDIV-3593: Expect List<OcrDataField> rather than List<KeyValue> in OcrDataValidationRequest

--- src/test/java/uk/gov/hmcts/divorce/bulkscan/validation/OcrValidatorTest.java
+++ src/test/java/uk/gov/hmcts/divorce/bulkscan/validation/OcrValidatorTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.ccd.sdk.type.KeyValue;
 import uk.gov.hmcts.divorce.bulkscan.endpoint.data.OcrDataValidationRequest;
 import uk.gov.hmcts.divorce.bulkscan.endpoint.data.OcrValidationResponse;
+import uk.gov.hmcts.divorce.bulkscan.endpoint.model.input.OcrDataField;
 
 import java.util.List;
 
@@ -21,7 +21,6 @@ import static uk.gov.hmcts.divorce.bulkscan.validation.OcrValidator.FIELD_EMPTY_
 import static uk.gov.hmcts.divorce.bulkscan.validation.OcrValidator.FIELD_EMPTY_OR_MISSING_WARNING;
 import static uk.gov.hmcts.divorce.bulkscan.validation.OcrValidator.WARNING_NOT_APPLYING_FINANCIAL_ORDER;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.populateD8OcrDataFields;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.populateKeyValue;
 
 @ExtendWith(MockitoExtension.class)
 public class OcrValidatorTest {
@@ -44,9 +43,9 @@ public class OcrValidatorTest {
 
     @Test
     void shouldValidateOcrDataSuccessfullyWhenOcrDataFieldValuesAreNull() {
-        List<KeyValue> ocrDataFields = populateD8OcrDataFields();
-        ocrDataFields.add(populateKeyValue("howToPayEmail", "null"));
-        ocrDataFields.add(populateKeyValue("debitCreditCardPayment", "null"));
+        List<OcrDataField> ocrDataFields = populateD8OcrDataFields();
+        ocrDataFields.add(new OcrDataField("howToPayEmail", "null"));
+        ocrDataFields.add(new OcrDataField("debitCreditCardPayment", "null"));
 
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(ocrDataFields)
@@ -91,12 +90,13 @@ public class OcrValidatorTest {
 
     @Test
     void shouldReturnErrorsIfDuplicateValuesPassedToValidateYourApplication() {
+
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(List.of(
-                populateKeyValue("aSoleApplication", "true"),
-                populateKeyValue("aJointApplication", "true"),
-                populateKeyValue("marriageOrCivilPartnershipCertificate", "true"),
-                populateKeyValue("translation", "true"))
+                new OcrDataField("aSoleApplication", "true"),
+                new OcrDataField("aJointApplication", "true"),
+                new OcrDataField("marriageOrCivilPartnershipCertificate", "true"),
+                new OcrDataField("translation", "true"))
             )
             .build();
 
@@ -125,7 +125,7 @@ public class OcrValidatorTest {
     @Test
     void shouldReturnErrorsAndWarningsIfValidateAboutYouValidationFails() {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
-            .ocrDataFields(List.of(populateKeyValue("soleOrApplicant1Solicitor", "Yes")))
+            .ocrDataFields(List.of(new OcrDataField("soleOrApplicant1Solicitor", "Yes")))
             .build();
 
         OcrValidationResponse response = validator.validateExceptionRecord(D8.getName(), request);
@@ -147,9 +147,9 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("aSoleApplication", "true"),
-                    populateKeyValue("respondentOrApplicant2MarriedName", "Yes"),
-                    populateKeyValue("respondentOrApplicant2Email", "test@email.com"))
+                    new OcrDataField("aSoleApplication", "true"),
+                    new OcrDataField("respondentOrApplicant2MarriedName", "Yes"),
+                    new OcrDataField("respondentOrApplicant2Email", "test@email.com"))
             )
             .build();
 
@@ -178,9 +178,9 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("marriageOutsideOfUK", "Yes"),
-                    populateKeyValue("makingAnApplicationWithoutCertificate", "Yes"),
-                    populateKeyValue("detailsOnCertCorrect", "No"))
+                    new OcrDataField("marriageOutsideOfUK", "Yes"),
+                    new OcrDataField("makingAnApplicationWithoutCertificate", "Yes"),
+                    new OcrDataField("detailsOnCertCorrect", "No"))
             )
             .build();
 
@@ -209,12 +209,12 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("marriageOutsideOfUK", "Yes"),
-                    populateKeyValue("makingAnApplicationWithoutCertificate", "Yes"),
-                    populateKeyValue("detailsOnCertCorrect", "No"),
-                    populateKeyValue("dateOfMarriageOrCivilPartnershipDay", "01"),
-                    populateKeyValue("dateOfMarriageOrCivilPartnershipMonth", "01"),
-                    populateKeyValue("dateOfMarriageOrCivilPartnershipYear", "year"))
+                    new OcrDataField("marriageOutsideOfUK", "Yes"),
+                    new OcrDataField("makingAnApplicationWithoutCertificate", "Yes"),
+                    new OcrDataField("detailsOnCertCorrect", "No"),
+                    new OcrDataField("dateOfMarriageOrCivilPartnershipDay", "01"),
+                    new OcrDataField("dateOfMarriageOrCivilPartnershipMonth", "01"),
+                    new OcrDataField("dateOfMarriageOrCivilPartnershipYear", "year"))
             )
             .build();
 
@@ -251,8 +251,8 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("aSoleApplication", "true"),
-                    populateKeyValue("applicant2ConfirmationOfBreakdown", "true"))
+                    new OcrDataField("aSoleApplication", "true"),
+                    new OcrDataField("applicant2ConfirmationOfBreakdown", "true"))
             )
             .build();
 
@@ -267,7 +267,7 @@ public class OcrValidatorTest {
     @Test
     void shouldReturnErrorsIfValidateStatementOfIrretrievableBreakdownJointValidationFails() {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
-            .ocrDataFields(List.of(populateKeyValue("aJointApplication", "true")))
+            .ocrDataFields(List.of(new OcrDataField("aJointApplication", "true")))
             .build();
 
         OcrValidationResponse response = validator.validateExceptionRecord(D8.getName(), request);
@@ -282,7 +282,7 @@ public class OcrValidatorTest {
     @Test
     void shouldReturnErrorsIfValidateExistingCourtCaseValidationFails() {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
-            .ocrDataFields(List.of(populateKeyValue("existingOrPreviousCourtCases", "Yes")))
+            .ocrDataFields(List.of(new OcrDataField("existingOrPreviousCourtCases", "Yes")))
             .build();
 
         OcrValidationResponse response = validator.validateExceptionRecord(D8.getName(), request);
@@ -298,8 +298,8 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("soleOrApplicant1FinancialOrder", "Yes"),
-                    populateKeyValue("applicant2FinancialOrder", "Yes"))
+                    new OcrDataField("soleOrApplicant1FinancialOrder", "Yes"),
+                    new OcrDataField("applicant2FinancialOrder", "Yes"))
             )
             .build();
 
@@ -326,12 +326,12 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("soleOrApplicant1FinancialOrder", "No"),
-                    populateKeyValue("applicant2FinancialOrder", "No"),
-                    populateKeyValue("soleOrApplicant1prayerFinancialOrder", "test"),
-                    populateKeyValue("soleOrApplicant1prayerFinancialOrderFor", "test"),
-                    populateKeyValue("applicant2PrayerFinancialOrder", "test"),
-                    populateKeyValue("applicant2PrayerFinancialOrderFor", "test"))
+                    new OcrDataField("soleOrApplicant1FinancialOrder", "No"),
+                    new OcrDataField("applicant2FinancialOrder", "No"),
+                    new OcrDataField("soleOrApplicant1prayerFinancialOrder", "test"),
+                    new OcrDataField("soleOrApplicant1prayerFinancialOrderFor", "test"),
+                    new OcrDataField("applicant2PrayerFinancialOrder", "test"),
+                    new OcrDataField("applicant2PrayerFinancialOrderFor", "test"))
             )
             .build();
 
@@ -352,8 +352,8 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("soleOrApplicant1FinancialOrder", "Yes"),
-                    populateKeyValue("applicant2FinancialOrder", "Yes"))
+                    new OcrDataField("soleOrApplicant1FinancialOrder", "Yes"),
+                    new OcrDataField("applicant2FinancialOrder", "Yes"))
             )
             .build();
 
@@ -376,10 +376,10 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("soleOrApplicant1FinancialOrder", "No"),
-                    populateKeyValue("applicant2FinancialOrder", "No"),
-                    populateKeyValue("soleOrApplicant1prayerFinancialOrderFor", "test"),
-                    populateKeyValue("applicant2PrayerFinancialOrderFor", "test"))
+                    new OcrDataField("soleOrApplicant1FinancialOrder", "No"),
+                    new OcrDataField("applicant2FinancialOrder", "No"),
+                    new OcrDataField("soleOrApplicant1prayerFinancialOrderFor", "test"),
+                    new OcrDataField("applicant2PrayerFinancialOrderFor", "test"))
             )
             .build();
 
@@ -396,7 +396,7 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("aJointApplication", "true")
+                    new OcrDataField("aJointApplication", "true")
                 )
             )
             .build();
@@ -446,8 +446,8 @@ public class OcrValidatorTest {
         final OcrDataValidationRequest request = OcrDataValidationRequest.builder()
             .ocrDataFields(
                 List.of(
-                    populateKeyValue("soleOrApplicant1HWFNo", "HWF1"),
-                    populateKeyValue("applicant2HWFNo", "HWF1")
+                    new OcrDataField("soleOrApplicant1HWFNo", "HWF1"),
+                    new OcrDataField("applicant2HWFNo", "HWF1")
                 )
             )
             .build();
