be83ab63c1b6b2927e4fb4780fd9ac5e5ade27cf
Linus Norton <linusnorton@gmail.com>
2023-09-25 17:05:49 +0100

    tidy up

--- bin/remove-case-type-from-def-store.sql
+++ bin/remove-case-type-from-def-store.sql
@@ -4,12 +4,6 @@ DROP TABLE IF EXISTS tmp_case_type_ids;
 CREATE TABLE tmp_case_type_ids_for_:caseTypeReference AS
   SELECT id FROM case_type WHERE reference = :'caseTypeReference';
 
---All DELETION queries below use ids stored in TEMP table 'tmp_case_type_ids_for_:caseTypeReference'
---To remove all historical versions (therefore not use the filtered list of ids within TEMP table)
---Replace tmp_case_type_ids_for_:caseTypeReference with view__case_type_to_remove in all of the statements below. This will
---delete all historical versions older than X months (highest and most current version is not processed
---in this script.
-
 DELETE FROM event_case_field_complex_type WHERE event_case_field_id IN
   (SELECT id FROM event_case_field WHERE event_id IN
       (SELECT id FROM event WHERE case_type_id IN
@@ -58,8 +52,6 @@ DELETE FROM complex_field_acl WHERE case_field_id IN
       (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference)
   );
 
-----takes very long to complete
-----fk_case_field_case_type_id is not indexed by default
 DELETE FROM case_field WHERE case_type_id IN
   (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference);
 
@@ -97,8 +89,6 @@ DELETE FROM state WHERE case_type_id IN
 DELETE FROM case_type_acl WHERE case_type_id IN
   (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference);
 
---Takes very long to complete (> 8min in AAT)
---fk_role_case_type_id_case_type_id is indexed by default
 DELETE FROM role WHERE case_type_id IN
   (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference);
 
@@ -114,7 +104,6 @@ DELETE FROM search_party WHERE case_type_id IN
 DELETE FROM category WHERE case_type_id IN
   (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference);
 
-----Takes very long to complete
 DELETE FROM event WHERE case_type_id IN
   (SELECT id FROM tmp_case_type_ids_for_:caseTypeReference);
 
