8ee433d4734cf894973eb5424c6ceb8306688339
tiagreaney <tiagreaney@gmail.com>
2023-10-04 13:30:11 +0100

    Fix: bootRun

--- src/functionalTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerRejectedFT.java
+++ src/functionalTest/java/uk/gov/hmcts/divorce/caseworker/CaseworkerRejectedFT.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.divorce.caseworker;
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Disabled;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.divorce.testutil.FunctionalTestSuite;
