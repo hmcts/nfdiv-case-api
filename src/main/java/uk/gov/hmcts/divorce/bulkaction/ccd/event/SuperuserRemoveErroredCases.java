e2fcb5cd1676504ca318d3323b22f0a22b5c5c09
Linus Norton <linusnorton@gmail.com>
2023-11-09 11:14:06 +0000

    Add event to remove errored cases from list

---
+++ src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/SuperuserRemoveErroredCases.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.divorce.bulkaction.ccd.event;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionPageBuilder;
+import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
+import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Created;
+import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Listed;
+import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Pronounced;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Component
+@Slf4j
+public class SuperuserRemoveErroredCases implements CCDConfig<BulkActionCaseData, BulkActionState, UserRole> {
+
+    public static final String SUPERUSER_REMOVE_ERRORED_CASES = "caseworker-remove-errored-cases";
+
+    @Override
+    public void configure(final ConfigBuilder<BulkActionCaseData, BulkActionState, UserRole> configBuilder) {
+        new BulkActionPageBuilder(configBuilder
+            .event(SUPERUSER_REMOVE_ERRORED_CASES)
+            .forStates(Created, Listed, Pronounced)
+            .name("Remove cases from bulk list")
+            .description("Remove cases from bulk list")
+            .showEventNotes()
+            .explicitGrants()
+            .grant(CREATE_READ_UPDATE, CASE_WORKER, SYSTEMUPDATE))
+            .page("removeCasesFromErroredList")
+            .pageLabel("Remove cases from bulk list")
+            .mandatoryNoSummary(BulkActionCaseData::getErroredCaseDetails);
+    }
+
+}
