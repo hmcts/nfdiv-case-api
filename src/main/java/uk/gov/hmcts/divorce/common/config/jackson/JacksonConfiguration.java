05f3605d8f05e57f973bde97c312d20b8cb03968
Linus Norton <linusnorton@gmail.com>
2023-11-14 15:35:35 +0000

    add jackson config for cdam

--- src/main/java/uk/gov/hmcts/divorce/common/config/jackson/JacksonConfiguration.java
+++ src/main/java/uk/gov/hmcts/divorce/common/config/jackson/JacksonConfiguration.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -35,6 +36,8 @@ public class JacksonConfiguration {
         JavaTimeModule datetime = new JavaTimeModule();
         datetime.addSerializer(LocalDateSerializer.INSTANCE);
         mapper.registerModule(datetime);
+
+        mapper.registerModule(new ParameterNamesModule());
 
         return mapper;
     }
