f91354154e82766c0e7cba60187bfd50adc432a7
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2023-11-22 11:05:42 +0000

    Rework the existing AOS Overdue offline document functionality to use new DocumentPack
    Refactor and remove existing code in the AOS Overdue flow. We now use a DocumentPack
    and a shared single LetterPrinter to generate, store and send documents.
    
    This refactor aims to cut down on complexity and duplication of code.

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemProgressCaseToAosOverdue.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemProgressCaseToAosOverdue.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -8,7 +8,6 @@ import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.citizen.notification.AosReminderNotifications;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
-import uk.gov.hmcts.divorce.common.service.task.GenerateAosOverdueLetterDocument;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
@@ -25,19 +24,15 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
+@RequiredArgsConstructor
 @Component
 public class SystemProgressCaseToAosOverdue implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String SYSTEM_PROGRESS_TO_AOS_OVERDUE = "system-progress-to-aos-overdue";
 
-    @Autowired
-    private AosReminderNotifications aosReminderNotifications;
+    private final NotificationDispatcher notificationDispatcher;
 
-    @Autowired
-    private NotificationDispatcher notificationDispatcher;
-
-    @Autowired
-    private GenerateAosOverdueLetterDocument generateAosOverdueLetterDocument;
+    private final AosReminderNotifications aosReminderNotifications;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -55,10 +50,6 @@ public class SystemProgressCaseToAosOverdue implements CCDConfig<CaseData, State
                                                                        CaseDetails<CaseData, State> beforeDetails) {
 
         CaseData data = details.getData();
-
-        if (data.getApplicant1().isApplicantOffline()) {
-            generateAosOverdueLetterDocument.apply(details);
-        }
 
         notificationDispatcher.send(aosReminderNotifications, data, details.getId());
 
