fbf61f8f92a20866ed801bd7a60f41b6322d6841
FaisalMoJ <faisal.amin@justice.gov.uk>
2023-11-24 14:28:51 +0000

    NFDIV-3784: Fix checkstyle

--- src/integrationTest/java/uk/gov/hmcts/divorce/document/content/UniqueTemplateContentProvisionTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/document/content/UniqueTemplateContentProvisionTestConfiguration.java
@@ -3,9 +3,9 @@ package uk.gov.hmcts.divorce.document.content;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import uk.gov.hmcts.divorce.document.content.templatecontent.GeneralLetterTemplateContent;
 
 import java.time.Clock;
-import uk.gov.hmcts.divorce.document.content.templatecontent.GeneralLetterTemplateContent;
 
 @TestConfiguration
 @ComponentScan(basePackages = {"uk.gov.hmcts.divorce.document.content.templatecontent"})
