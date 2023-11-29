86e624f847fab2caf42ecc102386ffb8e33f9b5f
FaisalMoJ <faisal.amin@justice.gov.uk>
2023-11-29 10:09:18 +0000

    NFDIV-3784: Fix IT test

--- src/integrationTest/java/uk/gov/hmcts/divorce/document/content/UniqueTemplateContentProvisionTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/document/content/UniqueTemplateContentProvisionTestConfiguration.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.document.content;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
-import uk.gov.hmcts.divorce.document.content.templatecontent.GeneralLetterTemplateContent;
 
 import java.time.Clock;
 
@@ -17,6 +16,4 @@ public class UniqueTemplateContentProvisionTestConfiguration {
     private Clock clock;
     @MockBean
     private DocmosisCommonContent docmosisCommonContent;
-    @MockBean
-    private GeneralLetterTemplateContent generalLetterTemplateContent;
 }
