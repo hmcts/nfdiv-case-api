1034a91a989debc3b45916ecb7ece092f87ccbb2
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-11-30 11:56:00 +0000

    Nfdiv 3790 apply for final order document pack (#3410)
    * NFDIV-3790: Apply for final order document pack
    
    * NFDIV-3790: Remove unused ApplyForFinalOrderPrinter

---
+++ src/test/java/uk/gov/hmcts/divorce/document/print/documentpack/ApplyForFinalOrderDocumentPackTest.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.divorce.document.print.documentpack;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateD36Form;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static uk.gov.hmcts.divorce.document.print.documentpack.ApplyForFinalOrderDocumentPack.APPLICANT1_OFFLINE_DOCUMENTPACK;
+import static uk.gov.hmcts.divorce.document.print.documentpack.ApplyForFinalOrderDocumentPack.APPLICANT2_OFFLINE_DOCUMENTPACK;
+import static uk.gov.hmcts.divorce.document.print.documentpack.ApplyForFinalOrderDocumentPack.LETTER_TYPE_APPLY_FOR_FINAL_ORDER_PACK;
+
+@ExtendWith(MockitoExtension.class)
+class ApplyForFinalOrderDocumentPackTest {
+
+    @Mock
+    GenerateD36Form generateD36Form;
+
+    @InjectMocks
+    ApplyForFinalOrderDocumentPack applyForFinalOrderDocumentPack;
+
+    @Test
+    void shouldreturnapplicant1OfflineDocumentpack() {
+        Applicant applicant = mock(Applicant.class);
+        CaseData caseData = CaseData.builder().applicant1(applicant).build();
+        assertEquals(APPLICANT1_OFFLINE_DOCUMENTPACK, applyForFinalOrderDocumentPack.getDocumentPack(caseData, applicant));
+        verify(generateD36Form).generateD36Document(caseData);
+    }
+
+    @Test
+    void shouldreturnapplicant2OfflineDocumentpack() {
+        Applicant applicant = mock(Applicant.class);
+        CaseData caseData = CaseData.builder().applicant2(applicant).build();
+        assertEquals(APPLICANT2_OFFLINE_DOCUMENTPACK, applyForFinalOrderDocumentPack.getDocumentPack(caseData, applicant));
+        verify(generateD36Form).generateD36Document(caseData);
+    }
+
+    @Test
+    void getLetterId() {
+        assertEquals(LETTER_TYPE_APPLY_FOR_FINAL_ORDER_PACK, applyForFinalOrderDocumentPack.getLetterId());
+    }
+}
