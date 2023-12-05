f543a6afbda1d0cba67cad4ce0fb90d2998f61d1
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-12-05 14:32:21 +0000

    Remove hystrix

--- src/main/java/uk/gov/hmcts/divorce/common/notification/Applicant1AppliedForFinalOrderNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/Applicant1AppliedForFinalOrderNotification.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.divorce.common.notification;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
