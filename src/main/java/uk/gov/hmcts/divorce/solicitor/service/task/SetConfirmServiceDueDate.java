01909d76d770fe136fd281736e7ee0c6d48e67a2
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-12-11 20:29:48 +0000

    NFDIV-3839: Always set dueDate to issueDate + 141

--- src/main/java/uk/gov/hmcts/divorce/solicitor/service/task/SetConfirmServiceDueDate.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/service/task/SetConfirmServiceDueDate.java
@@ -2,44 +2,26 @@ package uk.gov.hmcts.divorce.solicitor.service.task;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.divorce.common.service.HoldingPeriodService;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.SolicitorService;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
-
-import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Component
 @Slf4j
 public class SetConfirmServiceDueDate implements CaseTask {
 
-    @Value("${aos_pack.due_date_offset_days}")
-    private long dueDateOffsetDays;
-
     @Autowired
     private HoldingPeriodService holdingPeriodService;
 
-    /**
-     * For solicitor service the dueDate is set to 16 days after the date the solicitor says the served the respondent.
-     * If they confirm that this service was processed by a process server then due date is set to 141 days after issue date
-     */
     @Override
     public CaseDetails<CaseData, State> apply(final CaseDetails<CaseData, State> caseDetails) {
 
         CaseData caseData = caseDetails.getData();
-        SolicitorService solicitorService = caseData.getApplication().getSolicitorService();
 
-        var dueDate = solicitorService.getDateOfService().plusDays(dueDateOffsetDays);
-
-        if (!isEmpty(solicitorService.getServiceProcessedByProcessServer())) {
-            dueDate = holdingPeriodService.getDueDateFor(caseData.getApplication().getIssueDate());
-        }
-
-        caseData.setDueDate(dueDate);
+        caseData.setDueDate(holdingPeriodService.getDueDateFor(caseData.getApplication().getIssueDate()));
 
         log.info("Setting dueDate of {}, for CaseId: {}", caseData.getDueDate(), caseDetails.getId());
 
