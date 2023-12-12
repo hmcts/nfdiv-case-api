fe96f1f2a5570206c27f500f43a8cd68c2a12df3
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2023-12-12 11:43:14 +0000

    Fix unit tests error

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRemoveGeneralEmailsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRemoveGeneralEmailsTest.java
@@ -4,36 +4,32 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.document.DocumentRemovalService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerRemoveGeneralEmails.CASEWORKER_REMOVE_GENERAL_EMAILS;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 
 @ExtendWith(MockitoExtension.class)
 public class CaseworkerRemoveGeneralEmailsTest {
 
-    @Mock
-    private DocumentRemovalService documentRemovalService;
-
     @InjectMocks
-    private CaseworkerRemoveDocument caseworkerRemoveDocument;
+    private CaseworkerRemoveGeneralEmails caseworkerRemoveGeneralEmails;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
-        caseworkerRemoveDocument.configure(configBuilder);
+        caseworkerRemoveGeneralEmails.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CaseworkerRemoveDocument.CASEWORKER_REMOVE_DOCUMENT);
+            .contains(CASEWORKER_REMOVE_GENERAL_EMAILS);
     }
 }
