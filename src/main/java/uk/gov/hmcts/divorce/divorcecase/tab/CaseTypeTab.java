c8ea6fbac6459ed2a5991a9852524ed3dff05824
amoriartyMOJ <aaron.moriarty@justice.gov.uk>
2024-01-12 12:36:51 +0000

    NFDIV-3853 - Move scanned documents to confidential tab if any applicant is confidential
    To avoid possible data breaches, if any applicant is marked as confidenital on a case,
    then move the scannedDocuments collection onto the Confidential Documents tab.

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/CaseTypeTab.java
@@ -48,10 +48,13 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
 
     private static final String IS_SOLE = "applicationType=\"soleApplication\"";
     private static final String IS_JOINT = "applicationType=\"jointApplication\"";
-    private static final String IS_JOINT_AND_HWF_ENTERED = "applicationType=\"jointApplication\" AND applicant2HWFReferenceNumber=\"*\"";
+    private static final String IS_JOINT_AND_HWF_ENTERED =
+        "applicationType=\"jointApplication\" AND applicant2HWFReferenceNumber=\"*\"";
     private static final String IS_NEW_PAPER_CASE = "newPaperCase=\"Yes\"";
-    private static final String APPLICANT_1_CONTACT_DETAILS_PUBLIC = "applicant1ContactDetailsType!=\"private\"";
-    private static final String APPLICANT_1_CONTACT_DETAILS_PRIVATE = "applicant1ContactDetailsType=\"private\"";
+    private static final String APPLICANTS_CONTACT_DETAILS_PUBLIC =
+        "applicant1ContactDetailsType!=\"private\" AND applicant2ContactDetailsType!=\"private\"";
+    private static final String APPLICANTS_CONTACT_DETAILS_PRIVATE =
+        "applicant1ContactDetailsType=\"private\" OR applicant2ContactDetailsType=\"private\"";
     private static final String PAPER_FORM_APPLICANT_1_PAYMENT_OTHER_DETAILS =
         "paperFormApplicant1NoPaymentIncluded=\"Yes\" AND paperFormSoleOrApplicant1PaymentOther=\"Yes\"";
     private static final String PAPER_FORM_APPLICANT_2_PAYMENT_OTHER_DETAILS =
@@ -187,7 +190,7 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
             .field("documentsGenerated")
             .field("applicant1DocumentsUploaded")
             .field("applicant2DocumentsUploaded")
-            .field("scannedDocuments", APPLICANT_1_CONTACT_DETAILS_PUBLIC)
+            .field("scannedDocuments", APPLICANTS_CONTACT_DETAILS_PUBLIC)
             .field(CaseData::getGeneralOrders)
             .field("documentsUploaded")
             .field(CaseData::getGeneralEmails)
@@ -293,7 +296,7 @@ public class CaseTypeTab implements CCDConfig<CaseData, State, UserRole> {
             .forRoles(CASE_WORKER, LEGAL_ADVISOR, JUDGE, SUPER_USER)
             .field("confidentialDocumentsGenerated")
             .field("confidentialDocumentsUploaded")
-            .field("scannedDocuments", APPLICANT_1_CONTACT_DETAILS_PRIVATE);
+            .field("scannedDocuments", APPLICANTS_CONTACT_DETAILS_PRIVATE);
     }
 
     private void buildServiceApplicationTab(ConfigBuilder<CaseData, State, UserRole> configBuilder) {
