26855a17a9a95b981d21b61a0dff1f14537e4fb4
Barry Weeks <barry.weeks@justice.gov.uk>
2024-01-18 15:43:25 +0000

    NFDiv-2922 Fix imports

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
@@ -24,7 +24,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
