fa5727545d28950efcf6d3dc01b8cda7fea8ec82
Barry Weeks <barry.weeks@justice.gov.uk>
2024-01-25 15:26:17 +0000

    NFDiv-2922 Fix imports

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/FinalOrder.java
@@ -33,7 +33,6 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.divorcecase.model.PaymentStatus.SUCCESS;
 import static uk.gov.hmcts.divorce.divorcecase.model.SolicitorPaymentMethod.FEE_PAY_BY_ACCOUNT;
