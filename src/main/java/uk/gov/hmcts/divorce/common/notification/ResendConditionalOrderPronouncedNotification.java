2774075fecddc19461bbd56ee6c439add5b15913
DelythJustice <delyth.jones11@justice.gov.uk>
2024-02-29 14:15:46 +0000

    NFDIV-3788 missed unused import

--- src/main/java/uk/gov/hmcts/divorce/common/notification/ResendConditionalOrderPronouncedNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/ResendConditionalOrderPronouncedNotification.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.common.notification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.systemupdate.service.print.ConditionalOrderPronouncedPrinter;
