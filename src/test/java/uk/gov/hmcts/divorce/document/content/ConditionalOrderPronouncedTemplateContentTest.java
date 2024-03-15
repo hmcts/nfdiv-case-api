5f4ce9a68ef594f84c0c1e4866ab76b780021ba5
DelythJustice <delyth.jones11@justice.gov.uk>
2024-03-15 09:25:26 +0000

    NFDIV-3935 tests

--- src/test/java/uk/gov/hmcts/divorce/document/content/ConditionalOrderPronouncedTemplateContentTest.java
+++ src/test/java/uk/gov/hmcts/divorce/document/content/ConditionalOrderPronouncedTemplateContentTest.java
@@ -25,6 +25,7 @@ import static org.assertj.core.api.Assertions.entry;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderCourt.BIRMINGHAM;
 import static uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution.DIVORCE;
@@ -44,11 +45,11 @@ import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CO
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.COURT_NAME;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CO_PRONOUNCED_DATE;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DATE_FO_ELIGIBLE_FROM;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DATE_OF_HEARING;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER_TEXT;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_AND_DISSOLUTION_HEADER_TEXT_CY;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DOCUMENTS_ISSUED_ON;
-import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.IS_JOINT;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.IS_SOLE;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.JUDGE_NAME;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.MARRIAGE_DATE;
@@ -61,8 +62,12 @@ import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.SO
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.SOLICITOR_FIRM;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.SOLICITOR_NAME;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.TIME_OF_HEARING;
 import static uk.gov.hmcts.divorce.notification.CommonContent.IS_DIVORCE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_JOINT;
 import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.TIME_FORMATTER;
 import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.getExpectedLocalDateTime;
 import static uk.gov.hmcts.divorce.testutil.ClockTestUtil.getTemplateFormatDate;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.APPLICANT_2_FIRST_NAME;
@@ -156,7 +161,12 @@ class ConditionalOrderPronouncedTemplateContentTest {
             entry(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT),
             entry(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT),
             entry(CONTACT_EMAIL, CONTACT_DIVORCE_EMAIL),
-            entry(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT));
+            entry(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT),
+            entry(DocmosisTemplateConstants.CASE_REFERENCE, "1616-5914-0147-3378"),
+            entry(DATE_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(DATE_TIME_FORMATTER)),
+            entry(TIME_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(TIME_FORMATTER)),
+            entry(CommonContent.NAME, TEST_FIRST_NAME + " " + TEST_LAST_NAME)
+        );
     }
 
     @Test
@@ -226,29 +236,106 @@ class ConditionalOrderPronouncedTemplateContentTest {
             entry(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT_CY),
             entry(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT_CY),
             entry(CONTACT_EMAIL, CONTACT_DIVORCE_EMAIL),
-            entry(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT_CY));
+            entry(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT_CY),
+            entry(DocmosisTemplateConstants.CASE_REFERENCE, "1616-5914-0147-3378"),
+            entry(DATE_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(DATE_TIME_FORMATTER)),
+            entry(TIME_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(TIME_FORMATTER)),
+            entry(CommonContent.NAME, TEST_FIRST_NAME + " " + TEST_LAST_NAME)
+        );
+
     }
 
     @Test
+    void shouldApplyCoGrantedCoverLetterIdContent() {
+        Map<String, Object> basicDocmosisTemplateContent = new HashMap<>();
+        basicDocmosisTemplateContent.put(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT);
+        basicDocmosisTemplateContent.put(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT);
+        basicDocmosisTemplateContent.put(CONTACT_EMAIL, CONTACT_DIVORCE_EMAIL);
+        basicDocmosisTemplateContent.put(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT);
+
+        when(commonContent.getPartner(any(), any(), eq(ENGLISH))).thenReturn("husband");
+        when(docmosisCommonContent.getBasicDocmosisTemplateContent(ENGLISH)).thenReturn(basicDocmosisTemplateContent);
+
+        final Applicant applicant1 = Applicant.builder()
+            .firstName(TEST_FIRST_NAME)
+            .lastName(TEST_LAST_NAME)
+            .languagePreferenceWelsh(YesOrNo.NO)
+            .build();
+        final Applicant applicant2 = Applicant.builder()
+            .firstName(APPLICANT_2_FIRST_NAME)
+            .lastName(APPLICANT_2_LAST_NAME)
+            .build();
+
+        LocalDate coPronouncedDate = LocalDate.of(2022, 6, 10);
+        LocalDate marriageDate = LocalDate.of(2000, 1, 2);
+
+        final CaseData caseData = CaseData.builder()
+            .applicationType(JOINT_APPLICATION)
+            .divorceOrDissolution(DIVORCE)
+            .applicant1(applicant1)
+            .applicant2(applicant2)
+            .application(Application.builder()
+                .marriageDetails(MarriageDetails.builder()
+                    .placeOfMarriage("London")
+                    .countryOfMarriage("UK")
+                    .date(marriageDate)
+                    .build())
+                .build())
+            .conditionalOrder(ConditionalOrder.builder()
+                .dateAndTimeOfHearing(getExpectedLocalDateTime())
+                .court(BIRMINGHAM)
+                .grantedDate(coPronouncedDate)
+                .pronouncementJudge("District Judge")
+                .build())
+            .build();
+
+        final Map<String, Object> result =
+            conditionalOrderPronouncedTemplateContent.apply(caseData, TEST_CASE_ID, applicant1);
+
+        assertThat(result).contains(
+            entry(IS_SOLE, false),
+            entry(IS_DIVORCE, true),
+            entry(CASE_REFERENCE, FORMATTED_TEST_CASE_ID),
+            entry(DOCUMENTS_ISSUED_ON, getTemplateFormatDate()),
+            entry(APPLICANT_1_FULL_NAME, TEST_FIRST_NAME + " " + TEST_LAST_NAME),
+            entry(APPLICANT_2_FULL_NAME, APPLICANT_2_FIRST_NAME + " " + APPLICANT_2_LAST_NAME),
+            entry(MARRIAGE_OR_CIVIL_PARTNERSHIP, "marriage"),
+            entry(PARTNER, "husband"),
+            entry(PLACE_OF_MARRIAGE, "London"),
+            entry(COUNTRY_OF_MARRIAGE, "UK"),
+            entry(MARRIAGE_DATE, "2 January 2000"),
+            entry(JUDGE_NAME, "District Judge"),
+            entry(COURT_NAME, "Birmingham Civil and Family Justice Centre"),
+            entry(CO_PRONOUNCED_DATE, "10 June 2022"),
+            entry(DATE_FO_ELIGIBLE_FROM, "23 July 2022"),
+            entry(DIVORCE_AND_DISSOLUTION_HEADER, DIVORCE_AND_DISSOLUTION_HEADER_TEXT),
+            entry(COURTS_AND_TRIBUNALS_SERVICE_HEADER, COURTS_AND_TRIBUNALS_SERVICE_HEADER_TEXT),
+            entry(CONTACT_EMAIL, CONTACT_DIVORCE_EMAIL),
+            entry(PHONE_AND_OPENING_TIMES, PHONE_AND_OPENING_TIMES_TEXT),
+            entry(DocmosisTemplateConstants.CASE_REFERENCE, "1616-5914-0147-3378"),
+            entry(DATE_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(DATE_TIME_FORMATTER)),
+            entry(TIME_OF_HEARING, caseData.getConditionalOrder().getDateAndTimeOfHearing().format(TIME_FORMATTER)),
+            entry(CommonContent.NAME, TEST_FIRST_NAME + " " + TEST_LAST_NAME)
+        );
+
+    }
+    @Test
     void templateVarsForJSSolicitorShouldUpdateTemplateContentCorrectly() {
-        // Given
         CaseData caseData = buildCaseDataWithSolicitor();
 
-        // When
         Map<String, Object> templateContentMap = new HashMap<>();
         conditionalOrderPronouncedTemplateContent.templateVarsForJSSolicitor(
             templateContentMap, caseData, caseData.getApplicant1());
 
-        // Then
         assertThat(templateContentMap).contains(
             entry(SOLICITOR_NAME, "Solicitor Name"),
             entry(SOLICITOR_FIRM, "Solicitor Firm"),
             entry(SOLICITOR_ADDRESS, "Solicitor Address"),
-            entry(IS_JOINT, false),
+            entry(IS_JOINT, true),
             entry(APPLICANT_1_FULL_NAME, "Applicant 1 Full Name"),
             entry(APPLICANT_2_FULL_NAME, "Applicant 2 Full Name"),
             entry(APPLICANT_1_SOLICITOR_NAME, "Solicitor Name"),
-            entry(APPLICANT_2_SOLICITOR_NAME, "Not Represented"),
+            entry(APPLICANT_2_SOLICITOR_NAME, "Not represented"),
             entry(SOLICITOR_REFERENCE, "Solicitor Ref")
         );
     }
@@ -273,6 +360,7 @@ class ConditionalOrderPronouncedTemplateContentTest {
         return CaseData.builder()
             .applicant1(applicant1)
             .applicant2(applicant2)
+            .applicationType(JOINT_APPLICATION)
             .build();
     }
 }
