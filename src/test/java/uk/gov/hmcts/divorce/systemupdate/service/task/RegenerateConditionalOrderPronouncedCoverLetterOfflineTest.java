3af7b75295e8a5385221009a4c37079f7c7bc26d
DelythJustice <delyth.jones11@justice.gov.uk>
2024-03-20 12:37:42 +0000

    NFDIV-3935 adding test for coerage

--- src/test/java/uk/gov/hmcts/divorce/systemupdate/service/task/RegenerateConditionalOrderPronouncedCoverLetterOfflineTest.java
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/service/task/RegenerateConditionalOrderPronouncedCoverLetterOfflineTest.java
@@ -21,9 +21,7 @@ import static org.mockito.Mockito.*;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.APPLICATION;
-import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1;
-import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_2;
 
 @ExtendWith(MockitoExtension.class)
 class RegenerateConditionalOrderPronouncedCoverLetterOfflineTest {
@@ -40,36 +38,15 @@ class RegenerateConditionalOrderPronouncedCoverLetterOfflineTest {
     }
     @Test
     void apply_shouldRegenerateConditionalOrderPronouncedCoversheetForApplicant1AndSetFlag() {
-
-        final CaseData caseData = CaseData.builder()
-            .applicant1(Applicant.builder().offline(YES).build())
-            .applicant2(Applicant.builder().offline(NO).build())
-            .documents(CaseDocuments.builder()
-            .documentsGenerated(Lists.newArrayList(
-                ListValue.<DivorceDocument>builder()
-                    .id("1")
-                    .value(DivorceDocument.builder()
-                        .documentType(CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1)
-                        .build())
-                    .build(),
-                ListValue.<DivorceDocument>builder()
-                    .id("2")
-                    .value(DivorceDocument.builder()
-                        .documentType(APPLICATION)
-                        .build()).build()
-            ))
-            .build())
-            .build();
-
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        caseDetails.setData(caseData);
-        caseDetails.setId(1L);
+        // Given
+        final CaseData caseData = createCaseDataWithApplicant1OfflineAndExistingCoverSheetApp1();
+        final CaseDetails<CaseData, State> caseDetails = createCaseDetails(caseData);
 
         // When
         final CaseDetails<CaseData, State> result = task.apply(caseDetails);
 
-
-       verify(coverLetterHelper).generateConditionalOrderPronouncedCoversheet(
+        // Then
+        verify(coverLetterHelper).generateConditionalOrderPronouncedCoversheet(
             caseData,
             1L,
             caseData.getApplicant1(),
@@ -78,5 +55,55 @@ class RegenerateConditionalOrderPronouncedCoverLetterOfflineTest {
         assertEquals(YES, caseData.getApplicant1().getCoPronouncedCoverLetterRegenerated());
         assertSame(caseDetails, result);
     }
+
+    @Test
+    void apply_shouldRegenerateConditionalOrderPronouncedCoversheetForApplicant1WhenDocumentRemoved() {
+        // Given
+        final CaseData caseData = createCaseDataWithApplicant1OfflineAndExistingCoverSheetApp1();
+        final CaseDetails<CaseData, State> caseDetails = createCaseDetails(caseData);
+        final CaseDetails<CaseData, State> result = task.apply(caseDetails);
+
+        // Then
+        assertSame(YES, caseData.getApplicant1().getCoPronouncedCoverLetterRegenerated());
+        verify(coverLetterHelper).generateConditionalOrderPronouncedCoversheet(
+            caseData,
+            1L,
+            caseData.getApplicant1(),
+            CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1
+        );
+        assertSame(caseDetails, result);
+    }
+
+    // Helper method to create CaseData with applicant 1 offline and existing cover sheet App1
+    private CaseData createCaseDataWithApplicant1OfflineAndExistingCoverSheetApp1() {
+        return CaseData.builder()
+            .applicant1(Applicant.builder().offline(YES).build())
+            .applicant2(Applicant.builder().offline(NO).build())
+            .documents(CaseDocuments.builder()
+                .documentsGenerated(Lists.newArrayList(
+                    ListValue.<DivorceDocument>builder()
+                        .id("1")
+                        .value(DivorceDocument.builder()
+                            .documentType(CONDITIONAL_ORDER_GRANTED_COVERSHEET_APP_1)
+                            .build())
+                        .build(),
+                    ListValue.<DivorceDocument>builder()
+                        .id("2")
+                        .value(DivorceDocument.builder()
+                            .documentType(APPLICATION)
+                            .build()).build()
+                ))
+                .build())
+            .build();
+    }
+
+    // Helper method to create CaseDetails with given CaseData and ID
+    private CaseDetails<CaseData, State> createCaseDetails(CaseData caseData) {
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(1L);
+        return caseDetails;
+    }
+
 }
 
