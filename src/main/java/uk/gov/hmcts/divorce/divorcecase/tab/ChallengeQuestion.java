700531ed1eb603d32b2caffd36484ae29c0e6889
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-04-11 17:05:22 +0100

    Example of challenge questions configuration

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/ChallengeQuestion.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/ChallengeQuestion.java
@@ -2,19 +2,18 @@ package uk.gov.hmcts.divorce.divorcecase.tab;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.ChallengeQuestion;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static uk.gov.hmcts.divorce.divorcecase.NoFaultDivorce.getCaseType;
 
 @Component
-public class ChallengeQuestions implements CCDConfig<CaseData, State, UserRole> {
+public class ChallengeQuestion implements CCDConfig<CaseData, State, UserRole> {
 
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         configBuilder.challengeQuestions(List.of(
