2b1290403172cd191e6df5436c55c87bfb8ae950
pallavijustice <pallavi.saha@justice.gov.uk>
2024-04-18 13:15:27 +0100

    Resolved CheckStyle errors

--- src/main/java/uk/gov/hmcts/divorce/common/notification/FinancialOrderRequestedNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/FinancialOrderRequestedNotification.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.common.notification;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.divorce.common.config.EmailTemplatesConfig;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
