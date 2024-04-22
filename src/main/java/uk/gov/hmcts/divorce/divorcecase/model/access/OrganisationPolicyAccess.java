50f9b6c5c34d1444df3f8ad52915477d8e4ef52a
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-04-22 18:05:29 +0100

    Checkstyle correction

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/OrganisationPolicyAccess.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/OrganisationPolicyAccess.java
@@ -6,7 +6,6 @@ import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
