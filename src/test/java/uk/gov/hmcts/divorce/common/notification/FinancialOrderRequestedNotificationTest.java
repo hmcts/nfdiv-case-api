c264c39d29702472530f6713101d7dd3b43e4a7a
pallavijustice <pallavi.saha@justice.gov.uk>
2024-05-08 11:53:15 +0100

    Removed unused import

--- src/test/java/uk/gov/hmcts/divorce/common/notification/FinancialOrderRequestedNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/notification/FinancialOrderRequestedNotificationTest.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.divorce.common.notification;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
