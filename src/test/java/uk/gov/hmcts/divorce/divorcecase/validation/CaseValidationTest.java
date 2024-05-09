ffde2c88498408ca9ee9e45ac5773ae4eecc7c9f
Barry Weeks <barry.weeks@justice.gov.uk>
2024-05-09 17:32:42 +0100

    NFDIV-4031 Add Unit Tests

--- src/test/java/uk/gov/hmcts/divorce/divorcecase/validation/CaseValidationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/validation/CaseValidationTest.java
@@ -16,6 +16,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.SupplementaryCaseType;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -46,6 +47,7 @@ import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 public class CaseValidationTest {
 
     private static final String LESS_THAN_ONE_YEAR_AGO = " can not be less than one year ago.";
+    public static final String LESS_THAN_ONE_YEAR_SINCE_SUBMISSION = " can not be less than one year prior to application submission.";
     private static final String EMPTY = " cannot be empty or null";
     private static final String IN_THE_FUTURE = " can not be in the future.";
     private static final String MORE_THAN_ONE_HUNDRED_YEARS_AGO = " can not be more than 100 years ago.";
@@ -128,6 +130,25 @@ public class CaseValidationTest {
         List<String> response = validateMarriageDate(caseData, "field");
 
         assertThat(response).isEqualTo(List.of("field" + LESS_THAN_ONE_YEAR_AGO));
+    }
+
+    @Test
+    public void shouldReturnErrorWhenDateIsLessThanOneYearSinceApplicationSubmission() {
+
+        CaseData caseData = CaseData.builder()
+            .application(
+                Application.builder()
+                    .dateSubmitted(LocalDateTime.now())
+                    .marriageDetails(MarriageDetails.builder()
+                        .date(LocalDate.now().minus(1, YEARS))
+                        .build())
+                    .build()
+            )
+            .build();
+
+        List<String> response = validateMarriageDate(caseData, "field", true);
+
+        assertThat(response).isEqualTo(List.of("field" + LESS_THAN_ONE_YEAR_SINCE_SUBMISSION));
     }
 
     @Test
