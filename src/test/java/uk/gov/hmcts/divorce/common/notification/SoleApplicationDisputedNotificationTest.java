343bb7b4057afd013530d9b22c9ceced45f2425d
DelythJustice <delyth.jones11@justice.gov.uk>
2024-05-13 13:40:27 +0100

    NFDIV-4016 unused import

--- src/test/java/uk/gov/hmcts/divorce/common/notification/SoleApplicationDisputedNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/notification/SoleApplicationDisputedNotificationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
