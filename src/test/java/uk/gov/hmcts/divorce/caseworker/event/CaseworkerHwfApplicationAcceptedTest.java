57d4b4eb94bf2fdf7cf8abb339aef1756089620b
Barry Weeks <barry.weeks@justice.gov.uk>
2024-05-22 09:06:55 +0100

    NFDIV-4060 Add Unit Test

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerHwfApplicationAcceptedTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerHwfApplicationAcceptedTest.java
@@ -3,20 +3,29 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerHwfApplicationAccepted.CASEWORKER_HWF_APPLICATION_ACCEPTED;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Submitted;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CaseworkerHwfApplicationAcceptedTest {
+
+    @Mock
+    private CaseworkerHwfApplicationAndPaymentHelper caseworkerHwfApplicationAndPaymentHelper;
 
     @InjectMocks
     private CaseworkerHwfApplicationAccepted caseworkerHwfApplicationAccepted;
@@ -30,5 +39,20 @@ class CaseworkerHwfApplicationAcceptedTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_HWF_APPLICATION_ACCEPTED);
+    }
+
+    @Test
+    void shouldSetStateAndDates() {
+        final CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+
+        when(caseworkerHwfApplicationAndPaymentHelper.getState(caseData)).thenReturn(Submitted);
+        when(caseworkerHwfApplicationAndPaymentHelper.setDateSubmittedAndDueDate(caseData)).thenReturn(caseData);
+
+        caseworkerHwfApplicationAccepted.aboutToSubmit(caseDetails, null);
+
+        verify(caseworkerHwfApplicationAndPaymentHelper).getState(caseData);
+        verify(caseworkerHwfApplicationAndPaymentHelper).setDateSubmittedAndDueDate(caseData);
     }
 }
