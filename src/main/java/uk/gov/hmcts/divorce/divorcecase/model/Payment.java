3c23c6d8995dd43e58ca31745861b48c17dd1d27
pallavijustice <159147694+pallavijustice@users.noreply.github.com>
2024-06-06 17:26:40 +0100

    Update src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Payment.java
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Payment.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Payment.java
@@ -58,7 +58,7 @@ public class Payment {
     private String transactionId;
 
     @CCD(
-        label = "Service Requent Reference"
+        label = "Service Request Reference"
     )
     private String serviceRequestReference;
 }
