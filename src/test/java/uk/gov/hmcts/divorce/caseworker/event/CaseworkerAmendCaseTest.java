4c56fae59e4f6d81985fa673bd83bdea4a605512
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2024-06-21 11:06:27 +0100

    [NFDIV-4117] Revert D8 regeneration on update case (#3804)
    * Revert D8 regeneration on update case
    
    * Checkstyle corrections

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendCaseTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendCaseTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -12,43 +11,20 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.idam.IdamService;
-import uk.gov.hmcts.divorce.idam.User;
-import uk.gov.hmcts.divorce.systemupdate.service.CcdUpdateService;
-import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
-import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerAmendCase.CASEWORKER_AMEND_CASE;
-import static uk.gov.hmcts.divorce.common.event.RegenerateApplication.REGENERATE_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.LESS_THAN_ONE_YEAR_SINCE_SUBMISSION;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
-import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_AUTHORIZATION_TOKEN;
-import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
-import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SYSTEM_AUTHORISATION_TOKEN;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseDataWithMarriageDate;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.validCaseDataForIssueApplication;
 
 @ExtendWith(MockitoExtension.class)
 class CaseworkerAmendCaseTest {
-
-    @Mock
-    private IdamService idamService;
-
-    @Mock
-    private AuthTokenGenerator authTokenGenerator;
-
-    @Mock
-    private CcdUpdateService ccdUpdateService;
-
     @InjectMocks
     private CaseworkerAmendCase caseworkerAmendCase;
 
@@ -92,35 +68,5 @@ class CaseworkerAmendCaseTest {
 
         assertThat(response.getErrors()).isNull();
         assertThat(response.getWarnings()).isNull();
-    }
-
-    @Test
-    void shouldGenerateD8IfApplicationPreviouslyIssued() {
-        final CaseData caseData = validCaseDataForIssueApplication();
-        caseData.getApplication().setIssueDate(LocalDate.now());
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        caseDetails.setData(caseData);
-        caseDetails.setState(State.AwaitingAos);
-        caseDetails.setId(TEST_CASE_ID);
-
-        final User user = new User(TEST_AUTHORIZATION_TOKEN, UserInfo.builder().build());
-        when(idamService.retrieveSystemUpdateUserDetails()).thenReturn(user);
-        when(authTokenGenerator.generate()).thenReturn(TEST_SYSTEM_AUTHORISATION_TOKEN);
-
-        caseworkerAmendCase.submitted(caseDetails, caseDetails);
-
-        verify(ccdUpdateService).submitEvent(TEST_CASE_ID, REGENERATE_APPLICATION, user, TEST_SYSTEM_AUTHORISATION_TOKEN);
-    }
-
-    @Test
-    void shouldNotGenerateD8IfApplicationNotYetIssued() {
-        final CaseData caseData = validCaseDataForIssueApplication();
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        caseDetails.setData(caseData);
-        caseDetails.setState(State.AwaitingAos);
-
-        caseworkerAmendCase.submitted(caseDetails, caseDetails);
-
-        verifyNoInteractions(ccdUpdateService);
     }
 }
