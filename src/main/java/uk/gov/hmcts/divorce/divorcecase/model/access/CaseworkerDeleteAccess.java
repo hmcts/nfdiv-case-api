d47cef5b523a488db752ac44a7d677737828fcb8
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2024-07-01 11:47:46 +0100

    Give caseworkers permission to delete general emails (#3819)

---
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerDeleteAccess.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.divorce.divorcecase.model.access;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.SetMultimap;
+import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
+import uk.gov.hmcts.ccd.sdk.api.HasRole;
+import uk.gov.hmcts.ccd.sdk.api.Permission;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+
+public class CaseworkerDeleteAccess implements HasAccessControl {
+    @Override
+    public SetMultimap<HasRole, Permission> getGrants() {
+        SetMultimap<HasRole, Permission> grants = HashMultimap.create();
+        grants.putAll(CASE_WORKER, Permissions.CREATE_READ_UPDATE_DELETE);
+        return grants;
+    }
+}
