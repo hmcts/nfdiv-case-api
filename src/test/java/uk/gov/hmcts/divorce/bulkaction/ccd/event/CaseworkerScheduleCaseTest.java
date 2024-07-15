fdf2baf251d4836388308ca74afd7793dc9c7269
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-07-15 09:31:36 +0100

    NFDIV-3869: Add functionality to add cases in bulk list

--- src/test/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCaseTest.java
+++ src/test/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCaseTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.divorce.bulkaction.ccd.event;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,25 +12,59 @@ import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
 import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
+import uk.gov.hmcts.divorce.bulkaction.service.BulkTriggerService;
 import uk.gov.hmcts.divorce.bulkaction.service.ScheduleCaseService;
+import uk.gov.hmcts.divorce.bulkaction.task.BulkCaseCaseTaskFactory;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.idam.IdamService;
+import uk.gov.hmcts.divorce.idam.User;
+import uk.gov.hmcts.divorce.systemupdate.schedule.bulkaction.FailedBulkCaseRemover;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.bulkaction.ccd.event.CaseworkerScheduleCase.CASEWORKER_SCHEDULE_CASE;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.systemupdate.event.SystemLinkWithBulkCase.SYSTEM_LINK_WITH_BULK_CASE;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createBulkActionConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.AUTH_HEADER_VALUE;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_AUTHORIZATION_TOKEN;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SERVICE_AUTH_TOKEN;
 
 @ExtendWith(MockitoExtension.class)
 public class CaseworkerScheduleCaseTest {
     @Mock
     private ScheduleCaseService scheduleCaseService;
 
+    @Mock
+    private IdamService idamService;
+    @Mock
+    private AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    private FailedBulkCaseRemover failedBulkCaseRemover;
+
+    @Mock
+    private BulkTriggerService bulkTriggerService;
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private BulkCaseCaseTaskFactory bulkCaseCaseTaskFactory;
     @InjectMocks
     private CaseworkerScheduleCase scheduleCase;
 
@@ -50,12 +85,15 @@ public class CaseworkerScheduleCaseTest {
         details.setData(BulkActionCaseData.builder().build());
         details.setId(TEST_CASE_ID);
 
+        setUpUser("Random role");
+
         doNothing().when(scheduleCaseService).updateCourtHearingDetailsForCasesInBulk(details);
 
         SubmittedCallbackResponse submittedCallbackResponse = scheduleCase.submitted(details, details);
 
         assertThat(submittedCallbackResponse).isNotNull();
         verify(scheduleCaseService).updateCourtHearingDetailsForCasesInBulk(details);
+        verifyNoInteractions(bulkTriggerService);
     }
 
     @Test
@@ -86,5 +124,40 @@ public class CaseworkerScheduleCaseTest {
         AboutToStartOrSubmitResponse<BulkActionCaseData, BulkActionState> response = scheduleCase.aboutToSubmit(details, details);
 
         assertThat(response.getErrors()).containsExactly("Please enter a hearing date and time in the future");
+    }
+
+    @Test
+    void shouldSuccessfullyLinkCaseWithBulkListWhenRoleIsCaseWorker() {
+        final CaseDetails<BulkActionCaseData, BulkActionState> details = new CaseDetails<>();
+        details.setData(BulkActionCaseData.builder().build());
+        details.setId(TEST_CASE_ID);
+
+        User user =  setUpUser(CASE_WORKER.getRole());
+
+        doNothing().when(scheduleCaseService).updateCourtHearingDetailsForCasesInBulk(details);
+
+        SubmittedCallbackResponse submittedCallbackResponse = scheduleCase.submitted(details, details);
+
+        assertThat(submittedCallbackResponse).isNotNull();
+        verify(scheduleCaseService).updateCourtHearingDetailsForCasesInBulk(details);
+        verify(bulkTriggerService).bulkTrigger(
+                details.getData().getBulkListCaseDetails(),
+                SYSTEM_LINK_WITH_BULK_CASE,
+                bulkCaseCaseTaskFactory.getCaseTask(details, SYSTEM_LINK_WITH_BULK_CASE),
+                user,
+                TEST_SERVICE_AUTH_TOKEN);
+
+        verify(failedBulkCaseRemover).removeFailedCasesFromBulkListCaseDetails(any(), eq(details), eq(user), eq(TEST_SERVICE_AUTH_TOKEN));
+    }
+
+    private User setUpUser(String userRole) {
+        var userDetails = UserInfo.builder().roles(List.of(userRole)).build();
+        User user = new User(AUTH_HEADER_VALUE, userDetails);
+        when(request.getHeader(AUTHORIZATION)).thenReturn(TEST_AUTHORIZATION_TOKEN);
+        when(authTokenGenerator.generate()).thenReturn(TEST_SERVICE_AUTH_TOKEN);
+        when(idamService.retrieveUser(TEST_AUTHORIZATION_TOKEN))
+                .thenReturn(user);
+
+        return user;
     }
 }
