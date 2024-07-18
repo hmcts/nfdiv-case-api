3de5f6d427be396606d21fc0ee47a7f3ac15e230
Barry Weeks <barry.weeks@justice.gov.uk>
2024-07-18 17:34:14 +0100

    NFDIV-4142 Refactor.  Only regen cover letter.

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemRegenerateCoPronouncedCoverLetterOfflineConfidential.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemRegenerateCoPronouncedCoverLetterOfflineConfidential.java
@@ -8,14 +8,11 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
-import uk.gov.hmcts.divorce.common.notification.RegenerateConditionalOrderNotification;
+import uk.gov.hmcts.divorce.common.service.task.SendRegeneratedCOPronouncedCoverLetters;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
-import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateConditionalOrderPronouncedDocument;
 import uk.gov.hmcts.divorce.systemupdate.service.task.RegenerateConditionalOrderPronouncedCoverLetter;
-import uk.gov.hmcts.divorce.systemupdate.service.task.RemoveExistingConditionalOrderPronouncedDocument;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
@@ -28,23 +25,21 @@ import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class SystemRegenerateConditionalOrder implements CCDConfig<CaseData, State, UserRole> {
-    public static final String SYSTEM_REGENERATE_CONDITIONAL_ORDER = "system-regenerate-conditional-order";
-    public static final String REGENERATE_CONDITIONAL_ORDER = "Regenerate conditional order";
-
-    private final GenerateConditionalOrderPronouncedDocument generateConditionalOrderPronouncedDocument;
+public class SystemRegenerateCoPronouncedCoverLetterOfflineConfidential implements CCDConfig<CaseData, State, UserRole> {
+    public static final String SYSTEM_REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL =
+        "system-regen-co-pronounced-cover-letter-offline-confidential";
+    public static final String REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL =
+        "Regen coPronounced coverletter";
 
     private final RegenerateConditionalOrderPronouncedCoverLetter regenerateConditionalOrderPronouncedCoverLetter;
-    private final RegenerateConditionalOrderNotification regenerateConditionalOrderNotification;
-    private final NotificationDispatcher notificationDispatcher;
-    private final RemoveExistingConditionalOrderPronouncedDocument removeExistingConditionalOrderPronouncedDocument;
+    private final SendRegeneratedCOPronouncedCoverLetters sendRegeneratedCOPronouncedCoverLetters;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
-            .event(SYSTEM_REGENERATE_CONDITIONAL_ORDER)
+            .event(SYSTEM_REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL)
             .forAllStates()
-            .name(REGENERATE_CONDITIONAL_ORDER)
+            .name(REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL)
             .aboutToSubmitCallback(this::aboutToSubmit)
             .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
             .grantHistoryOnly(CASE_WORKER, LEGAL_ADVISOR, SOLICITOR));
@@ -54,22 +49,19 @@ public class SystemRegenerateConditionalOrder implements CCDConfig<CaseData, Sta
         final CaseDetails<CaseData, State> details,
         final CaseDetails<CaseData, State> beforeDetails
     ) {
-        log.info("System regenerate conditional order callback invoked for Case Id: {}", details.getId());
+        log.info("{} callback invoked for Case Id: {}", SYSTEM_REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL, details.getId());
 
         var caseData = details.getData();
 
         if (caseData.getDocuments().getDocumentGeneratedWithType(CONDITIONAL_ORDER_GRANTED).isPresent()) {
-            log.info("Regenerating CO Pronounced document for Case Id: {}", details.getId());
+            log.info("Regenerating CO Pronounced cover letter for Case Id: {}", details.getId());
 
             caseTasks(
-                removeExistingConditionalOrderPronouncedDocument,
-                generateConditionalOrderPronouncedDocument,
-                regenerateConditionalOrderPronouncedCoverLetter
+                regenerateConditionalOrderPronouncedCoverLetter,
+                sendRegeneratedCOPronouncedCoverLetters
             ).run(details);
-
-            notificationDispatcher.send(regenerateConditionalOrderNotification, caseData, details.getId());
         } else {
-            log.info("No CO Pronounced document to Regenerate on Case Id: {}", details.getId());
+            log.info("No CO Pronounced cover letter to Regenerate on Case Id: {}", details.getId());
         }
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
--- src/test/java/uk/gov/hmcts/divorce/systemupdate/event/SystemRegenerateCoPronouncedCoverLetterOfflineConfidentialTest.java
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/event/SystemRegenerateCoPronouncedCoverLetterOfflineConfidentialTest.java
@@ -10,57 +10,45 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.divorce.common.notification.RegenerateConditionalOrderNotification;
+import uk.gov.hmcts.divorce.common.service.task.SendRegeneratedCOPronouncedCoverLetters;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
-import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateConditionalOrderPronouncedDocument;
 import uk.gov.hmcts.divorce.systemupdate.service.task.RegenerateConditionalOrderPronouncedCoverLetter;
-import uk.gov.hmcts.divorce.systemupdate.service.task.RemoveExistingConditionalOrderPronouncedDocument;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED;
-import static uk.gov.hmcts.divorce.systemupdate.event.SystemRegenerateConditionalOrder.SYSTEM_REGENERATE_CONDITIONAL_ORDER;
+import static uk.gov.hmcts.divorce.systemupdate.event.SystemRegenerateCoPronouncedCoverLetterOfflineConfidential.SYSTEM_REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getDivorceDocumentListValue;
 
 @ExtendWith(MockitoExtension.class)
-class SystemRegenerateConditionalOrderTest {
-
-    @Mock
-    private GenerateConditionalOrderPronouncedDocument generateConditionalOrderPronouncedDocument;
+class SystemRegenerateCoPronouncedCoverLetterOfflineConfidentialTest {
 
     @Mock
     private RegenerateConditionalOrderPronouncedCoverLetter regenerateConditionalOrderPronouncedCoverLetter;
 
     @Mock
-    private RegenerateConditionalOrderNotification regenerateConditionalOrderNotification;
-
-    @Mock
-    private NotificationDispatcher notificationDispatcher;
-
-    @Mock
-    private RemoveExistingConditionalOrderPronouncedDocument removeExistingConditionalOrderPronouncedDocument;
+    private SendRegeneratedCOPronouncedCoverLetters sendRegeneratedCOPronouncedCoverLetters;
 
     @InjectMocks
-    private SystemRegenerateConditionalOrder systemRegenerateConditionalOrder;
+    private SystemRegenerateCoPronouncedCoverLetterOfflineConfidential systemRegenerateCoPronouncedCoverLetterOfflineConfidential;
 
     @Test
     void shouldAddConfigurationToConfigBuilder() {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
-        systemRegenerateConditionalOrder.configure(configBuilder);
+        systemRegenerateCoPronouncedCoverLetterOfflineConfidential.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(SYSTEM_REGENERATE_CONDITIONAL_ORDER);
+            .contains(SYSTEM_REGEN_CO_PRONOUNCED_COVER_LETTER_OFFLINE_CONFIDENTIAL);
     }
 
     @Test
@@ -72,13 +60,11 @@ class SystemRegenerateConditionalOrderTest {
         caseDetails.setData(caseData);
 
         final AboutToStartOrSubmitResponse<CaseData, State> response =
-            systemRegenerateConditionalOrder.aboutToSubmit(caseDetails, caseDetails);
+            systemRegenerateCoPronouncedCoverLetterOfflineConfidential.aboutToSubmit(caseDetails, caseDetails);
 
         assertThat(response.getData()).isEqualTo(caseData);
-        verifyNoInteractions(removeExistingConditionalOrderPronouncedDocument);
-        verifyNoInteractions(generateConditionalOrderPronouncedDocument);
         verifyNoInteractions(regenerateConditionalOrderPronouncedCoverLetter);
-        verifyNoInteractions(notificationDispatcher);
+        verifyNoInteractions(sendRegeneratedCOPronouncedCoverLetters);
     }
 
     @Test
@@ -103,18 +89,15 @@ class SystemRegenerateConditionalOrderTest {
         final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
         caseDetails.setData(caseData);
 
-        when(removeExistingConditionalOrderPronouncedDocument.apply(caseDetails)).thenReturn(caseDetails);
-        when(generateConditionalOrderPronouncedDocument.apply(caseDetails)).thenReturn(caseDetails);
         when(regenerateConditionalOrderPronouncedCoverLetter.apply(caseDetails)).thenReturn(caseDetails);
+        when(sendRegeneratedCOPronouncedCoverLetters.apply(caseDetails)).thenReturn(caseDetails);
 
         final AboutToStartOrSubmitResponse<CaseData, State> response =
-            systemRegenerateConditionalOrder.aboutToSubmit(caseDetails, caseDetails);
+            systemRegenerateCoPronouncedCoverLetterOfflineConfidential.aboutToSubmit(caseDetails, caseDetails);
 
         assertThat(response.getData()).isEqualTo(caseData);
 
-        verify(removeExistingConditionalOrderPronouncedDocument).apply(caseDetails);
-        verify(generateConditionalOrderPronouncedDocument).apply(caseDetails);
         verify(regenerateConditionalOrderPronouncedCoverLetter).apply(caseDetails);
-        verify(notificationDispatcher).send(regenerateConditionalOrderNotification, caseData, caseDetails.getId());
+        verify(sendRegeneratedCOPronouncedCoverLetters).apply(caseDetails);
     }
 }
