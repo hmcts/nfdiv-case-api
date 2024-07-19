f6e548e2d7e4f54bd7e5c402d1e44b2cd7f9d83c
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-07-19 13:06:28 +0100

    Apply same logic for applicant 1

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/ApplicationTab.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/tab/ApplicationTab.java
@@ -259,7 +259,9 @@ public class ApplicationTab implements CCDConfig<CaseData, State, UserRole> {
             .field("applicant1SolicitorAddressOverseas", APPLICANT_1_REPRESENTED_OVERSEAS)
             .field("applicant1SolicitorPhone", "applicant1SolicitorRepresented=\"Yes\"")
             .field("applicant1SolicitorEmail", "applicant1SolicitorRepresented=\"Yes\"")
-            .field("applicant1SolicitorOrganisationPolicy", "applicant1SolicitorRepresented=\"Yes\"")
+            .field(
+                "applicant1SolicitorOrganisationPolicy",
+                "applicant1SolicitorRepresented=\"Yes\" AND applicant1SolicitorOrganisationPolicy.Organisation.OrganisationID=\"*\"")
             .field("applicant1SolicitorAgreeToReceiveEmailsCheckbox", "applicant1SolicitorRepresented=\"Yes\"");
     }
 
