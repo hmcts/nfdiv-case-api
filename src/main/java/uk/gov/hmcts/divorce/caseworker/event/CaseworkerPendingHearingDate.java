c0b15621dc9c9b0773d4475d4754b912d5f698f1
pallavijustice <pallavi.saha@justice.gov.uk>
2024-07-22 12:55:01 +0100

    Resolve checkstyle error

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPendingHearingDate.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPendingHearingDate.java
@@ -16,7 +16,6 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE_DELETE;
 
 @Component
 @Slf4j
--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerListForHearingTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerListForHearingTest.java
@@ -8,7 +8,11 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.divorce.divorcecase.model.*;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.Hearing;
+import uk.gov.hmcts.divorce.divorcecase.model.HearingAttendance;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import java.util.Set;
 
