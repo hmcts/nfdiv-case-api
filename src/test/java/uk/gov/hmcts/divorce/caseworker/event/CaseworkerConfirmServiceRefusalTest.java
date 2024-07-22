e0ca0ebf6c64aebaffaac3757ca56ce675158316
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-07-22 12:45:04 +0100

    NFIDV-4019: Fix unit test

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerConfirmServiceRefusalTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerConfirmServiceRefusalTest.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.divorce.common.notification.ServiceApplicationNotification;
+import uk.gov.hmcts.divorce.divorcecase.model.AlternativeService;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
@@ -46,7 +47,13 @@ class CaseworkerConfirmServiceRefusalTest {
 
     @Test
     void shouldSendNotificationsWhenSubmittedCallback() {
-        CaseDetails<CaseData, State> details = CaseDetails.<CaseData, State>builder().build();
+        CaseDetails<CaseData, State> details = CaseDetails.<CaseData, State>builder()
+                .data(CaseData.builder()
+                        .alternativeService(AlternativeService
+                                .builder()
+                                .build())
+                        .build())
+                .build();
         CaseDetails<CaseData, State> beforeDetails = CaseDetails.<CaseData, State>builder().build();
 
         caseworkerConfirmServiceRefusal.submitted(details, beforeDetails);
