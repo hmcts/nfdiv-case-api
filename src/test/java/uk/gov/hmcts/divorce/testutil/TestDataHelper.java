fbb2561b8e1c5f8ab7e7da8380ea82489f799110
Barry Weeks <barry.weeks@justice.gov.uk>
2024-07-26 16:46:39 +0100

    NFDIV-3774 Add reference to helper sol builder

--- src/test/java/uk/gov/hmcts/divorce/testutil/TestDataHelper.java
+++ src/test/java/uk/gov/hmcts/divorce/testutil/TestDataHelper.java
@@ -175,6 +175,7 @@ import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_LAST_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_MIDDLE_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_ORG_ID;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_ORG_NAME;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_REFERENCE;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_FIRM_NAME;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_NAME;
@@ -289,6 +290,7 @@ public class TestDataHelper {
         return Solicitor.builder()
             .name(TEST_SOLICITOR_NAME)
             .firmName(TEST_SOLICITOR_FIRM_NAME)
+            .reference(TEST_REFERENCE)
             .address(addressGlobalUK.toString())
             .build();
     }
