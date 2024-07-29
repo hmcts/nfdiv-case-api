85a9646a0198dec558b8bdf330352b2745157d8c
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-07-29 11:57:46 +0100

    NFDIV-4193: Fix issue with online Aos

--- src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAndLinkRespondentAnswersTest.java
+++ src/test/java/uk/gov/hmcts/divorce/common/service/task/GenerateAndLinkRespondentAnswersTest.java
@@ -8,10 +8,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.divorce.divorcecase.model.AcknowledgementOfService;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.document.DocumentGenerator;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
+
+import java.time.LocalDateTime;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -76,5 +79,38 @@ class GenerateAndLinkRespondentAnswersTest {
             eq(caseData),
             eq(TEST_CASE_ID));
         assertThat(result.getData().getConditionalOrder().getRespondentAnswersLink()).isNull();
+    }
+
+    @Test
+    void shouldGenerateRespondentAnswersWhenOnlineAosSubmitted() {
+
+        final Document documentLink = new Document("url", "filename", "binary url");
+        final ListValue<DivorceDocument> respondentAnswersListValue = ListValue.<DivorceDocument>builder()
+                .value(DivorceDocument.builder()
+                        .documentType(RESPONDENT_ANSWERS)
+                        .documentLink(documentLink)
+                        .build())
+                .build();
+
+        final CaseData caseData = caseData();
+        caseData.getDocuments().setDocumentsGenerated(singletonList(respondentAnswersListValue));
+        AcknowledgementOfService acknowledgementOfService = AcknowledgementOfService
+                .builder()
+                .dateAosSubmitted(LocalDateTime.now())
+                .build();
+
+        caseData.setAcknowledgementOfService(acknowledgementOfService);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final CaseDetails<CaseData, State> result = generateAndLinkRespondentAnswers.apply(caseDetails);
+
+        verify(documentGenerator).generateAndStoreCaseDocument(RESPONDENT_ANSWERS,
+                RESPONDENT_ANSWERS_TEMPLATE_ID,
+                RESPONDENT_ANSWERS_DOCUMENT_NAME,
+                caseData,
+                TEST_CASE_ID);
     }
 }
