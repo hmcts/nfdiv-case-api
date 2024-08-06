781ef5b23105b5d95edf7b482fac58e3370354b6
Barry Weeks <barry.weeks@justice.gov.uk>
2024-08-06 16:58:41 +0100

    NFDIV-4237 Refactor

--- src/main/java/uk/gov/hmcts/divorce/common/notification/ResendJudicialSeparationCitizenAosResponseNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/common/notification/ResendJudicialSeparationCitizenAosResponseNotification.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.divorce.document.print.LetterPrinter;
 import uk.gov.hmcts.divorce.document.print.documentpack.AosResponseDocumentPack;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 
 @Component
@@ -24,9 +23,9 @@ public class ResendJudicialSeparationCitizenAosResponseNotification implements A
     public void sendToApplicant1Offline(final CaseData caseData, final Long caseId) {
         final AcknowledgementOfService acknowledgementOfService = caseData.getAcknowledgementOfService();
 
-        if (NO.equals(caseData.getApplication().getJsCitizenAosResponseLettersResent())) {
+        if (YES.equals(caseData.getApplicant1().getJsCitizenAosResponseLetterRegenerated())) {
             if (caseData.getApplicant1().isApplicantOffline()) {
-                log.info("Resending JS citizen aos response letter ({}) pack to bulk print as applicant1 is offline. Case id: {}",
+                log.info("Resending JS citizen aos response ({}) pack to bulk print as applicant1 is offline. Case id: {}",
                     acknowledgementOfService.isDisputed() ? "disputed" : "without dispute",
                     caseId
                 );
@@ -42,10 +41,10 @@ public class ResendJudicialSeparationCitizenAosResponseNotification implements A
                 );
 
             } else {
-                log.info("Not resending js citizen aos response letter pack to bulk print as applicant1 is not offline. Case id: {}", caseId);
+                log.info("Not resending js citizen aos response pack to bulk print as applicant1 is not offline. Case id: {}", caseId);
             }
         } else {
-            log.info("Not resending js citizen aos response letter pack to bulk print as already resent. Case id: {}", caseId);
+            log.info("Not resending js citizen aos response pack to bulk print as cover letter not regenerated. Case id: {}", caseId);
         }
     }
 }
