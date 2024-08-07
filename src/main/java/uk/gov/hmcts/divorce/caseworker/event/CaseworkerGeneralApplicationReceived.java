86c3c1e760330d44a629c0a8b74fd58996834ea8
pallavijustice <pallavi.saha@justice.gov.uk>
2024-08-07 11:47:04 +0100

    NFDIV-4226 Initial commit

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerGeneralApplicationReceived.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerGeneralApplicationReceived.java
@@ -1,13 +1,23 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.GeneralApplication;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.solicitor.event.page.GeneralApplicationSelectApplicationType;
+import uk.gov.hmcts.divorce.solicitor.event.page.GeneralApplicationUploadDocument;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.GeneralApplicationReceived;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.POST_SUBMISSION_STATES;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
@@ -16,18 +26,44 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
+@Slf4j
 @Component
 public class CaseworkerGeneralApplicationReceived implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_GENERAL_APPLICATION_RECEIVED = "caseworker-general-application-received";
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
-        new PageBuilder(configBuilder
+        final PageBuilder pageBuilder = addEventConfig(configBuilder);
+
+        final List<CcdPageConfiguration> pages = asList(
+            new GeneralApplicationSelectApplicationType(),
+            new GeneralApplicationUploadDocument()
+        );
+
+        pages.forEach(page -> page.addTo(pageBuilder));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(final CaseDetails<CaseData, State> details) {
+
+        log.info("{} about to start callback invoked for Case Id: {}", CASEWORKER_GENERAL_APPLICATION_RECEIVED, details.getId());
+        final CaseData data = details.getData();
+
+        data.setGeneralApplication(GeneralApplication.builder().build());
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(data)
+            .build();
+    }
+
+    private PageBuilder addEventConfig(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        return new PageBuilder(configBuilder
             .event(CASEWORKER_GENERAL_APPLICATION_RECEIVED)
             .forStateTransition(POST_SUBMISSION_STATES, GeneralApplicationReceived)
             .name("General application received")
             .description("General application received")
             .showEventNotes()
+            .showSummary()
+            .aboutToStartCallback(this::aboutToStart)
             .grant(CREATE_READ_UPDATE, CASE_WORKER)
             .grantHistoryOnly(SUPER_USER, LEGAL_ADVISOR, JUDGE));
     }
