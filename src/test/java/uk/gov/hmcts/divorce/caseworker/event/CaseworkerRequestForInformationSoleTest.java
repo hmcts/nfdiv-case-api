815304b1313284e2b67d4e0be27a2d0d8b744903
Barry Weeks <barry.weeks@justice.gov.uk>
2024-08-27 16:25:56 +0100

    NFDIV-4243
    Add unit tests

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRequestForInformationSoleTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRequestForInformationSoleTest.java
@@ -3,20 +3,33 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerRequestForInformationSole.CASEWORKER_REQUEST_FOR_INFORMATION_SOLE;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Submitted;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CaseworkerRequestForInformationSoleTest {
+
+    @Mock
+    private CaseworkerRequestForInformationHelper helper;
 
     @InjectMocks
     private CaseworkerRequestForInformationSole caseworkerRequestForInformationSole;
@@ -30,5 +43,49 @@ class CaseworkerRequestForInformationSoleTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_REQUEST_FOR_INFORMATION_SOLE);
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteMidEventWhenNoErrorsInEmailAddressValidation() {
+        CaseData caseData = caseData();
+        CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setState(Submitted);
+        when(helper.areEmailsValid(any())).thenReturn(new ArrayList<>());
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerRequestForInformationSole.midEvent(caseDetails, caseDetails);
+
+        assertThat(response.getErrors()).isNull();
+    }
+
+    @Test
+    void shouldFailMidEventWhenErrorsInEmailAddressValidation() {
+        CaseData caseData = caseData();
+        CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setState(Submitted);
+        when(helper.areEmailsValid(any())).thenReturn(Collections.singletonList("Error Text"));
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerRequestForInformationSole.midEvent(caseDetails, caseDetails);
+
+        assertThat(response.getErrors().size()).isEqualTo(1);
+        assertThat(response.getErrors()).isEqualTo(Collections.singletonList("Error Text"));
+    }
+
+    @Test
+    void shouldSuccessfullyCompleteAboutToSubmitEvent() {
+        CaseData caseData = caseData();
+        CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setState(Submitted);
+        when(helper.setParties(any())).thenReturn(caseData);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            caseworkerRequestForInformationSole.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getData()).isEqualTo(caseData);
     }
 }
