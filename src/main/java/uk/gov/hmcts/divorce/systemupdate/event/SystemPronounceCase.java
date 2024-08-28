31736d7df4b6b08fdc2688c7d58629194b01a9a7
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-08-28 13:10:41 +0100

    NFIDV-4179: Poc to set missing court and other fields

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemPronounceCase.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemPronounceCase.java
@@ -1,24 +1,47 @@
 package uk.gov.hmcts.divorce.systemupdate.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
+import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
+import uk.gov.hmcts.divorce.bulkaction.data.BulkListCaseDetails;
+import uk.gov.hmcts.divorce.bulkaction.task.UpdateCaseCourtHearingProvider;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.common.notification.ConditionalOrderPronouncedNotification;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
+import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrderCourt;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.idam.IdamService;
 import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdConflictException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchCaseException;
+import uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService;
 import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateConditionalOrderPronouncedDocument;
 import uk.gov.hmcts.divorce.systemupdate.service.task.RemoveExistingConditionalOrderPronouncedDocument;
 
 import java.util.EnumSet;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+import static org.elasticsearch.index.query.QueryBuilders.existsQuery;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.elasticsearch.index.query.QueryBuilders.rangeQuery;
+import static org.springframework.util.ObjectUtils.isEmpty;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPayment;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPronouncement;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderPronounced;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.OfflineDocumentReceived;
@@ -32,6 +55,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.task.CaseTaskRunner.caseTasks;
 import static uk.gov.hmcts.divorce.document.model.DocumentType.CONDITIONAL_ORDER_GRANTED;
+import static uk.gov.hmcts.divorce.systemupdate.service.CcdSearchService.STATE;
 
 @Component
 @RequiredArgsConstructor
@@ -43,6 +67,10 @@ public class SystemPronounceCase implements CCDConfig<CaseData, State, UserRole>
     private final GenerateConditionalOrderPronouncedDocument generateConditionalOrderPronouncedDocument;
     private final RemoveExistingConditionalOrderPronouncedDocument removeExistingConditionalOrderPronouncedDocument;
     private final NotificationDispatcher notificationDispatcher;
+    private final CcdSearchService ccdSearchService;
+    private final IdamService idamService;
+    private final AuthTokenGenerator authTokenGenerator;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -64,6 +92,10 @@ public class SystemPronounceCase implements CCDConfig<CaseData, State, UserRole>
 
         final CaseData caseData = details.getData();
         final Long caseId = details.getId();
+
+        if (isEmpty(caseData.getConditionalOrder().getCourt())) {
+            updateMissingFields(caseData);
+        }
 
         log.info("Conditional order pronounced for Case({})", caseId);
 
@@ -102,4 +134,32 @@ public class SystemPronounceCase implements CCDConfig<CaseData, State, UserRole>
         }
     }
 
+    private void updateMissingFields(CaseData caseData) {
+
+        final var user = idamService.retrieveSystemUpdateUserDetails();
+        final var serviceAuth = authTokenGenerator.generate();
+
+        try {
+            final BoolQueryBuilder query = boolQuery()
+                    .must(matchQuery("data.bulkListCaseReferenceLink.CaseReference", caseData.getBulkListCaseReferenceLink().getCaseReference()))
+                    .must(existsQuery("data.coCourt"));
+
+            Map<String, Object> otherCaseData =
+                    ccdSearchService.searchForAllCasesWithQuery(query, user, serviceAuth).stream().findFirst().orElseThrow().getData();
+
+            CaseData convertedCaseData = objectMapper.convertValue(otherCaseData, CaseData.class);
+
+            ConditionalOrder conditionalOrder = caseData.getConditionalOrder();
+
+            conditionalOrder.setCourt(convertedCaseData.getConditionalOrder().getCourt());
+            conditionalOrder.setDateAndTimeOfHearing(convertedCaseData.getConditionalOrder().getDateAndTimeOfHearing());
+            conditionalOrder.setOfflineCertificateOfEntitlementDocumentSentToApplicant1(NO);
+            conditionalOrder.setOfflineCertificateOfEntitlementDocumentSentToApplicant2(NO);
+
+        } catch (final CcdSearchCaseException e) {
+            log.error("Error returned", e);
+        } catch (final CcdConflictException e) {
+            log.info("Error returned");
+        }
+    }
 }
