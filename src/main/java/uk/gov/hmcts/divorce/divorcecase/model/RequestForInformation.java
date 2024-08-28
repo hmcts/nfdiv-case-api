f61bb54ee3526e505becba1d7c3f90a6c3c00d78
Barry Weeks <barry.weeks@justice.gov.uk>
2024-08-28 18:38:56 +0100

    NFDIV-4243 Add additional ccd config

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformation.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
 import java.time.LocalDateTime;
 
@@ -21,47 +23,56 @@ public class RequestForInformation {
     @CCD(
         label = "Address to sole",
         typeOverride = FixedList,
-        typeParameterOverride = "RequestForInformationSoleParties"
+        typeParameterOverride = "RequestForInformationSoleParties",
+        access = {DefaultAccess.class}
     )
     private RequestForInformationSoleParties requestForInformationSoleParties;
 
     @CCD(
         label = "Address to joint",
         typeOverride = FixedList,
-        typeParameterOverride = "RequestForInformationJointParties"
+        typeParameterOverride = "RequestForInformationJointParties",
+        access = {DefaultAccess.class}
     )
     private RequestForInformationJointParties requestForInformationJointParties;
 
     @CCD(
-        label = "Name"
+        label = "Name",
+        access = {DefaultAccess.class}
     )
     private String requestForInformationName;
 
     @CCD(
         label = "Email address",
-        typeOverride = Email
+        typeOverride = Email,
+        access = {DefaultAccess.class}
     )
     private String requestForInformationEmailAddress;
 
     @CCD(
-        label = "Secondary Name"
+        label = "Secondary Name",
+        access = {DefaultAccess.class}
     )
     private String requestForInformationSecondaryName;
 
     @CCD(
         label = "Secondary Email address",
-        typeOverride = Email
+        typeOverride = Email,
+        access = {DefaultAccess.class}
     )
     private String requestForInformationSecondaryEmailAddress;
 
     @CCD(
-        label = "Date/Time Of Request"
+        label = "Date/Time Of Request",
+        access = {DefaultAccess.class}
     )
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime requestForInformationDateTime;
 
     @CCD(
         label = "Please provide details",
-        typeOverride = TextArea
+        typeOverride = TextArea,
+        access = {DefaultAccess.class}
     )
     private String requestForInformationDetails;
 }
--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.divorce.divorcecase.model.access.SystemUpdateAndSuperUserAccess;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
 import java.util.List;
 
@@ -23,12 +23,13 @@ public class RequestForInformationList {
         label = "Requests for information",
         typeOverride = Collection,
         typeParameterOverride = "RequestForInformation",
-        access = {SystemUpdateAndSuperUserAccess.class}
+        access = {DefaultAccess.class}
     )
     private List<ListValue<RequestForInformation>> requestsForInformation;
 
     @CCD(
-        label = "Request for information"
+        label = "Request for information",
+        access = {DefaultAccess.class}
     )
     @Builder.Default
     @JsonUnwrapped
