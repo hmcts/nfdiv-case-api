c25b0f994029ad075def417d577975e675d7c40a
Barry Weeks <barry.weeks@justice.gov.uk>
2024-08-28 11:33:29 +0100

    NFDIV-4243 Add notification unit tests

---
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/service/notification/RequestForInformationNotificationTest.java
@@ -0,0 +1,331 @@
+package uk.gov.hmcts.divorce.caseworker.service.notification;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformation;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import static java.lang.String.join;
+import static java.util.Objects.nonNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.APPLICANT1;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.APPLICANT2;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationSoleParties.APPLICANT;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationSoleParties.OTHER;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.ISSUE_DATE_POPULATED;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.NOT_YET_ISSUED;
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.RECIPIENT_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICANT_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.DATE_OF_ISSUE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_JOINT;
+import static uk.gov.hmcts.divorce.notification.CommonContent.IS_SOLE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.NO;
+import static uk.gov.hmcts.divorce.notification.CommonContent.REQUEST_FOR_INFORMATION_DETAILS;
+import static uk.gov.hmcts.divorce.notification.CommonContent.RESPONDENT_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_URL;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SMART_SURVEY;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_NAME;
+import static uk.gov.hmcts.divorce.notification.CommonContent.SOLICITOR_REFERENCE;
+import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.REQUEST_FOR_INFORMATION_JOINT;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.REQUEST_FOR_INFORMATION_JOINT_SOLICITOR;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.REQUEST_FOR_INFORMATION_OTHER;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.REQUEST_FOR_INFORMATION_SOLE;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.REQUEST_FOR_INFORMATION_SOLE_SOLICITOR;
+import static uk.gov.hmcts.divorce.notification.FormatUtil.DATE_TIME_FORMATTER;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.PROFESSIONAL_USERS_SIGN_IN_URL;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.SMART_SURVEY_TEST_URL;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_OTHER_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_OTHER_NAME;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_REFERENCE;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_NAME;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_TEXT;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.applicantRepresentedBySolicitor;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getApplicant;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getMainTemplateVars;
+
+@ExtendWith(MockitoExtension.class)
+public class RequestForInformationNotificationTest {
+
+    @Mock
+    private CommonContent commonContent;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @InjectMocks
+    private RequestForInformationNotification requestForInformationNotification;
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicantWhenNotRepresentedOnSoleCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(SOLE_APPLICATION);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationSoleParties(APPLICANT);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getApplicantTemplateContent();
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant1(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_SOLE),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicantSolicitorWhenRepresentedOnSoleCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(SOLE_APPLICATION);
+        caseData.setApplicant1(applicantRepresentedBySolicitor());
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationSoleParties(APPLICANT);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getSolicitorTemplateContent(caseData);
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getProfessionalUsersSignInUrl(TEST_CASE_ID)).thenReturn(PROFESSIONAL_USERS_SIGN_IN_URL);
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant1Solicitor(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_SOLICITOR_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_SOLE_SOLICITOR),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicant1WhenNotRepresentedOnJointCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(JOINT_APPLICATION);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationJointParties(APPLICANT1);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getApplicantTemplateContent();
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant1(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_JOINT),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicant1SolicitorWhenRepresentedOnJointCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(JOINT_APPLICATION);
+        caseData.setApplicant1(applicantRepresentedBySolicitor());
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationJointParties(APPLICANT1);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getSolicitorTemplateContent(caseData);
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getProfessionalUsersSignInUrl(TEST_CASE_ID)).thenReturn(PROFESSIONAL_USERS_SIGN_IN_URL);
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant1Solicitor(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_SOLICITOR_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_JOINT_SOLICITOR),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicant2WhenNotRepresentedOnJointCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(JOINT_APPLICATION);
+        caseData.setApplicant2(getApplicant());
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationJointParties(APPLICANT2);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getApplicantTemplateContent();
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant2(), caseData.getApplicant1()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant2(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_JOINT),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToApplicant2SolicitorWhenRepresentedOnJointCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(JOINT_APPLICATION);
+        caseData.setApplicant2(applicantRepresentedBySolicitor());
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationJointParties(APPLICANT2);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getSolicitorTemplateContent(caseData);
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant2(), caseData.getApplicant1()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getProfessionalUsersSignInUrl(TEST_CASE_ID)).thenReturn(PROFESSIONAL_USERS_SIGN_IN_URL);
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToApplicant2Solicitor(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_SOLICITOR_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_JOINT_SOLICITOR),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToOtherRecipientOnSoleCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(SOLE_APPLICATION);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationSoleParties(OTHER);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationName(TEST_OTHER_NAME);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationEmailAddress(TEST_OTHER_EMAIL);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getOtherRecipientTemplateContent(caseData);
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToOtherRecipient(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_OTHER_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_OTHER),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    @Test
+    void shouldSendRequestForInformationEmailToOtherRecipientOnJointCase() {
+        CaseData caseData = caseData();
+        caseData.setApplicationType(JOINT_APPLICATION);
+        caseData.getRequestForInformationList().getRequestForInformation()
+            .setRequestForInformationJointParties(RequestForInformationJointParties.OTHER);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationName(TEST_OTHER_NAME);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationEmailAddress(TEST_OTHER_EMAIL);
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationDetails(TEST_TEXT);
+
+        Map<String, String> templateContent = getOtherRecipientTemplateContent(caseData);
+
+        when(commonContent.mainTemplateVars(caseData, TEST_CASE_ID, caseData.getApplicant1(), caseData.getApplicant2()))
+            .thenReturn(getMainTemplateVars());
+
+        when(commonContent.getSmartSurvey()).thenReturn(SMART_SURVEY_TEST_URL);
+
+        requestForInformationNotification.sendToOtherRecipient(caseData, TEST_CASE_ID);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_OTHER_EMAIL),
+            eq(REQUEST_FOR_INFORMATION_OTHER),
+            eq(templateContent),
+            eq(ENGLISH),
+            eq(TEST_CASE_ID)
+        );
+    }
+
+    private Map<String, String> getApplicantTemplateContent() {
+        Map<String, String> templateVars = getMainTemplateVars();
+        templateVars.put(REQUEST_FOR_INFORMATION_DETAILS, TEST_TEXT);
+        templateVars.put(SMART_SURVEY, SMART_SURVEY_TEST_URL);
+
+        return templateVars;
+    }
+
+    private Map<String, String> getSolicitorTemplateContent(final CaseData caseData) {
+        Map<String, String> templateVars = getMainTemplateVars();
+
+        templateVars.put(APPLICANT_NAME,
+            join(" ", caseData.getApplicant1().getFirstName(), caseData.getApplicant1().getLastName()));
+        templateVars.put(RESPONDENT_NAME,
+            join(" ", caseData.getApplicant2().getFirstName(), caseData.getApplicant2().getLastName()));
+        templateVars.put(IS_SOLE, caseData.getApplicationType().isSole() ? YES : NO);
+        templateVars.put(IS_JOINT, !caseData.getApplicationType().isSole() ? YES : NO);
+        templateVars.put(SOLICITOR_NAME, TEST_SOLICITOR_NAME);
+        templateVars.put(DATE_OF_ISSUE, "");
+        templateVars.put(ISSUE_DATE_POPULATED, "false");
+        templateVars.put(NOT_YET_ISSUED, "true");
+        templateVars.put(SOLICITOR_REFERENCE, "not provided");
+        templateVars.put(REQUEST_FOR_INFORMATION_DETAILS, TEST_TEXT);
+        templateVars.put(SIGN_IN_URL, PROFESSIONAL_USERS_SIGN_IN_URL);
+        templateVars.put(SMART_SURVEY, SMART_SURVEY_TEST_URL);
+
+        return templateVars;
+    }
+
+    private Map<String, String> getOtherRecipientTemplateContent(final CaseData caseData) {
+        Map<String, String> templateVars = getMainTemplateVars();
+        templateVars.put(APPLICANT_NAME, caseData.getApplicant1().getFullName());
+        templateVars.put(RESPONDENT_NAME, caseData.getApplicant2().getFullName());
+        templateVars.put(RECIPIENT_NAME, TEST_OTHER_NAME);
+        templateVars.put(ISSUE_DATE_POPULATED, "false");
+        templateVars.put(NOT_YET_ISSUED, "true");
+        templateVars.put(REQUEST_FOR_INFORMATION_DETAILS, TEST_TEXT);
+        templateVars.put(SMART_SURVEY, SMART_SURVEY_TEST_URL);
+
+        return templateVars;
+    }
+}
