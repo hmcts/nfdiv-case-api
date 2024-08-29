69687caced45d067252ca09792df35aacea59f87
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-08-29 16:35:32 +0100

    Add scanned document test

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPrepareGeneralEmailAttachmentsTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerPrepareGeneralEmailAttachmentsTest.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.ccd.sdk.type.Document;
 import uk.gov.hmcts.ccd.sdk.type.DynamicListElement;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.ScannedDocument;
+import uk.gov.hmcts.ccd.sdk.type.ScannedDocumentType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.GeneralEmail;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
@@ -127,6 +128,27 @@ public class CaseworkerPrepareGeneralEmailAttachmentsTest {
 
         assertThat(response.getData().getGeneralEmail().getGeUploadedDocumentNames()).isNotNull();
         assertThat(response.getData().getGeneralEmail().getGeUploadedDocumentNames()
+            .getListItems().size()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldHandleScannedDocumentsWithoutFilesAttachedInAboutToStart() {
+        final CaseData caseData = caseData();
+
+        List<ListValue<ScannedDocument>> scannedDocuments = getListOfScannedDocument(1);
+        scannedDocuments.add(ListValue.<ScannedDocument>builder().value(
+            ScannedDocument.builder().type(ScannedDocumentType.COVERSHEET).build()
+        ).build());
+        caseData.getDocuments().setScannedDocuments(scannedDocuments);
+
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setId(TEST_CASE_ID);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = generalEmail.aboutToStart(caseDetails);
+
+        assertThat(response.getData().getGeneralEmail().getGeScannedDocumentNames()).isNotNull();
+        assertThat(response.getData().getGeneralEmail().getGeScannedDocumentNames()
             .getListItems().size()).isEqualTo(1);
     }
 
