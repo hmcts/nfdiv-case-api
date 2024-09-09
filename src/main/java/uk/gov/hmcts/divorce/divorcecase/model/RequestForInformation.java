b920651bbd7c244555eac4a687a75842266bd04a
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-09 16:22:08 +0100

    NFDIV-4243 Refactor Setters to rfi classes

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformation.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
 import java.time.LocalDateTime;
 
+import static java.lang.Boolean.TRUE;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Email;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
@@ -75,4 +77,35 @@ public class RequestForInformation {
         access = {DefaultAccess.class}
     )
     private String requestForInformationDetails;
+
+    @JsonIgnore
+    public void setValuesFromCaseData(CaseData caseData) {
+        this.setRequestForInformationDateTime(LocalDateTime.now());
+
+        final RequestForInformationSoleParties soleParties = this.getRequestForInformationSoleParties();
+        final RequestForInformationJointParties jointParties = this.getRequestForInformationJointParties();
+        if (RequestForInformationSoleParties.APPLICANT.equals(soleParties)
+            || RequestForInformationJointParties.APPLICANT1.equals(jointParties)) {
+            this.setValues(caseData.getApplicant1(), false);
+        } else if (RequestForInformationJointParties.APPLICANT2.equals(jointParties)) {
+            this.setValues(caseData.getApplicant2(), false);
+        } else if (RequestForInformationJointParties.BOTH.equals(jointParties)) {
+            this.setValues(caseData.getApplicant1(), false);
+            this.setValues(caseData.getApplicant2(), true);
+        }
+    }
+
+    @JsonIgnore
+    private void setValues(Applicant applicant, Boolean setSecondary) {
+        final boolean isRepresented = applicant.isRepresented();
+        final String emailAddress = isRepresented ? applicant.getSolicitor().getEmail() : applicant.getEmail();
+        final String name = isRepresented ? applicant.getSolicitor().getName() : applicant.getFullName();
+        if (TRUE.equals(setSecondary)) {
+            this.setRequestForInformationSecondaryEmailAddress(emailAddress);
+            this.setRequestForInformationSecondaryName(name);
+        } else {
+            this.setRequestForInformationEmailAddress(emailAddress);
+            this.setRequestForInformationName(name);
+        }
+    }
 }
--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,8 +10,10 @@ import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 
 @Data
@@ -34,4 +37,23 @@ public class RequestForInformationList {
     @Builder.Default
     @JsonUnwrapped
     private RequestForInformation requestForInformation = new RequestForInformation();
+
+    @JsonIgnore
+    public RequestForInformation getLatestRequest() {
+        return this.getRequestsForInformation().get(0).getValue();
+    }
+
+    @JsonIgnore
+    public void addRequestToList(RequestForInformation requestForInformation) {
+        final ListValue<RequestForInformation> newRequest = new ListValue<>();
+        newRequest.setValue(requestForInformation);
+
+        if (isEmpty(this.getRequestsForInformation())) {
+            List<ListValue<RequestForInformation>> requests = new ArrayList<>();
+            requests.add(newRequest);
+            this.setRequestsForInformation(requests);
+        } else {
+            this.getRequestsForInformation().add(0, newRequest);
+        }
+    }
 }
