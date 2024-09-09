6e2aaed6585af4b12e67051f1739eefc1f730fb0
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-09 18:40:45 +0100

    NFDIV-4254 WIP Add unit tests

--- src/test/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorRespondRequestForInformationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorRespondRequestForInformationTest.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.divorce.solicitor.event;
 
-import com.google.common.collect.ImmutableSetMultimap;
-import com.google.common.collect.SetMultimap;
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,54 +9,41 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
-import uk.gov.hmcts.ccd.sdk.api.Permission;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.Organisation;
-import uk.gov.hmcts.ccd.sdk.type.OrganisationPolicy;
-import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.MarriageDetails;
-import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformation;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationSoleParties;
-import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.C;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
-import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.JOINT_APPLICATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.Gender.MALE;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.APPLICANT1;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT1SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationSoleParties.APPLICANT;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
-import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.LESS_THAN_ONE_YEAR_AGO;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
 import static uk.gov.hmcts.divorce.solicitor.event.SolicitorCreateApplication.SOLICITOR_CREATE;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.LOCAL_DATE_TIME;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_TEXT;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.applicantRepresentedBySolicitor;
 import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
-import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseDataWithMarriageDate;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.documentWithType;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getApplicant;
 
 @ExtendWith(MockitoExtension.class)
 class SolicitorRespondRequestForInformationTest {
@@ -84,23 +69,132 @@ class SolicitorRespondRequestForInformationTest {
     }
 
     @Test
-    void shouldPopulateDataWithResponseFromApp1Solicitor() {
-        final CaseData caseData = caseData();
-        caseData.setApplicant1(applicantRepresentedBySolicitor());
-        caseData.setApplicationType(SOLE_APPLICATION);
-        final RequestForInformation requestForInformation = caseData.getRequestForInformationList().getRequestForInformation();
-        requestForInformation.setRequestForInformationSoleParties(APPLICANT);
-        requestForInformation.setValuesFromCaseData(caseData);
-        caseData.getRequestForInformationList().addRequestToList(requestForInformation);
+    void shouldPopulateDataWithResponseFromApplicantSolicitorOnSoleCase() {
+        final CaseDetails<CaseData, State> caseDetails = getCaseDetails(SOLE_APPLICATION, APPLICANT, true, false);
+        final CaseData caseData = caseDetails.getData();
+        caseData.getRequestForInformationList().getRequestForInformationResponse().setRequestForInformationResponseDetails(TEST_TEXT);
+        addDocumentToResponse(caseData);
 
+        when(ccdAccessService.isApplicant1(any(), any())).thenReturn(true);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            solicitorRespondRequestForInformation.aboutToSubmit(caseDetails, caseDetails);
+        assertThat(response.getData().getRequestForInformationList().getLatestRequest().getRequestForInformationResponses()).hasSize(1);
+
+        final RequestForInformationResponse responseRequestForInformationResponse =
+            response.getData().getRequestForInformationList().getLatestRequest().getRequestForInformationResponses().get(0).getValue();
+
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseParties()).isEqualTo(APPLICANT1SOLICITOR);
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseEmailAddress())
+            .isEqualTo(caseData.getApplicant1().getSolicitor().getEmail());
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseName())
+            .isEqualTo(caseData.getApplicant1().getSolicitor().getName());
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDetails()).isEqualTo(TEST_TEXT);
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDateTime()).isNotNull();
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDocs()).hasSize(1);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getState()).isEqualTo(RequestedInformationSubmitted);
+    }
+
+    @Test
+    void shouldPopulateDataWithResponseFromApplicant1SolicitorOnJointCase() {
+        final CaseDetails<CaseData, State> caseDetails = getCaseDetails(JOINT_APPLICATION, APPLICANT1, true, false);
+        final CaseData caseData = caseDetails.getData();
+        caseData.getRequestForInformationList().getRequestForInformationResponse().setRequestForInformationResponseDetails(TEST_TEXT);
+        addDocumentToResponse(caseData);
+
+        when(ccdAccessService.isApplicant1(any(), any())).thenReturn(true);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            solicitorRespondRequestForInformation.aboutToSubmit(caseDetails, caseDetails);
+        assertThat(response.getData().getRequestForInformationList().getLatestRequest().getRequestForInformationResponses()).hasSize(1);
+
+        final RequestForInformationResponse responseRequestForInformationResponse =
+            response.getData().getRequestForInformationList().getLatestRequest().getRequestForInformationResponses().get(0).getValue();
+
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseParties()).isEqualTo(APPLICANT1SOLICITOR);
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseEmailAddress())
+            .isEqualTo(caseData.getApplicant1().getSolicitor().getEmail());
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseName())
+            .isEqualTo(caseData.getApplicant1().getSolicitor().getName());
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDetails()).isEqualTo(TEST_TEXT);
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDateTime()).isNotNull();
+        assertThat(responseRequestForInformationResponse.getRequestForInformationResponseDocs()).hasSize(1);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getState()).isEqualTo(RequestedInformationSubmitted);
+    }
+
+    private CaseDetails<CaseData, State> getCaseDetails(ApplicationType applicationType,
+                                       RequestForInformationSoleParties requestForInformationSoleParties,
+                                       Boolean applicantRepresented,
+                                       Boolean applicant2Represented
+    ) {
+        CaseData caseData = getBaseData(applicationType, applicantRepresented, applicant2Represented);
+        setBaseRequestParties(caseData, requestForInformationSoleParties);
+        setBaseRequestValues(caseData);
+
+        return getBaseDetails(caseData);
+    }
+
+    private CaseDetails<CaseData, State> getCaseDetails(ApplicationType applicationType,
+                                       RequestForInformationJointParties requestForInformationJointParties,
+                                       Boolean applicantRepresented,
+                                       Boolean applicant2Represented
+    ) {
+        CaseData caseData = getBaseData(applicationType, applicantRepresented, applicant2Represented);
+        setBaseRequestParties(caseData, requestForInformationJointParties);
+        setBaseRequestValues(caseData);
+
+        return getBaseDetails(caseData);
+    }
+
+    private void setBaseRequestParties(CaseData caseData, RequestForInformationSoleParties requestForInformationSoleParties) {
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationSoleParties(
+            requestForInformationSoleParties
+        );
+    }
+
+    private void setBaseRequestParties(CaseData caseData, RequestForInformationJointParties requestForInformationJointParties) {
+        caseData.getRequestForInformationList().getRequestForInformation().setRequestForInformationJointParties(
+            requestForInformationJointParties
+        );
+    }
+
+    private CaseData getBaseData(ApplicationType applicationType, Boolean applicantRepresented, Boolean applicant2Represented) {
+        final CaseData caseData = caseData();
+        caseData.setApplicationType(applicationType);
+        if (applicantRepresented) {
+            caseData.setApplicant1(applicantRepresentedBySolicitor());
+        }
+        if (applicant2Represented) {
+            caseData.setApplicant2(applicantRepresentedBySolicitor());
+        } else {
+            caseData.setApplicant2(getApplicant(MALE));
+        }
+
+        return caseData;
+    }
+
+    private void setBaseRequestValues(CaseData caseData) {
+        caseData.getRequestForInformationList().getRequestForInformation().setValues(caseData);
+        caseData.getRequestForInformationList().addRequestToList(caseData.getRequestForInformationList().getRequestForInformation());
+    }
+
+    private CaseDetails<CaseData, State> getBaseDetails(CaseData caseData) {
         final CaseDetails<CaseData, State> details = new CaseDetails<>();
         details.setData(caseData);
         details.setState(AwaitingDocuments);
         details.setId(TEST_CASE_ID);
 
-        when(ccdAccessService.isApplicant1(any(), any())).thenReturn(true);
-        when(ccdAccessService.isApplicant2(any(), any())).thenReturn(false);
+        return details;
+    }
 
-
+    private void addDocumentToResponse(CaseData caseData) {
+        final ListValue<DivorceDocument> uploadedDocument = documentWithType(null);
+        List<ListValue<DivorceDocument>> docs = new ArrayList<>();
+        docs.add(uploadedDocument);
+        caseData.getRequestForInformationList().getRequestForInformationResponse().setRequestForInformationResponseDocs(docs);
     }
 }
