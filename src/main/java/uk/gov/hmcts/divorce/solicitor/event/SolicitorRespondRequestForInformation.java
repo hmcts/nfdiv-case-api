ae9cfd29642dfbce75e3ca3266ab10ced73cab61
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-11 16:25:19 +0100

    NFDIV-4254 Update forStates

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorRespondRequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorRespondRequestForInformation.java
@@ -21,7 +21,7 @@ import java.util.Collections;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT1SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT2SOLICITOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.InformationRequested;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
@@ -45,7 +45,7 @@ public class SolicitorRespondRequestForInformation implements CCDConfig<CaseData
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
                 .event(SOLICITOR_RESPOND_REQUEST_FOR_INFORMATION)
-                .forStates(AwaitingDocuments, RequestedInformationSubmitted)
+                .forStates(InformationRequested, RequestedInformationSubmitted)
                 .name("Submit Response")
                 .description("Submit response")
                 .showSummary()
