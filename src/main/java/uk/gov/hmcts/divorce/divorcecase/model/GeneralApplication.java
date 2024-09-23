743d1254cbb99182a6a560ded5fccc6ce0f95e15
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2024-09-23 14:13:01 +0100

    NFDIV-4285: Support multiple docs upload on general application received (#3993)
    * NFDIV-4285: Support multiple docs upload on general application received
    
    * NFDIV-4285 - Add checks on uploaded docs
    
    * NFDIV-4285 - Add test cases
    
    * NFDIV-4285 - Update integration tests
    
    * NFDIV-4285 - Refactor to move common code to CaseData.java
    
    * NFDIV-4285 - Updste request response json for SolicitorGeneralApplicationFT.java
    
    * NFDIV-4285 - Add test case to cover multiple uploads in general app event
    
    * NFDIV-4285 - Restrict the number of allowed uploads to 10
    
    ---------
    
    Co-authored-by: pallavijustice <pallavi.saha@justice.gov.uk>
    Co-authored-by: pallavijustice <159147694+pallavijustice@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralApplication.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralApplication.java
@@ -6,9 +6,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerDeleteAccess;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
+import java.util.List;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
@@ -40,9 +46,17 @@ public class GeneralApplication {
     private GeneralApplicationFee generalApplicationFeeType;
 
     @CCD(
-        label = "General Application Document"
+            label = "General Application Document"
     )
     private DivorceDocument generalApplicationDocument;
+
+    @CCD(
+            label = "General Application Documents",
+            typeOverride = Collection,
+            typeParameterOverride = "DivorceDocument",
+            access = {DefaultAccess.class, CaseworkerDeleteAccess.class}
+    )
+    private List<ListValue<DivorceDocument>> generalApplicationDocuments;
 
     @CCD(
         label = "Additional comments about the supporting document",
