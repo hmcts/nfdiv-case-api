f2b1af5c31e306d8850a38adc49306a3f8ee424d
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-26 15:27:32 +0100

    NFDIV-4246 Add response draft class

---
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationResponseDraft.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.divorce.divorcecase.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
+import uk.gov.hmcts.divorce.document.model.DivorceDocument;
+
+import java.util.List;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.Collection;
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
+
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestForInformationResponseDraft {
+
+    @CCD(
+        label = "Write your response below if the court has asked for additional information. If the court has just asked for documents, "
+            + " then you do not need to write anything unless you think it's useful information.",
+        typeOverride = TextArea,
+        access = {DefaultAccess.class}
+    )
+    private String rfiDraftResponseDetails;
+
+    @CCD(
+        label = "Upload documents",
+        typeOverride = Collection,
+        typeParameterOverride = "DivorceDocument"
+    )
+    private List<ListValue<DivorceDocument>> rfiDraftResponseDocs;
+}
