d31fd4ef6d3fcb8ce204a0ec5052f03ac42360ff
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-26 15:34:17 +0100

    NFDIV-4246 Refactor citizen event to utilise drafts

--- src/main/java/uk/gov/hmcts/divorce/common/event/RespondToRequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/RespondToRequestForInformation.java
@@ -3,42 +3,27 @@ package uk.gov.hmcts.divorce.common.event;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.divorce.citizen.notification.conditionalorder.ClarificationSubmittedNotification;
-import uk.gov.hmcts.divorce.citizen.notification.conditionalorder.PostInformationToCourtNotification;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.ClarificationResponse;
-import uk.gov.hmcts.divorce.divorcecase.model.ConditionalOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationList;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
-import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseDraft;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
 
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Collections;
 
-import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments.addDocumentToTop;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT1;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT2;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingClarification;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.ClarificationSubmitted;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.InformationRequested;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
@@ -52,11 +37,9 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class RespondToRequestForInformation implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String RESPOND_TO_REQUEST_FOR_INFORMATION = "respond-request-for-info";
-    private static final String NEVER_SHOW = "coRefusalDecision=\"NEVER_SHOW\"";
 
     private final CcdAccessService ccdAccessService;
     private final HttpServletRequest request;
-
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -75,13 +58,7 @@ public class RespondToRequestForInformation implements CCDConfig<CaseData, State
                 LEGAL_ADVISOR,
                 JUDGE))
             .page("respondRequestForInfo")
-            .pageLabel("Submit response for RFI")
-            .complex(CaseData::getRequestForInformationList)
-                .complex(RequestForInformationList::getRequestForInformationResponse)
-                    .readonly(RequestForInformationResponse::getRequestForInformationResponseDocs)
-                    .readonly(RequestForInformationResponse::getRequestForInformationResponseDetails)
-                .done()
-            .done();
+            .pageLabel("Submit response for RFI");
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
@@ -89,19 +66,21 @@ public class RespondToRequestForInformation implements CCDConfig<CaseData, State
 
         log.info("{} about to submit callback invoked for Case Id: {}", RESPOND_TO_REQUEST_FOR_INFORMATION, details.getId());
 
-        RequestForInformationResponse response = details.getData().getRequestForInformationList().getRequestForInformationResponse();
+        RequestForInformationList requestForInformationList = details.getData().getRequestForInformationList();
+        RequestForInformationResponse response = new RequestForInformationResponse();
 
         if (isApplicant1(details.getId())) {
             response.setValues(details.getData(), APPLICANT1);
+            requestForInformationList.setRequestForInformationResponseApplicant1(new RequestForInformationResponseDraft());
         } else if (isApplicant2(details.getId())) {
             response.setValues(details.getData(), APPLICANT2);
+            requestForInformationList.setRequestForInformationResponseApplicant2(new RequestForInformationResponseDraft());
         } else {
             return AboutToStartOrSubmitResponse.<CaseData, State>builder()
                 .errors(Collections.singletonList("Unable to determine citizen for Case Id: " + details.getId().toString()))
                 .build();
         }
-        details.getData().getRequestForInformationList().getLatestRequest().addResponseToList(response);
-        details.getData().getRequestForInformationList().setRequestForInformationResponse(new RequestForInformationResponse());
+        requestForInformationList.getLatestRequest().addResponseToList(response);
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(details.getData())
