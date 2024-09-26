8fb407c0d1ff5462385dd1b1d839dccd56c2354c
Barry Weeks <barry.weeks@justice.gov.uk>
2024-09-26 15:32:42 +0100

    Revert "NFDIV-4248 Add aboutToSubmit for easier debugging"
    This reverts commit 0994b2826bb81cf5a5a2ca75f0b1640f04f82264.

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateApplication.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateApplication.java
@@ -1,13 +1,9 @@
 package uk.gov.hmcts.divorce.citizen.event;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
@@ -26,7 +22,6 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
-@Slf4j
 public class CitizenUpdateApplication implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String CITIZEN_UPDATE = "citizen-update-application";
@@ -40,23 +35,8 @@ public class CitizenUpdateApplication implements CCDConfig<CaseData, State, User
                 AwaitingClarification, AwaitingFinalOrder, AwaitingFinalOrderPayment, AwaitingJointFinalOrder,
                 InformationRequested, RequestedInformationSubmitted)
             .showCondition(NEVER_SHOW)
-            .aboutToSubmitCallback(this::aboutToSubmit)
             .name("Patch case")
             .description("Patch a divorce or dissolution")
             .grant(CREATE_READ_UPDATE, CREATOR);
-    }
-
-//    Add about to submit to facilitate easier debug.  Remove before finalising.
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(final CaseDetails<CaseData, State> details,
-                                                                       final CaseDetails<CaseData, State> beforeDetails) {
-
-        log.info("{} about to submit callback invoked for Case Id: {}", CITIZEN_UPDATE, details.getId());
-
-        RequestForInformationResponse response = details.getData().getRequestForInformationList().getRequestForInformationResponse();
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(details.getData())
-            .state(details.getState()) // Don't change state whilst wip. Do this later.
-            .build();
     }
 }
