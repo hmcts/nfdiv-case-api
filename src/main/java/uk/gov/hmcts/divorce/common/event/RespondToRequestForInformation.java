1466d9d191c4272daa9dde8b4fb5c7d819094272
Barry Weeks <barry.weeks@justice.gov.uk>
2024-10-09 13:06:47 +0100

    NFDIV-4246 Refactor to enforce citizen and party relationship

--- src/main/java/uk/gov/hmcts/divorce/common/event/RespondToRequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/common/event/RespondToRequestForInformation.java
@@ -10,9 +10,13 @@ import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformation;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationList;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseDraft;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationSoleParties;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
@@ -40,6 +44,10 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class RespondToRequestForInformation implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String RESPOND_TO_REQUEST_FOR_INFORMATION = "respond-request-for-info";
+    public static final String UNABLE_TO_DETERMINE_CITIZEN_ERROR = "Unable to determine citizen for Case Id: ";
+    public static final String CITIZEN_NOT_VALID_FOR_PARTY_START_ERROR = "Unable to apply response. Applicant ";
+    public static final String CITIZEN_NOT_VALID_FOR_PARTY_MID_ERROR = "not valid for Party (";
+    public static final String CITIZEN_NOT_VALID_FOR_PARTY_END_ERROR = ") on latest RFI for Case Id: ";
 
     private final CcdAccessService ccdAccessService;
     private final HttpServletRequest request;
@@ -69,23 +77,31 @@ public class RespondToRequestForInformation implements CCDConfig<CaseData, State
 
         log.info("{} about to submit callback invoked for Case Id: {}", RESPOND_TO_REQUEST_FOR_INFORMATION, details.getId());
 
-        RequestForInformationList requestForInformationList = details.getData().getRequestForInformationList();
-        RequestForInformationResponse response = new RequestForInformationResponse();
-
+        final RequestForInformationList requestForInformationList = details.getData().getRequestForInformationList();
+        final RequestForInformationResponse response = new RequestForInformationResponse();
+        RequestForInformationResponseParties responseParty;
         if (isApplicant1(details.getId())) {
-            response.setValues(details.getData(), APPLICANT1);
-            requestForInformationList.setRequestForInformationResponseApplicant1(new RequestForInformationResponseDraft());
+            responseParty = APPLICANT1;
         } else if (isApplicant2(details.getId())) {
-            response.setValues(details.getData(), APPLICANT2);
-            requestForInformationList.setRequestForInformationResponseApplicant2(new RequestForInformationResponseDraft());
+            responseParty = APPLICANT2;
         } else {
             return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-                .errors(Collections.singletonList("Unable to determine citizen for Case Id: " + details.getId().toString()))
+                .errors(Collections.singletonList(UNABLE_TO_DETERMINE_CITIZEN_ERROR + details.getId().toString()))
                 .build();
         }
+
+        if (applicantCanRespondToRequestForInformation(details.getData(), responseParty)) {
+            response.setValues(details.getData(), responseParty);
+            clearDraft(details.getData(), responseParty);
+        } else {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(Collections.singletonList(buildErrorString(details, responseParty)))
+                .build();
+        }
+
         requestForInformationList.getLatestRequest().addResponseToList(response);
 
-        State state =
+        final State state =
             requestForInformationList.getLatestRequest().getLatestResponse().getRequestForInformationResponseCannotUploadDocs() == YES
             ? AwaitingRequestedInformation
             : RequestedInformationSubmitted;
@@ -104,4 +120,48 @@ public class RespondToRequestForInformation implements CCDConfig<CaseData, State
         return ccdAccessService.isApplicant2(request.getHeader(AUTHORIZATION), caseId);
     }
 
+    private boolean applicantCanRespondToRequestForInformation(CaseData data, RequestForInformationResponseParties party) {
+        boolean isJoint = !data.getApplicationType().isSole();
+        RequestForInformationSoleParties soleParties =
+            data.getRequestForInformationList().getLatestRequest().getRequestForInformationSoleParties();
+        RequestForInformationJointParties jointParties =
+            data.getRequestForInformationList().getLatestRequest().getRequestForInformationJointParties();
+        boolean solePartyApplicant = RequestForInformationSoleParties.APPLICANT.equals(soleParties);
+        boolean jointPartyApplicant1 = RequestForInformationJointParties.APPLICANT1.equals(jointParties);
+        boolean jointPartyApplicant2 = RequestForInformationJointParties.APPLICANT2.equals(jointParties);
+        boolean jointPartyBoth = RequestForInformationJointParties.BOTH.equals(jointParties);
+
+        if (APPLICANT1.equals(party)) {
+            return isJoint ? jointPartyApplicant1 || jointPartyBoth : solePartyApplicant;
+        } else {
+            return isJoint && (jointPartyApplicant2 || jointPartyBoth);
+        }
+    }
+
+    private void clearDraft(CaseData data, RequestForInformationResponseParties party) {
+        RequestForInformationList requestForInformationList = data.getRequestForInformationList();
+
+        if (APPLICANT1.equals(party)) {
+            requestForInformationList.setRequestForInformationResponseApplicant1(new RequestForInformationResponseDraft());
+        } else {
+            requestForInformationList.setRequestForInformationResponseApplicant2(new RequestForInformationResponseDraft());
+        }
+    }
+
+    private String buildErrorString(CaseDetails<CaseData, State> details, RequestForInformationResponseParties party) {
+        final boolean isSole = details.getData().getApplicationType().isSole();
+        final boolean isApplicant1 = APPLICANT1.equals(party);
+        final RequestForInformation latestRequest = details.getData().getRequestForInformationList().getLatestRequest();
+        String requestParty = isSole
+            ? latestRequest.getRequestForInformationSoleParties().toString()
+            : latestRequest.getRequestForInformationJointParties().toString();
+        String applicant = isApplicant1 ? "1 " : "2 ";
+
+        return CITIZEN_NOT_VALID_FOR_PARTY_START_ERROR
+            + (isSole && isApplicant1 ? "" : applicant)
+            + CITIZEN_NOT_VALID_FOR_PARTY_MID_ERROR
+            + requestParty
+            + CITIZEN_NOT_VALID_FOR_PARTY_END_ERROR
+            + details.getId();
+    }
 }
