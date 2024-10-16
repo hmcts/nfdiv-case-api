905c292d4e8cb0f0bc024f8f7f7f7b94556547de
Barry Weeks <barry.weeks@justice.gov.uk>
2024-10-16 18:07:04 +0100

    NFDIV-4434 Add unit tests

--- src/test/java/uk/gov/hmcts/divorce/solicitor/event/Applicant2SolicitorRespondRequestForInformationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/event/Applicant2SolicitorRespondRequestForInformationTest.java
@@ -21,11 +21,14 @@ import uk.gov.hmcts.divorce.solicitor.service.CcdAccessService;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.APPLICANT1;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.APPLICANT2;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.BOTH;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationJointParties.OTHER;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT1SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT2SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
+import static uk.gov.hmcts.divorce.solicitor.event.Applicant1SolicitorRespondRequestForInformation.NOT_AUTHORISED_TO_RESPOND_ERROR;
 import static uk.gov.hmcts.divorce.solicitor.event.Applicant2SolicitorRespondRequestForInformation.APP_2_SOLICITOR_RESPOND_REQUEST_INFO;
 import static uk.gov.hmcts.divorce.solicitor.event.Applicant2SolicitorRespondRequestForInformation.MUST_ADD_DOCS_OR_DETAILS_ERROR;
 import static uk.gov.hmcts.divorce.solicitor.event.Applicant2SolicitorRespondRequestForInformation.UNABLE_TO_SUBMIT_RESPONSE_ERROR;
@@ -59,6 +62,61 @@ class Applicant2SolicitorRespondRequestForInformationTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(APP_2_SOLICITOR_RESPOND_REQUEST_INFO);
+    }
+
+    @Test
+    void shouldReturnErrorIfRequestForOtherParty() {
+        final CaseDetails<CaseData, State> caseDetails = getRequestForInformationCaseDetails(OTHER, true, false);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            applicant2SolicitorRespondRequestForInformation.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).hasSize(1);
+        assertThat(response.getErrors()).contains(NOT_AUTHORISED_TO_RESPOND_ERROR);
+    }
+
+    @Test
+    void shouldReturnErrorIfRequestForApplicant1() {
+        final CaseDetails<CaseData, State> caseDetails = getRequestForInformationCaseDetails(APPLICANT1, true, false);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            applicant2SolicitorRespondRequestForInformation.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).hasSize(1);
+        assertThat(response.getErrors()).contains(NOT_AUTHORISED_TO_RESPOND_ERROR);
+    }
+
+    @Test
+    void shouldReturnErrorIfSoleApplication() {
+        final CaseDetails<CaseData, State> caseDetails = getRequestForInformationCaseDetails();
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            applicant2SolicitorRespondRequestForInformation.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).hasSize(1);
+        assertThat(response.getErrors()).contains(NOT_AUTHORISED_TO_RESPOND_ERROR);
+    }
+
+    @Test
+    void shouldNotReturnErrorIfRequestForApplicant2() {
+        final CaseDetails<CaseData, State> caseDetails = getRequestForInformationCaseDetails(APPLICANT2, false, true);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            applicant2SolicitorRespondRequestForInformation.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getData()).isEqualTo(caseDetails.getData());
+    }
+
+    @Test
+    void shouldNotReturnErrorIfRequestForBoth() {
+        final CaseDetails<CaseData, State> caseDetails = getRequestForInformationCaseDetails(BOTH, false, true);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response =
+            applicant2SolicitorRespondRequestForInformation.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).isNull();
+        assertThat(response.getData()).isEqualTo(caseDetails.getData());
     }
 
     @Test
