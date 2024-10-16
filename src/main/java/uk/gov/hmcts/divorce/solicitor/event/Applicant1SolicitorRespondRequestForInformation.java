89421c4c0e486d49e53f6db1dc0cc9bafa7cedce
Barry Weeks <barry.weeks@justice.gov.uk>
2024-10-16 18:06:52 +0100

    NFDIV-4434 Add showCondition and aboutToStart

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/Applicant1SolicitorRespondRequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/Applicant1SolicitorRespondRequestForInformation.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationList;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseDraft;
@@ -23,6 +24,8 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty.APPLICANT2;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty.OTHER;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT1SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.InformationRequested;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
@@ -39,7 +42,8 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class Applicant1SolicitorRespondRequestForInformation implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String APP_1_SOLICITOR_RESPOND_REQUEST_INFO = "app1-solicitor-respond-request-info";
-    public static final String MUST_ADD_DOCS_OR_DETAILS_ERROR = "You must upload a document or write a response";
+    public static final String NOT_AUTHORISED_TO_RESPOND_ERROR = "You are not authorised to respond to this request.";
+    public static final String MUST_ADD_DOCS_OR_DETAILS_ERROR = "You must upload a document or write a response.";
     public static final String UNABLE_TO_SUBMIT_RESPONSE_ERROR = "Unable to submit response for Case Id: ";
 
     private final CcdAccessService ccdAccessService;
@@ -50,11 +54,13 @@ public class Applicant1SolicitorRespondRequestForInformation implements CCDConfi
         new PageBuilder(configBuilder
                 .event(APP_1_SOLICITOR_RESPOND_REQUEST_INFO)
                 .forState(InformationRequested)
+                .showCondition("requestForInformationAuthParty=\"applicant1\" OR requestForInformationAuthParty=\"both\"")
                 .name("Submit Response")
                 .description("Submit response")
                 .showSummary()
                 .showEventNotes()
                 .endButtonLabel("Submit")
+                .aboutToStartCallback(this::aboutToStart)
                 .aboutToSubmitCallback(this::aboutToSubmit)
                 .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR)
                 .grantHistoryOnly(CASE_WORKER, SUPER_USER, LEGAL_ADVISOR, JUDGE))
@@ -66,6 +72,23 @@ public class Applicant1SolicitorRespondRequestForInformation implements CCDConfi
                         .optional(RequestForInformationResponseDraft::getRfiDraftResponseDocs)
                     .done()
                 .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(CaseDetails<CaseData, State> details) {
+
+        log.info("{} aboutToStart callback invoked for Case Id: {}", APP_1_SOLICITOR_RESPOND_REQUEST_INFO, details.getId());
+
+        RequestForInformationAuthParty authParty = details.getData().getRequestForInformationList().getRequestForInformationAuthParty();
+
+        if (APPLICANT2.equals(authParty) || OTHER.equals(authParty)) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(Collections.singletonList(NOT_AUTHORISED_TO_RESPOND_ERROR))
+                .build();
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .build();
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/Applicant2SolicitorRespondRequestForInformation.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/Applicant2SolicitorRespondRequestForInformation.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationList;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseDraft;
@@ -23,6 +24,9 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty.APPLICANT1;
+import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty.OTHER;
 import static uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties.APPLICANT2SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.InformationRequested;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
@@ -39,7 +43,8 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class Applicant2SolicitorRespondRequestForInformation implements CCDConfig<CaseData, State, UserRole> {
 
     public static final String APP_2_SOLICITOR_RESPOND_REQUEST_INFO = "app2-solicitor-respond-request-info";
-    public static final String MUST_ADD_DOCS_OR_DETAILS_ERROR = "You must upload a document or write a response";
+    public static final String NOT_AUTHORISED_TO_RESPOND_ERROR = "You are not authorised to respond to this request.";
+    public static final String MUST_ADD_DOCS_OR_DETAILS_ERROR = "You must upload a document or write a response.";
     public static final String UNABLE_TO_SUBMIT_RESPONSE_ERROR = "Unable to submit response for Case Id: ";
 
     private final CcdAccessService ccdAccessService;
@@ -50,11 +55,13 @@ public class Applicant2SolicitorRespondRequestForInformation implements CCDConfi
         new PageBuilder(configBuilder
                 .event(APP_2_SOLICITOR_RESPOND_REQUEST_INFO)
                 .forState(InformationRequested)
+                .showCondition("requestForInformationAuthParty=\"applicant2\" OR requestForInformationAuthParty=\"both\"")
                 .name("Submit Response")
                 .description("Submit response")
                 .showSummary()
                 .showEventNotes()
                 .endButtonLabel("Submit")
+                .aboutToStartCallback(this::aboutToStart)
                 .aboutToSubmitCallback(this::aboutToSubmit)
                 .grant(CREATE_READ_UPDATE, APPLICANT_2_SOLICITOR)
                 .grantHistoryOnly(CASE_WORKER, SUPER_USER, LEGAL_ADVISOR, JUDGE))
@@ -66,6 +73,23 @@ public class Applicant2SolicitorRespondRequestForInformation implements CCDConfi
                         .optional(RequestForInformationResponseDraft::getRfiDraftResponseDocs)
                     .done()
                 .done();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(CaseDetails<CaseData, State> details) {
+
+        log.info("{} aboutToStart callback invoked for Case Id: {}", APP_2_SOLICITOR_RESPOND_REQUEST_INFO, details.getId());
+
+        RequestForInformationAuthParty authParty = details.getData().getRequestForInformationList().getRequestForInformationAuthParty();
+
+        if (SOLE_APPLICATION.equals(details.getData().getApplicationType()) || APPLICANT1.equals(authParty) || OTHER.equals(authParty)) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(Collections.singletonList(NOT_AUTHORISED_TO_RESPOND_ERROR))
+                .build();
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .build();
     }
 
     public AboutToStartOrSubmitResponse<CaseData, State> midEvent(CaseDetails<CaseData, State> details,
