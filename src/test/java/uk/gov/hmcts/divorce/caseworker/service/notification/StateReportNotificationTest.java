3175495c4f82f3671cd82f32a7593185423931a6
DelythJustice <delyth.jones11@justice.gov.uk>
2024-10-24 17:13:13 +0100

    NFDIV-4422 this checkstyle is crazy strict

--- src/test/java/uk/gov/hmcts/divorce/caseworker/service/notification/StateReportNotificationTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/service/notification/StateReportNotificationTest.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
