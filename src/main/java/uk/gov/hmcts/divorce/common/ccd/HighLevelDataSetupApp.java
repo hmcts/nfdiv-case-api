e528052f425eab1724b28d8ff4fe057026ab0503
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-10-25 10:24:01 +0100

    Allow high level data setup to fail in demo, preview and ithc

--- src/main/java/uk/gov/hmcts/divorce/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/divorce/common/ccd/HighLevelDataSetupApp.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.divorce.common.ccd;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import uk.gov.hmcts.befta.BeftaMain;
 import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
@@ -47,6 +49,11 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
 
     public static void main(String[] args) throws Throwable {
         main(HighLevelDataSetupApp.class, args);
+    }
+
+    @Override
+    protected boolean shouldTolerateDataSetupFailure() {
+        return BeftaMain.getConfig().getDefinitionStoreUrl().matches(".*\\.(demo|preview|ithc)\\..*");
     }
 
     @Override
