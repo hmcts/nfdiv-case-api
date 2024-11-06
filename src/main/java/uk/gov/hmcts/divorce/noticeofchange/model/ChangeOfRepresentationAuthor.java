3e097eb5bdd772d767e34d470cfe8460a1ad3907
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-11-06 14:21:54 +0000

    Add new event and unit tests

--- src/main/java/uk/gov/hmcts/divorce/noticeofchange/model/ChangeOfRepresentationAuthor.java
+++ src/main/java/uk/gov/hmcts/divorce/noticeofchange/model/ChangeOfRepresentationAuthor.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ChangeOfRepresentationAuthor {
     CASEWORKER_NOTICE_OF_CHANGE("Caseworker notice of change"),
-    SOLICITOR_NOTICE_OF_CHANGE("Solicitor notice of change");
+    SOLICITOR_NOTICE_OF_CHANGE("Solicitor notice of change"),
+    SOLICITOR_REMOVE_REPRESENTATION("Solicitor remove representation");
 
     private final String value;
 }
