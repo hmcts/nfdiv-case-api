e32900e8fc5088f60772619bc37e03f80577c49e
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-11-07 16:25:06 +0000

    Add webform text variable

--- src/main/java/uk/gov/hmcts/divorce/notification/CommonContent.java
+++ src/main/java/uk/gov/hmcts/divorce/notification/CommonContent.java
@@ -360,6 +360,7 @@ public class CommonContent {
         templateVars.put(NAME, beforeApplicant.getSolicitor().getName());
         templateVars.put(APPLICANT_NAME, beforeApplicant.getFullName());
         templateVars.put(SMART_SURVEY, getSmartSurvey());
+        templateVars.put(WEB_FORM_TEXT, getContactWebFormText(beforeApplicant.getLanguagePreference()));
         return templateVars;
     }
 
