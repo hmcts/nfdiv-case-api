8338a89e41fd4dad3a8afa8d97a60a3683dab4c4
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-12 12:16:05 +0000

    Sow 014 changes

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAddNote.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAddNote.java
@@ -3,13 +3,13 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.ccd.sdk.type.ListValue;
-import uk.gov.hmcts.divorce.caseworker.model.CaseNote;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
@@ -19,12 +19,8 @@ import uk.gov.hmcts.divorce.idam.User;
 
 import java.time.Clock;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.util.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.POST_SUBMISSION_STATES_WITH_WITHDRAWN_AND_REJECTED;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
@@ -46,6 +42,10 @@ public class CaseworkerAddNote implements CCDConfig<CaseData, State, UserRole> {
 
     @Autowired
     private Clock clock;
+
+    @Autowired
+    @Lazy
+    private JdbcTemplate db;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -75,40 +75,17 @@ public class CaseworkerAddNote implements CCDConfig<CaseData, State, UserRole> {
         final User caseworkerUser = idamService.retrieveUser(request.getHeader(AUTHORIZATION));
 
         var caseData = details.getData();
+        db.update(
+            """
+            insert into case_notes(reference, date, note, author)
+            values (?, ?, ?, ?)
+            """,
+            details.getId(),
+            LocalDate.now(clock),
+            caseData.getNote(),
+            caseworkerUser.getUserDetails().getName());
 
-        String note = caseData.getNote();
-
-        var caseNote = new CaseNote();
-        caseNote.setNote(note);
-        caseNote.setDate(LocalDate.now(clock));
-        caseNote.setAuthor(caseworkerUser.getUserDetails().getName());
-
-        if (isEmpty(caseData.getNotes())) {
-            List<ListValue<CaseNote>> listValues = new ArrayList<>();
-
-            var listValue = ListValue
-                .<CaseNote>builder()
-                .id("1")
-                .value(caseNote)
-                .build();
-
-            listValues.add(listValue);
-
-            caseData.setNotes(listValues);
-        } else {
-            AtomicInteger listValueIndex = new AtomicInteger(0);
-            var listValue = ListValue
-                .<CaseNote>builder()
-                .value(caseNote)
-                .build();
-
-            caseData.getNotes().add(0, listValue); // always add new note as first element so that it is displayed on top
-
-            caseData.getNotes().forEach(caseNoteListValue -> caseNoteListValue.setId(String.valueOf(listValueIndex.incrementAndGet())));
-
-        }
-
-        caseData.setNote(null); //Clear note text area as notes value is stored in notes collection
+        caseData.setNote(null); //Clear note text area as notes value is stored in notes table
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(caseData)
