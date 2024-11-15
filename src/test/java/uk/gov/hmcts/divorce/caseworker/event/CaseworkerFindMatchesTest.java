b063097325d2e992dcecb9d9520f790a54a19589
DelythJustice <delyth.jones11@justice.gov.uk>
2024-11-15 15:45:35 +0000

    NFDIV-4512 expanding find matches query to clean data and do wildcard regexp

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerFindMatchesTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerFindMatchesTest.java
@@ -33,7 +33,6 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
