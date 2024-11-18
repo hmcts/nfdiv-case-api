1ba043a005f5771db65c6e17eefabc5aa551c9d7
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-18 12:27:57 +0000

    Correct directory structure

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -4,5 +4,5 @@
 roles=("caseworker-divorce-courtadmin_beta" "caseworker-divorce-superuser" "caseworker-divorce-courtadmin-la" "caseworker-divorce-courtadmin" "caseworker-divorce-solicitor" "caseworker-divorce-judg" "caseworker-divorce-pcqextractor" "caseworker-divorce-systemupdate" "caseworker-divorce-bulkscan" "caseworker-caa" "caseworker-approver" "citizen" "caseworker-divorce" "caseworker" "payments" "pui-case-manager" "pui-finance-manager" "pui-organisation-manager" "pui-user-manager")
 for role in "${roles[@]}"
 do
-  ./bin/ccd-add-role.sh "${role}"
+  ./ccd-add-role.sh "${role}"
 done
