3a3822dcc5b5024dc7e453e8ac818305282cf7d4
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-18 11:48:49 +0000

    resolve formatting issue

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -4,5 +4,5 @@
 roles=("caseworker-divorce-courtadmin_beta" "caseworker-divorce-superuser" "caseworker-divorce-courtadmin-la" "caseworker-divorce-courtadmin" "caseworker-divorce-solicitor" "caseworker-divorce-judg" "caseworker-divorce-pcqextractor" "caseworker-divorce-systemupdate" "caseworker-divorce-bulkscan" "caseworker-caa" "caseworker-approver" "citizen" "caseworker-divorce" "caseworker" "payments" "pui-case-manager" "pui-finance-manager" "pui-organisation-manager" "pui-user-manager")
 for role in "${roles[@]}"
 do
-  ./bin/utils/ccd-add-role.sh "${role}"
+  ./bin/ccd-add-role.sh "${role}"
 done
