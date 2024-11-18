4634b3d99c87f4890eb1a0d6184244bab9498b7b
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-18 17:08:40 +0000

    Read excel sheets

--- bin/ccd-import-definitions.sh
+++ bin/ccd-import-definitions.sh
@@ -8,9 +8,9 @@ echo "Script Path ${scriptPath}"
 root_dir=$(realpath $(dirname ${0})/..)
 build_dir=${root_dir}/build/ccd-config
 
-for file in $(find ${build_dir}/ -name "*.xlsx" \;)
+for file in $(find ${build_dir}/ -name "*.xlsx")
 do
-   echo "files for upload is " + file
+   echo "files for upload is " + $file
   (${scriptPath}/ccd-import-definition.sh $build_dir $file)
 done
 
