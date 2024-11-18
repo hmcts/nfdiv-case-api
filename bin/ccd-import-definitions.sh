e95fa3c5610f8508140405452d76547eb9051f72
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-18 18:26:40 +0000

    Upload only one file

--- bin/ccd-import-definitions.sh
+++ bin/ccd-import-definitions.sh
@@ -10,10 +10,7 @@ build_dir=${root_dir}/build/ccd-config
 
 #for file in $(find  ${build_dir} -name "*.xlsx")
 #do
-   echo "files for upload is " + $file
-   filename=$(basename $file)
-   echo "file name is " + $filename
-  (${scriptPath}/ccd-import-definition.sh "${build_dir}/ccd-NFD-4154-preview.xlsx" ccd-NFD-4154-preview.xlsx)
+(${scriptPath}/ccd-import-definition.sh "${build_dir}/ccd-NFD-4154-preview.xlsx" ccd-NFD-4154-preview.xlsx)
 #done
 
 wait
