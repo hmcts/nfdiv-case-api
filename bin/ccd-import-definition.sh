4a79fd2c5ba4bf7549ff786a9913f5cf9fc492c3
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-18 17:55:33 +0000

    remove version check

--- bin/ccd-import-definition.sh
+++ bin/ccd-import-definition.sh
@@ -12,18 +12,6 @@ uploadFilename="$(date +"%Y%m%d-%H%M%S")-${filename}"
 userToken=$(${dir}/idam-lease-user-token.sh ${DEFINITION_IMPORTER_USERNAME:-ccd.docker.default@hmcts.net} ${DEFINITION_IMPORTER_PASSWORD:-Password12!})
 serviceToken=$(${dir}/idam-lease-service-token.sh ccd_gw $(docker run --rm hmctspublic.azurecr.io/imported/toolbelt/oathtool --totp -b ${CCD_API_GATEWAY_S2S_KEY:-AAAAAAAAAAAAAAAC}))
 
-version="n/a"
-newVersion="n/a"
-
-#if [ "${ENVIRONMENT}" == "preview" ] || [ "${ENVIRONMENT}" == "aat" ]; then
-#  version=$(curl --insecure --silent --show-error -X GET \
-#    ${DEFINITION_STORE_URL_BASE:-http://localhost:4451}/api/data/case-type/NFD/version \
-#    -H "Authorization: Bearer ${userToken}" \
-#    -H "ServiceAuthorization: Bearer ${serviceToken}" || echo 'bypass-if-error')
-#
-#  echo "Current version is ${version}"
-#fi
-
 uploadResponse=$(curl --insecure --silent -w "\n%{http_code}"  --show-error --max-time 60  -X POST \
   ${DEFINITION_STORE_URL_BASE:-http://localhost:4451}/import \
   -H "Authorization: Bearer ${userToken}" \
@@ -36,47 +24,5 @@ echo "Upload response ${uploadResponse}"
 upload_http_code=$(echo "$uploadResponse" | tail -n1)
 upload_response_content=$(echo "$uploadResponse" | sed '$d')
 
-if [ "${ENVIRONMENT}" == "preview" ] || [ "${ENVIRONMENT}" == "aat" ]; then
- if [ "${upload_http_code}" != "201" ]; then
-  echo "Bypassing audit check as on preview - will wait 45s and then verify the version has changed"
-  sleep 45
-
-  newVersion=$(curl --insecure --silent --show-error -X GET \
-    ${DEFINITION_STORE_URL_BASE:-http://localhost:4451}/api/data/case-type/NFD/version \
-    -H "Authorization: Bearer ${userToken}" \
-    -H "ServiceAuthorization: Bearer ${serviceToken}" || echo 'bypass-if-error')
-
-    echo "Current version is ${newVersion}"
-    if [[ "$newVersion" == "$version" ]]; then
-      echo "Version has not changed - the definition was not imported successfully"
-      exit 1
-    fi
- fi
-  echo "CCD definition version has changed, definition successfully uploaded"
-  exit 0
-fi
-
-if [[ "${upload_http_code}" == '504' ]]; then
-  for try in {1..10}
-  do
-    sleep 5
-    echo "Checking status of ${filename} (${uploadFilename}) upload (Try ${try})"
-    audit_response=$(curl --insecure --silent --show-error -X GET \
-      ${DEFINITION_STORE_URL_BASE:-http://localhost:4451}/api/import-audits \
-      -H "Authorization: Bearer ${userToken}" \
-      -H "ServiceAuthorization: Bearer ${serviceToken}")
-
-    if [[ ${audit_response} == *"${uploadFilename}"* ]]; then
-      echo "${filename} (${uploadFilename}) uploaded"
-      exit 0
-    fi
-  done
-else
-  if [[ "${upload_response_content}" == 'Case Definition data successfully imported' ]]; then
-    echo "${filename} (${uploadFilename}) uploaded"
-    exit 0
-  fi
-fi
-
-echo "${filename} (${uploadFilename}) upload failed (${upload_response_content})"
-exit 1;
+echo "File upload status : ${upload_http_code} "
+echo "Upload response content : ${upload_response_content}
