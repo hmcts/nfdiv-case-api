4dfd173505d8c7ab79aec6b18d966345dc6592f5
Alex McAusland <a.m@user.com>
2024-11-21 16:56:02 +0000

    fix jooq

--- flyway/src/main/java/uk/gov/hmcts/rse/Migrator.java
+++ flyway/src/main/java/uk/gov/hmcts/rse/Migrator.java
@@ -4,15 +4,23 @@ import org.flywaydb.core.Flyway;
 import org.postgresql.jdbc.PgConnection;
 
 import javax.sql.DataSource;
+import java.io.File;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Migrator {
     public static void init(Connection connection) throws SQLException {
+        String path;
+        if (new File("nfdiv").exists()) {
+            path = "filesystem:nfdiv/src/main/resources/db/migration";
+        } else {
+            path = "filesystem:src/main/resources/db/migration";
+        }
+
         PgConnection c = (PgConnection) connection;
         Flyway.configure().dataSource(c.getURL(), "test", "test")
                 .detectEncoding(true)
-                .locations("filesystem:nfdiv/src/main/resources/db/migration")
+                .locations(path)
                           .load()
                           .migrate();
     }
