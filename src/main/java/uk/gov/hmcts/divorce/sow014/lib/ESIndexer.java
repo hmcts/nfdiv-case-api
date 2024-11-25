4e662d3881f2a8567cd0206857c64765fd98f833
dharmendra kumar <dharmendra.kumar@hmcts.net>
2024-11-25 16:51:40 +0000

    Put them behind feture try catch block

--- src/main/java/uk/gov/hmcts/divorce/sow014/lib/ESIndexer.java
+++ src/main/java/uk/gov/hmcts/divorce/sow014/lib/ESIndexer.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.divorce.sow014.lib;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.index.IndexRequest;
@@ -24,6 +25,7 @@ import java.util.Set;
 // Simple indexer replicating logstash functionality
 // but saving up to ~1GB of RAM.
 @Component
+@Slf4j
 public class ESIndexer implements CommandLineRunner {
 
     @Autowired
@@ -50,17 +52,17 @@ public class ESIndexer implements CommandLineRunner {
     @SneakyThrows
     private void index() {
 
+        try {
         RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(
             new HttpHost(esHost, 9200)));
+            try (Connection c = db.getDataSource().getConnection()) {
+                c.setAutoCommit(false);
+                while (true) {
+                    Thread.sleep(250);
 
-        try (Connection c = db.getDataSource().getConnection()) {
-            c.setAutoCommit(false);
-            while (true) {
-                Thread.sleep(250);
-
-                // Replicates the behaviour of the previous logstash configuration.
-                // https://github.com/hmcts/rse-cft-lib/blob/94aa0edeb0e1a4337a411ed8e6e20f170ed30bae/cftlib/lib/runtime/compose/logstash/logstash_conf.in#L3
-                var results = c.prepareStatement("""
+                    // Replicates the behaviour of the previous logstash configuration.
+                    // https://github.com/hmcts/rse-cft-lib/blob/94aa0edeb0e1a4337a411ed8e6e20f170ed30bae/cftlib/lib/runtime/compose/logstash/logstash_conf.in#L3
+                    var results = c.prepareStatement("""
                     with updated as (
                       delete from es_queue es where id in (select id from es_queue limit 2000)
                       returning id
@@ -88,47 +90,51 @@ public class ESIndexer implements CommandLineRunner {
                     ) row
                     """).executeQuery();
 
-                BulkRequest request = new BulkRequest();
-                while (results.next()) {
-                    var row = results.getString("row");
-                    request.add(new IndexRequest(results.getString("index_id"))
-                        .id(results.getString("id"))
-                        .source(row, XContentType.JSON));
-
-                    // Replicate CCD's globalsearch logstash setup.
-                    // Where cases define a 'SearchCriteria' field we index certain fields into CCD's central
-                    // 'global search' index.
-                    // https://github.com/hmcts/cnp-flux-config/blob/master/apps/ccd/ccd-logstash/ccd-logstash.yaml#L99-L175
-                    var mapper = new ObjectMapper();
-                    Map<String, Object> map = mapper.readValue(row, new TypeReference<>() {
-                    });
-                    var data = (Map<String, Map<String, Object>>) map.get("data");
-                    if (data.containsKey("SearchCriteria")) {
-                        filter(data, "SearchCriteria", "caseManagementLocation", "CaseAccessCategory",
-                            "caseNameHmctsInternal", "caseManagementCategory");
-                        filter((Map<String, Map<String, Object>>) map.get("supplementary_data"), "HMCTSServiceId");
-                        filter((Map<String, Map<String, Object>>) map.get("data_classification"), "SearchCriteria",
-                            "CaseAccessCategory", "caseManagementLocation", "caseNameHmctsInternal",
-                            "caseManagementCategory");
-                        map.remove("last_state_modified_date");
-                        map.remove("last_modified");
-                        map.remove("created_date");
-                        map.put("index_id", "global_search");
-
-                        request.add(new IndexRequest("global_search")
+                    BulkRequest request = new BulkRequest();
+                    while (results.next()) {
+                        var row = results.getString("row");
+                        request.add(new IndexRequest(results.getString("index_id"))
                             .id(results.getString("id"))
-                            .source(mapper.writeValueAsString(map), XContentType.JSON));
+                            .source(row, XContentType.JSON));
+
+                        // Replicate CCD's globalsearch logstash setup.
+                        // Where cases define a 'SearchCriteria' field we index certain fields into CCD's central
+                        // 'global search' index.
+                        // https://github.com/hmcts/cnp-flux-config/blob/master/apps/ccd/ccd-logstash/ccd-logstash.yaml#L99-L175
+                        var mapper = new ObjectMapper();
+                        Map<String, Object> map = mapper.readValue(row, new TypeReference<>() {
+                        });
+                        var data = (Map<String, Map<String, Object>>) map.get("data");
+                        if (data.containsKey("SearchCriteria")) {
+                            filter(data, "SearchCriteria", "caseManagementLocation", "CaseAccessCategory",
+                                "caseNameHmctsInternal", "caseManagementCategory");
+                            filter((Map<String, Map<String, Object>>) map.get("supplementary_data"), "HMCTSServiceId");
+                            filter((Map<String, Map<String, Object>>) map.get("data_classification"), "SearchCriteria",
+                                "CaseAccessCategory", "caseManagementLocation", "caseNameHmctsInternal",
+                                "caseManagementCategory");
+                            map.remove("last_state_modified_date");
+                            map.remove("last_modified");
+                            map.remove("created_date");
+                            map.put("index_id", "global_search");
+
+                            request.add(new IndexRequest("global_search")
+                                .id(results.getString("id"))
+                                .source(mapper.writeValueAsString(map), XContentType.JSON));
+                        }
                     }
-                }
-                if (request.numberOfActions() > 0) {
-                    var r = client.bulk(request, RequestOptions.DEFAULT);
-                    if (r.hasFailures()) {
-                        throw new RuntimeException("**** Cftlib elasticsearch indexing error(s): "
-                            + r.buildFailureMessage());
+                    if (request.numberOfActions() > 0) {
+                        var r = client.bulk(request, RequestOptions.DEFAULT);
+                        if (r.hasFailures()) {
+                            throw new RuntimeException("**** Cftlib elasticsearch indexing error(s): "
+                                + r.buildFailureMessage());
+                        }
                     }
+                    c.commit();
                 }
-                c.commit();
             }
+        } catch (Exception e) {
+
+            log.info("failed to index", e);
         }
     }
 
