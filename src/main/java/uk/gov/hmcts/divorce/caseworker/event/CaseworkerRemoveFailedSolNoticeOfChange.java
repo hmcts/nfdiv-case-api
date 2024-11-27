67c06bb688a56d65cc33369ea5b4ecca3383152a
Barry Weeks <barry.weeks@justice.gov.uk>
2024-11-27 16:52:40 +0000

    NFDIV-4547 Add SU event to clear Sol NoC Request

---
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerRemoveFailedSolNoticeOfChange.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.divorce.caseworker.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import java.util.Collections;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CaseworkerRemoveFailedSolNoticeOfChange implements CCDConfig<CaseData, State, UserRole> {
+    public static final String CASEWORKER_REMOVE_FAILED_SOL_NOC_REQUEST = "caseworker-remove-failed-sol-noc-request";
+    public static final String NO_NOC_REQUEST_ERROR = "No NoC Request Found for Case Id: ";
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        new PageBuilder(configBuilder
+            .event(CASEWORKER_REMOVE_FAILED_SOL_NOC_REQUEST)
+            .forAllStates()
+            .name("Remove Failed Sol NoC Request")
+            .description("Remove Failed Solicitor Notice of Change Request")
+            .showSummary()
+            .showEventNotes()
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .grant(CREATE_READ_UPDATE, SUPER_USER)
+            .grantHistoryOnly(SUPER_USER, LEGAL_ADVISOR, JUDGE));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        log.info("{} about to submit callback invoked for Case Id: {}", CASEWORKER_REMOVE_FAILED_SOL_NOC_REQUEST, details.getId());
+
+        if (details.getData().getChangeOrganisationRequestField() == null) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(Collections.singletonList(NO_NOC_REQUEST_ERROR + details.getId()))
+                .build();
+        }
+
+        details.getData().setChangeOrganisationRequestField(null);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .build();
+    }
+}
