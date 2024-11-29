6df002c024f71ace179e09b971f8c81a44d7a79c
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-11-29 16:01:54 +0000

    Update interceptor path exclusions

--- src/main/java/uk/gov/hmcts/divorce/common/config/WebMvcConfig.java
+++ src/main/java/uk/gov/hmcts/divorce/common/config/WebMvcConfig.java
@@ -20,6 +20,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 "/swagger-ui/**",
                 "/swagger-ui.html",
                 "/swagger-resources/**",
+                "/payment-test",
+                "/payment-update",
                 "/v3/**",
                 "/favicon.ico",
                 "/health",
