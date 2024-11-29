7d8b0152969946453e2ca214a31f1bd453e99505
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-11-29 11:02:13 +0000

    NFDIV-4493: Fix checkstyle

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerAndSuperUserAccess.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerAndSuperUserAccess.java
@@ -9,7 +9,6 @@ import uk.gov.hmcts.ccd.sdk.api.Permission;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 
--- src/test/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerAndSuperUserAccessTest.java
+++ src/test/java/uk/gov/hmcts/divorce/divorcecase/model/access/CaseworkerAndSuperUserAccessTest.java
@@ -14,7 +14,6 @@ import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
-import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 
