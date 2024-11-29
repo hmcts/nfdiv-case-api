8916a06c8d63e202e5e872f37c4bdd1ad428e130
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-11-29 10:45:31 +0000

    NFDIV-4519: send letter before email notfication

--- src/main/java/uk/gov/hmcts/divorce/notification/NotificationDispatcher.java
+++ src/main/java/uk/gov/hmcts/divorce/notification/NotificationDispatcher.java
@@ -52,19 +52,20 @@ public class NotificationDispatcher {
     private void sendRepresentationGrantedNotifications(boolean isApplicant1, CaseData caseData,
                                                         long caseId, ApplicantNotification applicantNotification) {
         if (isApplicant1) {
+            applicantNotification.sendToApplicant1Offline(caseData, caseId);
+
             if (StringUtils.isNotEmpty(caseData.getApplicant1().getEmail())) {
                 applicantNotification.sendToApplicant1(caseData, caseId);
             }
 
-            applicantNotification.sendToApplicant1Offline(caseData, caseId);
             applicantNotification.sendToApplicant1Solicitor(caseData, caseId);
-
         } else {
+            applicantNotification.sendToApplicant2Offline(caseData, caseId);
+
             if (StringUtils.isNotEmpty(caseData.getApplicant2().getEmail())) {
                 applicantNotification.sendToApplicant2(caseData, caseId);
             }
 
-            applicantNotification.sendToApplicant2Offline(caseData, caseId);
             applicantNotification.sendToApplicant2Solicitor(caseData, caseId);
         }
     }
