814271357d45eed9d0e28ee44e6557bedbaa73ca
Barry Weeks <barry.weeks@justice.gov.uk>
2024-12-18 11:49:49 +0000

    NFDIV-4465 Update and Add ITs

--- src/integrationTest/java/uk/gov/hmcts/divorce/common/SoleApplicationNotDisputedNotificationIT.java
+++ src/integrationTest/java/uk/gov/hmcts/divorce/common/SoleApplicationNotDisputedNotificationIT.java
@@ -14,6 +14,8 @@ import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
+import java.time.LocalDate;
+
 import static java.time.LocalDate.now;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.collection.IsMapContaining.hasEntry;
@@ -21,7 +23,10 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.hamcrest.MockitoHamcrest.argThat;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
 import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.WELSH;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingConditionalOrder;
+import static uk.gov.hmcts.divorce.divorcecase.model.State.Holding;
 import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.CONTACT_DIVORCE_EMAIL;
 import static uk.gov.hmcts.divorce.notification.CommonContent.APPLICATION_REFERENCE;
 import static uk.gov.hmcts.divorce.notification.CommonContent.COURT_EMAIL;
@@ -34,6 +39,7 @@ import static uk.gov.hmcts.divorce.notification.CommonContent.PARTNER;
 import static uk.gov.hmcts.divorce.notification.CommonContent.SIGN_IN_URL;
 import static uk.gov.hmcts.divorce.notification.CommonContent.YES;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_APPLICANT_AOS_SUBMITTED;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLE_APPLICANT_AOS_SUBMITTED_AWAITING_CO;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.FORMATTED_TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
 import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_FIRST_NAME;
@@ -53,6 +59,68 @@ public class SoleApplicationNotDisputedNotificationIT {
     private SoleApplicationNotDisputedNotification soleApplicationNotDisputedNotification;
 
     @Test
+    void shouldSendEmailToApplicant1WithEnglishContentIfApplicant1LanguagePreferenceIsNotWelsh() {
+
+        final CaseData data = validApplicant1CaseData();
+        data.getApplicant1().setLanguagePreferenceWelsh(YesOrNo.NO);
+        data.getApplication().setIssueDate(LocalDate.now());
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(data);
+        caseDetails.setId(TEST_CASE_ID);
+        caseDetails.setState(Holding);
+
+        soleApplicationNotDisputedNotification.sendToApplicant1(caseDetails);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(SOLE_APPLICANT_AOS_SUBMITTED),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, FORMATTED_TEST_CASE_ID),
+                hasEntry(IS_DIVORCE, YES),
+                hasEntry(IS_DISSOLUTION, NO),
+                hasEntry(FIRST_NAME, TEST_FIRST_NAME),
+                hasEntry(LAST_NAME, TEST_LAST_NAME),
+                hasEntry(PARTNER, "husband"),
+                hasEntry(COURT_EMAIL, CONTACT_DIVORCE_EMAIL),
+                hasEntry(SIGN_IN_URL, "https://nfdiv-apply-for-divorce.aat.platform.hmcts.net")
+            )),
+            eq(ENGLISH),
+            anyLong()
+        );
+    }
+
+    @Test
+    void shouldSendEmailToApplicant1AtAwaitingConditionalOrderWithEnglishContentIfApplicant1LanguagePreferenceIsNotWelsh() {
+
+        final CaseData data = validApplicant1CaseData();
+        data.getApplicant1().setLanguagePreferenceWelsh(YesOrNo.NO);
+        data.getApplication().setIssueDate(LocalDate.now());
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(data);
+        caseDetails.setId(TEST_CASE_ID);
+        caseDetails.setState(AwaitingConditionalOrder);
+
+        soleApplicationNotDisputedNotification.sendToApplicant1(caseDetails);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(SOLE_APPLICANT_AOS_SUBMITTED_AWAITING_CO),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, FORMATTED_TEST_CASE_ID),
+                hasEntry(IS_DIVORCE, YES),
+                hasEntry(IS_DISSOLUTION, NO),
+                hasEntry(FIRST_NAME, TEST_FIRST_NAME),
+                hasEntry(LAST_NAME, TEST_LAST_NAME),
+                hasEntry(PARTNER, "husband"),
+                hasEntry(COURT_EMAIL, CONTACT_DIVORCE_EMAIL),
+                hasEntry(SIGN_IN_URL, "https://nfdiv-apply-for-divorce.aat.platform.hmcts.net")
+            )),
+            eq(ENGLISH),
+            anyLong()
+        );
+    }
+
+    @Test
     void shouldSendEmailToApplicant1WithWelshContentIfApplicant1LanguagePreferenceWelsh() {
 
         final CaseData data = validApplicant1CaseData();
@@ -61,12 +129,44 @@ public class SoleApplicationNotDisputedNotificationIT {
         final CaseDetails<CaseData, State> details = new CaseDetails<>();
         details.setData(data);
         details.setId(TEST_CASE_ID);
+        details.setState(Holding);
 
         soleApplicationNotDisputedNotification.sendToApplicant1(details);
 
         verify(notificationService).sendEmail(
             eq(TEST_USER_EMAIL),
             eq(SOLE_APPLICANT_AOS_SUBMITTED),
+            argThat(allOf(
+                hasEntry(APPLICATION_REFERENCE, FORMATTED_TEST_CASE_ID),
+                hasEntry(IS_DIVORCE, YES),
+                hasEntry(IS_DISSOLUTION, NO),
+                hasEntry(FIRST_NAME, TEST_FIRST_NAME),
+                hasEntry(LAST_NAME, TEST_LAST_NAME),
+                hasEntry(PARTNER, "gŵr"),
+                hasEntry(COURT_EMAIL, CONTACT_DIVORCE_EMAIL),
+                hasEntry(SIGN_IN_URL, "https://nfdiv-apply-for-divorce.aat.platform.hmcts.net")
+            )),
+            eq(WELSH),
+            anyLong()
+        );
+    }
+
+    @Test
+    void shouldSendEmailToApplicant1AtAwaitingConditionalOrderWithWelshContentIfApplicant1LanguagePreferenceWelsh() {
+
+        final CaseData data = validApplicant1CaseData();
+        data.getApplicant1().setLanguagePreferenceWelsh(YesOrNo.YES);
+        data.setDueDate(now().plusDays(141));
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(data);
+        details.setId(TEST_CASE_ID);
+        details.setState(AwaitingConditionalOrder);
+
+        soleApplicationNotDisputedNotification.sendToApplicant1(details);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_USER_EMAIL),
+            eq(SOLE_APPLICANT_AOS_SUBMITTED_AWAITING_CO),
             argThat(allOf(
                 hasEntry(APPLICATION_REFERENCE, FORMATTED_TEST_CASE_ID),
                 hasEntry(IS_DIVORCE, YES),
