da20329a9e4e08111ffbac64aa272a2d3ad8d51c
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-01-07 09:04:48 +0000

    Grant back address read permissions for citizen case roles

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CitizenAccess.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/access/CitizenAccess.java
@@ -6,13 +6,18 @@ import uk.gov.hmcts.ccd.sdk.api.HasAccessControl;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_2;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CITIZEN;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 public class CitizenAccess implements HasAccessControl {
     @Override
     public SetMultimap<HasRole, Permission> getGrants() {
         SetMultimap<HasRole, Permission> grants = HashMultimap.create();
-        grants.putAll(CITIZEN, Permissions.CREATE_READ_UPDATE);
+        grants.putAll(CITIZEN, CREATE_READ_UPDATE);
+        grants.putAll(CREATOR, CREATE_READ_UPDATE);
+        grants.putAll(APPLICANT_2, CREATE_READ_UPDATE);
         return grants;
     }
 }
