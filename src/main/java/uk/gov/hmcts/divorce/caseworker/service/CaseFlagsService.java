ac082843f9435e0fdd0d81a64b2317aad7d37e68
pallavijustice <159147694+pallavijustice@users.noreply.github.com>
2025-01-27 10:55:42 +0000

    NFDIV-4343 - Implementation of Case Flags 2.1 for NFD (#4281)
    * NFDIV-4319 - Refactoring code
    
    * NFDIV-4319 - Additional changes to set a flag on the case that case flags setup is complete
    
    * NFDIV-4319 - Assigning access level to field to use in events
    
    * NFDIV-4319 Setting HMCTSServiceId when caseworker progress papercase event is run
    
    * NFDIV-4319 Adding submitted callback configuration to event
    
    * Setting up case flags too when case is being submitted.
    
    * NFDIV-4632 Add unit tests for CaseFlagsService class
    
    * NFDIV-4632 Add test for InternalCaseFlagsAcces
    
    * NFDIV-4343 -Case flags events for NFD
    
    * NFDIV-4343 -Initialize only party flags in aboutToStart callback.
    
    * NFDIV-4343 -Initialise case flags in prepare for case flags event
    
    * NFDIV-4343 - case flag switching when running SwitchToSole event
    
    * NFDIV-4343 - Remove unnecessary code block
    
    * NFDIV-4343 - Update name of solicitor in case flags if name has been updated
    
    * NFDIV-4343 - Fix checkstyle

--- src/main/java/uk/gov/hmcts/divorce/caseworker/service/CaseFlagsService.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/service/CaseFlagsService.java
@@ -28,6 +28,13 @@ public class CaseFlagsService {
     public static final String TEXT_APPLICANT1_SOL_FLAGS_ROLE = "applicant or applicant1 solicitor";
     public static final String TEXT_APPLICANT2_SOL_FLAGS_ROLE = "respondent or applicant2 solicitor";
 
+    public enum PartyFlagType {
+        APPLICANT_1,
+        APPLICANT_2,
+        APPLICANT_1_SOLICITOR,
+        APPLICANT_2_SOLICITOR
+    }
+
     public void setSupplementaryDataForCaseFlags(Long caseId) {
 
         String sysUserToken = idamService.retrieveSystemUpdateUserDetails().getAuthToken();
@@ -112,6 +119,76 @@ public class CaseFlagsService {
                 .groupId(UUID.fromString(caseData.getPartyFlags().getApplicant2SolicitorGroupId()))
                 .visibility(FlagVisibility.INTERNAL)
                 .build());
+        }
+    }
+
+    public void updatePartyNameInCaseFlags(final CaseData caseData, PartyFlagType type) {
+        if (type == PartyFlagType.APPLICANT_1) {
+            if (caseData.getPartyFlags().getApplicant1Flags() != null) {
+                caseData.getPartyFlags().getApplicant1Flags().setPartyName(caseData.getApplicant1().getFullName());
+            }
+        } else if (type == PartyFlagType.APPLICANT_2) {
+            if (caseData.getPartyFlags().getApplicant2Flags() != null) {
+                caseData.getPartyFlags().getApplicant2Flags().setPartyName(caseData.getApplicant2().getFullName());
+            }
+        } else if (type == PartyFlagType.APPLICANT_1_SOLICITOR) {
+            if (caseData.getPartyFlags().getApplicant1SolicitorFlags() != null) {
+                caseData.getPartyFlags().getApplicant1SolicitorFlags().setPartyName(caseData.getApplicant1().getSolicitor().getName());
+            }
+        } else if (type == PartyFlagType.APPLICANT_2_SOLICITOR) {
+            if (caseData.getPartyFlags().getApplicant2SolicitorFlags() != null) {
+                caseData.getPartyFlags().getApplicant2SolicitorFlags().setPartyName(caseData.getApplicant2().getSolicitor().getName());
+            }
+        }
+    }
+
+    public void switchCaseFlags(final CaseData caseData) {
+        Flags app1Flags = caseData.getPartyFlags().getApplicant1Flags();
+        Flags app2Flags = caseData.getPartyFlags().getApplicant2Flags();
+
+        caseData.getPartyFlags().setApplicant1Flags(app2Flags);
+        if (app2Flags != null) {
+            caseData.getPartyFlags().getApplicant1Flags().setRoleOnCase(TEXT_APPLICANT1_FLAGS_ROLE);
+            caseData.getPartyFlags().getApplicant1Flags().setGroupId(UUID.fromString(caseData.getPartyFlags().getApplicant1GroupId()));
+        }
+
+        caseData.getPartyFlags().setApplicant2Flags(app1Flags);
+        if (app1Flags != null) {
+            caseData.getPartyFlags().getApplicant2Flags().setRoleOnCase(TEXT_APPLICANT2_FLAGS_ROLE);
+            caseData.getPartyFlags().getApplicant2Flags().setGroupId(UUID.fromString(caseData.getPartyFlags().getApplicant2GroupId()));
+        }
+
+        Flags app1SolFlags = caseData.getPartyFlags().getApplicant1SolicitorFlags();
+        Flags app2SolFlags = caseData.getPartyFlags().getApplicant2SolicitorFlags();
+
+        if (caseData.getApplicant1().isRepresented()) {
+            caseData.getPartyFlags().setApplicant1SolicitorFlags(app2SolFlags);
+            if (app2SolFlags != null) {
+                caseData.getPartyFlags().getApplicant1SolicitorFlags().setRoleOnCase(TEXT_APPLICANT1_SOL_FLAGS_ROLE);
+                caseData.getPartyFlags().getApplicant1SolicitorFlags().setGroupId(
+                    UUID.fromString(caseData.getPartyFlags().getApplicant1SolicitorGroupId()));
+            }
+        } else {
+            caseData.getPartyFlags().setApplicant1SolicitorFlags(null);
+        }
+
+        if (caseData.getApplicant2().isRepresented()) {
+            caseData.getPartyFlags().setApplicant2SolicitorFlags(app1SolFlags);
+            if (app1SolFlags != null) {
+                caseData.getPartyFlags().getApplicant2SolicitorFlags().setRoleOnCase(TEXT_APPLICANT2_SOL_FLAGS_ROLE);
+                caseData.getPartyFlags().getApplicant2SolicitorFlags().setGroupId(
+                    UUID.fromString(caseData.getPartyFlags().getApplicant2SolicitorGroupId()));
+            }
+        } else {
+            caseData.getPartyFlags().setApplicant2SolicitorFlags(null);
+        }
+    }
+
+    public void resetSolicitorCaseFlags(final CaseData caseData, boolean isApplicant) {
+        if (isApplicant) {
+            caseData.getPartyFlags().setApplicant1SolicitorFlags(null);
+        } else {
+            caseData.getPartyFlags().setApplicant2SolicitorFlags(null);
         }
     }
 }
