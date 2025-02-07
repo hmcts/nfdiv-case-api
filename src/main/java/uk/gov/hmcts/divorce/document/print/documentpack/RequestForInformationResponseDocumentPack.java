c03e1d7262993e10486abfb01a6d0183901e642b
Barry Weeks <barry.weeks@justice.gov.uk>
2025-02-07 18:23:28 +0000

    NFDIV-4558 Add document packs

---
+++ src/main/java/uk/gov/hmcts/divorce/document/print/documentpack/RequestForInformationResponseDocumentPack.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.divorce.document.print.documentpack;
+
+import com.google.common.collect.ImmutableMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationAuthParty;
+import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponseParties;
+
+import java.util.Optional;
+
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_PARTNER_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_PARTNER_RESPONSE_LETTER_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_PARTNER_SOLICITOR_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_PARTNER_SOLICITOR_RESPONSE_LETTER_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_RESPONSE_LETTER_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_SOLICITOR_RESPONSE_LETTER_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.REQUEST_FOR_INFORMATION_SOLICITOR_RESPONSE_LETTER_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.REQUEST_FOR_INFORMATION_RESPONSE;
+
+@Component
+@RequiredArgsConstructor
+public class RequestForInformationResponseDocumentPack implements DocumentPack {
+
+    private static final String LETTER_TYPE_REQUEST_FOR_INFORMATION_RESPONSE = "request-for-information-response-letter";
+
+    private static final DocumentPackInfo APPLICANT_REQUEST_FOR_INFORMATION_RESPONSE_PACK = new DocumentPackInfo(
+        ImmutableMap.of(
+            REQUEST_FOR_INFORMATION_RESPONSE, Optional.of(REQUEST_FOR_INFORMATION_RESPONSE_LETTER_TEMPLATE_ID)
+        ),
+        ImmutableMap.of(
+            REQUEST_FOR_INFORMATION_RESPONSE_LETTER_TEMPLATE_ID, REQUEST_FOR_INFORMATION_RESPONSE_LETTER_DOCUMENT_NAME
+        )
+    );
+
+    private static final DocumentPackInfo SOLICITOR_REQUEST_FOR_INFORMATION_RESPONSE_PACK = new DocumentPackInfo(
+        ImmutableMap.of(
+            REQUEST_FOR_INFORMATION_RESPONSE, Optional.of(REQUEST_FOR_INFORMATION_SOLICITOR_RESPONSE_LETTER_TEMPLATE_ID)
+        ),
+        ImmutableMap.of(
+            REQUEST_FOR_INFORMATION_SOLICITOR_RESPONSE_LETTER_TEMPLATE_ID, REQUEST_FOR_INFORMATION_SOLICITOR_RESPONSE_LETTER_DOCUMENT_NAME
+        )
+    );
+
+    @Override
+    public DocumentPackInfo getDocumentPack(final CaseData caseData, final Applicant applicant) {
+        return applicant.isRepresented()
+            ? SOLICITOR_REQUEST_FOR_INFORMATION_RESPONSE_PACK
+            : APPLICANT_REQUEST_FOR_INFORMATION_RESPONSE_PACK;
+    }
+
+    @Override
+    public String getLetterId() {
+        return LETTER_TYPE_REQUEST_FOR_INFORMATION_RESPONSE;
+    }
+}
