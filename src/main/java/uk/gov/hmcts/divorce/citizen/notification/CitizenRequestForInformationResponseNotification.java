3613d85413e44ab814c6a3639d94dcb7265fab1a
Barry Weeks <barry.weeks@justice.gov.uk>
2025-02-10 16:04:27 +0000

    NFDIV-4558 Refactor response notification

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/CitizenRequestForInformationResponseNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/CitizenRequestForInformationResponseNotification.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.RequestForInformationResponse;
+import uk.gov.hmcts.divorce.document.print.LetterPrinter;
+import uk.gov.hmcts.divorce.document.print.documentpack.RequestForInformationResponseDocumentPack;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.EmailTemplateName;
@@ -26,8 +28,16 @@ public class CitizenRequestForInformationResponseNotification implements Applica
     public static final String REQUEST_FOR_INFORMATION_RESPONSE_NOTIFICATION_TO_FOR_CASE_ID =
         "Sending Request For Information Response Notification to {} for case id: {}";
 
+    public static final String REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID =
+        "Sending Request For Information Response Offline Notification to {} for case id: {}";
+
+    public static final String SKIP_REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID =
+        "Skipping Request For Information Response Offline Notification to {} for case id: {}. Requested Documents Not Provided.";
+
     private final NotificationService notificationService;
     private final CommonContent commonContent;
+    private final RequestForInformationResponseDocumentPack requestForInformationResponseDocumentPack;
+    private final LetterPrinter letterPrinter;
 
     @Override
     public void sendToApplicant1(CaseData caseData, Long caseId) {
@@ -52,6 +62,40 @@ public class CitizenRequestForInformationResponseNotification implements Applica
     }
 
     @Override
+    public void sendToApplicant1Offline(final CaseData caseData, final Long caseId) {
+        if (allDocsProvided(caseData, caseId, caseData.getApplicationType().isSole() ? "applicant" : "applicant 1")) {
+            log.info(REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID,
+                caseData.getApplicationType().isSole() ? "applicant" : "applicant 1", caseId);
+
+            Applicant applicant1 = caseData.getApplicant1();
+            var documentPackInfo = requestForInformationResponseDocumentPack.getDocumentPack(caseData, applicant1);
+            letterPrinter.sendLetters(
+                caseData,
+                caseId,
+                applicant1,
+                documentPackInfo,
+                requestForInformationResponseDocumentPack.getLetterId()
+            );
+        }
+    }
+
+    @Override
+    public void sendToApplicant1SolicitorOffline(final CaseData caseData, final Long caseId) {
+        if (allDocsProvided(
+            caseData,
+            caseId,
+            caseData.getApplicationType().isSole() ? "applicant solicitor" : "applicant 1 solicitor")
+        ) {
+            log.info(REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID,
+                caseData.getApplicationType().isSole() ? "applicant solicitor" : "applicant 1 solicitor", caseId);
+
+            Applicant applicant1 = caseData.getApplicant1();
+            var documentPackInfo = requestForInformationResponseDocumentPack.getDocumentPack(caseData, applicant1);
+            letterPrinter.sendLetters(caseData, caseId, applicant1, documentPackInfo, requestForInformationResponseDocumentPack.getLetterId());
+        }
+    }
+
+    @Override
     public void sendToApplicant2(CaseData caseData, Long caseId) {
         log.info(REQUEST_FOR_INFORMATION_RESPONSE_NOTIFICATION_TO_FOR_CASE_ID, "applicant 2", caseId);
 
@@ -70,6 +114,40 @@ public class CitizenRequestForInformationResponseNotification implements Applica
             caseData.getApplicant2().getLanguagePreference(),
             caseId
         );
+    }
+
+    @Override
+    public void sendToApplicant2Offline(final CaseData caseData, final Long caseId) {
+        if (allDocsProvided(caseData, caseId,  "applicant 2")) {
+            log.info(REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID, "applicant 2", caseId);
+
+            Applicant applicant2 = caseData.getApplicant2();
+            var documentPackInfo = requestForInformationResponseDocumentPack.getDocumentPack(caseData, applicant2);
+            letterPrinter.sendLetters(
+                caseData,
+                caseId,
+                applicant2,
+                documentPackInfo,
+                requestForInformationResponseDocumentPack.getLetterId()
+            );
+        }
+    }
+
+    @Override
+    public void sendToApplicant2SolicitorOffline(final CaseData caseData, final Long caseId) {
+        if (allDocsProvided(caseData, caseId, "applicant 2 solicitor")) {
+            log.info(REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID, "applicant 2 solicitor", caseId);
+
+            Applicant applicant2 = caseData.getApplicant2();
+            var documentPackInfo = requestForInformationResponseDocumentPack.getDocumentPack(caseData, applicant2);
+            letterPrinter.sendLetters(
+                caseData,
+                caseId,
+                applicant2,
+                documentPackInfo,
+                requestForInformationResponseDocumentPack.getLetterId()
+            );
+        }
     }
 
     private Map<String, String> applicantTemplateContent(final CaseData caseData,
@@ -93,5 +171,14 @@ public class CitizenRequestForInformationResponseNotification implements Applica
                 ? REQUEST_FOR_INFORMATION_RESPONSE_CANNOT_UPLOAD_DOCS
                 : REQUEST_FOR_INFORMATION_RESPONSE;
         }
+    }
+
+    private boolean allDocsProvided(final CaseData caseData, final Long caseId, final String errorText) {
+        RequestForInformationResponse latestResponse = caseData.getRequestForInformationList().getLatestRequest().getLatestResponse();
+        if (latestResponse.areAllDocsUploaded()) {
+            return true;
+        }
+        log.info(SKIP_REQUEST_FOR_INFORMATION_RESPONSE_OFFLINE_NOTIFICATION_TO_FOR_CASE_ID, errorText, caseId);
+        return false;
     }
 }
