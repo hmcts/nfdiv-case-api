c3087301c370d3419df271ae642be0154dbe4a61
Barry Weeks <barry.weeks@justice.gov.uk>
2025-02-14 18:42:56 +0000

    NFDIV-4628 Add dropdown + map scanned doc to GO

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/page/CreateGeneralOrder.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/page/CreateGeneralOrder.java
@@ -6,13 +6,19 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.ccd.sdk.type.Document;
+import uk.gov.hmcts.ccd.sdk.type.DynamicList;
+import uk.gov.hmcts.ccd.sdk.type.ListValue;
+import uk.gov.hmcts.ccd.sdk.type.ScannedDocument;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseDocuments;
 import uk.gov.hmcts.divorce.divorcecase.model.GeneralOrder;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.document.CaseDataDocumentService;
 import uk.gov.hmcts.divorce.document.content.GeneralOrderTemplateContent;
+import uk.gov.hmcts.divorce.document.model.DivorceDocument;
+import uk.gov.hmcts.divorce.document.model.DocumentType;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
@@ -47,6 +53,9 @@ public class CreateGeneralOrder implements CcdPageConfiguration {
                 .optional(GeneralOrder::getGeneralOrderJudgeOrLegalAdvisorName)
                 .optional(GeneralOrder::getGeneralOrderJudgeOrLegalAdvisorVenue)
                 .mandatory(GeneralOrder::getGeneralOrderDetails)
+            .done()
+            .complex(CaseData::getDocuments)
+                .optional(CaseDocuments::getScannedDocumentNames)
             .done();
     }
 
@@ -56,8 +65,8 @@ public class CreateGeneralOrder implements CcdPageConfiguration {
     ) {
         log.info("Mid-event callback triggered for CreateGeneralOrder");
 
-        var caseDataCopy = details.getData().toBuilder().build();
-        var generalOrder = caseDataCopy.getGeneralOrder();
+        final CaseData caseData = details.getData();
+        final GeneralOrder generalOrder = caseData.getGeneralOrder();
 
         final Long caseId = details.getId();
 
@@ -66,19 +75,29 @@ public class CreateGeneralOrder implements CcdPageConfiguration {
         log.info("Generating general order document for templateId : {} case caseId: {}",
             DIVORCE_GENERAL_ORDER, caseId);
 
-        Document generalOrderDocument = caseDataDocumentService.renderDocument(
-            generalOrderTemplateContent.apply(caseDataCopy, caseId),
-            caseId,
-            DIVORCE_GENERAL_ORDER,
-            caseDataCopy.getApplicant1().getLanguagePreference(),
-            filename
-        );
+        DynamicList scannedDocNames = caseData.getDocuments().getScannedDocumentNames();
+        String selectedScannedDoc = scannedDocNames == null ? null : scannedDocNames.getValue().getLabel();
+        if (selectedScannedDoc != null) {
+            ListValue<ScannedDocument> scannedDoc = caseData.getDocuments().getScannedDocuments().stream()
+                .filter(g -> g.getValue().getFileName().equals(selectedScannedDoc))
+                .findFirst()
+                .orElseThrow();
+            generalOrder.setGeneralOrderScannedDraft(scannedDoc.getValue());
+        } else {
+            Document generalOrderDocument = caseDataDocumentService.renderDocument(
+                generalOrderTemplateContent.apply(caseData, caseId),
+                caseId,
+                DIVORCE_GENERAL_ORDER,
+                caseData.getApplicant1().getLanguagePreference(),
+                filename
+            );
+            generalOrder.setGeneralOrderDraft(generalOrderDocument);
+        }
 
-        generalOrder.setGeneralOrderDraft(generalOrderDocument);
-        caseDataCopy.setGeneralOrder(generalOrder);
+        caseData.setGeneralOrder(generalOrder);
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(caseDataCopy)
+            .data(caseData)
             .build();
     }
 }
