137b67d9178e4135a62bf90b1dcbf12669145e13
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-03-05 09:14:04 +0000

    Move logging into the frontend

--- src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemCancelCaseInvite.java
+++ src/main/java/uk/gov/hmcts/divorce/systemupdate/event/SystemCancelCaseInvite.java
@@ -3,15 +3,10 @@ package uk.gov.hmcts.divorce.systemupdate.event;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
-import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
-import uk.gov.hmcts.divorce.divorcecase.model.CaseInviteApp1;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-
-import java.util.Optional;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
@@ -31,30 +26,6 @@ public class SystemCancelCaseInvite implements CCDConfig<CaseData, State, UserRo
             .name("Cancel User Case Invite")
             .grant(CREATE_READ_UPDATE, SYSTEMUPDATE)
             .grantHistoryOnly(SUPER_USER)
-            .retries(120, 120)
-            .aboutToSubmitCallback(this::aboutToSubmit);
-    }
-
-    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(CaseDetails<CaseData, State> details,
-                                                                       CaseDetails<CaseData, State> beforeDetails) {
-        long caseId = details.getId();
-        log.info("{} aboutToSubmit callback invoked for case id: {}",  SYSTEM_CANCEL_CASE_INVITE, caseId);
-
-        boolean app1InviteWasCancelled = app1InviteWasCancelled(details.getData(), beforeDetails.getData());
-        log.info("Case invite cancelled by {} for case id: {}", app1InviteWasCancelled ? "Applicant 1" : "Respondent/Applicant 2", caseId);
-
-        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
-            .data(details.getData())
-            .build();
-    }
-
-    private boolean app1InviteWasCancelled(CaseData data, CaseData beforeData) {
-        return app1AccessCodePresent(beforeData) && !app1AccessCodePresent(data);
-    }
-
-    private boolean app1AccessCodePresent(CaseData data) {
-        return Optional.ofNullable(data.getCaseInviteApp1())
-            .map(CaseInviteApp1::accessCodeApplicant1)
-            .isPresent();
+            .retries(120, 120);
     }
 }
