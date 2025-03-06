661c299d784f99fd33a0310e1ddec0d522ded24e
Barry Weeks <barry.weeks@justice.gov.uk>
2025-03-06 17:45:31 +0000

    NFDIV-4686 Add new fields and methods

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/RequestForInformationList.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.divorcecase.model.access.Applicant2Access;
 import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAccessOnlyAccess;
 import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
+import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -93,6 +94,58 @@ public class RequestForInformationList {
     )
     private RequestForInformationAuthParty requestForInformationAuthParty;
 
+    @CCD(
+        label = "Temp Response Document Collection with Indexes",
+        typeOverride = Collection,
+        typeParameterOverride = "RfiResponseDocWithRfiIndex",
+        access = {DefaultAccess.class}
+    )
+    private List<ListValue<RfiResponseDocWithRfiIndex>> responseDocsWithIndexes;
+
+    @CCD(
+        label = "Temp Offline Response Document Collection with Indexes",
+        typeOverride = Collection,
+        typeParameterOverride = "RfiResponseDocWithRfiIndex",
+        access = {DefaultAccess.class}
+    )
+    private List<ListValue<RfiResponseDocWithRfiIndex>> offlineResponseDocsWithIndexes;
+
+    @CCD(
+        label = "Request For Information Online Response Documents",
+        typeOverride = Collection,
+        typeParameterOverride = "DivorceDocument",
+        access = {DefaultAccess.class}
+    )
+    private List<ListValue<DivorceDocument>> rfiOnlineResponseDocuments;
+
+    @JsonIgnore
+    private boolean isNullOrEmpty(List list) {
+        return list == null || list.isEmpty();
+    }
+
+    @JsonIgnore
+    private void addResponseDocToCollection(
+        int rfiId,
+        int rfiResponseId,
+        int rfiDocId,
+        DivorceDocument rfiResponseDoc,
+        List<ListValue<RfiResponseDocWithRfiIndex>> targetCollection
+    ) {
+        RfiResponseDocWithRfiIndex doc = new RfiResponseDocWithRfiIndex();
+        doc.setRfiId(rfiId);
+        doc.setRfiResponseId(rfiResponseId);
+        doc.setRfiResponseDocId(rfiDocId);
+        doc.setRfiResponseDoc(rfiResponseDoc);
+
+        ListValue<DivorceDocument> documentListValue = new ListValue<>();
+        documentListValue.setValue(rfiResponseDoc);
+
+        ListValue<RfiResponseDocWithRfiIndex> docListValue = new ListValue<>();
+        docListValue.setValue(doc);
+
+        targetCollection.add(docListValue);
+    }
+
     @JsonIgnore
     public RequestForInformation getLatestRequest() {
         return this.getRequestsForInformation().get(0).getValue();
@@ -113,5 +166,73 @@ public class RequestForInformationList {
 
         this.setRequestForInformationAuthParty(requestForInformation.getAuthorisedResponseParty());
         this.setLatestRequestForInformationDetails(requestForInformation.getRequestForInformationDetails());
+    }
+
+    @JsonIgnore
+    public void buildResponseDocList() {
+        buildTempDocLists();
+        this.setRfiOnlineResponseDocuments(null);
+        if (!isNullOrEmpty(this.getResponseDocsWithIndexes())) {
+            List<ListValue<DivorceDocument>> responseDocs = new ArrayList<>();
+            this.getResponseDocsWithIndexes().forEach(doc -> {
+                ListValue<DivorceDocument> responseDoc = new ListValue<>();
+                responseDoc.setValue(doc.getValue().getRfiResponseDoc());
+                responseDocs.add(responseDoc);
+            });
+            this.setRfiOnlineResponseDocuments(responseDocs);
+        }
+    }
+
+    @JsonIgnore
+    public void buildTempDocLists() {
+        if (!isNullOrEmpty(this.getRequestsForInformation())) {
+            clearTempDocLists();
+            for (int rfiIdx = 0; rfiIdx < this.getRequestsForInformation().size(); rfiIdx += 1) {
+                final RequestForInformation rfi = this.getRequestsForInformation().get(rfiIdx).getValue();
+                if (!isNullOrEmpty(rfi.getRequestForInformationResponses())) {
+                    for (int resIdx = 0; resIdx < rfi.getRequestForInformationResponses().size(); resIdx += 1) {
+                        final RequestForInformationResponse rfiResponse = rfi.getRequestForInformationResponses().get(resIdx).getValue();
+                        if (!isNullOrEmpty(rfiResponse.getRequestForInformationResponseDocs())) {
+                            if (isNullOrEmpty(this.getResponseDocsWithIndexes())) {
+                                this.setResponseDocsWithIndexes(new ArrayList<>());
+                            }
+                            for (int docIdx = 0; docIdx < rfiResponse.getRequestForInformationResponseDocs().size(); docIdx += 1) {
+                                final DivorceDocument rfiResponseDoc =
+                                    rfiResponse.getRequestForInformationResponseDocs().get(docIdx).getValue();
+                                addResponseDocToCollection(rfiIdx, resIdx, docIdx, rfiResponseDoc, this.getResponseDocsWithIndexes());
+                            }
+                        }
+
+                        if (!isNullOrEmpty(rfiResponse.getRfiOfflineResponseDocs())) {
+                            if (isNullOrEmpty(this.getOfflineResponseDocsWithIndexes())) {
+                                this.setOfflineResponseDocsWithIndexes(new ArrayList<>());
+                            }
+                            for (int docIdx = 0; docIdx < rfiResponse.getRfiOfflineResponseDocs().size(); docIdx += 1) {
+                                final DivorceDocument rfiOfflineResponseDoc =
+                                    rfiResponse.getRfiOfflineResponseDocs().get(docIdx).getValue();
+                                addResponseDocToCollection(
+                                    rfiIdx,
+                                    resIdx,
+                                    docIdx,
+                                    rfiOfflineResponseDoc,
+                                    this.getOfflineResponseDocsWithIndexes()
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @JsonIgnore
+    public void clearTempDocLists() {
+        this.setResponseDocsWithIndexes(null);
+        this.setOfflineResponseDocsWithIndexes(null);
+    }
+
+    @JsonIgnore
+    public RequestForInformation getRequestForInformationByIndex(int index) {
+        return this.getRequestsForInformation().get(index).getValue();
     }
 }
