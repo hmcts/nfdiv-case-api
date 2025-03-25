c0e03ee60c46baf7143148b5476bbd3d2908538f
pallavijustice <pallavi.saha@justice.gov.uk>
2025-03-25 14:19:37 +0000

    NFDIV-4671 - Add test cases for sonar coverage

--- src/test/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant2Test.java
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/event/page/SolAboutApplicant2Test.java
@@ -12,6 +12,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,6 +43,26 @@ public class SolAboutApplicant2Test {
         assertThat(response.getErrors()).isNotNull();
         assertThat(response.getErrors().size()).isEqualTo(1);
         assertThat(response.getErrors().get(0)).isEqualTo("Error");
+
+        validationUtilMockedStatic.close();
+    }
+
+    @Test
+    public void shouldNotReturnAnyErrorsWhenValidationIsSuccessful() {
+        final CaseData caseData = validApplicant2CaseData();
+
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setData(caseData);
+        details.setId(TEST_CASE_ID);
+
+        List<String> errors = Collections.emptyList();
+
+        MockedStatic<ValidationUtil> validationUtilMockedStatic = Mockito.mockStatic(ValidationUtil.class);
+        validationUtilMockedStatic.when(() -> ValidationUtil.validateApplicant2NameForAllowedCharacters(caseData)).thenReturn(errors);
+
+        AboutToStartOrSubmitResponse<CaseData, State> response = page.midEvent(details, details);
+
+        assertThat(response.getErrors()).isNull();
 
         validationUtilMockedStatic.close();
     }
