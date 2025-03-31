5ba922ba006ffa023efd5afdfdb3b0974eadd4c6
Barry Weeks <barry.weeks@justice.gov.uk>
2025-03-31 15:09:11 +0100

    NFDIV-4714 Update event desc & fix imports

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenNoResponseJourney.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenNoResponseJourney.java
@@ -8,18 +8,7 @@ import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
 import static uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration.NEVER_SHOW;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingApplicant1Response;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingClarification;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrder;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingFinalOrderPayment;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingJointFinalOrder;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingRequestedInformation;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderDrafted;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.ConditionalOrderPending;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.Draft;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.InformationRequested;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.POST_ISSUE_STATES;
-import static uk.gov.hmcts.divorce.divorcecase.model.State.RequestedInformationSubmitted;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
@@ -35,8 +24,8 @@ public class CitizenNoResponseJourney implements CCDConfig<CaseData, State, User
             .event(CITIZEN_NO_RESPONSE_JOURNEY)
             .forStates(POST_ISSUE_STATES)
             .showCondition(NEVER_SHOW)
-            .name("Patch case")
-            .description("Patch a divorce or dissolution")
+            .name("Set noResponseJourney options")
+            .description("Set no response journey options")
             .grant(CREATE_READ_UPDATE, CREATOR);
     }
 }
