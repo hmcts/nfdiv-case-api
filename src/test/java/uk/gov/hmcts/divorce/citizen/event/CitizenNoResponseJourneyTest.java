350637de138921f0b11a489bda033510ea560a38
Barry Weeks <barry.weeks@justice.gov.uk>
2025-03-31 15:10:20 +0100

    NFDIV-4714 Add unit test

---
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenNoResponseJourneyTest.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.divorce.citizen.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.divorce.citizen.event.CitizenNoResponseJourney.CITIZEN_NO_RESPONSE_JOURNEY;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+
+@ExtendWith(MockitoExtension.class)
+class CitizenNoResponseJourneyTest {
+
+    @InjectMocks
+    private CitizenNoResponseJourney citizenNoResponseJourney;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        citizenNoResponseJourney.configure(configBuilder);
+
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(CITIZEN_NO_RESPONSE_JOURNEY);
+    }
+}
