c93acce503bb5069aca02ae41193b88805fbefcd
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-04-01 13:46:17 +0100

    Revert "NFDIV-4670 Add Validations in CW events to allow only valid characters in names (#4368)"
    This reverts commit 745bfd9821778ca4455d1eeae15eb471278c9443.

--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendCaseTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerAmendCaseTest.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.divorce.caseworker.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
@@ -13,16 +11,12 @@ import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
-import uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyList;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerAmendCase.CASEWORKER_AMEND_CASE;
 import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.LESS_THAN_ONE_YEAR_SINCE_SUBMISSION;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
@@ -74,31 +68,5 @@ class CaseworkerAmendCaseTest {
 
         assertThat(response.getErrors()).isNull();
         assertThat(response.getWarnings()).isNull();
-    }
-
-    @Test
-    void shouldCallValidationUtilMethodToValidateMarriageCertificateNames() {
-        final CaseData caseData = caseDataWithMarriageDate();
-
-        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
-        caseDetails.setData(caseData);
-        caseDetails.setState(State.Submitted);
-
-        List<String> errors = new ArrayList<>();
-        errors.add("Error");
-
-        MockedStatic<ValidationUtil> validationUtilMockedStatic = Mockito.mockStatic(ValidationUtil.class);
-        validationUtilMockedStatic.when(() -> ValidationUtil.validateMarriageCertificateNames(caseData)).thenReturn(errors);
-        validationUtilMockedStatic.when(() -> ValidationUtil.validateMarriageDate(
-            caseData, "MarriageDate", true)).thenReturn(Collections.emptyList());
-        validationUtilMockedStatic.when(() -> ValidationUtil.flattenLists(anyList(), anyList())).thenReturn(errors);
-
-        AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerAmendCase.aboutToSubmit(caseDetails, caseDetails);
-
-        assertThat(response.getErrors()).isNotNull();
-        assertThat(response.getErrors().size()).isEqualTo(1);
-        assertThat(response.getErrors().get(0)).isEqualTo("Error");
-
-        validationUtilMockedStatic.close();
     }
 }
