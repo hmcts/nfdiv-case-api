0065738f9fca3444acfa86cd528713837b7b1467
Barry Weeks <barry.weeks@justice.gov.uk>
2025-04-02 13:39:51 +0100

    NFDIV-4714 Fix Imports

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralApplicationOptions.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/GeneralApplicationOptions.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
-
-import static uk.gov.hmcts.ccd.sdk.type.FieldType.FixedRadioList;
 
 @Data
 @Builder
