8f5f869816c40a24b43ce930124de5f5b082e9d6
pallavijustice <pallavi.saha@justice.gov.uk>
2025-04-25 13:56:19 +0100

    NFDIV-4837 - Revert changes for some classes

--- src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1IntendToSwitchToSoleFoNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/notification/Applicant1IntendToSwitchToSoleFoNotification.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.divorce.citizen.notification;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.notification.ApplicantNotification;
@@ -14,12 +14,13 @@ import static uk.gov.hmcts.divorce.notification.EmailTemplateName.PARTNER_INTEND
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 public class Applicant1IntendToSwitchToSoleFoNotification extends IntendToSwitchToSoleFoNotification implements ApplicantNotification {
 
-    private final NotificationService notificationService;
+    @Autowired
+    private NotificationService notificationService;
 
-    private final SwitchToSoleSolicitorTemplateContent solicitorTemplateContent;
+    @Autowired
+    SwitchToSoleSolicitorTemplateContent solicitorTemplateContent;
 
     @Override
     public void sendToApplicant1(final CaseData caseData, final Long id) {
