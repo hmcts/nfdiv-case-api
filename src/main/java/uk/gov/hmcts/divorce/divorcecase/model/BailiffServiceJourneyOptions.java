d3c0a5e0ace8d539bb9976d8a8bd1f93a09b8029
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-06-12 16:36:38 +0100

    Add new pages covered in nfdiv-4770

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/BailiffServiceJourneyOptions.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/BailiffServiceJourneyOptions.java
@@ -5,16 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
+import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class BailiffServiceJourneyOptions {
-    @CCD(
-        label = "Enter your partner's name",
-        access = {DefaultAccess.class}
-    )
+    @CCD(label = "Enter your partner's name")
     private String bailiffPartnersName;
+
+    @CCD(
+        label = "Is your partner currently resident in a refuge?"
+    )
+    private YesOrNoOrNotKnown bailiffPartnerInARefuge;
+
+    @CCD(
+        label = "Do you know your partner's phone number?"
+    )
+    private YesOrNo bailiffKnowPartnersPhone;
+
+    @CCD(
+        label = "Partners phone number",
+        regex = "^[0-9 +().-]{9,}$"
+    )
+    private String bailiffPartnersPhone;
 }
