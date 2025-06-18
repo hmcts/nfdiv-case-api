feb80977be96bdb4aa40f027d07812102ff28e7d
Barry Weeks <barry.weeks@justice.gov.uk>
2025-06-18 18:35:36 +0100

    NFDIV-4763 Add new field

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DispenseWithServiceJourneyOptions.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DispenseWithServiceJourneyOptions.java
@@ -1,12 +1,16 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
 import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
+import uk.gov.hmcts.divorce.divorcecase.model.access.CaseworkerAccess;
 import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -15,8 +19,15 @@ import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
 public class DispenseWithServiceJourneyOptions {
 
     @CCD(
-        label = "Do you and your partner live together?",
+        label = "Did you and your partner live together?",
         access = {DefaultAccess.class}
     )
     private YesOrNo dispenseLiveTogether;
+
+    @CCD(
+        label = "Date when you last lived together",
+        access = {CaseworkerAccess.class}
+    )
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dispenseLivedTogetherDate;
 }
