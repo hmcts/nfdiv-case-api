f4fe360c69e71c727a7d3f90ded4ef27c7d5f1a8
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-06-25 15:49:17 +0100

    NFDIV-4763 Refactor partner past address

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DispensePartnerPastAddress.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DispensePartnerPastAddress.java
@@ -5,9 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.ccd.sdk.api.CCD;
-import uk.gov.hmcts.ccd.sdk.type.AddressGlobalUK;
-import uk.gov.hmcts.ccd.sdk.type.YesOrNo;
 import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccess;
+
+import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 
 @Data
 @Builder
@@ -17,12 +17,10 @@ public class DispensePartnerPastAddress {
 
     @CCD(
         label = "Where did your partner live after you parted?",
+        typeOverride = TextArea,
         access = {DefaultAccess.class}
     )
-    private AddressGlobalUK address;
-
-    @CCD(label = "Was this an international address?")
-    private YesOrNo addressOverseas;
+    private String address;
 
     @CCD(
         label = "Results of any enquiries made about this address",
