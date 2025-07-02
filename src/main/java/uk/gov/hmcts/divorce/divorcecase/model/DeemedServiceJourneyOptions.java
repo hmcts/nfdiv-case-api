3b05b8063ed836fee5da0bacca951aae5cf620a9
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-07-02 11:48:01 +0100

    NFDIV-4733 Add missing annotations

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DeemedServiceJourneyOptions.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/DeemedServiceJourneyOptions.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.divorce.divorcecase.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,6 +15,7 @@ import static uk.gov.hmcts.ccd.sdk.type.FieldType.TextArea;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.UpperCamelCaseStrategy.class)
 public class DeemedServiceJourneyOptions {
 
     @CCD(
