df9b4d25862d628b65445d00269f02c4a6e97d48
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-08-11 16:41:58 +0100

    Correct type override

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/NoResponseJourneyOptions.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/NoResponseJourneyOptions.java
@@ -36,7 +36,7 @@ public class NoResponseJourneyOptions {
     @CCD(
         label = "I have a new postal or email address for my partner",
         access = {DefaultAccess.class},
-        typeParameterOverride = "NoResponsePartnerNewEmailOrPostalAddress"
+        typeParameterOverride = "NoResponsePartnerNewEmailOrAddress"
     )
     private NoResponsePartnerNewEmailOrAddress noResponsePartnerNewEmailOrAddress;
 
