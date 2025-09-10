60fa3fda9433eef9944e95522b0727c382c8a34c
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-09-10 16:50:25 +0100

    NFDIV-4917 Add unit tests

---
+++ src/test/java/uk/gov/hmcts/divorce/citizen/event/CitizenGenerateProcessServerDocsTest.java
@@ -0,0 +1,82 @@
+package uk.gov.hmcts.divorce.citizen.event;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateApplicant2NoticeOfProceedings;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateApplication;
+import uk.gov.hmcts.divorce.caseworker.service.task.GenerateD10Form;
+import uk.gov.hmcts.divorce.caseworker.service.task.SetNoticeOfProceedingDetailsForRespondent;
+import uk.gov.hmcts.divorce.divorcecase.model.*;
+import uk.gov.hmcts.divorce.divorcecase.task.CaseTaskRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.divorce.citizen.event.CitizenApplicant1ConfirmReceipt.APPLICANT_1_CONFIRM_RECEIPT;
+import static uk.gov.hmcts.divorce.citizen.event.CitizenGenerateProcessServerDocs.CITIZEN_GENERATE_PROCESS_SERVER_DOCS;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
+import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_USER_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.validApplicant1CaseData;
+
+@ExtendWith(MockitoExtension.class)
+class CitizenGenerateProcessServerDocsTest {
+
+    @Mock
+    private SetNoticeOfProceedingDetailsForRespondent setNoticeOfProceedingDetailsForRespondent;
+
+    @Mock
+    private GenerateApplicant2NoticeOfProceedings generateApplicant2NoticeOfProceedings;
+
+    @Mock
+    private GenerateApplication generateApplication;
+
+    @Mock
+    private GenerateD10Form generateD10Form;
+
+    @Mock
+    private CaseTaskRunner caseTaskRunner;
+
+    @InjectMocks
+    private CitizenGenerateProcessServerDocs citizenGenerateProcessServerDocs;
+
+    @Test
+    void shouldAddConfigurationToConfigBuilder() {
+        final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
+
+        citizenGenerateProcessServerDocs.configure(configBuilder);
+
+        assertThat(getEventsFrom(configBuilder).values())
+            .extracting(Event::getId)
+            .contains(CITIZEN_GENERATE_PROCESS_SERVER_DOCS);
+    }
+
+    @Test
+    void shouldSetAwaitingServiceState() {
+        final long caseId = 2L;
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        CaseData caseData = validApplicant1CaseData();
+
+        caseDetails.setData(caseData);
+        caseDetails.setId(caseId);
+
+        when(CaseTaskRunner.caseTasks(
+            setNoticeOfProceedingDetailsForRespondent,
+            generateApplicant2NoticeOfProceedings,
+            generateApplication,
+            generateD10Form
+        ).run(any())).thenReturn(caseDetails);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = citizenGenerateProcessServerDocs.aboutToSubmit(caseDetails, caseDetails);
+
+        assertThat(response.getState()).isEqualTo(State.AwaitingService);
+    }
+}
