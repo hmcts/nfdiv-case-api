5863eede561c584c6ecf1f9c3dc7fdc1e5c1b3c6
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-09-15 08:30:14 +0100

    Remove unused import

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Applicant.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/Applicant.java
@@ -22,7 +22,6 @@ import uk.gov.hmcts.divorce.divorcecase.model.access.DefaultAccessExcludingSolic
 import uk.gov.hmcts.divorce.document.model.DivorceDocument;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
