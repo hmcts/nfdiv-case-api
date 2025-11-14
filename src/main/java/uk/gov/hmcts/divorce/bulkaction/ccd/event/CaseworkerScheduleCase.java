0de22801e54bfc7b26c04fa93feff0461ab4ac6c
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-11-14 14:26:41 +0000

    Schedule case event refactor

--- src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCase.java
+++ src/main/java/uk/gov/hmcts/divorce/bulkaction/ccd/event/CaseworkerScheduleCase.java
@@ -8,6 +8,7 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.ccd.sdk.type.CaseLink;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
 import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionPageBuilder;
 import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
@@ -24,7 +25,16 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 
 import java.time.LocalDateTime;
+<<<<<<< Updated upstream
 import java.util.List;
+=======
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+>>>>>>> Stashed changes
 
 import static org.apache.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.util.ObjectUtils.isEmpty;
@@ -33,6 +43,7 @@ import static uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState.Listed;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.flattenLists;
 import static uk.gov.hmcts.divorce.divorcecase.validation.ValidationUtil.validateBulkListErroredCases;
 import static uk.gov.hmcts.divorce.systemupdate.event.SystemLinkWithBulkCase.SYSTEM_LINK_WITH_BULK_CASE;
 
@@ -142,4 +153,89 @@ public class CaseworkerScheduleCase implements CCDConfig<BulkActionCaseData, Bul
         scheduleCaseService.updateCourtHearingDetailsForCasesInBulk(bulkCaseDetails);
         return SubmittedCallbackResponse.builder().build();
     }
+<<<<<<< Updated upstream
+=======
+
+    private List<String> validateData(final BulkActionCaseData bulkData, final BulkActionCaseData beforeBulkData, final Long bulkCaseId) {
+        final Set<String> beforeUniqueCaseRefs = new HashSet<>(beforeBulkData.getCaseReferences());
+        final Set<String> afterUniqueCaseRefs = new HashSet<>(bulkData.getCaseReferences());
+
+        Set<String> newlyAddedCaseReferences = new HashSet<>(afterUniqueCaseRefs);
+        newlyAddedCaseReferences.removeAll(beforeUniqueCaseRefs);
+
+        return flattenLists(
+            validateHearingDate(bulkData),
+            validateCasesNotRemoved(afterUniqueCaseRefs, beforeUniqueCaseRefs),
+            validateDuplicates(bulkData.getCaseReferences(), afterUniqueCaseRefs),
+            validateLinkedCases(newlyAddedCaseReferences, bulkCaseId)
+        );
+    }
+
+    private List<String> validateHearingDate(final BulkActionCaseData bulkData) {
+        return bulkData.getDateAndTimeOfHearing().isBefore(LocalDateTime.now()) ?
+            List.of("Please do not use a hearing date in the past") :
+            Collections.emptyList();
+    }
+
+    private List<String> validateCasesNotRemoved(final Set<String> afterCaseRefs, final Set<String> beforeCaseRefs) {
+        Set<String> removedCases = new HashSet<>(beforeCaseRefs);
+        removedCases.removeAll(afterCaseRefs);
+
+        return !removedCases.isEmpty()
+            ? List.of("Please do not remove cases from the list.")
+            : Collections.emptyList();
+    }
+
+    private List<String> validateDuplicates(List<String> afterCaseReferences, Set<String> uniqueCaseReferences) {
+        boolean hasDuplicates = uniqueCaseReferences.size() < afterCaseReferences.size();
+
+        return hasDuplicates ?
+            List.of("Please remove duplicate case references from the bulk list.") :
+            Collections.emptyList();
+    }
+
+    private List<String> validateLinkedCases(Set<String> addedCaseRefs, Long bulkCaseId) {
+        if (addedCaseRefs.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        final List<uk.gov.hmcts.reform.ccd.client.model.CaseDetails> caseSearchResults = ccdSearchService.searchForCases(
+            addedCaseRefs.stream().toList(),
+            idamService.retrieveSystemUpdateUserDetails(),
+            authTokenGenerator.generate()
+        );
+
+        List<String> errors = new ArrayList<>();
+
+        Set<String> missingCaseRefs = new HashSet<>(addedCaseRefs);
+        caseSearchResults.forEach(caseDetails -> {
+            missingCaseRefs.remove(caseDetails.getId().toString());
+            errors.addAll(validateLinkedCaseDetails(caseDetails, bulkCaseId));
+        });
+
+        errors.add(String.format(
+            "Some cases were not found: %s", String.join(", ", missingCaseRefs)
+        ));
+
+        return errors;
+    }
+
+    private List<String> validateLinkedCaseDetails(uk.gov.hmcts.reform.ccd.client.model.CaseDetails details, Long bulkCaseId) {
+        List<String> errors = new ArrayList<>();
+        long caseId = details.getId();
+
+        if (!AwaitingPronouncement.toString().equals(details.getState())) {
+            errors.add(String.format("Case %s is not in the correct state for bulk list processing.", caseId));
+        }
+
+        CaseLink bulkCaseLink = objectMapper.convertValue(details.getData(), CaseData.class)
+            .getBulkListCaseReferenceLink();
+
+        if (bulkCaseLink != null && !bulkCaseLink.getCaseReference().equals(bulkCaseId.toString())) {
+            errors.add(String.format("Case %s is already linked to bulk list %s.", caseId, bulkCaseLink.getCaseReference()));
+        }
+
+        return errors;
+    }
+>>>>>>> Stashed changes
 }
