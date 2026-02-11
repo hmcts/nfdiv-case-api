61ecef5ea0c82593c6648b35397f655e25c47cee
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-02-11 17:24:46 +0000

    NFDIV-5206 Add CO CourtBulk enum and initial POC refactor

---
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/ConditionalOrderCourtBulk.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.divorce.divorcecase.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import uk.gov.hmcts.ccd.sdk.api.HasLabel;
+
+import static uk.gov.hmcts.divorce.divorcecase.constant.ConditionalOrderCourtConstants.birmingham;
+import static uk.gov.hmcts.divorce.divorcecase.constant.ConditionalOrderCourtConstants.buryStEdmunds;
+
+@Getter
+@AllArgsConstructor
+public enum ConditionalOrderCourtBulk implements HasLabel {
+
+    @JsonProperty("birmingham")
+    BIRMINGHAM("birmingham", birmingham),
+
+    @JsonProperty("buryStEdmunds")
+    BURY_ST_EDMUNDS("buryStEdmunds", buryStEdmunds);
+
+    private String courtId;
+    private String label;
+
+    public ConditionalOrderCourt getConditionalOrderCourt() {
+        return ConditionalOrderCourt.valueOf(this.name());
+    }
+}
