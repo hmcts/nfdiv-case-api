3b26796899fb6e1aa59ddd62f1992c345aac5b0c
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-02-13 13:36:22 +0000

    Correct test config

--- src/contractTest/java/uk/gov/hmcts/divorce/consumer/FeesRegisterConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/divorce/consumer/FeesRegisterConsumerTest.java
@@ -3,19 +3,20 @@ package uk.gov.hmcts.divorce.consumer;
 
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.consumer.junit.PactProviderRule;
-import au.com.dius.pact.consumer.junit.PactVerification;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import org.apache.hc.core5.http.HttpStatus;
 import org.json.JSONException;
-import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.divorce.payment.client.FeesAndPaymentsClient;
 import uk.gov.hmcts.divorce.payment.model.FeeResponse;
 
@@ -27,15 +28,15 @@ import static uk.gov.hmcts.divorce.payment.service.PaymentService.FAMILY_COURT;
 import static uk.gov.hmcts.divorce.payment.service.PaymentService.KEYWORD_DIVORCE;
 import static uk.gov.hmcts.divorce.payment.service.PaymentService.SERVICE_DIVORCE;
 
+@ExtendWith(PactConsumerTestExt.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
+@PactTestFor(providerName = "feeRegister_lookUp", port = "4411")
+@TestPropertySource(locations = {"/application.properties"})
 public class FeesRegisterConsumerTest {
 
     @Autowired
     FeesAndPaymentsClient feesClient;
-
-    @Rule
-    public PactProviderRule mockProvider = new PactProviderRule("feeRegister_lookUp", "localhost", 8889, this);
 
     @Pact(provider = "feeRegister_lookUp", consumer = "nfdiv_case_api")
     public RequestResponsePact createApplicationFeeFragment(PactDslWithProvider builder) {
@@ -59,7 +60,7 @@ public class FeesRegisterConsumerTest {
     }
 
     @Test
-    @PactVerification(fragment = "createApplicationFeeFragment")
+    @PactTestFor(pactMethod = "createApplicationFeeFragment")
     public void verifyApplicationFeeServicePact() throws JSONException {
         FeeResponse result = feesClient.getPaymentServiceFee(
             DEFAULT_CHANNEL,
