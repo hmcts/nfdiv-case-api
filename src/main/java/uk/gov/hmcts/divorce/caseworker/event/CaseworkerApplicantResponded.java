1d3ecc3243d532023f7c9cf13236068da40a4a3f
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-03-27 16:59:09 +0000

    NFDIV-5225 Block event for issued cases

--- src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerApplicantResponded.java
+++ src/main/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerApplicantResponded.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.divorce.caseworker.event;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
+import java.util.Collections;
 import java.util.EnumSet;
 
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingDocuments;
@@ -23,22 +27,25 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
+@Slf4j
 public class CaseworkerApplicantResponded implements CCDConfig<CaseData, State, UserRole> {
     public static final String CASEWORKER_APPLICANT_RESPONDED = "caseworker-submit-from-awaitingdocuments-app1resp";
+    public static final String ERROR_ALREADY_ISSUED = "Unable to proceed - Case has already been issued";
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         new PageBuilder(configBuilder
             .event(CASEWORKER_APPLICANT_RESPONDED)
             .forStateTransition(
-                EnumSet.of(
-                    AwaitingDocuments,
-                    AwaitingRequestedInformation,
-                    InformationRequested,
-                    RequestedInformationSubmitted
-                ),
-                Submitted
+            EnumSet.of(
+                AwaitingDocuments,
+                AwaitingRequestedInformation,
+                InformationRequested,
+                RequestedInformationSubmitted
+            ),
+            Submitted
             )
+            .aboutToStartCallback(this::aboutToStart)
             .showEventNotes()
             .name("Applicant responded")
             .description("Applicant responded")
@@ -49,5 +56,20 @@ public class CaseworkerApplicantResponded implements CCDConfig<CaseData, State, 
                 SUPER_USER,
                 LEGAL_ADVISOR,
                 JUDGE));
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(final CaseDetails<CaseData, State> details) {
+
+        log.info("{} about to start callback invoked for Case Id: {}", CASEWORKER_APPLICANT_RESPONDED, details.getId());
+
+        if (details.getData().getApplication().getIssueDate() != null) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(Collections.singletonList(ERROR_ALREADY_ISSUED))
+                .build();
+        }
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData())
+            .build();
     }
 }
--- src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerApplicantRespondedTest.java
+++ src/test/java/uk/gov/hmcts/divorce/caseworker/event/CaseworkerApplicantRespondedTest.java
@@ -5,15 +5,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerApplicantResponded.CASEWORKER_APPLICANT_RESPONDED;
+import static uk.gov.hmcts.divorce.caseworker.event.CaseworkerApplicantResponded.ERROR_ALREADY_ISSUED;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class CaseworkerApplicantRespondedTest {
@@ -30,5 +36,29 @@ class CaseworkerApplicantRespondedTest {
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
             .contains(CASEWORKER_APPLICANT_RESPONDED);
+    }
+
+    @Test
+    void shouldThrowIfCaseAlreadyIssued() {
+        final CaseData caseData = caseData();
+        caseData.getApplication().setIssueDate(LocalDate.now());
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData);
+        caseDetails.setState(State.AwaitingDocuments);
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerApplicantResponded.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).contains(ERROR_ALREADY_ISSUED);
+    }
+
+    @Test
+    void shouldNotThrowIfCaseNotIssued() {
+        final CaseData caseData = caseData();
+        final CaseDetails<CaseData, State> caseDetails = new CaseDetails<>();
+        caseDetails.setData(caseData());
+
+        final AboutToStartOrSubmitResponse<CaseData, State> response = caseworkerApplicantResponded.aboutToStart(caseDetails);
+
+        assertThat(response.getErrors()).isNull();
     }
 }
