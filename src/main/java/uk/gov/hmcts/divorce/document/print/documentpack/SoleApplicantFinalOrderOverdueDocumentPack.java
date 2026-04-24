1027d7624e0f1242dc0c84e34f094068f939cff8
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-04-24 15:46:05 +0100

    NFDIV-5158 Add sole app FO overdue doc pack & notification

---
+++ src/main/java/uk/gov/hmcts/divorce/document/print/documentpack/SoleApplicantFinalOrderOverdueDocumentPack.java
@@ -0,0 +1,56 @@
+package uk.gov.hmcts.divorce.document.print.documentpack;
+
+import com.google.common.collect.ImmutableMap;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.ApplicationType;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateD11Form;
+import uk.gov.hmcts.divorce.systemupdate.service.task.GenerateD36Form;
+
+import java.util.Optional;
+
+import static uk.gov.hmcts.divorce.document.DocumentConstants.COVERSHEET_APPLICANT;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.COVERSHEET_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.FINAL_ORDER_CAN_APPLY_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.FINAL_ORDER_CAN_APPLY_RESPONDENT_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.FINAL_ORDER_CAN_APPLY_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.SOLE_APPLICANT_FINAL_ORDER_OVERDUE_DOCUMENT_NAME;
+import static uk.gov.hmcts.divorce.document.DocumentConstants.SOLE_APPLICANT_FINAL_ORDER_OVERDUE_TEMPLATE_ID;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.COVERSHEET;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.D11;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.D36;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.FINAL_ORDER_CAN_APPLY_APP1;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.FINAL_ORDER_CAN_APPLY_APP2;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.FINAL_ORDER_CAN_APPLY_RESPONDENT;
+import static uk.gov.hmcts.divorce.document.model.DocumentType.SOLE_APPLICANT_FINAL_ORDER_OVERDUE;
+
+@Component
+@RequiredArgsConstructor
+public class SoleApplicantFinalOrderOverdueDocumentPack implements DocumentPack {
+
+    static final String SOLE_APPLICANT_FINAL_ORDER_OVERDUE_PACK = "sole-applicant-final-order-overdue-pack";
+
+    static final DocumentPackInfo APPLICANT1_OFFLINE_DOCUMENTPACK = new DocumentPackInfo(
+        ImmutableMap.of(
+            COVERSHEET, Optional.of(COVERSHEET_APPLICANT),
+            SOLE_APPLICANT_FINAL_ORDER_OVERDUE, Optional.of(SOLE_APPLICANT_FINAL_ORDER_OVERDUE_TEMPLATE_ID),
+            D36, Optional.empty() // empty optional if generated from case data for same doc send ti mulitple apps
+        ),
+        ImmutableMap.of(
+            COVERSHEET_APPLICANT, COVERSHEET_DOCUMENT_NAME,
+            SOLE_APPLICANT_FINAL_ORDER_OVERDUE_TEMPLATE_ID, SOLE_APPLICANT_FINAL_ORDER_OVERDUE_DOCUMENT_NAME
+        )
+    );
+
+    @Override
+    public DocumentPackInfo getDocumentPack(CaseData caseData, Applicant applicant) {
+        return APPLICANT1_OFFLINE_DOCUMENTPACK;
+    }
+
+    @Override
+    public String getLetterId() {
+        return SOLE_APPLICANT_FINAL_ORDER_OVERDUE_PACK;
+    }
+}
