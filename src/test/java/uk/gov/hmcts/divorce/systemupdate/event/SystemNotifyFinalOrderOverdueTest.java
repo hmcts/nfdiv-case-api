2480a00d4a496b67fac465501d206183fbdfdce3
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-04-24 16:06:54 +0100

    NFDIV-5158 Update unit tests

--- src/test/java/uk/gov/hmcts/divorce/systemupdate/event/SystemNotifyFinalOrderOverdueTest.java
+++ src/test/java/uk/gov/hmcts/divorce/systemupdate/event/SystemNotifyFinalOrderOverdueTest.java
@@ -3,16 +3,20 @@ package uk.gov.hmcts.divorce.systemupdate.event;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.notification.ApplicantFinalOrderOverdueNotification;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.notification.NotificationDispatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
 import static uk.gov.hmcts.divorce.systemupdate.event.SystemNotifyFinalOrderOverdue.SYSTEM_FINAL_ORDER_OVERDUE;
 import static uk.gov.hmcts.divorce.testutil.ConfigTestUtil.createCaseDataConfigBuilder;
@@ -22,6 +26,12 @@ import static uk.gov.hmcts.divorce.testutil.TestDataHelper.caseData;
 
 @ExtendWith(MockitoExtension.class)
 class SystemNotifyFinalOrderOverdueTest {
+
+    @Mock
+    private NotificationDispatcher notificationDispatcher;
+
+    @Mock
+    private ApplicantFinalOrderOverdueNotification applicantFinalOrderOverdueNotification;
 
     @InjectMocks
     private SystemNotifyFinalOrderOverdue systemNotifyFinalOrderOverdue;
@@ -47,5 +57,16 @@ class SystemNotifyFinalOrderOverdueTest {
             systemNotifyFinalOrderOverdue.aboutToSubmit(details, details);
 
         assertThat(response.getData().getFinalOrder().getIsFinalOrderOverdue()).isEqualTo(YES);
+    }
+
+    @Test
+    void shouldSendNotificationToSoleApplicant() {
+        final CaseDetails<CaseData, State> details = new CaseDetails<>();
+        details.setId(TEST_CASE_ID);
+        details.setData(caseData());
+
+        systemNotifyFinalOrderOverdue.aboutToSubmit(details, details);
+
+        verify(notificationDispatcher).send(applicantFinalOrderOverdueNotification, details.getData(), TEST_CASE_ID);
     }
 }
