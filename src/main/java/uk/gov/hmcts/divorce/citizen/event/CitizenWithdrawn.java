0c96a40df27da60b2d8a6c1055f8ea39ac3d552b
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2026-04-27 08:23:40 +0100

    NFDIV-4925 (#4592)
    * NFDIV-4924: Add a cron and an event for overdue payment
    
    * NFDIV-4925: Add cron and new event
    
    * NFDIV-4925: Reject cases and add notification for fee not paid
    
    * NFDIV-4925: Fix checkstyle
    
    * NFDIV-4925: Rename event add test class
    
    * NFDIV-4925: Add unit test
    
    * NFDIV-4925: Unlink applicant1 from the case
    
    * NFDIV-4925: Add guard against unwatned rejection
    
    * NFDIV-4925: Fix checkstyle
    
    * NFDIV-4925: Fix issue with query
    
    * NFDIV-4925: Remove app2 user role
    
    * Update src/main/java/uk/gov/hmcts/divorce/common/notification/ApplicationRejectedFeeNotPaidNotification.java
    
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
    
    * Update src/main/java/uk/gov/hmcts/divorce/systemupdate/schedule/SystemRejectCasesWithPaymentOverdueTask.java
    
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
    
    * NFDIV-4925: Fix unit test
    
    * NFDIV-4925: Exlude JS and offline cases from reject event
    
    * NFDIV-4925: Fix Elastic Search query
    
    * NFDIV-4925: Added new endpoint for all payments and refactor for edge case
    
    * NFDIV-4925: Refactor code to move logic to common class
    
    * NFDIV-4925: Remove unwanted conditional check
    
    * NFDIV-4925: Fix unit test
    
    * NFDIV-4925: Refactor code to check for all service
    
    * NFDIV-4925: Remove unwanted conditional check
    
    * NFDIV-4925: Fix logic
    
    * NFDIV-4925: Fix logic
    
    * NFDIV-4925: Refactor rejection logic
    
    * NFDIV-4925: Fix unit test
    
    * NFDIV-4925: Fix checkstyle
    
    * NFDIV-4925: Add payment created within grace period check
    
    * Refactor with Faisal
    
    * Correct guard clause
    
    * NFDIV-4925: Fix unit tests
    
    * NFDIV-4925: Add overloaded method with max cases
    
    * NFDIV-4925: Fix unit test
    
    * NFDIV-4925: Add a log line
    
    * NFDIV-4925: Revert adding overloaded method and temporarily comment out rejection
    
    * NFDIV-4925: Fix checkstyle
    
    * NFDIV-4925: Fix checkstyle
    
    * NFDIV-4925: Add Welsh template
    
    * NFDIV-4925: Fix issue with compile java
    
    * NFDIV-4925: Fix issue with notifications
    
    * NFDIV-4925: Update condition to check for date updated
    
    * NFDIF-4925: Update log line
    
    * NFDIV-4925: Limit cases to the max results
    
    * NFDIV-4925: Use correct method for send email
    
    * NFDIV-4925: Fix unit tests
    
    * NFDIV-4925: Disable reject cases functionality
    
    ---------
    
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
    Co-authored-by: eliswyndavies <168655403+eliswyndavies@users.noreply.github.com>
    Co-authored-by: BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
    Co-authored-by: adamg-hmcts <adam.goddard@justice.gov.uk>

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenWithdrawn.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenWithdrawn.java
@@ -8,7 +8,7 @@ import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
-import uk.gov.hmcts.divorce.common.service.WithdrawCaseService;
+import uk.gov.hmcts.divorce.common.service.CaseTerminationService;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
@@ -40,7 +40,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_R
 public class CitizenWithdrawn implements CCDConfig<CaseData, State, UserRole> {
     public static final String CITIZEN_WITHDRAWN = "citizen-withdrawn";
 
-    private final WithdrawCaseService withdrawCaseService;
+    private final CaseTerminationService caseTerminationService;
 
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
@@ -76,7 +76,7 @@ public class CitizenWithdrawn implements CCDConfig<CaseData, State, UserRole> {
 
         State stateToSet = (details.getData().getApplication().getDateSubmitted() == null) ? Withdrawn : PendingRefund;
 
-        withdrawCaseService.withdraw(details);
+        caseTerminationService.withdraw(details);
 
         return AboutToStartOrSubmitResponse.<CaseData, State>builder()
             .data(details.getData())
