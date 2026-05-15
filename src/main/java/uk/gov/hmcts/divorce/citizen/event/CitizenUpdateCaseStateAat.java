58743ec204f8c5a7b48ebf4c7aad431190871f80
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-05-15 07:25:05 +0100

    NFDIV-4881: Add solicitor role to the event

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateCaseStateAat.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenUpdateCaseStateAat.java
@@ -16,6 +16,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
@@ -35,7 +36,7 @@ public class CitizenUpdateCaseStateAat implements CCDConfig<CaseData, State, Use
                 .forAllStates()
                 .name("Citizen update case state AAT")
                 .description("Citizen update the case state in AAT")
-                .grant(CREATE_READ_UPDATE, CREATOR, APPLICANT_2, SYSTEMUPDATE)
+                .grant(CREATE_READ_UPDATE, CREATOR, APPLICANT_2, SOLICITOR, SYSTEMUPDATE)
                 .grantHistoryOnly(CASE_WORKER, SUPER_USER, LEGAL_ADVISOR, JUDGE)
                 .aboutToSubmitCallback(this::aboutToSubmit);
         }
