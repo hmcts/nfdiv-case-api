d927106200c7e7bfdad080b46ab9f9dd7f8635b7
pallavijustice <159147694+pallavijustice@users.noreply.github.com>
2026-05-19 16:53:47 +0100

    NFDIV-5351 - Update solicitor notification templates to have defined header (#5049)
    * NFDIV-5351 - Update solicitor notification templates to have defined header
    
    * NFDIV-5351 - Fix IT test cases
    
    * NFDIV-5351 - Refactor to use common content for issue date
    
    * NFDIV-5351 - refactor code
    
    * NFDIV-5351 - update email references

--- src/main/java/uk/gov/hmcts/divorce/notification/CommonContent.java
+++ src/main/java/uk/gov/hmcts/divorce/notification/CommonContent.java
@@ -249,12 +249,15 @@ public class CommonContent {
 
     public Map<String, String> solicitorTemplateVars(CaseData data, Long id, Applicant applicant) {
         Map<String, String> templateVars = solicitorTemplateVarsPreIssue(data, id, applicant);
-        final LocalDate issueDate = data.getApplication().getIssueDate();
-
-        templateVars.put(DocmosisTemplateConstants.ISSUE_DATE,
-            issueDate == null ? "" : data.getApplication().getIssueDate().format(DATE_TIME_FORMATTER)
-        );
+        templateVars.put(DocmosisTemplateConstants.ISSUE_DATE, getIssueDateInPreferredLanguage(data, applicant));
         return templateVars;
+    }
+
+    public String getIssueDateInPreferredLanguage(CaseData data, Applicant applicant) {
+        final LocalDate issueDate = data.getApplication().getIssueDate();
+        return
+            issueDate == null ? "" : issueDate.format(
+                getDateTimeFormatterForPreferredLanguage(applicant.getLanguagePreference()));
     }
 
     public Map<String, String> serviceApplicationTemplateVars(CaseData data, Long id, Applicant applicant) {
@@ -463,9 +466,7 @@ public class CommonContent {
             applicant.getLanguagePreference())
         );
 
-        boolean isSole = caseData.getApplicationType().isSole();
-        templateVars.put(APPLICANT1_LABEL, isSole ? APPLICANT : APPLICANT_1);
-        templateVars.put(APPLICANT2_LABEL, isSole ? RESPONDENT : APPLICANT_2);
+        setApplicantLabels(caseData, templateVars);
         templateVars.put(APPLICANT_1_FULL_NAME, caseData.getApplicant1().getFullName());
         templateVars.put(APPLICANT_2_FULL_NAME, caseData.getApplicant2().getFullName());
 
@@ -511,6 +512,12 @@ public class CommonContent {
         return templateVars;
     }
 
+    public Map<String, String> getGeneralEmailSolicitorVars(CaseData caseData, Long caseId, Applicant applicant) {
+        Map<String, String> templateVars = solicitorTemplateVars(caseData, caseId, applicant);
+        setApplicantLabels(caseData, templateVars);
+        return templateVars;
+    }
+
     public void setOverdueAndInTimeVariables(CaseData caseData, Map<String, String> templateVars) {
         if (YesOrNo.YES.equals(caseData.getFinalOrder().getIsFinalOrderOverdue())) {
             templateVars.put(IS_OVERDUE, YES);
@@ -524,6 +531,12 @@ public class CommonContent {
     public void setIsDivorceAndIsDissolutionVariables(CaseData caseData, Map<String, String> templateVars) {
         templateVars.put(IS_DIVORCE, caseData.isDivorce() ? YES : NO);
         templateVars.put(IS_DISSOLUTION, !caseData.isDivorce() ? YES : NO);
+    }
+
+    private void setApplicantLabels(CaseData caseData, Map<String, String> templateVars) {
+        boolean isSole = caseData.getApplicationType().isSole();
+        templateVars.put(APPLICANT1_LABEL, isSole ? APPLICANT : APPLICANT_1);
+        templateVars.put(APPLICANT2_LABEL, isSole ? RESPONDENT : APPLICANT_2);
     }
 
     public String getContactWebFormText(LanguagePreference languagePreference) {
