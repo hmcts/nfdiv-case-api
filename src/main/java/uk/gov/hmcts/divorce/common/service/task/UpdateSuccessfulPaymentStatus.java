a616bed2b0ffaf212b9ae14713211968e51ffc0e
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-06-10 18:10:37 +0100

    Set payment status for service and gen apps

--- src/main/java/uk/gov/hmcts/divorce/common/service/task/UpdateSuccessfulPaymentStatus.java
+++ src/main/java/uk/gov/hmcts/divorce/common/service/task/UpdateSuccessfulPaymentStatus.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.divorce.common.service.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.type.ListValue;
@@ -12,8 +13,6 @@ import uk.gov.hmcts.divorce.divorcecase.task.CaseTask;
 
 import java.util.List;
 
-import static uk.gov.hmcts.divorce.divorcecase.model.State.AwaitingPayment;
-
 @Component
 @Slf4j
 public class UpdateSuccessfulPaymentStatus implements CaseTask {
@@ -23,20 +22,28 @@ public class UpdateSuccessfulPaymentStatus implements CaseTask {
 
         log.info("Setting payment status to Success for Case ID: {}", caseDetails.getId());
 
-        if (caseDetails.getState() == AwaitingPayment) {
-            List<ListValue<Payment>> applicationPayment =
-                    caseDetails.getData().getApplication().getApplicationPayments();
-
-            caseDetails.getData().getApplication().getApplicationPayments().get(applicationPayment.size() - 1)
-                    .getValue().setStatus(PaymentStatus.SUCCESS);
-        } else {
-            List<ListValue<Payment>> finalOrderPayment =
-                    caseDetails.getData().getFinalOrder().getFinalOrderPayments();
-
-            caseDetails.getData().getFinalOrder().getFinalOrderPayments().get(finalOrderPayment.size() - 1)
-                    .getValue().setStatus(PaymentStatus.SUCCESS);
+        List<ListValue<Payment>> activePayments = getPayments(caseDetails);
+        if (CollectionUtils.isEmpty(activePayments)) {
+            return caseDetails;
         }
 
+        activePayments.getLast().getValue().setStatus(PaymentStatus.SUCCESS);
+
         return caseDetails;
+    }
+
+    private List<ListValue<Payment>> getPayments(final CaseDetails<CaseData, State> caseDetails) {
+        final State state = caseDetails.getState();
+        if (State.AwaitingPayment.equals(state)) {
+            return caseDetails.getData().getApplication().getApplicationPayments();
+        } else if (State.AwaitingFinalOrderPayment.equals(state)) {
+            return caseDetails.getData().getFinalOrder().getFinalOrderPayments();
+        } else if (State.AwaitingServicePayment.equals(state)) {
+            return caseDetails.getData().getAlternativeService().getServicePayments();
+        } else if (State.AwaitingGeneralApplicationPayment.equals(state)) {
+            return caseDetails.getData().getApplicant1().getGeneralAppPayments();
+        }
+
+        return null;
     }
 }
