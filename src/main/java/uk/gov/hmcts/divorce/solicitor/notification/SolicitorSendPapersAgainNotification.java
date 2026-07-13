89a2a2a6ffaf513f6d447ade8338b70847b90ed0
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-07-13 12:30:45 +0100

    NFDIV-5412: Fix unit test

--- src/main/java/uk/gov/hmcts/divorce/solicitor/notification/SolicitorSendPapersAgainNotification.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/notification/SolicitorSendPapersAgainNotification.java
@@ -9,12 +9,22 @@ import uk.gov.hmcts.divorce.notification.ApplicantNotification;
 import uk.gov.hmcts.divorce.notification.CommonContent;
 import uk.gov.hmcts.divorce.notification.NotificationService;
 
+import static uk.gov.hmcts.divorce.document.content.DocmosisTemplateConstants.DIVORCE_OR_DISSOLUTION;
 import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_SENT_PAPERS_AGAIN;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class SolicitorSendPapersAgainNotification implements ApplicantNotification {
+
+    private static final String SOLICITOR_SERVICE = "solicitorService";
+    private static final String COURT_SERVICE = "courtService";
+    public static final String COURT_SERVICE_TEXT = """
+     We will now serve the %s again on the respondent. The respondent will have 14 days
+     from receiving the papers to respond.""";
+    public static final String SOLICITOR_SERVICE_TEXT = """
+     You will need to arrange delivery of the %s yourself.You can download the papers from the case and arrange for them to be served on
+      the respondent. The documents are usually available in the Documents tab.""";
 
     private final NotificationService notificationService;
     private final CommonContent commonContent;
@@ -24,8 +34,13 @@ public class SolicitorSendPapersAgainNotification implements ApplicantNotificati
         if (caseData.getApplicationType().isSole()) {
 
             Applicant applicant = caseData.getApplicant1();
+            String divorceOrDissolution = caseData.isDivorce() ? "divorce papers" : "papers to end the civil partnership";
+            String divorceOrDissolutionType = caseData.isDivorce() ? "divorce application" : "application to end the civil partnership";
 
             var templateVars = commonContent.solicitorTemplateVars(caseData, caseId, applicant);
+            templateVars.put(COURT_SERVICE, String.format(COURT_SERVICE_TEXT, divorceOrDissolution));
+            templateVars.put(SOLICITOR_SERVICE, String.format(SOLICITOR_SERVICE_TEXT, divorceOrDissolution));
+            templateVars.put(DIVORCE_OR_DISSOLUTION, divorceOrDissolutionType);
 
             log.info("Sending Applicant 1 Solicitor notification informing them that they sent papers again: {}", caseId);
 
