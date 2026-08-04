90204a259813d90880a46983a4eb3889f3df3b9f
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-08-04 13:23:18 +0100

    Open up service payment made to system user for payment callbacks

--- src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenServicePaymentMade.java
+++ src/main/java/uk/gov/hmcts/divorce/citizen/event/CitizenServicePaymentMade.java
@@ -35,6 +35,7 @@ import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CREATOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SYSTEMUPDATE;
 import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
 
 @Component
@@ -62,7 +63,7 @@ public class CitizenServicePaymentMade implements CCDConfig<CaseData, State, Use
             .aboutToSubmitCallback(this::aboutToSubmit)
             .submittedCallback(this::submitted)
             .showCondition(NEVER_SHOW)
-            .grant(CREATE_READ_UPDATE, CREATOR)
+            .grant(CREATE_READ_UPDATE, CREATOR, SYSTEMUPDATE)
             .grantHistoryOnly(CASE_WORKER, SUPER_USER, JUDGE, LEGAL_ADVISOR, APPLICANT_1_SOLICITOR));
     }
 
