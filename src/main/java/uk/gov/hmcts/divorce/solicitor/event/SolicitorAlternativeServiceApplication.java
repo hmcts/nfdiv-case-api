60766a9fb3639c51534bed272da17b7a7c6673d1
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-08-04 15:30:42 +0100

    NFDIV-5419: Add unit test and show condition for the event

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorAlternativeServiceApplication.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorAlternativeServiceApplication.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.InterimApplicationOptions;
+import uk.gov.hmcts.divorce.divorcecase.model.InterimApplicationType;
 import uk.gov.hmcts.divorce.divorcecase.model.State;
 import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
 import uk.gov.hmcts.divorce.solicitor.event.page.AlternativeServiceConfirmPage;
@@ -16,6 +21,7 @@ import uk.gov.hmcts.divorce.solicitor.event.page.AlternativeServicePaymentPage;
 import uk.gov.hmcts.divorce.solicitor.event.page.SolicitorAlternativeServiceAbleToUploadEvidence;
 import uk.gov.hmcts.divorce.solicitor.event.page.SolicitorAlternativeServiceMethodPage;
 import uk.gov.hmcts.divorce.solicitor.event.page.SolicitorAlternativeServiceReasonPage;
+import uk.gov.hmcts.divorce.solicitor.service.ServiceApplicationDraftSubmissionService;
 
 import java.util.EnumSet;
 import java.util.List;
@@ -39,6 +45,8 @@ public class SolicitorAlternativeServiceApplication implements CCDConfig<CaseDat
 
     public static final String SOLICITOR_ALTERNATIVE_SERVICE_APPLICATION = "sol-alternative-service-app";
 
+    private final ServiceApplicationDraftSubmissionService serviceApplicationBuilderService;
+
     @Override
     public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         final PageBuilder pageBuilder = addEventConfig(configBuilder);
@@ -55,6 +63,25 @@ public class SolicitorAlternativeServiceApplication implements CCDConfig<CaseDat
         pages.forEach(page -> page.addTo(pageBuilder));
     }
 
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        log.info("{} about to submit callback invoked for Case Id: {}", ALTERNATIVE_SERVICE, details.getId());
+        final CaseData caseData = details.getData();
+        final Applicant applicant = caseData.getApplicant1();
+
+        InterimApplicationOptions options = applicant.getInterimApplicationOptions();
+        options.setInterimApplicationType(InterimApplicationType.ALTERNATIVE_SERVICE);
+
+        serviceApplicationBuilderService.submitFromInterimOptions(details.getId(), caseData, applicant);
+
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
     private PageBuilder addEventConfig(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
         var states = EnumSet.complementOf(POST_SUBMISSION_STATES);
         states.add(AosOverdue);
@@ -62,11 +89,15 @@ public class SolicitorAlternativeServiceApplication implements CCDConfig<CaseDat
         return new PageBuilder(configBuilder
             .event(SOLICITOR_ALTERNATIVE_SERVICE_APPLICATION)
             .forStates(states)
+            .showCondition(
+                "alternativeServiceType!=\"deemed\" AND alternativeServiceType!=\"dispensed\" AND alternativeServiceType!=\"bailiff\" "
+                    + "AND alternativeServiceType!=\"alternativeService\"")
             .name(ALTERNATIVE_SERVICE)
             .description(ALTERNATIVE_SERVICE)
             .showSummary()
             .showEventNotes()
             .endButtonLabel("Save Application")
+            .aboutToSubmitCallback(this::aboutToSubmit)
             .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR)
             .grantHistoryOnly(CASE_WORKER, JUDGE, LEGAL_ADVISOR, SUPER_USER));
     }
