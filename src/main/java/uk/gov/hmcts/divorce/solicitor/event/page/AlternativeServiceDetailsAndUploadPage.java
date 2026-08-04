6e58591276cc6a3cae6ce29d1709480bae644070
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-08-04 12:09:37 +0100

    NFDIV-54D19: Reuse existing fields

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/AlternativeServiceDetailsAndUploadPage.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/page/AlternativeServiceDetailsAndUploadPage.java
@@ -6,21 +6,23 @@ import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
 import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
 import uk.gov.hmcts.divorce.divorcecase.model.InterimApplicationOptions;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-
 public class AlternativeServiceDetailsAndUploadPage implements CcdPageConfiguration {
 
-    private static final String PAGE_SHOW_CONDITION = "applicant1SolAltServiceCanUploadEvidence=\"Yes\"";
+    private static final String PAGE_SHOW_CONDITION = "applicant1InterimAppsCanUploadEvidence=\"Yes\"";
     private static final String RESPONDENT_EMAIL = "- the respondent's email address";
     private static final String RESPONDENT_EMAIL_SHOW_CONDITION = "applicant1AltServiceMethod != \"inADifferentWay\"";
     private static final String RESPONDENT_PHONE = "- the respondent's phone number";
-    private static final String RESPONDENT_PHONE_SHOW_CONDITION = "applicant1AltServiceDifferentWaysCONTAINS\"whatsapp\"";
+    private static final String RESPONDENT_PHONE_SHOW_CONDITION =
+        "applicant1AltServiceDifferentWaysCONTAINS\"whatsapp\" OR applicant1AltServiceDifferentWaysCONTAINS\"textMessage\"";
     private static final String RESPONDENT_SOCIAL_MEDIA = "- the respondent's social media username";
     private static final String RESPONDENT_SOCIAL_MEDIA_SHOW_CONDITION = "applicant1AltServiceDifferentWaysCONTAINS\"socialMedia\"";
-    private static final String EVIDENCE_LABEL =
-        "If your evidence is a conversation in a language other than English, you'll need to provide a certified translation.";
-    private static final String UPLOAD_DOCUMENTS_LABEL = "## Applicant 1 uploaded documents";
+    private static final String EVIDENCE_LABEL = """
+        If your evidence is a conversation in a language other than English, you'll need to provide a
+        <a href="https://www.gov.uk/certifying-a-document#certifying-a-translation" target="_blank">certified translation</a>
 
+
+         ## Applicant 1 uploaded documents;
+        """;
     private static final String ALTERNATIVE_EVIDENCE_DOCS_LABEL = """
         ## Upload your documents
 
@@ -31,26 +33,22 @@ public class AlternativeServiceDetailsAndUploadPage implements CcdPageConfigurat
 
         It may be helpful if your images show:
 
+        - the respondent's name
+
+        - the date the messages were sent
+
         """;
 
     @Override
     public void addTo(PageBuilder pageBuilder) {
-        addWithShowCondition(pageBuilder, PAGE_SHOW_CONDITION);
-    }
-
-    @Override
-    public void addWithShowCondition(PageBuilder pageBuilder, String pageShowCondition) {
-        var page = pageBuilder.page("alternativeServiceDetailsAndUpload")
-            .pageLabel("Alternative Service App");
-        if (isNotBlank(pageShowCondition)) {
-            page.showCondition(pageShowCondition);
-        }
-        page.label("alternativeEvidenceDocsLabel", ALTERNATIVE_EVIDENCE_DOCS_LABEL)
+        pageBuilder.page("alternativeServiceDetailsAndUpload")
+            .pageLabel("Alternative Service App")
+            .showCondition(PAGE_SHOW_CONDITION)
+            .label("alternativeEvidenceDocsLabel", ALTERNATIVE_EVIDENCE_DOCS_LABEL)
             .label("respondentEmail", RESPONDENT_EMAIL, RESPONDENT_EMAIL_SHOW_CONDITION)
             .label("respondentPhone", RESPONDENT_PHONE, RESPONDENT_PHONE_SHOW_CONDITION)
             .label("respondentSocialMedia", RESPONDENT_SOCIAL_MEDIA, RESPONDENT_SOCIAL_MEDIA_SHOW_CONDITION)
             .label("alternativeEvidenceLabel", EVIDENCE_LABEL)
-            .label("documentsUploadLabel", UPLOAD_DOCUMENTS_LABEL)
             .complex(CaseData::getApplicant1)
             .complex(Applicant::getInterimApplicationOptions)
             .optionalWithLabel(InterimApplicationOptions::getInterimAppsEvidenceDocs, "Applicant 1 uploaded documents")
