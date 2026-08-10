aeab18ea53b9a14f6ca9f19b18a64f6e646233f1
pallavijustice <pallavi.saha@justice.gov.uk>
2026-08-10 08:40:35 +0100

    NFDIV-5067 - Revert last couple of commits

--- src/main/java/uk/gov/hmcts/divorce/payment/service/PaymentService.java
+++ src/main/java/uk/gov/hmcts/divorce/payment/service/PaymentService.java
@@ -211,12 +211,20 @@ public class PaymentService {
         } catch (FeignException exception) {
             HttpStatus httpStatus = Optional.ofNullable(HttpStatus.resolve(exception.status()))
                 .orElseGet(() -> INTERNAL_SERVER_ERROR);
-            log.error("For case id {} unsuccessful payment for account number {} with exception {}",
+            if (httpStatus.is5xxServerError()) {
+                log.error("For case id {} unsuccessful payment for account number {} with exception {}",
                     caseId,
                     pbaNumber,
                     exception.getMessage(),
                     exception
-            );
+                );
+            } else {
+                log.info("For case id {} unsuccessful payment for account number {} with exception {}",
+                    caseId,
+                    pbaNumber,
+                    exception.getMessage()
+                );
+            }
             return getPbaErrorResponse(pbaNumber, exception);
         }
         return new PbaResponse(INTERNAL_SERVER_ERROR, ERROR_GENERIC, null);
