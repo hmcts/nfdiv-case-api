26aad0e301a1044141de234ef94641ef1249bdd0
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2026-08-18 10:58:42 +0100

    NFDIV-5418: Add solicitor alternative service event (#5226)
    * NFDIV-5418: Add solicitor alternative service event
    
    * NFDIV-5418: Add test class and refactor pages
    
    * NFDIV-5418: Fix show condition
    
    * NFDIV-5418: Resolve PR comments
    
    * NFDIV:5418: Add show condition
    
    * NFDIV-5418: Rename show condition constant
    
    * NFDIV-5418: Refactor code to removed method call
    
    * NFDIV-5418: Remove heading tag
    
    * NFDIV-5418: Add a new enum for service method
    
    * NFDIV-5418: Fix checkstyle and rename event id
    
    * NFDIV-5419: Add document upload (#5230)
    
    * NFDIV-5419: Add document upload
    
    * NFDIV-54D19: Reuse existing fields
    
    * NFDIV:5419: Fix bullet point font
    
    * NFDIV-5419: Add unit test and show condition for the event
    
    * NFDIV-5419: Remove ; from label
    
    * NFDIV-5564: Add amend alternative service event (#5245)
    
    * NFDIV-5419: Add document upload
    
    * NFDIV-54D19: Reuse existing fields
    
    * NFDIV:5419: Fix bullet point font
    
    * NFDIV-5419: Add unit test and show condition for the event
    
    * NFDIV-5419: Remove ; from label
    
    * NFDIV-5564: Add amend alternative service event
    
    * NFDIV-5564: Fix show condition
    
    * NFDIV-5564: Make show condition inline
    
    * NFDIV-5564: Refactor class name and add tests
    
    * NFDIV-5564: Remove duplicate page label
    
    * NFDIV-5564: Change end button label

---
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorAmendAlternativeServiceApplication.java
@@ -0,0 +1,95 @@
+package uk.gov.hmcts.divorce.solicitor.event;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.State;
+import uk.gov.hmcts.divorce.divorcecase.model.UserRole;
+import uk.gov.hmcts.divorce.divorcecase.validation.ServiceApplicationValidation;
+import uk.gov.hmcts.divorce.solicitor.event.page.AmendServiceApplicationActionPage;
+import uk.gov.hmcts.divorce.solicitor.event.page.serviceapplicationpages.ServiceApplicationPages;
+import uk.gov.hmcts.divorce.solicitor.service.ServiceApplicationDraftSubmissionService;
+
+import java.util.List;
+
+import static uk.gov.hmcts.divorce.divorcecase.model.State.AosOverdue;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.JUDGE;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.LEGAL_ADVISOR;
+import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.SUPER_USER;
+import static uk.gov.hmcts.divorce.divorcecase.model.access.Permissions.CREATE_READ_UPDATE;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SolicitorAmendAlternativeServiceApplication implements CCDConfig<CaseData, State, UserRole> {
+
+    public static final String SOLICITOR_AMEND_ALTERNATIVE_SERVICE_APPLICATION = "sol-amend-alternative-service-app";
+
+    private final ServiceApplicationDraftSubmissionService serviceApplicationDraftSubmissionService;
+
+    private static final String AMEND_SHOW_CONDITION = "applicant1DraftApplicationAction=\"amend\"";
+
+    @Override
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+        final PageBuilder pageBuilder = addEventConfig(configBuilder);
+        new AmendServiceApplicationActionPage().addTo(pageBuilder);
+        ServiceApplicationPages.addAlternativeServicePages(pageBuilder, AMEND_SHOW_CONDITION);
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToStart(final CaseDetails<CaseData, State> details) {
+        log.info("{} About to start callback invoked for Case Id: {}", SOLICITOR_AMEND_ALTERNATIVE_SERVICE_APPLICATION, details.getId());
+
+        List<String> validationError = ServiceApplicationValidation.validateNotAlreadySubmitted(details.getData());
+
+        if (!validationError.isEmpty()) {
+            return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+                .errors(validationError)
+                .build();
+        }
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(details.getData()).build();
+    }
+
+    public AboutToStartOrSubmitResponse<CaseData, State> aboutToSubmit(
+        final CaseDetails<CaseData, State> details,
+        final CaseDetails<CaseData, State> beforeDetails
+    ) {
+        log.info("{} about to submit callback invoked for Case Id: {}", SOLICITOR_AMEND_ALTERNATIVE_SERVICE_APPLICATION, details.getId());
+
+        final CaseData caseData = details.getData();
+        final Applicant applicant = caseData.getApplicant1();
+
+        serviceApplicationDraftSubmissionService.submitFromInterimOptions(details.getId(), caseData, applicant);
+
+        return AboutToStartOrSubmitResponse.<CaseData, State>builder()
+            .data(caseData)
+            .build();
+    }
+
+    private PageBuilder addEventConfig(
+        final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
+
+        return new PageBuilder(configBuilder
+            .event(SOLICITOR_AMEND_ALTERNATIVE_SERVICE_APPLICATION)
+            .forState(AosOverdue)
+            .name("Amend Service App")
+            .description("Amend Service App")
+            .showCondition("serviceApplicationSubmittedOnline=\"Yes\" AND alternativeServiceType=\"alternativeService\"")
+            .showSummary()
+            .showEventNotes()
+            .aboutToStartCallback(this::aboutToStart)
+            .aboutToSubmitCallback(this::aboutToSubmit)
+            .endButtonLabel("Submit")
+            .grant(CREATE_READ_UPDATE, APPLICANT_1_SOLICITOR)
+            .grantHistoryOnly(CASE_WORKER, SUPER_USER, LEGAL_ADVISOR, JUDGE));
+    }
+}
