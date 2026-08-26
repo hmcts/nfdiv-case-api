ccef4fe25cafe0535f44e69c7bacd5c4bd7b74f9
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2026-08-26 10:01:21 +0100

    NFDIV-5412: Add solicitor send papers again event  (#5267)
    * NFDIV-5412: Add solicitor send papers again event
    
    # Conflicts:
    #	src/main/java/uk/gov/hmcts/divorce/notification/EmailTemplateName.java
    #	src/main/resources/application.yaml
    
    * NFDIV-5412: Fix typo and add unit test
    
    * NFDIV-5412: Fix validation error message
    
    * NFDIV-5412: Fix unit test
    
    * NFDIV-5412: Fix text
    
    * NFDIV-5412: Fix checkstyle
    
    * NFDIV-5412: Refactor validation logic
    
    * NFDIV-5412: Add check for application type
    
    * NFDIV-5412: Fix notification content
    
    * NFDIV-5412: Refactor notification logic
    
    * NFDIV-5412: Remove duplicate label
    
    * NFDIV-5412: Fix validation issue
    
    * NFDIV-5412: Update end button label

---
+++ src/test/java/uk/gov/hmcts/divorce/solicitor/notification/SolicitorSendPapersAgainNotificationTest.java
@@ -0,0 +1,79 @@
+package uk.gov.hmcts.divorce.solicitor.notification;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
+import uk.gov.hmcts.divorce.divorcecase.model.Application;
+import uk.gov.hmcts.divorce.divorcecase.model.CaseData;
+import uk.gov.hmcts.divorce.divorcecase.model.DivorceOrDissolution;
+import uk.gov.hmcts.divorce.divorcecase.model.ServiceMethod;
+import uk.gov.hmcts.divorce.divorcecase.model.Solicitor;
+import uk.gov.hmcts.divorce.notification.CommonContent;
+import uk.gov.hmcts.divorce.notification.NotificationService;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.hamcrest.MockitoHamcrest.argThat;
+import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.YES;
+import static uk.gov.hmcts.divorce.divorcecase.model.ApplicationType.SOLE_APPLICATION;
+import static uk.gov.hmcts.divorce.divorcecase.model.LanguagePreference.ENGLISH;
+import static uk.gov.hmcts.divorce.notification.EmailTemplateName.SOLICITOR_SENT_PAPERS_AGAIN;
+import static uk.gov.hmcts.divorce.solicitor.notification.SolicitorSendPapersAgainNotification.COURT_SERVICE_TEXT;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_CASE_ID;
+import static uk.gov.hmcts.divorce.testutil.TestConstants.TEST_SOLICITOR_EMAIL;
+import static uk.gov.hmcts.divorce.testutil.TestDataHelper.getBasicTemplateVars;
+
+@ExtendWith(MockitoExtension.class)
+class SolicitorSendPapersAgainNotificationTest {
+
+    @Mock
+    private CommonContent commonContent;
+
+    @Mock
+    private NotificationService notificationService;
+
+    @InjectMocks
+    private SolicitorSendPapersAgainNotification notification;
+
+    @Test
+    void shouldSendApplicant1SolicitorNotification() {
+
+        Long caseId = TEST_CASE_ID;
+        CaseData caseData = CaseData.builder()
+            .divorceOrDissolution(DivorceOrDissolution.DIVORCE)
+            .applicationType(SOLE_APPLICATION)
+            .application(Application.builder().serviceMethod(ServiceMethod.COURT_SERVICE).build())
+            .applicant1(
+                Applicant.builder()
+                    .solicitorRepresented(YES)
+                    .solicitor(Solicitor.builder()
+                        .email(TEST_SOLICITOR_EMAIL)
+                        .build())
+                    .build()
+            )
+            .build();
+
+        when(commonContent.solicitorTemplateVars(caseData, caseId, caseData.getApplicant1())).thenReturn(getBasicTemplateVars());
+
+        notification.sendToApplicant1Solicitor(caseData, caseId);
+
+        verify(notificationService).sendEmail(
+            eq(TEST_SOLICITOR_EMAIL),
+            eq(SOLICITOR_SENT_PAPERS_AGAIN),
+            argThat(allOf(
+                hasEntry("serviceMethod", String.format(COURT_SERVICE_TEXT, "divorce papers")),
+                hasEntry("divorceOrDissolution", "divorce application"))),
+            eq(ENGLISH),
+            eq(caseId)
+        );
+        verifyNoMoreInteractions(notificationService);
+        verify(commonContent).solicitorTemplateVars(caseData, caseId, caseData.getApplicant1());
+    }
+}
