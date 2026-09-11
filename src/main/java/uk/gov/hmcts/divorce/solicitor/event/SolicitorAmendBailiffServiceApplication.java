14e9fb3991d2081642608e7b5cde1a556d241dfe
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-09-11 17:53:26 +0100

    NFDIV-5565 Fix imports

--- src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorAmendBailiffServiceApplication.java
+++ src/main/java/uk/gov/hmcts/divorce/solicitor/event/SolicitorAmendBailiffServiceApplication.java
@@ -7,8 +7,6 @@ import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
 import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
-import uk.gov.hmcts.divorce.bulkaction.ccd.BulkActionState;
-import uk.gov.hmcts.divorce.bulkaction.data.BulkActionCaseData;
 import uk.gov.hmcts.divorce.common.ccd.CcdPageConfiguration;
 import uk.gov.hmcts.divorce.common.ccd.PageBuilder;
 import uk.gov.hmcts.divorce.divorcecase.model.Applicant;
@@ -30,8 +28,6 @@ import uk.gov.hmcts.divorce.solicitor.service.ServiceApplicationDraftSubmissionS
 
 import java.util.List;
 
-import static java.util.Collections.singletonList;
-import static uk.gov.hmcts.ccd.sdk.type.YesOrNo.NO;
 import static uk.gov.hmcts.divorce.divorcecase.model.State.AosOverdue;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.APPLICANT_1_SOLICITOR;
 import static uk.gov.hmcts.divorce.divorcecase.model.UserRole.CASE_WORKER;
