9547203fb4b6c81a9e3cf2653d9a1210cab24296
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-09-17 10:35:41 +0100

    NFDIV-5565 Add label property to YesNoOrNotKnown

--- src/main/java/uk/gov/hmcts/divorce/divorcecase/model/YesOrNoOrNotKnown.java
+++ src/main/java/uk/gov/hmcts/divorce/divorcecase/model/YesOrNoOrNotKnown.java
@@ -3,17 +3,27 @@ package uk.gov.hmcts.divorce.divorcecase.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import uk.gov.hmcts.ccd.sdk.api.CCD;
 
 @Getter
 @RequiredArgsConstructor
 public enum YesOrNoOrNotKnown {
     @JsonProperty("Yes")
+    @CCD(
+        label = "Yes"
+    )
     YES("Yes"),
 
     @JsonProperty("No")
+    @CCD(
+        label = "No"
+    )
     NO("No"),
 
     @JsonProperty("NotKnown")
+    @CCD(
+        label = "Not known"
+    )
     NOT_KNOWN("Not known");
 
     private final String value;
